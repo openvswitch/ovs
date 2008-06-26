89d08b1a1760ca31b4f1ce22945ba31ed16f7484
Ben Pfaff <blp@nicira.com>
2008-06-26 16:21:18 -0700

    Modified the naming scheme for hardware tables.
    Because of historical string munging during the kernel build process
    if you named your hardware table hwtable-something, when it was built
    the filename would look appropriate, but the actual module info would change
    the internal name to be hwtable_something, thus after an insmod with the filename
    you could not perform an rmmod with the same filename. As a result
    the naming scheme has been changed to hwtable_NAME, NAME being the hardware
    the table supports.
    
    Further, include files in the hardware table should be prefixed with the
    folder name, e.g. #include "hwtable_NAME/my_header.h".
    
    Based on commits from David Erickson <derickso@stanford.edu>.

--- datapath/hwtable-dummy/Modules.mk
+++
@@ -1,6 +0,0 @@
-# Specify the module to build.
-all_modules += hwtable-dummy
-
-# Specify the source files that comprise the module.
-hwtable-dummy_sources = \
-	hwtable-dummy/hwtable-dummy.c
---
+++ datapath/hwtable_dummy/Modules.mk
@@ -0,0 +1,7 @@
+# Specify the module to build.
+all_modules += hwtable_dummy
+
+# Specify the source files that comprise the module.
+hwtable_dummy_sources = \
+	hwtable_dummy/hwtable_dummy.c
+
