f1126f43ad62618cf1b6f6ab48d9635fc5cbcad6
Justin Pettit <jpettit@nicira.com>
2008-10-22 17:17:50 -0700

    Switch default OpenFlow port from 975 and 976 to 6633.

--- include/openflow.h
+++ include/openflow.h
@@ -68,8 +68,8 @@
 #define OFP_MAX_TABLE_NAME_LEN 32
 #define OFP_MAX_PORT_NAME_LEN  16
 
-#define OFP_TCP_PORT  975
-#define OFP_SSL_PORT  976
+#define OFP_TCP_PORT  6633
+#define OFP_SSL_PORT  6633
 
 #define OFP_ETH_ALEN 6          /* Bytes in an Ethernet address. */
 
