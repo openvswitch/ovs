6bc995e46d64b90ef0b3186abec92f63eab2c2ad
Justin Pettit <jpettit@nicira.com>
2009-09-11 07:15:24 -0700

    logging: Lower logging level for coverage messages due to slow poll loops
    By default, many OVS processes keep track of their time through a poll
    loop.  If it takes an unusually long time (measured as some distance
    from the mean), the processes will log stats it has been keeping about
    coverage.  It was doing this at level WARN.
    
    On Xen systems, syslog messages written at level INFO and higher are
    written to /var/log/messages synchronously.  This would mean that there
    would be dire messages that it took a few dozen milliseconds to go
    through the loop, meanwhile, it would take up to 6(!) seconds writing
    those.  Meanwhile, the process would do no other processing, which could
    be quite serious in the case of a process such as ovs-vswitchd.
    This problem was somewhat masked because the time used by this logging
    was not used in the calculations for determining how long it was taking
    to get through the loop.
    
    This commit lowers the default log level for those coverage messages to
    INFO.  On Xen systems, it raises the default level at which messages are
    written to syslog to WARN.
    
    Diagnosed and fixed with the help of Ian Campbell.

--- lib/timeval.c
+++ lib/timeval.c
@@ -294,7 +294,12 @@ log_poll_interval(long long int last_wakeup, const struct rusage *last_rusage)
                       rusage.ru_nvcsw - last_rusage->ru_nvcsw,
                       rusage.ru_nivcsw - last_rusage->ru_nivcsw);
         }
-        coverage_log(VLL_WARN, true);
+
+        /* Care should be taken in the value chosen for logging.  Depending 
+         * on the configuration, syslog can write changes synchronously, 
+         * which can cause the coverage messages to take longer to log 
+         * than the processing delay that triggered it. */
+        coverage_log(VLL_INFO, true);
     }
 
     /* Update exponentially weighted moving average.  With these parameters, a
