7c6a5f146742506a7a15cceface63ec3ba1e3076
Ben Pfaff <blp@nicira.com>
2010-01-26 10:47:50 -0800

    Fix "make distcleancheck" by deleting the nicira-ext.h stamp file too.

--- include/openflow/automake.mk
+++ include/openflow/automake.mk
@@ -9,12 +9,14 @@ include/openflow/openflow.h.stamp: \
 	include/openflow/openflow.h build-aux/check-structs
 	$(PYTHON) $(srcdir)/build-aux/check-structs $(srcdir)/include/openflow/openflow.h
 	touch $@
+DISTCLEANFILES += include/openflow/openflow.h.stamp
 
 all-local: include/openflow/nicira-ext.h.stamp
 include/openflow/nicira-ext.h.stamp: include/openflow/openflow.h include/openflow/nicira-ext.h build-aux/check-structs
 	$(PYTHON) $(srcdir)/build-aux/check-structs $(srcdir)/include/openflow/openflow.h $(srcdir)/include/openflow/nicira-ext.h
 	touch $@
+DISTCLEANFILES += include/openflow/nicira-ext.h.stamp
 endif
 
 EXTRA_DIST += build-aux/check-structs
-DISTCLEANFILES += include/openflow/openflow.h.stamp 
+
