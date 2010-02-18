82ff0d0a931ddc610c366551a948ffb33e9c8008
Jesse Gross <jesse@nicira.com>
2010-02-18 11:43:29 -0500

    gre: Add functions to determine address type to compat layer.
    Allows older kernels to classify IPv4/IPv6 addresses as loopback,
    broadcast, etc.  The IPv6 functions actually exist in all supported
    kernels but add a dependency on the IPv6 code on older kernels (they
    are always available on more recent kernels).  This allows us to
    process IPv6 packets without dragging in the entire IPv6 subsystem
    if it is compiled as a module (such as on Xen).  This is only done
    for packets that are passing through the system and does not use
    the IPv6 core if it is not configured.

--- datapath/linux-2.6/compat-2.6/include/linux/in.h
+++ datapath/linux-2.6/compat-2.6/include/linux/in.h
@@ -5,9 +5,30 @@
 
 #ifndef HAVE_IPV4_IS_MULTICAST
 
+static inline bool ipv4_is_loopback(__be32 addr)
+{
+	return (addr & htonl(0xff000000)) == htonl(0x7f000000);
+}
+
 static inline bool ipv4_is_multicast(__be32 addr)
 {
 	return (addr & htonl(0xf0000000)) == htonl(0xe0000000);
+}
+
+static inline bool ipv4_is_local_multicast(__be32 addr)
+{
+	return (addr & htonl(0xffffff00)) == htonl(0xe0000000);
+}
+
+static inline bool ipv4_is_lbcast(__be32 addr)
+{
+	/* limited broadcast */
+	return addr == htonl(INADDR_BROADCAST);
+}
+
+static inline bool ipv4_is_zeronet(__be32 addr)
+{
+	return (addr & htonl(0xff000000)) == htonl(0x00000000);
 }
 
 #endif /* !HAVE_IPV4_IS_MULTICAST */
