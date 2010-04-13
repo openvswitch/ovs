cd05c6ad2fdcedce229f962ca29c167287b9b21b
Ben Pfaff <blp@nicira.com>
2010-04-13 16:50:31 -0700

    odp-util: Print bad action numbers in hexadecimal in format_odp_action().
    Given that dpif.c clears action numbers to 0xcccc it can be much less
    mystifying to see that instead of 52428.

--- lib/odp-util.c
+++ lib/odp-util.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Nicira Networks.
+ * Copyright (c) 2009, 2010 Nicira Networks.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ format_odp_action(struct ds *ds, const union odp_action *a)
         ds_put_format(ds, "set_tp_dst(%"PRIu16")", ntohs(a->tp_port.tp_port));
         break;
     default:
-        ds_put_format(ds, "***bad action %"PRIu16"***", a->type);
+        ds_put_format(ds, "***bad action 0x%"PRIx16"***", a->type);
         break;
     }
 }
