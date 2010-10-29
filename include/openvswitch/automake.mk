bd6258126dfe144b4cfe99aaa42ba970307a1b36
Ben Pfaff <blp@nicira.com>
2010-10-29 10:33:03 -0700

    Fix "make dist" by adding include/openvswitch/types.h to the distribution.

--- include/openvswitch/automake.mk
+++ include/openvswitch/automake.mk
@@ -1,5 +1,6 @@
 noinst_HEADERS += \
 	include/openvswitch/brcompat-netlink.h \
 	include/openvswitch/datapath-protocol.h \
-	include/openvswitch/tunnel.h
+	include/openvswitch/tunnel.h \
+	include/openvswitch/types.h
 
