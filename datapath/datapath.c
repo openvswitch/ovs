openvswitch/ovs

commit cdee00fd635d1e0f1eeb5d9c009daeb59abd4777

datapath: Replace "struct odp_action" by Netlink attributes.
In the medium term, we plan to migrate the datapath to use Netlink as its
communication channel.  In the short term, we need to be able to have
actions with 64-bit arguments but "struct odp_action" only has room for
48 bits.  So this patch shifts to variable-length arguments using Netlink
attributes, which starts in on the Netlink transition and makes 64-bit
arguments possible at the same time.

Signed-off-by: Ben Pfaff <blp@nicira.com>
Acked-by: Jesse Gross <jesse@nicira.com>

datapath/datapath.c
@@ -516,7 +516,7 @@ void dp_process_received_packet(struct vport *p, struct sk_buff *skb)
 
 	/* Execute actions. */
 	execute_actions(dp, skb, &OVS_CB(skb)->flow->key, acts->actions,
-			acts->n_actions);
+			acts->actions_len);
 	stats_counter_off = offsetof(struct dp_stats_percpu, n_hit);
 
 	/* Check whether sub-actions looped too much. */
@@ -658,50 +658,76 @@ static int flush_flows(struct datapath *dp)
 	return 0;
 }
 
-static int validate_actions(const struct sw_flow_actions *actions)
+static int validate_actions(const struct nlattr *actions, u32 actions_len)
 {
-	unsigned int i;
+        const struct nlattr *a;
+        int rem;
 
-	for (i = 0; i < actions->n_actions; i++) {
-		const union odp_action *a = &actions->actions[i];
+        nla_for_each_attr(a, actions, actions_len, rem) {
+                static const u32 action_lens[ODPAT_MAX + 1] = {
+                        [ODPAT_OUTPUT] = 4,
+                        [ODPAT_CONTROLLER] = 4,
+                        [ODPAT_SET_DL_TCI] = 2,
+                        [ODPAT_STRIP_VLAN] = 0,
+                        [ODPAT_SET_DL_SRC] = ETH_ALEN,
+                        [ODPAT_SET_DL_DST] = ETH_ALEN,
+                        [ODPAT_SET_NW_SRC] = 4,
+                        [ODPAT_SET_NW_DST] = 4,
+                        [ODPAT_SET_NW_TOS] = 1,
+                        [ODPAT_SET_TP_SRC] = 2,
+                        [ODPAT_SET_TP_DST] = 2,
+                        [ODPAT_SET_TUNNEL] = 4,
+                        [ODPAT_SET_PRIORITY] = 4,
+                        [ODPAT_POP_PRIORITY] = 0,
+                        [ODPAT_DROP_SPOOFED_ARP] = 0,
+                };
+                int type = nla_type(a);
 
-		switch (a->type) {
-		case ODPAT_CONTROLLER:
-		case ODPAT_STRIP_VLAN:
-		case ODPAT_SET_DL_SRC:
-		case ODPAT_SET_DL_DST:
-		case ODPAT_SET_NW_SRC:
-		case ODPAT_SET_NW_DST:
-		case ODPAT_SET_TP_SRC:
-		case ODPAT_SET_TP_DST:
-		case ODPAT_SET_TUNNEL:
-		case ODPAT_SET_PRIORITY:
-		case ODPAT_POP_PRIORITY:
-		case ODPAT_DROP_SPOOFED_ARP:
-			/* No validation needed. */
-			break;
+                if (type > ODPAT_MAX || nla_len(a) != action_lens[type])
+                        return -EINVAL;
 
-		case ODPAT_OUTPUT:
-			if (a->output.port >= DP_MAX_PORTS)
+                switch (type) {
+		case ODPAT_UNSPEC:
+			return -EINVAL;
+
+                case ODPAT_CONTROLLER:
+                case ODPAT_STRIP_VLAN:
+                case ODPAT_SET_DL_SRC:
+                case ODPAT_SET_DL_DST:
+                case ODPAT_SET_NW_SRC:
+                case ODPAT_SET_NW_DST:
+                case ODPAT_SET_TP_SRC:
+                case ODPAT_SET_TP_DST:
+                case ODPAT_SET_TUNNEL:
+                case ODPAT_SET_PRIORITY:
+                case ODPAT_POP_PRIORITY:
+                case ODPAT_DROP_SPOOFED_ARP:
+                        /* No validation needed. */
+                        break;
+
+                case ODPAT_OUTPUT:
+                        if (nla_get_u32(a) >= DP_MAX_PORTS)
+                                return -EINVAL;
+
+                case ODPAT_SET_DL_TCI:
+			if (nla_get_be16(a) & htons(VLAN_CFI_MASK))
 				return -EINVAL;
-			break;
+                        break;
 
-		case ODPAT_SET_DL_TCI:
-			if (a->dl_tci.tci & htons(VLAN_CFI_MASK))
-				return -EINVAL;
-			break;
+                case ODPAT_SET_NW_TOS:
+                        if (nla_get_u8(a) & INET_ECN_MASK)
+                                return -EINVAL;
+                        break;
 
-		case ODPAT_SET_NW_TOS:
-			if (a->nw_tos.nw_tos & INET_ECN_MASK)
-				return -EINVAL;
-			break;
+                default:
+                        return -EOPNOTSUPP;
+                }
+        }
 
-		default:
-			return -EOPNOTSUPP;
-		}
-	}
+        if (rem > 0)
+                return -EINVAL;
 
-	return 0;
+        return 0;
 }
 
 static struct sw_flow_actions *get_actions(const struct odp_flow *flow)
@@ -709,16 +735,15 @@ static struct sw_flow_actions *get_actions(const struct odp_flow *flow)
 	struct sw_flow_actions *actions;
 	int error;
 
-	actions = flow_actions_alloc(flow->n_actions);
+	actions = flow_actions_alloc(flow->actions_len);
 	error = PTR_ERR(actions);
 	if (IS_ERR(actions))
 		goto error;
 
 	error = -EFAULT;
-	if (copy_from_user(actions->actions, flow->actions,
-			   flow->n_actions * sizeof(union odp_action)))
+	if (copy_from_user(actions->actions, flow->actions, flow->actions_len))
 		goto error_free_actions;
-	error = validate_actions(actions);
+	error = validate_actions(actions->actions, actions->actions_len);
 	if (error)
 		goto error_free_actions;
 
@@ -842,9 +867,9 @@ static int do_put_flow(struct datapath *dp, struct odp_flow_put *uf,
 		if (IS_ERR(new_acts))
 			goto error;
 		old_acts = rcu_dereference(flow->sf_acts);
-		if (old_acts->n_actions != new_acts->n_actions ||
+		if (old_acts->actions_len != new_acts->actions_len ||
 		    memcmp(old_acts->actions, new_acts->actions,
-			   sizeof(union odp_action) * old_acts->n_actions)) {
+			   old_acts->actions_len)) {
 			rcu_assign_pointer(flow->sf_acts, new_acts);
 			flow_deferred_free_acts(old_acts);
 		} else {
@@ -892,12 +917,12 @@ static int put_flow(struct datapath *dp, struct odp_flow_put __user *ufp)
 
 static int do_answer_query(struct sw_flow *flow, u32 query_flags,
 			   struct odp_flow_stats __user *ustats,
-			   union odp_action __user *actions,
-			   u32 __user *n_actionsp)
+			   struct nlattr __user *actions,
+			   u32 __user *actions_lenp)
 {
 	struct sw_flow_actions *sf_acts;
 	struct odp_flow_stats stats;
-	u32 n_actions;
+	u32 actions_len;
 
 	spin_lock_bh(&flow->lock);
 	get_stats(flow, &stats);
@@ -907,17 +932,16 @@ static int do_answer_query(struct sw_flow *flow, u32 query_flags,
 	spin_unlock_bh(&flow->lock);
 
 	if (copy_to_user(ustats, &stats, sizeof(struct odp_flow_stats)) ||
-	    get_user(n_actions, n_actionsp))
+	    get_user(actions_len, actions_lenp))
 		return -EFAULT;
 
-	if (!n_actions)
+	if (!actions_len)
 		return 0;
 
 	sf_acts = rcu_dereference(flow->sf_acts);
-	if (put_user(sf_acts->n_actions, n_actionsp) ||
+	if (put_user(sf_acts->actions_len, actions_lenp) ||
 	    (actions && copy_to_user(actions, sf_acts->actions,
-				     sizeof(union odp_action) *
-				     min(sf_acts->n_actions, n_actions))))
+				     min(sf_acts->actions_len, actions_len))))
 		return -EFAULT;
 
 	return 0;
@@ -926,13 +950,13 @@ static int do_answer_query(struct sw_flow *flow, u32 query_flags,
 static int answer_query(struct sw_flow *flow, u32 query_flags,
 			struct odp_flow __user *ufp)
 {
-	union odp_action *actions;
+	struct nlattr *actions;
 
 	if (get_user(actions, &ufp->actions))
 		return -EFAULT;
 
 	return do_answer_query(flow, query_flags, 
-			       &ufp->stats, actions, &ufp->n_actions);
+			       &ufp->stats, actions, &ufp->actions_len);
 }
 
 static struct sw_flow *do_del_flow(struct datapath *dp, struct odp_flow_key *key)
@@ -1073,18 +1097,17 @@ static int do_execute(struct datapath *dp, const struct odp_execute *execute)
 	if (execute->length < ETH_HLEN || execute->length > 65535)
 		goto error;
 
-	actions = flow_actions_alloc(execute->n_actions);
+	actions = flow_actions_alloc(execute->actions_len);
 	if (IS_ERR(actions)) {
 		err = PTR_ERR(actions);
 		goto error;
 	}
 
 	err = -EFAULT;
-	if (copy_from_user(actions->actions, execute->actions,
-			   execute->n_actions * sizeof *execute->actions))
+	if (copy_from_user(actions->actions, execute->actions, execute->actions_len))
 		goto error_free_actions;
 
-	err = validate_actions(actions);
+	err = validate_actions(actions->actions, execute->actions_len);
 	if (err)
 		goto error_free_actions;
 
@@ -1114,7 +1137,7 @@ static int do_execute(struct datapath *dp, const struct odp_execute *execute)
 		goto error_free_skb;
 
 	rcu_read_lock();
-	err = execute_actions(dp, skb, &key, actions->actions, actions->n_actions);
+	err = execute_actions(dp, skb, &key, actions->actions, actions->actions_len);
 	rcu_read_unlock();
 
 	kfree(actions);
@@ -1498,7 +1521,7 @@ static int compat_get_flow(struct odp_flow *flow, const struct compat_odp_flow _
 	    __copy_from_user(&flow->stats, &compat->stats, sizeof(struct odp_flow_stats)) ||
 	    __copy_from_user(&flow->key, &compat->key, sizeof(struct odp_flow_key)) ||
 	    __get_user(actions, &compat->actions) ||
-	    __get_user(flow->n_actions, &compat->n_actions) ||
+	    __get_user(flow->actions_len, &compat->actions_len) ||
 	    __get_user(flow->flags, &compat->flags))
 		return -EFAULT;
 
@@ -1536,7 +1559,7 @@ static int compat_answer_query(struct sw_flow *flow, u32 query_flags,
 		return -EFAULT;
 
 	return do_answer_query(flow, query_flags, &ufp->stats,
-			       compat_ptr(actions), &ufp->n_actions);
+			       compat_ptr(actions), &ufp->actions_len);
 }
 
 static int compat_del_flow(struct datapath *dp, struct compat_odp_flow __user *ufp)
@@ -1659,7 +1682,7 @@ static int compat_execute(struct datapath *dp, const struct compat_odp_execute _
 
 	if (!access_ok(VERIFY_READ, uexecute, sizeof(struct compat_odp_execute)) ||
 	    __get_user(actions, &uexecute->actions) ||
-	    __get_user(execute.n_actions, &uexecute->n_actions) ||
+	    __get_user(execute.actions_len, &uexecute->actions_len) ||
 	    __get_user(data, &uexecute->data) ||
 	    __get_user(execute.length, &uexecute->length))
 		return -EFAULT;

datapath/actions.c
@@ -73,9 +73,9 @@ static struct sk_buff *vlan_pull_tag(struct sk_buff *skb)
 
 static struct sk_buff *modify_vlan_tci(struct datapath *dp, struct sk_buff *skb,
 				       const struct odp_flow_key *key,
-				       const union odp_action *a, int n_actions)
+				       const struct nlattr *a, u32 actions_len)
 {
-	__be16 tci = a->dl_tci.tci;
+	__be16 tci = nla_get_be16(a);
 
 	skb = make_writable(skb, VLAN_HLEN);
 	if (!skb)
@@ -132,12 +132,17 @@ static struct sk_buff *modify_vlan_tci(struct datapath *dp, struct sk_buff *skb,
 		 * support hardware-accelerated VLAN tagging without VLAN
 		 * groups configured). */
 		if (skb_is_gso(skb)) {
+			const struct nlattr *actions_left;
+			u32 actions_len_left;
 			struct sk_buff *segs;
 
 			segs = skb_gso_segment(skb, 0);
 			kfree_skb(skb);
 			if (unlikely(IS_ERR(segs)))
 				return ERR_CAST(segs);
+
+			actions_len_left = actions_len;
+			actions_left = nla_next(a, &actions_len_left);
 
 			do {
 				struct sk_buff *nskb = segs->next;
@@ -151,9 +156,9 @@ static struct sk_buff *modify_vlan_tci(struct datapath *dp, struct sk_buff *skb,
 				segs = __vlan_put_tag(segs, ntohs(tci));
 				err = -ENOMEM;
 				if (segs) {
-					err = execute_actions(dp, segs,
-							      key, a + 1,
-							      n_actions - 1);
+					err = execute_actions(
+						dp, segs, key, actions_left,
+						actions_len_left);
 				}
 
 				if (unlikely(err)) {
@@ -199,20 +204,6 @@ static struct sk_buff *strip_vlan(struct sk_buff *skb)
 	return skb;
 }
 
-static struct sk_buff *set_dl_addr(struct sk_buff *skb,
-				   const struct odp_action_dl_addr *a)
-{
-	skb = make_writable(skb, 0);
-	if (skb) {
-		struct ethhdr *eh = eth_hdr(skb);
-		if (a->type == ODPAT_SET_DL_SRC)
-			memcpy(eh->h_source, a->dl_addr, ETH_ALEN);
-		else
-			memcpy(eh->h_dest, a->dl_addr, ETH_ALEN);
-	}
-	return skb;
-}
-
 static bool is_ip(struct sk_buff *skb, const struct odp_flow_key *key)
 {
 	return (key->dl_type == htons(ETH_P_IP) &&
@@ -234,8 +225,9 @@ static __sum16 *get_l4_checksum(struct sk_buff *skb, const struct odp_flow_key *
 
 static struct sk_buff *set_nw_addr(struct sk_buff *skb,
 				   const struct odp_flow_key *key,
-				   const struct odp_action_nw_addr *a)
+				   const struct nlattr *a)
 {
+	__be32 new_nwaddr = nla_get_be32(a);
 	struct iphdr *nh;
 	__sum16 *check;
 	__be32 *nwaddr;
@@ -248,21 +240,21 @@ static struct sk_buff *set_nw_addr(struct sk_buff *skb,
 		return NULL;
 
 	nh = ip_hdr(skb);
-	nwaddr = a->type == ODPAT_SET_NW_SRC ? &nh->saddr : &nh->daddr;
+	nwaddr = nla_type(a) == ODPAT_SET_NW_SRC ? &nh->saddr : &nh->daddr;
 
 	check = get_l4_checksum(skb, key);
 	if (likely(check))
-		inet_proto_csum_replace4(check, skb, *nwaddr, a->nw_addr, 1);
-	csum_replace4(&nh->check, *nwaddr, a->nw_addr);
+		inet_proto_csum_replace4(check, skb, *nwaddr, new_nwaddr, 1);
+	csum_replace4(&nh->check, *nwaddr, new_nwaddr);
 
-	*nwaddr = a->nw_addr;
+	*nwaddr = new_nwaddr;
 
 	return skb;
 }
 
 static struct sk_buff *set_nw_tos(struct sk_buff *skb,
-				   const struct odp_flow_key *key,
-				   const struct odp_action_nw_tos *a)
+				  const struct odp_flow_key *key,
+				  u8 nw_tos)
 {
 	if (unlikely(!is_ip(skb, key)))
 		return skb;
@@ -275,7 +267,7 @@ static struct sk_buff *set_nw_tos(struct sk_buff *skb,
 		u8 new;
 
 		/* Set the DSCP bits and preserve the ECN bits. */
-		new = a->nw_tos | (nh->tos & INET_ECN_MASK);
+		new = nw_tos | (nh->tos & INET_ECN_MASK);
 		csum_replace4(&nh->check, (__force __be32)old,
 					  (__force __be32)new);
 		*f = new;
@@ -285,7 +277,7 @@ static struct sk_buff *set_nw_tos(struct sk_buff *skb,
 
 static struct sk_buff *set_tp_port(struct sk_buff *skb,
 				   const struct odp_flow_key *key,
-				   const struct odp_action_tp_port *a)
+				   const struct nlattr *a)
 {
 	struct udphdr *th;
 	__sum16 *check;
@@ -311,9 +303,9 @@ static struct sk_buff *set_tp_port(struct sk_buff *skb,
 	 * supports those protocols.
 	 */
 	th = udp_hdr(skb);
-	port = a->type == ODPAT_SET_TP_SRC ? &th->source : &th->dest;
-	inet_proto_csum_replace2(check, skb, *port, a->tp_port, 0);
-	*port = a->tp_port;
+	port = nla_type(a) == ODPAT_SET_TP_SRC ? &th->source : &th->dest;
+	inet_proto_csum_replace2(check, skb, *port, nla_get_be16(a), 0);
+	*port = nla_get_be16(a);
 
 	return skb;
 }
@@ -376,21 +368,20 @@ static int output_control(struct datapath *dp, struct sk_buff *skb, u32 arg)
 /* Send a copy of this packet up to the sFlow agent, along with extra
  * information about what happened to it. */
 static void sflow_sample(struct datapath *dp, struct sk_buff *skb,
-			 const union odp_action *a, int n_actions,
+			 const struct nlattr *a, u32 actions_len,
 			 struct vport *vport)
 {
 	struct odp_sflow_sample_header *hdr;
-	unsigned int actlen = n_actions * sizeof(union odp_action);
 	unsigned int hdrlen = sizeof(struct odp_sflow_sample_header);
 	struct sk_buff *nskb;
 
-	nskb = skb_copy_expand(skb, actlen + hdrlen, 0, GFP_ATOMIC);
+	nskb = skb_copy_expand(skb, actions_len + hdrlen, 0, GFP_ATOMIC);
 	if (!nskb)
 		return;
 
-	memcpy(__skb_push(nskb, actlen), a, actlen);
+	memcpy(__skb_push(nskb, actions_len), a, actions_len);
 	hdr = (struct odp_sflow_sample_header*)__skb_push(nskb, hdrlen);
-	hdr->n_actions = n_actions;
+	hdr->actions_len = actions_len;
 	hdr->sample_pool = atomic_read(&vport->sflow_pool);
 	dp_output_control(dp, nskb, _ODPL_SFLOW_NR, 0);
 }
@@ -398,7 +389,7 @@ static void sflow_sample(struct datapath *dp, struct sk_buff *skb,
 /* Execute a list of actions against 'skb'. */
 int execute_actions(struct datapath *dp, struct sk_buff *skb,
 		    const struct odp_flow_key *key,
-		    const union odp_action *a, int n_actions)
+		    const struct nlattr *actions, u32 actions_len)
 {
 	/* Every output action needs a separate clone of 'skb', but the common
 	 * case is just a single output action, so that doing a clone and
@@ -406,7 +397,8 @@ int execute_actions(struct datapath *dp, struct sk_buff *skb,
 	 * is slightly obscure just to avoid that. */
 	int prev_port = -1;
 	u32 priority = skb->priority;
-	int err;
+	const struct nlattr *a;
+	int rem, err;
 
 	if (dp->sflow_probability) {
 		struct vport *p = OVS_CB(skb)->vport;
@@ -414,25 +406,25 @@ int execute_actions(struct datapath *dp, struct sk_buff *skb,
 			atomic_inc(&p->sflow_pool);
 			if (dp->sflow_probability == UINT_MAX ||
 			    net_random() < dp->sflow_probability)
-				sflow_sample(dp, skb, a, n_actions, p);
+				sflow_sample(dp, skb, actions, actions_len, p);
 		}
 	}
 
 	OVS_CB(skb)->tun_id = 0;
 
-	for (; n_actions > 0; a++, n_actions--) {
+	for (a = actions, rem = actions_len; rem > 0; a = nla_next(a, &rem)) {
 		if (prev_port != -1) {
 			do_output(dp, skb_clone(skb, GFP_ATOMIC), prev_port);
 			prev_port = -1;
 		}
 
-		switch (a->type) {
+		switch (nla_type(a)) {
 		case ODPAT_OUTPUT:
-			prev_port = a->output.port;
+			prev_port = nla_get_u32(a);
 			break;
 
 		case ODPAT_CONTROLLER:
-			err = output_control(dp, skb, a->controller.arg);
+			err = output_control(dp, skb, nla_get_u32(a));
 			if (err) {
 				kfree_skb(skb);
 				return err;
@@ -440,11 +432,11 @@ int execute_actions(struct datapath *dp, struct sk_buff *skb,
 			break;
 
 		case ODPAT_SET_TUNNEL:
-			OVS_CB(skb)->tun_id = a->tunnel.tun_id;
+			OVS_CB(skb)->tun_id = nla_get_be32(a);
 			break;
 
 		case ODPAT_SET_DL_TCI:
-			skb = modify_vlan_tci(dp, skb, key, a, n_actions);
+			skb = modify_vlan_tci(dp, skb, key, a, rem);
 			if (IS_ERR(skb))
 				return PTR_ERR(skb);
 			break;
@@ -454,26 +446,35 @@ int execute_actions(struct datapath *dp, struct sk_buff *skb,
 			break;
 
 		case ODPAT_SET_DL_SRC:
+			skb = make_writable(skb, 0);
+			if (!skb)
+				return -ENOMEM;
+			memcpy(eth_hdr(skb)->h_source, nla_data(a), ETH_ALEN);
+			break;
+
 		case ODPAT_SET_DL_DST:
-			skb = set_dl_addr(skb, &a->dl_addr);
+			skb = make_writable(skb, 0);
+			if (!skb)
+				return -ENOMEM;
+			memcpy(eth_hdr(skb)->h_dest, nla_data(a), ETH_ALEN);
 			break;
 
 		case ODPAT_SET_NW_SRC:
 		case ODPAT_SET_NW_DST:
-			skb = set_nw_addr(skb, key, &a->nw_addr);
+			skb = set_nw_addr(skb, key, a);
 			break;
 
 		case ODPAT_SET_NW_TOS:
-			skb = set_nw_tos(skb, key, &a->nw_tos);
+			skb = set_nw_tos(skb, key, nla_get_u8(a));
 			break;
 
 		case ODPAT_SET_TP_SRC:
 		case ODPAT_SET_TP_DST:
-			skb = set_tp_port(skb, key, &a->tp_port);
+			skb = set_tp_port(skb, key, a);
 			break;
 
 		case ODPAT_SET_PRIORITY:
-			skb->priority = a->priority.priority;
+			skb->priority = nla_get_u32(a);
 			break;
 
 		case ODPAT_POP_PRIORITY:

datapath/actions.h
@@ -15,10 +15,9 @@
 struct datapath;
 struct sk_buff;
 struct odp_flow_key;
-union odp_action;
 
 int execute_actions(struct datapath *dp, struct sk_buff *skb,
 		    const struct odp_flow_key *key,
-		    const union odp_action *, int n_actions);
+		    const struct nlattr *, u32 actions_len);
 
 #endif /* actions.h */

datapath/flow.c
@@ -104,22 +104,24 @@ void flow_used(struct sw_flow *flow, struct sk_buff *skb)
 	spin_unlock_bh(&flow->lock);
 }
 
-struct sw_flow_actions *flow_actions_alloc(size_t n_actions)
+struct sw_flow_actions *flow_actions_alloc(u32 actions_len)
 {
 	struct sw_flow_actions *sfa;
+
+	if (actions_len % NLA_ALIGNTO)
+		return ERR_PTR(-EINVAL);
 
 	/* At least DP_MAX_PORTS actions are required to be able to flood a
 	 * packet to every port.  Factor of 2 allows for setting VLAN tags,
 	 * etc. */
-	if (n_actions > 2 * DP_MAX_PORTS)
+	if (actions_len > 2 * DP_MAX_PORTS * nla_total_size(4))
 		return ERR_PTR(-EINVAL);
 
-	sfa = kmalloc(sizeof *sfa + n_actions * sizeof(union odp_action),
-		      GFP_KERNEL);
+	sfa = kmalloc(sizeof *sfa + actions_len, GFP_KERNEL);
 	if (!sfa)
 		return ERR_PTR(-ENOMEM);
 
-	sfa->n_actions = n_actions;
+	sfa->actions_len = actions_len;
 	return sfa;
 }
 

datapath/flow.h
@@ -24,8 +24,8 @@ struct sk_buff;
 
 struct sw_flow_actions {
 	struct rcu_head rcu;
-	unsigned int n_actions;
-	union odp_action actions[];
+	u32 actions_len;
+	struct nlattr actions[];
 };
 
 struct sw_flow {
@@ -67,7 +67,7 @@ struct sw_flow *flow_alloc(void);
 void flow_deferred_free(struct sw_flow *);
 void flow_free_tbl(struct tbl_node *);
 
-struct sw_flow_actions *flow_actions_alloc(size_t n_actions);
+struct sw_flow_actions *flow_actions_alloc(u32 actions_len);
 void flow_deferred_free_acts(struct sw_flow_actions *);
 
 void flow_hold(struct sw_flow *);

datapath/loop_counter.c
@@ -20,7 +20,7 @@ void loop_suppress(struct datapath *dp, struct sw_flow_actions *actions)
 	if (net_ratelimit())
 		pr_warn("%s: flow looped %d times, dropping\n",
 			dp_name(dp), MAX_LOOPS);
-	actions->n_actions = 0;
+	actions->actions_len = 0;
 }
 
 #ifndef CONFIG_PREEMPT_RT

datapath/odp-compat.h
@@ -32,7 +32,7 @@ struct compat_odp_flow {
 	struct odp_flow_stats stats;
 	struct odp_flow_key key;
 	compat_uptr_t actions;
-	u32 n_actions;
+	u32 actions_len;
 	u32 flags;
 };
 
@@ -48,7 +48,7 @@ struct compat_odp_flowvec {
 
 struct compat_odp_execute {
 	compat_uptr_t actions;
-	u32 n_actions;
+	u32 actions_len;
 
 	compat_uptr_t data;
 	u32 length;

include/openvswitch/datapath-protocol.h
@@ -152,12 +152,12 @@ struct odp_stats {
  * encapsulated this packet. It is 0 if the packet was not received on a tunnel.
  *
  * For @type == %_ODPL_ACTION_NR, the header is followed by packet data.  The
- * @arg member is copied from the &struct odp_action_controller that caused
- * the &struct odp_msg to be composed.
+ * @arg member is copied from the %ODPAT_CONTROLLER action that caused the
+ * &struct odp_msg to be composed.
  *
  * For @type == %_ODPL_SFLOW_NR, the header is followed by &struct
- * odp_sflow_sample_header, then by an array of &union odp_action (the number
- * of which is specified in &struct odp_sflow_sample_header), then by packet
+ * odp_sflow_sample_header, then by a series of Netlink attributes (whose
+ * length is specified in &struct odp_sflow_sample_header), then by packet
  * data.
  */
 struct odp_msg {
@@ -172,15 +172,16 @@ struct odp_msg {
  * struct odp_sflow_sample_header - header added to sFlow sampled packet.
  * @sample_pool: Number of packets that were candidates for sFlow sampling,
  * regardless of whether they were actually chosen and sent down to userspace.
- * @n_actions: Number of "union odp_action"s immediately following this header.
+ * @actions_len: Number of bytes of actions immediately following this header.
  *
  * This header follows &struct odp_msg when that structure's @type is
- * %_ODPL_SFLOW_NR, and it is itself followed by an array of &union odp_action
- * (the number of which is specified in @n_actions) and then by packet data.
+ * %_ODPL_SFLOW_NR, and it is itself followed by a series of Netlink attributes
+ * (the number of bytes of which is specified in @actions_len) and then by
+ * packet data.
  */
 struct odp_sflow_sample_header {
     uint32_t sample_pool;
-    uint32_t n_actions;
+    uint32_t actions_len;
 };
 
 #define VPORT_TYPE_SIZE     16
@@ -240,8 +241,8 @@ struct odp_flow_key {
 struct odp_flow {
     struct odp_flow_stats stats;
     struct odp_flow_key key;
-    union odp_action *actions;
-    uint32_t n_actions;
+    struct nlattr *actions;
+    uint32_t actions_len;
     uint32_t flags;
 };
 
@@ -262,101 +263,31 @@ struct odp_flowvec {
 };
 
 /* Action types. */
-#define ODPAT_OUTPUT            0    /* Output to switch port. */
-#define ODPAT_CONTROLLER        2    /* Send copy to controller. */
-#define ODPAT_SET_DL_TCI        3    /* Set the 802.1q TCI value. */
-#define ODPAT_STRIP_VLAN        5    /* Strip the 802.1q header. */
-#define ODPAT_SET_DL_SRC        6    /* Ethernet source address. */
-#define ODPAT_SET_DL_DST        7    /* Ethernet destination address. */
-#define ODPAT_SET_NW_SRC        8    /* IP source address. */
-#define ODPAT_SET_NW_DST        9    /* IP destination address. */
-#define ODPAT_SET_NW_TOS        10   /* IP ToS/DSCP field (6 bits). */
-#define ODPAT_SET_TP_SRC        11   /* TCP/UDP source port. */
-#define ODPAT_SET_TP_DST        12   /* TCP/UDP destination port. */
-#define ODPAT_SET_TUNNEL        13   /* Set the encapsulating tunnel ID. */
-#define ODPAT_SET_PRIORITY      14   /* Set skb->priority. */
-#define ODPAT_POP_PRIORITY      15   /* Restore original skb->priority. */
-#define ODPAT_DROP_SPOOFED_ARP  16   /* Drop ARPs with spoofed source MAC. */
-#define ODPAT_N_ACTIONS         17
-
-struct odp_action_output {
-    uint16_t type;              /* ODPAT_OUTPUT. */
-    uint16_t port;              /* Output port. */
-    uint16_t reserved1;
-    uint16_t reserved2;
+enum odp_action_type {
+    ODPAT_UNSPEC,
+    ODPAT_OUTPUT,		/* Output to switch port. */
+    ODPAT_CONTROLLER,		/* Send copy to controller. */
+    ODPAT_SET_DL_TCI,		/* Set the 802.1q TCI value. */
+    ODPAT_STRIP_VLAN,		/* Strip the 802.1q header. */
+    ODPAT_SET_DL_SRC,		/* Ethernet source address. */
+    ODPAT_SET_DL_DST,		/* Ethernet destination address. */
+    ODPAT_SET_NW_SRC,		/* IP source address. */
+    ODPAT_SET_NW_DST,		/* IP destination address. */
+    ODPAT_SET_NW_TOS,		/* IP ToS/DSCP field (6 bits). */
+    ODPAT_SET_TP_SRC,		/* TCP/UDP source port. */
+    ODPAT_SET_TP_DST,		/* TCP/UDP destination port. */
+    ODPAT_SET_TUNNEL,		/* Set the encapsulating tunnel ID. */
+    ODPAT_SET_PRIORITY,		/* Set skb->priority. */
+    ODPAT_POP_PRIORITY,		/* Restore original skb->priority. */
+    ODPAT_DROP_SPOOFED_ARP,	/* Drop ARPs with spoofed source MAC. */
+    __ODPAT_MAX
 };
 
-struct odp_action_controller {
-    uint16_t type;              /* ODPAT_OUTPUT_CONTROLLER. */
-    uint16_t reserved;
-    uint32_t arg;               /* Copied to struct odp_msg 'arg' member. */
-};
-
-struct odp_action_tunnel {
-    uint16_t type;              /* ODPAT_SET_TUNNEL. */
-    uint16_t reserved;
-    ovs_be32 tun_id;            /* Tunnel ID. */
-};
-
-/* Action structure for ODPAT_SET_DL_TCI. */
-struct odp_action_dl_tci {
-    uint16_t type;              /* ODPAT_SET_DL_TCI. */
-    ovs_be16 tci;               /* New TCI.  CFI bit must be zero. */
-    uint32_t reserved;
-};
-
-/* Action structure for ODPAT_SET_DL_SRC/DST. */
-struct odp_action_dl_addr {
-    uint16_t type;              /* ODPAT_SET_DL_SRC/DST. */
-    uint8_t dl_addr[6];         /* Ethernet address. */
-};
-
-/* Action structure for ODPAT_SET_NW_SRC/DST. */
-struct odp_action_nw_addr {
-    uint16_t type;              /* ODPAT_SET_TW_SRC/DST. */
-    uint16_t reserved;
-    ovs_be32 nw_addr;           /* IP address. */
-};
-
-struct odp_action_nw_tos {
-    uint16_t type;              /* ODPAT_SET_NW_TOS. */
-    uint8_t nw_tos;             /* IP ToS/DSCP field (6 bits). */
-    uint8_t reserved1;
-    uint16_t reserved2;
-    uint16_t reserved3;
-};
-
-/* Action structure for ODPAT_SET_TP_SRC/DST. */
-struct odp_action_tp_port {
-    uint16_t type;              /* ODPAT_SET_TP_SRC/DST. */
-    ovs_be16 tp_port;           /* TCP/UDP port. */
-    uint16_t reserved1;
-    uint16_t reserved2;
-};
-
-/* Action structure for ODPAT_SET_PRIORITY. */
-struct odp_action_priority {
-    uint16_t type;              /* ODPAT_SET_PRIORITY. */
-    uint16_t reserved;
-    uint32_t priority;          /* skb->priority value. */
-};
-
-union odp_action {
-    uint16_t type;
-    struct odp_action_output output;
-    struct odp_action_controller controller;
-    struct odp_action_tunnel tunnel;
-    struct odp_action_dl_tci dl_tci;
-    struct odp_action_dl_addr dl_addr;
-    struct odp_action_nw_addr nw_addr;
-    struct odp_action_nw_tos nw_tos;
-    struct odp_action_tp_port tp_port;
-    struct odp_action_priority priority;
-};
+#define ODPAT_MAX (__ODPAT_MAX - 1)
 
 struct odp_execute {
-    union odp_action *actions;
-    uint32_t n_actions;
+    struct nlattr *actions;
+    uint32_t actions_len;
 
     const void *data;
     uint32_t length;

commit 3b1fc5f33a533bc8399a987e2220359931cebe5c

datapath: Fix validation of ODPAT_OUTPUT actions.
The missing "break" here meant that an attempt to output to any port
number that happened to include the wrong bit would fail.

Problem introduced by commit cdee00fd635 (datapath: Replace "struct
odp_action" by Netlink attributes.)

Signed-off-by: Ben Pfaff <blp@nicira.com>
Acked-by: Jesse Gross <jess@nicira.com>
Reported-by: Michael Mao <mmao@nicira.com>
Bug #4385.

datapath/datapath.c
@@ -724,6 +724,7 @@ static int validate_actions(const struct nlattr *actions, u32 actions_len)
                 case ODPAT_OUTPUT:
                         if (nla_get_u32(a) >= DP_MAX_PORTS)
                                 return -EINVAL;
+			break;
 
                 case ODPAT_SET_DL_TCI:
 			if (nla_get_be16(a) & htons(VLAN_CFI_MASK))