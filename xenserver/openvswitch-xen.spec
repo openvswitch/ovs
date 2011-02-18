openvswitch/ovs

commit 92dbd5c9e3134eabc6e57b397dd18493bc96c9b8

xenserver: Replace customized xen-bugtool with plugin to collect qdisc info.
XenServer RPMs have installed a modified /usr/sbin/xen-bugtool that is
identical to upstream except for an additional command to collect tc qdisc
class configuration for each network interface. Since xen-bugtool has an
extension mechanism, use that instead of maintaining a parallel patched
xen-bugtool.

This does change the content of the bug report slightly. Instead of one file
per network interface, qdisc class info for all interfaces is written to one
file. This is an artifact of the xen-bugtool extension mechanism: a single
extension command's output must go to one file, and it is impractical to create
individual <command> tags for each interface dynamically.

Reviewed by Ben Pfaff.

xenserver/openvswitch-xen.spec
@@ -60,6 +60,9 @@ install -m 755 xenserver/etc_profile.d_openvswitch.sh \
 install -d -m 755 $RPM_BUILD_ROOT/etc/xapi.d/plugins
 install -m 755 xenserver/etc_xapi.d_plugins_openvswitch-cfg-update \
          $RPM_BUILD_ROOT/etc/xapi.d/plugins/openvswitch-cfg-update
+install -d -m 755 $RPM_BUILD_ROOT/etc/xensource/bugtool/network-status
+install -m 644 xenserver/etc_xensource_bugtool_network-status_openvswitch.xml \
+         $RPM_BUILD_ROOT/etc/xensource/bugtool/network-status/openvswitch.xml
 install -d -m 755 $RPM_BUILD_ROOT/usr/share/openvswitch/scripts
 install -m 644 vswitchd/vswitch.ovsschema \
          $RPM_BUILD_ROOT/usr/share/openvswitch/vswitch.ovsschema
@@ -75,10 +78,10 @@ install -m 755 xenserver/etc_xensource_scripts_vif \
              $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/vif
 install -m 755 xenserver/usr_share_openvswitch_scripts_ovs-xapi-sync \
                $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/ovs-xapi-sync
-install -m 755 xenserver/usr_sbin_xen-bugtool \
-             $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/xen-bugtool
 install -m 755 xenserver/usr_share_openvswitch_scripts_sysconfig.template \
          $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/sysconfig.template
+install -m 755 xenserver/usr_share_openvswitch_scripts_xen-bugtool-tc-class-show \
+         $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/xen-bugtool-tc-class-show
 install -m 755 utilities/ovs-save \
          $RPM_BUILD_ROOT/usr/share/openvswitch/scripts/ovs-save
 install -d -m 755 $RPM_BUILD_ROOT/usr/lib/xsconsole/plugins-base
@@ -186,8 +189,7 @@ for f in \
     /opt/xensource/libexec/InterfaceReconfigure.py \
     /opt/xensource/libexec/InterfaceReconfigureBridge.py \
     /opt/xensource/libexec/InterfaceReconfigureVswitch.py \
-    /etc/xensource/scripts/vif \
-    /usr/sbin/xen-bugtool
+    /etc/xensource/scripts/vif
 do
     s=$(basename "$f")
     t=$(readlink "$f")
@@ -248,15 +250,13 @@ rm -f /usr/share/openvswitch/scripts/InterfaceReconfigure.pyc \
     /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyc \
     /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyo 
 
-# Restore original XenServer scripts. It's important to do this even on upgrade
-# since the version to be installed may be missing some of these scripts.
+# Restore original XenServer scripts
 for f in \
     /opt/xensource/libexec/interface-reconfigure \
     /opt/xensource/libexec/InterfaceReconfigure.py \
     /opt/xensource/libexec/InterfaceReconfigureBridge.py \
     /opt/xensource/libexec/InterfaceReconfigureVswitch.py \
-    /etc/xensource/scripts/vif \
-    /usr/sbin/xen-bugtool
+    /etc/xensource/scripts/vif
 do
     s=$(basename "$f")
     if [ ! -f "/usr/lib/openvswitch/xs-original/$s" ]; then
@@ -290,6 +290,7 @@ fi
 /etc/init.d/openvswitch
 /etc/init.d/openvswitch-xapi-update
 /etc/xapi.d/plugins/openvswitch-cfg-update
+/etc/xensource/bugtool/network-status/openvswitch.xml
 /etc/logrotate.d/openvswitch
 /etc/profile.d/openvswitch.sh
 /lib/modules/%{xen_version}/kernel/extra/openvswitch/openvswitch_mod.ko
@@ -321,8 +322,8 @@ fi
 /usr/share/openvswitch/scripts/InterfaceReconfigureBridge.py
 /usr/share/openvswitch/scripts/InterfaceReconfigureVswitch.py
 /usr/share/openvswitch/scripts/vif
-/usr/share/openvswitch/scripts/xen-bugtool
 /usr/share/openvswitch/scripts/sysconfig.template
+/usr/share/openvswitch/scripts/xen-bugtool-tc-class-show
 /usr/share/openvswitch/scripts/ovs-save
 /usr/share/openvswitch/vswitch.ovsschema
 /usr/sbin/ovs-vswitchd

debian/copyright.in
@@ -25,7 +25,6 @@ License:
 	xenserver/opt_xensource_libexec_InterfaceReconfigureVswitch.py
 	xenserver/opt_xensource_libexec_interface-reconfigure
 	xenserver/usr_lib_xsconsole_plugins-base_XSFeatureVSwitch.py
-	xenserver/usr_sbin_xen-bugtool
 
 	* These components are only distributed in the source package.
 	  They do not appear in any binary packages.
@@ -54,10 +53,6 @@ License:
   GNU Lesser General Public Licence version 2.1.
 
 	debian/ovs-bugtool
-	xenserver/usr_sbin_xen-bugtool [*]
-
-	* This component is only distributed in the source package.
-	  It does not appear in any binary packages.
 
   On Debian systems, the complete text of the
   GNU Lesser General Public Licence version 2.1 can be found in

xenserver/README
@@ -34,6 +34,12 @@ files are:
         in the ovs-vswitchd configuration that are managed in the
         xapi database when integrated with Citrix management tools.
 
+    etc_xensource_bugtool_network-status_openvswitch.xml
+
+        xen-bugtool extension to collect networking information relevant to
+        Open vSwitch. Runs
+        usr_share_openvswitch_scripts_xen-bugtool-tc-class-show.
+
     etc_xensource_scripts_vif
 
         Open vSwitch-aware replacement for Citrix script of the same name.
@@ -57,15 +63,17 @@ files are:
         Template for Open vSwitch's /etc/sysconfig/openvswitch 
         configuration file.
 
+    usr_share_openvswitch_scripts_xen-bugtool-tc-class-show
+
+        Script to dump tc class configuration for all network
+        interfaces. Intended to be run by xen-bugtool via
+        etc_xensource_bugtool_network-status_openvswitch.xml.
+
     usr_lib_xsconsole_plugins-base_XSFeatureVSwitch.py
 
         xsconsole plugin to configure the pool-wide configuration keys
         used to control Open vSwitch when integrated with Citrix 
         management tools.
-
-    usr_sbin_xen-bugtool
-
-        Open vSwitch-aware replacement for Citrix script of the same name.
 
     uuid.py
 

xenserver/automake.mk
@@ -15,6 +15,7 @@ EXTRA_DIST += \
 	xenserver/etc_logrotate.d_openvswitch \
 	xenserver/etc_profile.d_openvswitch.sh \
 	xenserver/etc_xapi.d_plugins_openvswitch-cfg-update \
+	xenserver/etc_xensource_bugtool_network-status_openvswitch.xml \
 	xenserver/etc_xensource_scripts_vif \
 	xenserver/openvswitch-xen.spec \
 	xenserver/opt_xensource_libexec_InterfaceReconfigure.py \
@@ -22,7 +23,7 @@ EXTRA_DIST += \
 	xenserver/opt_xensource_libexec_InterfaceReconfigureVswitch.py \
 	xenserver/opt_xensource_libexec_interface-reconfigure \
 	xenserver/usr_lib_xsconsole_plugins-base_XSFeatureVSwitch.py \
-	xenserver/usr_sbin_xen-bugtool \
 	xenserver/usr_share_openvswitch_scripts_ovs-xapi-sync \
 	xenserver/usr_share_openvswitch_scripts_sysconfig.template \
+	xenserver/usr_share_openvswitch_scripts_xen-bugtool-tc-class-show \
 	xenserver/uuid.py

xenserver/etc_xensource_bugtool_network-status_openvswitch.xml
@@ -1,0 +1,21 @@
+<!--
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of version 2.1 of the GNU Lesser General Public
+ License as published by the Free Software Foundation.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA.
+
+ Copyright (C) 2011 Nicira Networks.
+-->
+
+<collect>
+  <command label="tc-class-show">/usr/share/openvswitch/scripts/xen-bugtool-tc-class-show</command>
+</collect>

xenserver/usr_share_openvswitch_scripts_xen-bugtool-tc-class-show
@@ -1,0 +1,25 @@
+#! /bin/sh
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General
+# Public License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+#
+# Copyright (C) 2011 Nicira Networks.
+
+for iface in $(cd /sys/class/net && echo *); do
+    if [ -d /sys/class/net/$iface ]; then
+	echo Interface $iface:
+	# indent tc output so it's clear which interface it pertains to
+	/sbin/tc -s -d class show dev $iface | /bin/sed 's/^/ /'
+    fi
+done

commit 5cb12f2eeffdfcb7696bdcb652b581cfd13d1c3a

xenserver: Revert only the XenServer scripts no longer replaced by OVS.
Commit d66880ee (xenserver: Clean up /usr/sbin/brctl dangling symlink.)
attempted to revert the original XenServer-shipped versions of scripts replaced
by OVS during an RPM upgrade, but the logic was incorrect. It assumed that
%postun of the package being replaced was run before the %post of the new
version replacing it. The reverse is actually true.

Make upgrade and erase cases both work correctly by simply checking whether any
of the files ever replaced in any OVS version are dangling symlinks, and if so,
attempt to copy the saved XenServer original back to its rightful place. In the
upgrade case, if the newly-installed version of OVS lacks any of the scripts in
the previous version, those will be reverted. In the erase case, none of the
OVS replacements will exist, so they will all be dangling symlinks and will all
be reverted.

Furthermore, replace any dangling symlink from /usr/sbin/xen-bugtool to the
now-nonexistent OVS replacement (caused by commit 92dbd5c9 (xenserver: Replace
customized xen-bugtool with plugin to collect qdisc info.)).

Lastly, prevent accidental reversion of files replaced by OVS during rpm -U
(also caused by commit 92dbd5c9).

Bug #4696.

xenserver/openvswitch-xen.spec
@@ -181,8 +181,17 @@ else
     done
 fi
 
+# Deliberately break %postun in broken OVS builds that revert original
+# XenServer scripts during rpm -U by moving the directory where it thinks
+# they are saved.
+if [ -d /usr/lib/openvswitch/xs-original ]; then
+    mkdir -p /usr/lib/openvswitch/xs-saved
+    mv /usr/lib/openvswitch/xs-original/* /usr/lib/openvswitch/xs-saved/ &&
+        rmdir /usr/lib/openvswitch/xs-original
+fi
+
 # Replace XenServer files by our versions.
-mkdir -p /usr/lib/openvswitch/xs-original \
+mkdir -p /usr/lib/openvswitch/xs-saved \
     || printf "Could not create script backup directory.\n"
 for f in \
     /opt/xensource/libexec/interface-reconfigure \
@@ -194,19 +203,24 @@ do
     s=$(basename "$f")
     t=$(readlink "$f")
     if [ -f "$f" ] && [ "$t" != "/usr/share/openvswitch/scripts/$s" ]; then
-        mv "$f" /usr/lib/openvswitch/xs-original/ \
+        mv "$f" /usr/lib/openvswitch/xs-saved/ \
             || printf "Could not save original XenServer $s script\n"
         ln -s "/usr/share/openvswitch/scripts/$s" "$f" \
             || printf "Could not link to Open vSwitch $s script\n"
     fi
 done
 
-# Bug #4667: one-time cleanup of brctl removal in commit 54f16a10
-# (xenserver: Remove brctl wrapper script).
-if [ -h /usr/sbin/brctl ] &&
-    [ "$(readlink /usr/sbin/brctl)" = /usr/share/openvswitch/scripts/brctl ]; then
-    mv -f /usr/lib/openvswitch/xs-original/brctl /usr/sbin/
-fi
+# Clean up dangling symlinks to removed OVS replacement scripts no longer
+# provided by OVS. Any time a replacement script is removed from OVS,
+# it should be added here to ensure correct reversion from old versions of
+# OVS that don't clean up dangling symlinks during the uninstall phase.
+for orig in \
+    /usr/sbin/brctl \
+    /usr/sbin/xen-bugtool
+do
+    saved=/usr/lib/openvswitch/xs-saved/$(basename "$orig")
+    [ -e "$saved" ] && mv -f "$saved" "$orig"
+done
 
 # Ensure all required services are set to run
 for s in openvswitch openvswitch-xapi-update; do
@@ -240,41 +254,56 @@ fi
 
 
 %postun
-rm -f /usr/lib/xsconsole/plugins-base/XSFeatureVSwitch.pyc \
-    /usr/lib/xsconsole/plugins-base/XSFeatureVSwitch.pyo
-
-rm -f /usr/share/openvswitch/scripts/InterfaceReconfigure.pyc \
-    /usr/share/openvswitch/scripts/InterfaceReconfigure.pyo \
-    /usr/share/openvswitch/scripts/InterfaceReconfigureBridge.pyc \
-    /usr/share/openvswitch/scripts/InterfaceReconfigureBridge.pyo \
-    /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyc \
-    /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyo 
-
-# Restore original XenServer scripts
+# Restore original XenServer scripts if the OVS equivalent no longer exists.
+# This works both in the upgrade and erase cases.
+# This lists every file that every version of OVS has ever replaced. Never
+# remove old files that OVS no longer replaces, or upgrades from old versions
+# will fail to restore the XS originals, leaving the system in a broken state.
+# Also be sure to add removed script paths to the %post scriptlet above to
+# prevent the same problem when upgrading from old versions of OVS that lack
+# this restore-on-upgrade logic.
 for f in \
+    /etc/xensource/scripts/vif \
+    /usr/sbin/brctl \
+    /usr/sbin/xen-bugtool \
     /opt/xensource/libexec/interface-reconfigure \
     /opt/xensource/libexec/InterfaceReconfigure.py \
     /opt/xensource/libexec/InterfaceReconfigureBridge.py \
-    /opt/xensource/libexec/InterfaceReconfigureVswitch.py \
-    /etc/xensource/scripts/vif
+    /opt/xensource/libexec/InterfaceReconfigureVswitch.py
 do
-    s=$(basename "$f")
-    if [ ! -f "/usr/lib/openvswitch/xs-original/$s" ]; then
-        printf "Original XenServer $s script not present in /usr/lib/openvswitch/xs-original\n"
-        printf "Could not restore original XenServer script.\n"
-    else
-        (rm -f "$f" \
-            && mv "/usr/lib/openvswitch/xs-original/$s" "$f") \
-            || printf "Could not restore original XenServer $s script.\n"
+    # Only revert dangling symlinks.
+    if [ -h "$f" ] && [ ! -e "$f" ]; then
+        s=$(basename "$f")
+        if [ ! -f "/usr/lib/openvswitch/xs-saved/$s" ]; then
+            printf "Original XenServer $s script not present in /usr/lib/openvswitch/xs-saved\n" >&2
+            printf "Could not restore original XenServer script.\n" >&2
+        else
+            (rm -f "$f" \
+                && mv "/usr/lib/openvswitch/xs-saved/$s" "$f") \
+                || printf "Could not restore original XenServer $s script.\n" >&2
+        fi
     fi
 done
 
 if [ "$1" = "0" ]; then     # $1 = 1 for upgrade
+    rm -f /usr/lib/xsconsole/plugins-base/XSFeatureVSwitch.pyc \
+        /usr/lib/xsconsole/plugins-base/XSFeatureVSwitch.pyo
+
+    rm -f /usr/share/openvswitch/scripts/InterfaceReconfigure.pyc \
+        /usr/share/openvswitch/scripts/InterfaceReconfigure.pyo \
+        /usr/share/openvswitch/scripts/InterfaceReconfigureBridge.pyc \
+        /usr/share/openvswitch/scripts/InterfaceReconfigureBridge.pyo \
+        /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyc \
+        /usr/share/openvswitch/scripts/InterfaceReconfigureVSwitch.pyo
+
     # Remove all configuration files
     rm -f /etc/openvswitch/conf.db
     rm -f /etc/sysconfig/openvswitch
     rm -f /etc/openvswitch/vswitchd.cacert
     rm -f /var/xapi/network.dbcache
+
+    # Remove saved XenServer scripts directory, but only if it's empty
+    rmdir -p /usr/lib/openvswitch/xs-saved 2>/dev/null
 
     # Configure system to use bridge
     echo bridge > /etc/xensource/network.conf