0e191d8fae33819ce953b0d02e0b2df0cd9b400f
Ben Pfaff <blp@nicira.com>
2011-02-22 10:36:13 -0800

    string: Implement strnlen() if it is missing.

--- lib/string.c
+++ lib/string.c
@@ -14,27 +14,15 @@
  * limitations under the License.
  */
 
-#ifndef STRING_WRAPPER_H
-#define STRING_WRAPPER_H 1
+#include <config.h>
 
-#include_next <string.h>
-
-/* Glibc 2.7 has a bug in strtok_r when compiling with optimization that can
- * cause segfaults if the delimiters argument is a compile-time constant that
- * has exactly 1 character:
- *
- *      http://sources.redhat.com/bugzilla/show_bug.cgi?id=5614
- *
- * The bug is only present in the inline version of strtok_r(), so force the
- * out-of-line version to be used instead. */
-#if HAVE_STRTOK_R_BUG
-#undef strtok_r
-#endif
+#include <string.h>
 
 #ifndef HAVE_STRNLEN
-#undef strnlen
-#define strnlen rpl_strnlen
-size_t strnlen(const char *, size_t maxlen);
+size_t
+strnlen(const char *s, size_t maxlen)
+{
+    const char *end = memchr(s, '\0', maxlen);
+    return end ? end - s : maxlen;
+}
 #endif
-
-#endif /* string.h wrapper */
