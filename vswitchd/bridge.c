openvswitch/ovs

commit 1101a0b4708d87505d80c4641c4adadd5c6ece36

bridge: Populate interface status/statistics as soon as a port is added.
Currently there's a lag of up to five seconds before the status and statistics
columns in the Interface table are populated when a port is first added to a
bridge. This may confuse systems that expect those columns to be populated
right away.

Bug #6145.

vswitchd/bridge.c
@@ -195,7 +195,9 @@ static void iface_set_mac(struct iface *);
 static void iface_set_ofport(const struct ovsrec_interface *, int64_t ofport);
 static void iface_configure_qos(struct iface *, const struct ovsrec_qos *);
 static void iface_configure_cfm(struct iface *);
-static bool iface_refresh_cfm_stats(struct iface *iface);
+static bool iface_refresh_cfm_stats(struct iface *);
+static void iface_refresh_stats(struct iface *);
+static void iface_refresh_status(struct iface *);
 static bool iface_get_carrier(const struct iface *);
 static bool iface_is_synthetic(const struct iface *);
 
@@ -864,6 +866,12 @@ bridge_add_ofproto_ports(struct bridge *br)
                 VLOG_WARN("could not %s network device %s (%s)",
                           iface->netdev ? "reconfigure" : "open",
                           iface->name, strerror(error));
+            }
+
+            /* Populate stats columns in new Interface rows. */
+            if (!iface->cfg->mtu) {
+                iface_refresh_stats(iface);
+                iface_refresh_status(iface);
             }
 
             /* Add the port, if necessary. */

commit 316dec68688aeccc1bbb90e555d9c0ca259d9fc7

bridge: Fix null pointer dereference.
If the netdev_open() fails then iface->netdev will be NULL and
iface_refresh_stats() will cause a null pointer dereference in
netdev_get_stats().

Fixes a problem introduced by commit 1101a0b47 "bridge: Populate interface
status/statistics as soon as a port is added."

Reported-by: Aaron Rosen <arosen@clemson.edu>

vswitchd/bridge.c
@@ -869,7 +869,7 @@ bridge_add_ofproto_ports(struct bridge *br)
             }
 
             /* Populate stats columns in new Interface rows. */
-            if (!iface->cfg->mtu) {
+            if (iface->netdev && !iface->cfg->mtu) {
                 iface_refresh_stats(iface);
                 iface_refresh_status(iface);
             }