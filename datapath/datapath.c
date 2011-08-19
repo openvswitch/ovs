b867ca7598a796069928aa9d78b9bff06d71ac9e
Ben Pfaff <blp@nicira.com>
2011-08-19 15:43:06 -0700

    datapath: Remove unneeded { } around single statement.
    I noticed this looking around at other code.
    
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    Acked-by: Jesse Gross <jesse@nicira.com>

--- datapath/datapath.c
+++ datapath/datapath.c
@@ -2007,9 +2007,8 @@ static void dp_unregister_genl(int n_families)
 {
 	int i;
 
-	for (i = 0; i < n_families; i++) {
+	for (i = 0; i < n_families; i++)
 		genl_unregister_family(dp_genl_families[i].family);
-	}
 }
 
 static int dp_register_genl(void)
