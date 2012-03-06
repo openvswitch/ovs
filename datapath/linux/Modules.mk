3cd3869da305d4e9e7139b235f5901e018bb1c07
Jesse Gross <jesse@nicira.com>
2012-03-06 13:09:13 -0800

    datapath: Fix checksum update for actions on UDP packets.
    When modifying IP addresses or ports on a UDP packet we don't
    correctly follow the rules for unchecksummed packets.  This meant
    that packets without a checksum can be given a incorrect new checksum
    and packets with a checksum can become marked as being unchecksummed.
    This fixes it to handle those requirements.
    
    Bug #8937
    
    Signed-off-by: Jesse Gross <jesse@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- datapath/linux/Modules.mk
+++ datapath/linux/Modules.mk
@@ -12,6 +12,7 @@ openvswitch_sources += \
 	linux/compat/time.c	\
 	linux/compat/workqueue.c
 openvswitch_headers += \
+	linux/compat/include/linux/checksum.h \
 	linux/compat/include/linux/compiler.h \
 	linux/compat/include/linux/compiler-gcc.h \
 	linux/compat/include/linux/cpumask.h \
