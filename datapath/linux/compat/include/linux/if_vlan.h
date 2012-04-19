3d192f8e5655cbffe50356f46bd8fd8c3e8875b0
Simon Horman <horms@verge.net.au>
2012-04-19 16:38:39 +0900

    datapath: Use ETH_ALEN instead of VLAN_ETH_ALEN
    VLAN_ETH_ALEN will be removed in version 3.5 of the Linux kernel.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- datapath/linux/compat/include/linux/if_vlan.h
+++ datapath/linux/compat/include/linux/if_vlan.h
@@ -31,7 +31,7 @@ static inline struct sk_buff *__vlan_put_tag(struct sk_buff *skb, u16 vlan_tci)
 	veth = (struct vlan_ethhdr *)skb_push(skb, VLAN_HLEN);
 
 	/* Move the mac addresses to the beginning of the new header. */
-	memmove(skb->data, skb->data + VLAN_HLEN, 2 * VLAN_ETH_ALEN);
+	memmove(skb->data, skb->data + VLAN_HLEN, 2 * ETH_ALEN);
 	skb->mac_header -= VLAN_HLEN;
 
 	/* first, the ethernet type */
