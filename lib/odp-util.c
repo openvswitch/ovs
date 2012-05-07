58a1590cb3da958cc0d1407dcc83d48c1e893c14
Ben Pfaff <blp@nicira.com>
2012-05-07 14:01:57 -0700

    ofproto: Treat a packet-out in_port of OFPP_CONTROLLER as OFPP_NONE.
    Some OpenFlow 1.0 controllers incorrectly use OPFP_CONTROLLER as the
    in_port in packet-out messages, when OFPP_NONE is their intent.  Until now,
    Open vSwitch has rejected such requests with an error message.  This commit
    makes Open vSwitch instead treat OFPP_CONTROLLER the same as OFPP_NONE for
    compatibility with those controllers.
    
    (Also, as of this writing, OpenFlow 1.0.1 appears to be changing the port
    to use from OFPP_NONE to OFPP_CONTROLLER.)
    
    Suggested-by: Rob Sherwood <rob.sherwood@bigswitch.com>
    Signed-off-by: Ben Pfaff <blp@nicira.com>

--- lib/odp-util.c
+++ lib/odp-util.c
@@ -1157,7 +1157,7 @@ odp_flow_key_from_flow(struct ofpbuf *buf, const struct flow *flow)
         nl_msg_put_be64(buf, OVS_KEY_ATTR_TUN_ID, flow->tun_id);
     }
 
-    if (flow->in_port != OFPP_NONE) {
+    if (flow->in_port != OFPP_NONE && flow->in_port != OFPP_CONTROLLER) {
         nl_msg_put_u32(buf, OVS_KEY_ATTR_IN_PORT,
                        ofp_port_to_odp_port(flow->in_port));
     }
