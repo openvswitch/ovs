271c896ee455422992f8717f5c715ae8522c4000
Arun Sharma <arun.sharma@calsoftinc.com>
2012-05-08 23:47:30 -0700

    bugtool: Collect bond state information from ovs.
    This is an enhancement in bugtool archive output to determine the bond
    state information. It is implemented as a plugin which internally calls
    "ovs-appctl bond/show" command to get bond state.
    
    Feature #11283.
    Signed-off-by: Arun Sharma <arun.sharma@calsoftinc.com>
    Signed-off-by: Ben Pfaff <blp@nicira.com>

--- utilities/bugtool/automake.mk
+++ utilities/bugtool/automake.mk
@@ -18,7 +18,8 @@ bugtool_scripts = \
 	utilities/bugtool/ovs-bugtool-tc-class-show \
 	utilities/bugtool/ovs-bugtool-vsctl-show \
 	utilities/bugtool/ovs-bugtool-ovsdb-dump \
-	utilities/bugtool/ovs-bugtool-daemons-ver
+	utilities/bugtool/ovs-bugtool-daemons-ver \
+	utilities/bugtool/ovs-bugtool-bond-show
 scripts_SCRIPTS += $(bugtool_scripts)
 
 bugtoolpluginsdir = $(pkgdatadir)/bugtool-plugins
