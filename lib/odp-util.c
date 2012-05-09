211f89447bd7602c26a7d86d01d40ef433e8c0da
Ben Pfaff <blp@nicira.com>
2012-05-09 12:21:54 -0700

    odp-util: Fix parsing of actions encapsulated within "sample" actions.
    Signed-off-by: Ben Pfaff <blp@nicira.com>

--- lib/odp-util.c
+++ lib/odp-util.c
@@ -434,7 +434,7 @@ parse_odp_action(const char *s, const struct shash *port_names,
             for (;;) {
                 int retval;
 
-                s += strspn(s, delimiters);
+                n += strspn(s + n, delimiters);
                 if (s[n] == ')') {
                     break;
                 }
@@ -444,7 +444,6 @@ parse_odp_action(const char *s, const struct shash *port_names,
                     return retval;
                 }
                 n += retval;
-
             }
             nl_msg_end_nested(actions, actions_ofs);
             nl_msg_end_nested(actions, sample_ofs);
