1f1936d935e8012ff24fed3d309cd7ef955790dd
Ansis Atteka <aatteka@nicira.com>
2012-11-06 13:39:58 +0200

    datapath: Add ipv6_find_hdr() compatibility function.
    Implementation of the IPv6 'set' action depends on ipv6_find_exthdr()
    function to find routing header. By looking at the routing headers,
    it is possible to tell, whether L4 checksums will need to be
    recalculated, whenever the destination address is changed in
    the main IPv6 header.
    
    This function will need to be customized so that it would skip all
    routing headers, where segements_left is equal to 0.
    
    Signed-off-by: Ansis Atteka <aatteka@nicira.com>

--- datapath/linux/compat/include/net/ipv6.h
+++ datapath/linux/compat/include/net/ipv6.h
@@ -3,11 +3,22 @@
 
 #include_next <net/ipv6.h>
 
+enum {
+	OVS_IP6T_FH_F_FRAG	= (1 << 0),
+	OVS_IP6T_FH_F_AUTH	= (1 << 1),
+};
+
 /* This function is upstream but not the version which supplies the
  * fragment offset.  We plan to propose the extended version.
  */
 #define ipv6_skip_exthdr rpl_ipv6_skip_exthdr
 extern int ipv6_skip_exthdr(const struct sk_buff *skb, int start,
 				u8 *nexthdrp, __be16 *frag_offp);
+
+/* This function is upstream, but not the version which skips routing
+ * headers with 0 segments_left. We plan to propose the extended version. */
+#define ipv6_find_hdr rpl_ipv6_find_hdr
+extern int ipv6_find_hdr(const struct sk_buff *skb, unsigned int *offset,
+			 int target, unsigned short *fragoff, int *fragflg);
 
 #endif
