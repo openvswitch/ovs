3d974905842e211a62d198b0b8ae752f173d0efc
Ansis Atteka <aatteka@nicira.com>
2012-11-07 19:14:34 +0200

    sparse: Add ip6_rthdr struct to the ip6.h
    Acked-by: Ben Pfaff <blp@nicira.com>
    Signed-off-by: Ansis Atteka <aatteka@nicira.com>

--- include/sparse/netinet/ip6.h
+++ include/sparse/netinet/ip6.h
@@ -44,6 +44,13 @@ struct ip6_hdr {
 #define ip6_hlim  ip6_ctlun.ip6_un1.ip6_un1_hlim
 #define ip6_hops  ip6_ctlun.ip6_un1.ip6_un1_hlim
 
+struct ip6_rthdr {
+    uint8_t ip6r_nxt;
+    uint8_t ip6r_len;
+    uint8_t ip6r_type;
+    uint8_t ip6r_segleft;
+};
+
 struct ip6_ext {
     uint8_t ip6e_nxt;
     uint8_t ip6e_len;
