523284fcdd710ff123ff866f8db57c06cf3594d8
Jesse Gross <jesse@nicira.com>
2012-12-06 11:10:08 -0800

    netdev-vport: Drop extra parentheses around "vxlan".
    Cc: Kyle Mestery <kmestery@cisco.com>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- lib/netdev-vport.c
+++ lib/netdev-vport.c
@@ -177,7 +177,7 @@ netdev_vport_get_netdev_type(const struct dpif_linux_vport *vport)
         return "capwap";
 
     case OVS_VPORT_TYPE_VXLAN:
-        return ("vxlan");
+        return "vxlan";
 
     case OVS_VPORT_TYPE_FT_GRE:
     case __OVS_VPORT_TYPE_MAX:
