f3d85db38e04bd994fa6f16ac6270ac0361340e5
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2013-01-10 09:41:16 -0800

    datapath: Use FIELD_SIZEOF() in dp_init().
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- datapath/datapath.c
+++ datapath/datapath.c
@@ -2091,10 +2091,9 @@ static struct pernet_operations ovs_net_ops = {
 
 static int __init dp_init(void)
 {
-	struct sk_buff *dummy_skb;
 	int err;
 
-	BUILD_BUG_ON(sizeof(struct ovs_skb_cb) > sizeof(dummy_skb->cb));
+	BUILD_BUG_ON(sizeof(struct ovs_skb_cb) > FIELD_SIZEOF(struct sk_buff, cb));
 
 	pr_info("Open vSwitch switching datapath %s, built "__DATE__" "__TIME__"\n",
 		VERSION);
