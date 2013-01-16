465b6c7e352ff4950fb627d383467260b16d3d7f
James Page <james.page@ubuntu.com>
2013-01-16 11:19:21 +0000

    datapath: support Linux 3.8 kernel
    Add Linux 3.8 kernel to the range of supported kernel versions.
    
    Signed-off-by: James Page <james.page@ubuntu.com>
    [jesse: Update NEWS and FAQ]
    Signed-off-by: Jesse Gross <jesse@nicira.com>
    
    Conflicts:
    
    	FAQ

--- datapath/datapath.c
+++ datapath/datapath.c
@@ -61,8 +61,8 @@
 #include "vport-internal_dev.h"
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18) || \
-    LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0)
-#error Kernels before 2.6.18 or after 3.7 are not supported by this version of Open vSwitch.
+    LINUX_VERSION_CODE >= KERNEL_VERSION(3,9,0)
+#error Kernels before 2.6.18 or after 3.8 are not supported by this version of Open vSwitch.
 #endif
 
 #define REHASH_FLOW_INTERVAL (10 * 60 * HZ)
