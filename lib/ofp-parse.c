b36667fd3d08b3246eaff821836a5e9ded123f3c
Justin Pettit <jpettit@nicira.com>
2013-02-01 00:11:32 -0800

    ofp-parse: Ignore "idle_age" and "hard_age" when parsing a flow string.
    It should be possible to feed to output of "ovs-ofctl dump-flows" to
    "ovs-ofctl add-flows".  However, some of the metadata needs to be
    ignored.  "idle_age" and "hard_age" was recently added to the output of
    "ovs-ofctl dump-flows", but they were not ignored like the other
    metadata.  This commit ignores them.
    
    Signed-off-by: Justin Pettit <jpettit@nicira.com>

--- lib/ofp-parse.c
+++ lib/ofp-parse.c
@@ -669,7 +669,9 @@ parse_ofp_str(struct ofputil_flow_mod *fm, int command, const char *str_,
                 parse_field(mf_from_name(name), value, &fm->cr);
             } else if (!strcmp(name, "duration")
                        || !strcmp(name, "n_packets")
-                       || !strcmp(name, "n_bytes")) {
+                       || !strcmp(name, "n_bytes")
+                       || !strcmp(name, "idle_age")
+                       || !strcmp(name, "hard_age")) {
                 /* Ignore these, so that users can feed the output of
                  * "ovs-ofctl dump-flows" back into commands that parse
                  * flows. */
