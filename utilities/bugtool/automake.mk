740f68d71a7333700e5af6e9761b8643f1d51924
Gurucharan Shetty <gshetty@nicira.com>
2013-03-25 09:48:55 -0700

    ovs-bugtool: Add ovs-appctl dpif commands to debug bundle.
    With single datapath, 'ovs-dpctl dump-flows ' dumps datapath flows for
    all the userspace bridges in a system. It can get a little harder
    to figure out the datapath flows belonging to a particular userspace
    bridge.
    
    This patch adds the 'ovs-appctl dpif/show' and 'ovs-appctl dpif/dump-flows'
    output for each userspace bridge. This gives us a summary of configured
    datapaths and all datapath flow entries for each bridge separately.
    
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>

--- utilities/bugtool/automake.mk
+++ utilities/bugtool/automake.mk
@@ -24,6 +24,7 @@ bugtool_scripts = \
 	utilities/bugtool/ovs-bugtool-daemons-ver \
 	utilities/bugtool/ovs-bugtool-ovs-ofctl-show \
 	utilities/bugtool/ovs-bugtool-ovs-ofctl-dump-flows \
+	utilities/bugtool/ovs-bugtool-ovs-appctl-dpif \
 	utilities/bugtool/ovs-bugtool-bond-show
 scripts_SCRIPTS += $(bugtool_scripts)
 
