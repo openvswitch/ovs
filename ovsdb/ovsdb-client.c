2b4ee5c418729f0b25e7127f4e2c311de55fd047
Justin Pettit <jpettit@nicira.com>
2013-05-06 21:30:26 -0700

    ovsdb-client: Fix recently introduced svec_sort() bug.
    Commit 66980be9 (ovsdb-client: Avoid assertion with multiple databases.)
    passed in a pointer to an svec pointer, when it should have just been an
    svec pointer.  This corrects the bug.
    
    Signed-off-by: Justin Pettit <jpettit@nicira.com>

--- ovsdb/ovsdb-client.c
+++ ovsdb/ovsdb-client.c
@@ -393,7 +393,7 @@ fetch_dbs(struct jsonrpc *rpc, struct svec *dbs)
         svec_add(dbs, name->u.string);
     }
     jsonrpc_msg_destroy(reply);
-    svec_sort(&dbs);
+    svec_sort(dbs);
 }
 
 static void
