9450a6e061533afc1728c37b826cb35efbd6edb8
Jesse Gross <jesse@nicira.com>
2013-07-09 16:59:56 -0700

    datpath: Fix tunnel TTL flow rejection message.
    There is no default value for the tunnel TTL, so it must be
    specified when setting up a new flow. However, the flow rejection
    log message indicates that the TTL must be non-zero, which is not
    true.
    
    CC: Andy Zhou <azhou@nicira.com>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- datapath/flow.c
+++ datapath/flow.c
@@ -1260,7 +1260,7 @@ int ipv4_tun_from_nlattr(const struct nlattr *attr,
 	}
 
 	if (!ttl) {
-		OVS_NLERR("IPv4 tunnel TTL is zero.\n");
+		OVS_NLERR("IPv4 tunnel TTL not specified.\n");
 		return -EINVAL;
 	}
 
