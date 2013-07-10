a4fff9cc390b69bde1257b414212505a72c4b450
Gurucharan Shetty <gshetty@nicira.com>
2013-07-10 09:42:37 -0700

    ovs-bugtool: Add ovsdb-server/list-dbs command to bugtool.
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- utilities/bugtool/automake.mk
+++ utilities/bugtool/automake.mk
@@ -17,6 +17,7 @@ bugtool_scripts = \
 	utilities/bugtool/ovs-bugtool-cfm-show \
 	utilities/bugtool/ovs-bugtool-coverage-show \
 	utilities/bugtool/ovs-bugtool-lacp-show \
+	utilities/bugtool/ovs-bugtool-list-dbs \
 	utilities/bugtool/ovs-bugtool-memory-show \
 	utilities/bugtool/ovs-bugtool-tc-class-show \
 	utilities/bugtool/ovs-bugtool-vsctl-show \
