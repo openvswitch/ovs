982956bc6f37c0413b39c1bc68eafcd64cfae5da
Pravin B Shelar <pshelar@nicira.com>
2013-08-26 23:43:29 -0700

    datapath: Cleanup compat support.
    cleanup various header file.
    
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>
    Acked-by: Jesse Gross <jesse@nicira.com>

--- datapath/linux/compat/include/linux/rculist.h
+++ datapath/linux/compat/include/linux/rculist.h
@@ -1,25 +1,7 @@
 #ifndef __LINUX_RCULIST_WRAPPER_H
 #define __LINUX_RCULIST_WRAPPER_H
 
-#include <linux/version.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,26)
 #include_next <linux/rculist.h>
-#else
-/* Prior to 2.6.26, the contents of rculist.h were part of list.h. */
-#include <linux/list.h>
-#include <linux/rcupdate.h>
-#endif
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,27)
-#define hlist_del_init_rcu rpl_hlist_del_init_rcu
-static inline void hlist_del_init_rcu(struct hlist_node *n)
-{
-	if (!hlist_unhashed(n)) {
-		__hlist_del(n);
-		n->pprev = NULL;
-	}
-}
-#endif
 
 #ifndef hlist_first_rcu
 #define hlist_first_rcu(head)   (*((struct hlist_node __rcu **)(&(head)->first)))
