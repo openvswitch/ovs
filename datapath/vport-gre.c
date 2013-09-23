ed5331132d9c95abac0bce91200121323e1e6b8a
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-09-23 13:38:13 -0700

    datapath: remove duplicated include from vport-gre.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- datapath/vport-gre.c
+++ datapath/vport-gre.c
@@ -26,8 +26,6 @@
 #include <linux/if_tunnel.h>
 #include <linux/if_vlan.h>
 #include <linux/in.h>
-#include <linux/if_vlan.h>
-#include <linux/in.h>
 #include <linux/in_route.h>
 #include <linux/inetdevice.h>
 #include <linux/jhash.h>
