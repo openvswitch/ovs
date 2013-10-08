add17b6945ab6d484becd19616d353e09a328b9a
Bruce Davie <bdavie@vmware.com>
2013-10-08 14:37:51 -0700

    vtep: Initial checkin of vtep schema.
    The hardware VTEP OVSDB schema specifies relations that a VTEP can use
    to integrate physical ports into logical switches maintained by a
    network virtualization controller such as NVP.
    
    Co-authored-by: Ben Pfaff <blp@nicira.com>
    Co-authored-by: Kenneth Duda <kduda@aristanetworks.com>
    Co-authored-by: Justin Pettit <jpettit@nicira.com>
    Signed-off-by: Bruce Davie <bdavie@vmware.com>
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    Signed-off-by: Kenneth Duda <kduda@aristanetworks.com>
    Signed-off-by: Justin Pettit <jpettit@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- lib/automake.mk
+++ lib/automake.mk
@@ -229,7 +229,9 @@ lib_libopenvswitch_a_SOURCES = \
 	lib/vlog.c \
 	lib/vlog.h \
 	lib/vswitch-idl.c \
-	lib/vswitch-idl.h
+	lib/vswitch-idl.h \
+	lib/vtep-idl.c \
+	lib/vtep-idl.h
 
 nodist_lib_libopenvswitch_a_SOURCES = \
 	lib/dirs.c
@@ -334,7 +336,10 @@ MAN_FRAGMENTS += \
 OVSIDL_BUILT += \
 	$(srcdir)/lib/vswitch-idl.c \
 	$(srcdir)/lib/vswitch-idl.h \
-	$(srcdir)/lib/vswitch-idl.ovsidl
+	$(srcdir)/lib/vswitch-idl.ovsidl \
+	$(srcdir)/lib/vtep-idl.c \
+	$(srcdir)/lib/vtep-idl.h \
+	$(srcdir)/lib/vtep-idl.ovsidl
 
 EXTRA_DIST += $(srcdir)/lib/vswitch-idl.ann
 VSWITCH_IDL_FILES = \
@@ -342,6 +347,14 @@ VSWITCH_IDL_FILES = \
 	$(srcdir)/lib/vswitch-idl.ann
 $(srcdir)/lib/vswitch-idl.ovsidl: $(VSWITCH_IDL_FILES)
 	$(OVSDB_IDLC) annotate $(VSWITCH_IDL_FILES) > $@.tmp
+	mv $@.tmp $@
+
+EXTRA_DIST += $(srcdir)/lib/vtep-idl.ann
+VTEP_IDL_FILES = \
+	$(srcdir)/vtep/vtep.ovsschema \
+	$(srcdir)/lib/vtep-idl.ann
+$(srcdir)/lib/vtep-idl.ovsidl: $(VTEP_IDL_FILES)
+	$(OVSDB_IDLC) annotate $(VTEP_IDL_FILES) > $@.tmp
 	mv $@.tmp $@
 
 lib/dirs.c: lib/dirs.c.in Makefile
---
+++ vtep/automake.mk
@@ -0,0 +1,46 @@
+# VTEP schema and IDL
+EXTRA_DIST += vtep/vtep.ovsschema
+pkgdata_DATA += vtep/vtep.ovsschema
+
+# VTEP E-R diagram
+#
+# If "python" or "dot" is not available, then we do not add graphical diagram
+# to the documentation.
+if HAVE_PYTHON
+if HAVE_DOT
+vtep/vtep.gv: ovsdb/ovsdb-dot.in vtep/vtep.ovsschema
+	$(OVSDB_DOT) --no-arrows $(srcdir)/vtep/vtep.ovsschema > $@
+vtep/vtep.pic: vtep/vtep.gv ovsdb/dot2pic
+	(dot -T plain < vtep/vtep.gv | $(srcdir)/ovsdb/dot2pic -f 3) > $@;
+VTEP_PIC = vtep/vtep.pic
+VTEP_DOT_DIAGRAM_ARG = --er-diagram=$(VTEP_PIC)
+DISTCLEANFILES += vtep/vtep.gv vtep/vtep.pic
+endif
+endif
+
+# VTEP schema documentation
+EXTRA_DIST += vtep/vtep.xml
+DISTCLEANFILES += vtep/vtep.5
+dist_man_MANS += vtep/vtep.5
+$(srcdir)/vtep/vtep.5: \
+	ovsdb/ovsdb-doc vtep/vtep.xml vtep/vtep.ovsschema $(VTEP_PIC)
+	$(OVSDB_DOC) \
+		--title="vtep" \
+		$(VTEP_DOT_DIAGRAM_ARG) \
+		$(srcdir)/vtep/vtep.ovsschema \
+		$(srcdir)/vtep/vtep.xml > $@.tmp
+	mv $@.tmp $@
+
+# Version checking for vtep.ovsschema.
+ALL_LOCAL += vtep/vtep.ovsschema.stamp
+vtep/vtep.ovsschema.stamp: vtep/vtep.ovsschema
+	@sum=`sed '/cksum/d' $? | cksum`; \
+	expected=`sed -n 's/.*"cksum": "\(.*\)".*/\1/p' $?`; \
+	if test "X$$sum" = "X$$expected"; then \
+	  touch $@; \
+	else \
+	  ln=`sed -n '/"cksum":/=' $?`; \
+	  echo >&2 "$?:$$ln: checksum \"$$sum\" does not match (you should probably update the version number and fix the checksum)"; \
+	  exit 1; \
+	fi
+CLEANFILES += vtep/vtep.ovsschema.stamp
