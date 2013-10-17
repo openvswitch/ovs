239ad7d18ffab19cbaab3bb4035c0e1bf570a065
Gurucharan Shetty <gshetty@nicira.com>
2013-10-17 04:25:19 -0700

    ofproto: Fix coding style.
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -451,7 +451,7 @@ ofproto_enumerate_names(const char *type, struct sset *names)
 {
     const struct ofproto_class *class = ofproto_class_find__(type);
     return class ? class->enumerate_names(type, names) : EAFNOSUPPORT;
- }
+}
 
 int
 ofproto_create(const char *datapath_name, const char *datapath_type,
