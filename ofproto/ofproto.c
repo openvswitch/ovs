c799c30657373c68b243bdb2576bf1883bcfce4a
Ben Pfaff <blp@nicira.com>
2013-12-09 15:38:25 -0800

    ofproto: Make ofproto_enumerate_types() match its comment.
    The comment says that it clears the passed-in sset, but it didn't.  The bug
    didn't actually affect any of the existing callers, which all passed in an
    empty sset.
    
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    Acked-by: Jarno Rajahalme <jrajahalme@nicira.com>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -432,6 +432,7 @@ ofproto_enumerate_types(struct sset *types)
 {
     size_t i;
 
+    sset_clear(types);
     for (i = 0; i < n_ofproto_classes; i++) {
         ofproto_classes[i]->enumerate_types(types);
     }
