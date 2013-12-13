ed1be7871bb20e9fb7f44f1f546386a833b43c47
Ben Pfaff <blp@nicira.com>
2013-12-13 09:46:44 -0800

    configure: Add macros to check for #include_next and add a workaround.
    This will be used for the #include_next in string.h in the following
    commit.
    
    Signed-off-by: Ben Pfaff <blp@nicira.com>

---
+++ m4/automake.mk
@@ -0,0 +1,10 @@
+# Copyright (C) 2013 Nicira, Inc.
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without warranty of any kind.
+
+EXTRA_DIST += \
+	m4/absolute-header.m4 \
+	m4/include_next.m4
