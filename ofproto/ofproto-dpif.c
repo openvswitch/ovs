3aeeb15612efbde53d3c180ef2990b351e02d7bb
Alex Wang <alexw@nicira.com>
2014-01-08 16:52:14 -0800

    ofproto/trace: Change output field name.
    This commit changes the output field name from "Relevant fields"
    to "Megaflow".
    
    Signed-off-by: Alex Wang <alexw@nicira.com>
    Signed-off-by: Ben Pfaff <blp@nicira.com>

--- ofproto/ofproto-dpif.c
+++ ofproto/ofproto-dpif.c
@@ -3867,7 +3867,7 @@ ofproto_trace(struct ofproto_dpif *ofproto, const struct flow *flow,
         trace_format_flow(ds, 0, "Final flow", &trace);
 
         match_init(&match, &trace.flow, &trace.xout.wc);
-        ds_put_cstr(ds, "Relevant fields: ");
+        ds_put_cstr(ds, "Megaflow: ");
         match_format(&match, ds, OFP_DEFAULT_PRIORITY);
         ds_put_char(ds, '\n');
 
