openvswitch/ovs

commit 738a2445c0c124f53df6abcffc6379746e99dee8

datapath: Fix kernel panic on ovs_flow_free
Both mega flow mask's reference counter and per flow table mask list
should only be accessed when holding ovs_mutex() lock.  However
this is not true with ovs_flow_table_flush(). The patch fixes this bug.

Reported-by: Joe Stringer <joestringer@nicira.com>
Signed-off-by: Andy Zhou <azhou@nicira.com>
Acked-by: Jesse Gross <jesse@nicira.com>

datapath/flow.c
@@ -502,27 +502,9 @@ static struct flow_table *__flow_tbl_alloc(int new_size)
 
 static void __flow_tbl_destroy(struct flow_table *table)
 {
-	int i;
-
-	if (table->keep_flows)
-		goto skip_flows;
-
-	for (i = 0; i < table->n_buckets; i++) {
-		struct sw_flow *flow;
-		struct hlist_head *head = flex_array_get(table->buckets, i);
-		struct hlist_node *n;
-		int ver = table->node_ver;
-
-		hlist_for_each_entry_safe(flow, n, head, hash_node[ver]) {
-			hlist_del(&flow->hash_node[ver]);
-			ovs_flow_free(flow, false);
-		}
-	}
-
 	BUG_ON(!list_empty(table->mask_list));
 	kfree(table->mask_list);
 
-skip_flows:
 	free_buckets(table->buckets);
 	kfree(table);
 }
@@ -554,9 +536,27 @@ static void flow_tbl_destroy_rcu_cb(struct rcu_head *rcu)
 
 void ovs_flow_tbl_destroy(struct flow_table *table, bool deferred)
 {
+	int i;
+
 	if (!table)
 		return;
 
+	if (table->keep_flows)
+		goto skip_flows;
+
+	for (i = 0; i < table->n_buckets; i++) {
+		struct sw_flow *flow;
+		struct hlist_head *head = flex_array_get(table->buckets, i);
+		struct hlist_node *n;
+		int ver = table->node_ver;
+
+		hlist_for_each_entry_safe(flow, n, head, hash_node[ver]) {
+			hlist_del_rcu(&flow->hash_node[ver]);
+			ovs_flow_free(flow, deferred);
+		}
+	}
+
+skip_flows:
 	if (deferred)
 		call_rcu(&table->rcu, flow_tbl_destroy_rcu_cb);
 	else

datapath/datapath.c
@@ -166,7 +166,6 @@ static void destroy_dp_rcu(struct rcu_head *rcu)
 {
 	struct datapath *dp = container_of(rcu, struct datapath, rcu);
 
-	ovs_flow_tbl_destroy((__force struct flow_table *)dp->table, false);
 	free_percpu(dp->stats_percpu);
 	release_net(ovs_dp_get_net(dp));
 	kfree(dp->ports);
@@ -1778,6 +1777,8 @@ static void __dp_destroy(struct datapath *dp)
  	 * all port in datapath are destroyed first before freeing datapath.
  	 */
 	ovs_dp_detach_port(ovs_vport_ovsl(dp, OVSP_LOCAL));
+
+	ovs_flow_tbl_destroy((__force struct flow_table *)dp->table, true);
 
 	call_rcu(&dp->rcu, destroy_dp_rcu);
 }

commit c97c48505a2a37a143f6bd8bac4f550022fa3d2a

datapath: Do not free in use mask list, fixes a kernel crash bug
Last commit 738a2445c0 introduced a bug where the mask list of
a flow table was freed even when the table's flows and mask list is
reused by another table.

Bug #32932

Reported-by: Len Gao <leng@vmware.com>
Signed-off-by: Andy Zhou <azhou@nicira.com>
Acked-by: Jesse Gross <jesse@nicira.com>

datapath/flow.c
@@ -502,8 +502,10 @@ static struct flow_table *__flow_tbl_alloc(int new_size)
 
 static void __flow_tbl_destroy(struct flow_table *table)
 {
-	BUG_ON(!list_empty(table->mask_list));
-	kfree(table->mask_list);
+	if (!table->keep_flows) {
+		BUG_ON(!list_empty(table->mask_list));
+		kfree(table->mask_list);
+	}
 
 	free_buckets(table->buckets);
 	kfree(table);