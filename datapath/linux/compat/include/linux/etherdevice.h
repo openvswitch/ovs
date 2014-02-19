982a47eceac1be710f354b2083aff488e224cbb8
Joe Perches <joe@perches.com>
2014-02-19 11:08:50 -0800

    datapath: Use ether_addr_copy
    It's slightly smaller/faster for some architectures.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>

--- datapath/linux/compat/include/linux/etherdevice.h
+++ datapath/linux/compat/include/linux/etherdevice.h
@@ -34,4 +34,19 @@ static inline int eth_mac_addr(struct net_device *dev, void *p)
 }
 #endif
 
+static inline void ether_addr_copy(u8 *dst, const u8 *src)
+{
+#if defined(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS)
+	*(u32 *)dst = *(const u32 *)src;
+	*(u16 *)(dst + 4) = *(const u16 *)(src + 4);
+#else
+	u16 *a = (u16 *)dst;
+	const u16 *b = (const u16 *)src;
+
+	a[0] = b[0];
+	a[1] = b[1];
+	a[2] = b[2];
+#endif
+}
+
 #endif
