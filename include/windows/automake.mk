5e3411b1a15d2dd927cb59cd81da78cf3198dde5
Gurucharan Shetty <gshetty@nicira.com>
2014-03-10 10:06:52 -0700

    windows: Add stub headers for windows.
    Windows does not have a bunch of headers that
    are available in Linux. Instead of littering the code
    with #ifndef _WIN32, add stub headers.
    
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- include/windows/automake.mk
+++ include/windows/automake.mk
@@ -6,7 +6,25 @@
 # without warranty of any kind.
 
 noinst_HEADERS += \
+	include/windows/arpa/inet.h \
 	include/windows/getopt.h \
+	include/windows/net/if.h \
+	include/windows/netdb.h \
+	include/windows/netinet/icmp6.h \
+	include/windows/netinet/in.h \
+	include/windows/netinet/in_systm.h \
+	include/windows/netinet/ip.h \
+	include/windows/netinet/ip6.h \
+	include/windows/netinet/tcp.h \
+	include/windows/poll.h \
+	include/windows/strings.h \
 	include/windows/syslog.h \
+	include/windows/sys/ioctl.h \
 	include/windows/sys/resource.h \
+	include/windows/sys/socket.h \
+	include/windows/sys/time.h \
+	include/windows/sys/uio.h \
+	include/windows/sys/un.h \
+	include/windows/sys/wait.h \
+	include/windows/unistd.h \
 	include/windows/windefs.h
