f6cea7c9e73de0a3c9643d300966e5cbe91d1635
Gurucharan Shetty <gshetty@nicira.com>
2014-03-12 13:57:42 -0700

    windows/unistd: Add definitions for STD*_FILENO.
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>

--- include/windows/unistd.h
+++ include/windows/unistd.h
@@ -18,4 +18,9 @@
 
 #define fsync _commit
 
+/* Standard file descriptors.  */
+#define STDIN_FILENO    0   /* Standard input.  */
+#define STDOUT_FILENO   1   /* Standard output.  */
+#define STDERR_FILENO   2   /* Standard error output.  */
+
 #endif /* unistd.h  */
