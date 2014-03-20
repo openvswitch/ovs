openvswitch/ovs

commit fce314cdb330167ca10c00b09fc46b67c5b9af97

socket-util: Fix definition of LINUX.
Reported-by: Mukesh Hira <mhira@vmware.com>
Signed-off-by: Ben Pfaff <blp@nicira.com>
Acked-by: Pritesh Kothari <pritesh.kothari@cisco.com>

lib/socket-util.c
@@ -53,9 +53,9 @@ VLOG_DEFINE_THIS_MODULE(socket_util);
  * Thus, this file compiles all of the code regardless of the target, by
  * writing "if (LINUX)" instead of "#ifdef __linux__". */
 #ifdef __linux__
-#define LINUX 0
-#else
 #define LINUX 1
+#else
+#define LINUX 0
 #endif
 
 #ifndef O_DIRECTORY

commit 6723e8af39e40001f5c141b5ad9a8dfe65459c40

socket-util: Fix an inverted use of LINUX
Fix a regression introduced by commit fce314cd.
("socket-util: Fix definition of LINUX.")

Signed-off-by: YAMAMOTO Takashi <yamamoto@valinux.co.jp>
Signed-off-by: Ben Pfaff <blp@nicira.com>

lib/socket-util.c
@@ -360,7 +360,7 @@ shorten_name_via_proc(const char *name, char short_name[MAX_UN_LEN + 1],
     int dirfd;
     int len;
 
-    if (LINUX) {
+    if (!LINUX) {
         return ENAMETOOLONG;
     }
 