b8da6cce47501f003c9791054a59a5a6632e4c2c
Andy Zhou <azhou@nicira.com>
2014-03-24 21:10:39 -0700

    dpif-netdev: Fix a compilation warning
    Building OVS tree without DPDK produced the following warning message:
        lib/dpif-netdev.c:1868:5: error: statement with no effect
    
    This error message is complaining the return value of the following
    macro not being used.
    	#define pmd_thread_setaffinity_cpu(c) (0)
    
    The patch fixed this warnning by making the stub functions
    as inline funtions.
    
    Signed-off-by: Andy Zhou <azhou@nicira.com>
    Acked-by: Pravin B Shelar <pshelar@nicira.com>

--- lib/netdev-dpdk.h
+++ lib/netdev-dpdk.h
@@ -1,8 +1,10 @@
 #ifndef NETDEV_DPDK_H
 #define NETDEV_DPDK_H
 
-#ifdef DPDK_NETDEV
 #include <config.h>
+#include "ofpbuf.h"
+
+#ifdef DPDK_NETDEV
 
 #include <rte_config.h>
 #include <rte_eal.h>
@@ -16,8 +18,6 @@
 #include <rte_launch.h>
 #include <rte_malloc.h>
 
-#include "ofpbuf.h"
-
 int dpdk_init(int argc, char **argv);
 void netdev_dpdk_register(void);
 void free_dpdk_buf(struct ofpbuf *);
@@ -25,10 +25,29 @@ int pmd_thread_setaffinity_cpu(int cpu);
 
 #else
 
-#define dpdk_init(arg1, arg2) (0)
-#define netdev_dpdk_register()
-#define free_dpdk_buf(arg)
-#define pmd_thread_setaffinity_cpu(c) (0)
+static inline int
+dpdk_init(int arg1 OVS_UNUSED, char **arg2 OVS_UNUSED)
+{
+    return 0;
+}
+
+static inline void
+netdev_dpdk_register(void)
+{
+    /* Nothing */
+}
+
+static inline void
+free_dpdk_buf(struct ofpbuf *buf OVS_UNUSED)
+{
+    /* Nothing */
+}
+
+static inline int
+pmd_thread_setaffinity_cpu(int cpu OVS_UNUSED)
+{
+    return 0;
+}
 
 #endif /* DPDK_NETDEV */
 #endif
