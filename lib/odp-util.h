b96986ed11001494542a043e5dcc3469463a1f71
Andy Zhou <azhou@nicira.com>
2014-06-12 22:39:51 -0700

    odp-util: update comments on ODPUTIL_FLOW_KEY_BYTES estimation
    Signed-off-by: Andy Zhou <azhou@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- lib/odp-util.h
+++ lib/odp-util.h
@@ -108,6 +108,8 @@ void odp_portno_names_destroy(struct hmap *portno_names);
  *  - OVS_TUNNEL_KEY_ATTR_CSUM           0    --     4      4
  *  OVS_KEY_ATTR_IN_PORT                 4    --     4      8
  *  OVS_KEY_ATTR_SKB_MARK                4    --     4      8
+ *  OVS_KEY_ATTR_DP_HASH                 4    --     4      8
+ *  OVS_KEY_ATTR_RECIRC_ID               4    --     4      8
  *  OVS_KEY_ATTR_ETHERNET               12    --     4     16
  *  OVS_KEY_ATTR_ETHERTYPE               2     2     4      8  (outer VLAN ethertype)
  *  OVS_KEY_ATTR_8021Q                   4    --     4      8
@@ -117,7 +119,7 @@ void odp_portno_names_destroy(struct hmap *portno_names);
  *  OVS_KEY_ATTR_ICMPV6                  2     2     4      8
  *  OVS_KEY_ATTR_ND                     28    --     4     32
  *  ----------------------------------------------------------
- *  total                                                 208
+ *  total                                                 224
  *
  * We include some slack space in case the calculation isn't quite right or we
  * add another field and forget to adjust this value.
