271e6bc7d27bf19c666a37beeaeeec8017850c44
Jesse Gross <jesse@nicira.com>
2014-06-20 17:23:33 -0700

    doc: Additional documentation updates for Geneve.
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- include/openflow/nicira-ext.h
+++ include/openflow/nicira-ext.h
@@ -1608,9 +1608,9 @@ OFP_ASSERT(sizeof(struct nx_action_output_reg) == 24);
 
 /* Tunnel ID.
  *
- * For a packet received via a GRE, VXLAN or LISP tunnel including a (32-bit)
- * key, the key is stored in the low 32-bits and the high bits are zeroed.  For
- * other packets, the value is 0.
+ * For a packet received via a Geneve, GRE, VXLAN or LISP tunnel including a
+ * key less than 64 bits, the key is stored in the low bits and the high bits
+ * are zeroed.  For other packets, the value is 0.
  *
  * All zero bits, for packets not received via a keyed tunnel.
  *
