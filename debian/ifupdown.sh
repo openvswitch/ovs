5b245e73d61eddc985180888f6044a1029f148a5
Gurucharan Shetty <gshetty@nicira.com>
2014-07-11 14:32:48 -0700

    debian: Option to create patch ports through 'interfaces'
    This is a port of commit d7aab661 ( rhel: Add Patch Port support to
    initscripts.) from rhel to debian's ifupdown script.
    
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- debian/ifupdown.sh
+++ debian/ifupdown.sh
@@ -66,6 +66,12 @@ if [ "${MODE}" = "start" ]; then
                     ifconfig "${slave}" up
                 done
                 ;;
+        OVSPatchPort)
+                ovs_vsctl -- --may-exist add-port "${IF_OVS_BRIDGE}"\
+                    "${IFACE}" ${IF_OVS_OPTIONS} -- set Interface "${IFACE}" \
+                    type=patch options:peer="${IF_OVS_PATCH_PEER}" \
+                    ${OVS_EXTRA+-- $OVS_EXTRA}
+                ;;
         OVSTunnel)
                 ovs_vsctl -- --may-exist add-port "${IF_OVS_BRIDGE}"\
                     "${IFACE}" ${IF_OVS_OPTIONS} -- set Interface "${IFACE}" \
@@ -85,7 +91,7 @@ elif [ "${MODE}" = "stop" ]; then
 
                 ovs_vsctl -- --if-exists del-br "${IFACE}"
                 ;;
-        OVSPort|OVSIntPort|OVSBond|OVSTunnel)
+        OVSPort|OVSIntPort|OVSBond|OVSPatchPort|OVSTunnel)
                 ovs_vsctl -- --if-exists del-port "${IF_OVS_BRIDGE}" "${IFACE}"
                 ;;
         *)
