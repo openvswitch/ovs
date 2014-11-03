390a6ec40c9d38e82870359bf44e0427da6b0025
Pravin B Shelar <pshelar@nicira.com>
2014-11-03 08:55:12 -0800

    datapath: Fix compat checks for ipv6_skip_exthdr()
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>

--- datapath/linux/compat/include/net/ipv6.h
+++ datapath/linux/compat/include/net/ipv6.h
@@ -9,6 +9,12 @@
 #define NEXTHDR_SCTP    132 /* Stream Control Transport Protocol */
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
+#define ipv6_skip_exthdr rpl_ipv6_skip_exthdr
+extern int ipv6_skip_exthdr(const struct sk_buff *skb, int start,
+			    u8 *nexthdrp, __be16 *frag_offp);
+#endif
+
 #ifndef HAVE_IP6_FH_F_SKIP_RH
 
 enum {
@@ -16,12 +22,6 @@ enum {
 	IP6_FH_F_AUTH           = (1 << 1),
 	IP6_FH_F_SKIP_RH        = (1 << 2),
 };
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0)
-#define ipv6_skip_exthdr rpl_ipv6_skip_exthdr
-extern int ipv6_skip_exthdr(const struct sk_buff *skb, int start,
-			    u8 *nexthdrp, __be16 *frag_offp);
-#endif
 
 /* This function is upstream, but not the version which skips routing
  * headers with 0 segments_left. We fixed it when we introduced
