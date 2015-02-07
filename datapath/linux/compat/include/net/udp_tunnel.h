a42ec67e1bc2a1081a1b40c505044b555368edd7
Jesse Gross <jesse@nicira.com>
2015-02-06 19:25:09 -0800

    datapath: Account for "udp-tunnel: Add a few more UDP tunnel APIs"
    Upstream commit:
        udp-tunnel: Add a few more UDP tunnel APIs
    
        Added a few more UDP tunnel APIs that can be shared by UDP based
        tunnel protocol implementation. The main ones are highlighted below.
    
        setup_udp_tunnel_sock() configures UDP listener socket for
        receiving UDP encapsulated packets.
    
        udp_tunnel_xmit_skb() and upd_tunnel6_xmit_skb() transmit skb
        using UDP encapsulation.
    
        udp_tunnel_sock_release() closes the UDP tunnel listener socket.
    
        Signed-off-by: Andy Zhou <azhou@nicira.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Upstream: 6a93cc90 ("udp-tunnel: Add a few more UDP tunnel APIs")
    Signed-off-by: Jesse Gross <jesse@nicira.com>
    Acked-by: Thomas Graf <tgraf@noironetworks.com>
    Acked-by: Pravin B Shelar <pshelar@nicira.com>

--- datapath/linux/compat/include/net/udp_tunnel.h
+++ datapath/linux/compat/include/net/udp_tunnel.h
@@ -4,9 +4,25 @@
 #include <linux/version.h>
 #include <linux/kconfig.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,17,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,18,0)
 #include_next <net/udp_tunnel.h>
+
+static inline struct sk_buff *
+rpl_udp_tunnel_handle_offloads(struct sk_buff *skb, bool udp_csum,
+			       bool is_vxlan)
+{
+	if (skb_is_gso(skb) && skb_is_encapsulated(skb)) {
+		kfree_skb(skb);
+		return ERR_PTR(-ENOSYS);
+	}
+	return udp_tunnel_handle_offloads(skb, udp_csum);
+}
+#define udp_tunnel_handle_offloads rpl_udp_tunnel_handle_offloads
+
 #else
+
+#include <net/ip_tunnels.h>
+#include <net/udp.h>
 
 struct udp_port_cfg {
 	u8			family;
@@ -36,5 +52,53 @@ struct udp_port_cfg {
 int udp_sock_create(struct net *net, struct udp_port_cfg *cfg,
 		    struct socket **sockp);
 
-#endif /* Linux version < 3.17 */
+typedef int (*udp_tunnel_encap_rcv_t)(struct sock *sk, struct sk_buff *skb);
+typedef void (*udp_tunnel_encap_destroy_t)(struct sock *sk);
+
+struct udp_tunnel_sock_cfg {
+	void *sk_user_data;     /* user data used by encap_rcv call back */
+	/* Used for setting up udp_sock fields, see udp.h for details */
+	__u8  encap_type;
+	udp_tunnel_encap_rcv_t encap_rcv;
+	udp_tunnel_encap_destroy_t encap_destroy;
+};
+
+/* Setup the given (UDP) sock to receive UDP encapsulated packets */
+void setup_udp_tunnel_sock(struct net *net, struct socket *sock,
+			   struct udp_tunnel_sock_cfg *sock_cfg);
+
+/* Transmit the skb using UDP encapsulation. */
+int udp_tunnel_xmit_skb(struct socket *sock, struct rtable *rt,
+			struct sk_buff *skb, __be32 src, __be32 dst,
+			__u8 tos, __u8 ttl, __be16 df, __be16 src_port,
+			__be16 dst_port, bool xnet);
+
+void udp_tunnel_sock_release(struct socket *sock);
+
+void ovs_udp_gso(struct sk_buff *skb);
+void ovs_udp_csum_gso(struct sk_buff *skb);
+
+static inline struct sk_buff *udp_tunnel_handle_offloads(struct sk_buff *skb,
+                                                         bool udp_csum,
+							 bool is_vxlan)
+{
+	int type = udp_csum ? SKB_GSO_UDP_TUNNEL_CSUM : SKB_GSO_UDP_TUNNEL;
+	void (*fix_segment)(struct sk_buff *);
+
+	if (!udp_csum)
+		fix_segment = ovs_udp_gso;
+	else
+		fix_segment = ovs_udp_csum_gso;
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,18,0)
+	if (!is_vxlan)
+		type = 0;
+#endif
+
+	return ovs_iptunnel_handle_offloads(skb, udp_csum, type, fix_segment);
+}
+
+#define udp_tunnel_encap_enable(sock) udp_encap_enable()
+
+#endif /* Linux version < 3.18 */
 #endif
--- datapath/linux/compat/udp_tunnel.c
+++ datapath/linux/compat/udp_tunnel.c
@@ -1,6 +1,6 @@
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,18,0)
 
 #include <linux/module.h>
 #include <linux/errno.h>
@@ -8,6 +8,7 @@
 #include <linux/udp.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
+#include <net/ip_tunnels.h>
 #include <net/udp.h>
 #include <net/udp_tunnel.h>
 #include <net/net_namespace.h>
@@ -15,7 +16,7 @@
 int udp_sock_create(struct net *net, struct udp_port_cfg *cfg,
 		    struct socket **sockp)
 {
-	int err = -EINVAL;
+	int err;
 	struct socket *sock = NULL;
 
 #if IS_ENABLED(CONFIG_IPV6)
@@ -95,4 +96,74 @@ error:
 	return err;
 }
 
-#endif /* Linux version < 3.17 */
+void setup_udp_tunnel_sock(struct net *net, struct socket *sock,
+			   struct udp_tunnel_sock_cfg *cfg)
+{
+	struct sock *sk = sock->sk;
+
+	/* Disable multicast loopback */
+	inet_sk(sk)->mc_loop = 0;
+
+	rcu_assign_sk_user_data(sk, cfg->sk_user_data);
+
+	udp_sk(sk)->encap_type = cfg->encap_type;
+	udp_sk(sk)->encap_rcv = cfg->encap_rcv;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,9,0)
+	udp_sk(sk)->encap_destroy = cfg->encap_destroy;
+#endif
+
+	udp_tunnel_encap_enable(sock);
+}
+
+void ovs_udp_gso(struct sk_buff *skb)
+{
+	int udp_offset = skb_transport_offset(skb);
+	struct udphdr *uh;
+
+	uh = udp_hdr(skb);
+	uh->len = htons(skb->len - udp_offset);
+}
+
+void ovs_udp_csum_gso(struct sk_buff *skb)
+{
+	struct iphdr *iph = ip_hdr(skb);
+	int udp_offset = skb_transport_offset(skb);
+
+	ovs_udp_gso(skb);
+
+	/* csum segment if tunnel sets skb with csum. The cleanest way
+	 * to do this just to set it up from scratch. */
+	skb->ip_summed = CHECKSUM_NONE;
+	udp_set_csum(true, skb, iph->saddr, iph->daddr,
+		     skb->len - udp_offset);
+}
+
+int udp_tunnel_xmit_skb(struct socket *sock, struct rtable *rt,
+			struct sk_buff *skb, __be32 src, __be32 dst,
+			__u8 tos, __u8 ttl, __be16 df, __be16 src_port,
+			__be16 dst_port, bool xnet)
+{
+	struct udphdr *uh;
+
+	__skb_push(skb, sizeof(*uh));
+	skb_reset_transport_header(skb);
+	uh = udp_hdr(skb);
+
+	uh->dest = dst_port;
+	uh->source = src_port;
+	uh->len = htons(skb->len);
+
+	udp_set_csum(true, skb, src, dst, skb->len);
+
+	return iptunnel_xmit(sock->sk, rt, skb, src, dst, IPPROTO_UDP,
+			     tos, ttl, df, xnet);
+}
+
+void udp_tunnel_sock_release(struct socket *sock)
+{
+	rcu_assign_sk_user_data(sock->sk, NULL);
+	kernel_sock_shutdown(sock, SHUT_RDWR);
+	sk_release_kernel(sock->sk);
+}
+
+#endif /* Linux version < 3.18 */
