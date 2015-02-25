e2d9f480240c6fab93333f81b9eb0722e618313c
Ben Pfaff <blp@nicira.com>
2015-02-25 08:46:02 -0800

    json: Fix error message for corner case in json_string_unescape().
    The error message should not include bytes already copied from the input
    string.
    
    Found by inspection.
    
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    Acked-by: Alex Wang <alexw@nicira.com>

--- lib/json.c
+++ lib/json.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2010, 2011, 2012, 2014 Nicira, Inc.
+ * Copyright (c) 2009, 2010, 2011, 2012, 2014, 2015 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -830,6 +830,7 @@ json_string_unescape(const char *in, size_t in_len, char **outp)
              * lexer will never pass in a string that ends in a single
              * backslash, but json_string_unescape() has other callers that
              * are not as careful.*/
+            ds_clear(&out);
             ds_put_cstr(&out, "quoted string may not end with backslash");
             goto exit;
         }
