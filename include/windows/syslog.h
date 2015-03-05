8f07d902f124d0cf0cebcf3c2aea09183d2699e0
Gurucharan Shetty <shettyg@nicira.com>
2015-03-05 06:58:52 -0800

    windows/syslog: Remove duplicate definition.
    Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>
    Acked-by: Ben Pfaff <blp@nicira.com>

--- include/windows/syslog.h
+++ include/windows/syslog.h
@@ -26,7 +26,6 @@
 #define LOG_INFO        6       /* informational */
 #define LOG_DEBUG       7       /* debug-level messages */
 #define LOG_NDELAY      8       /* don't delay open */
-#define LOG_DAEMON      24      /* system daemons */
 
 #define LOG_KERN      (0<<3)  /* kernel messages */
 #define LOG_USER      (1<<3)  /* user-level messages */
