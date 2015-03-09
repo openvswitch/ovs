openvswitch/ovs

commit 9d82156981bfcbf539ff80938c7d3b90bc3a8ab2

vtep: Move vtep IDL files into new "libvtep.la".
There's no reason to build it in "lib" and include it in
"libopenvswitch.la".  This commit moves it to "vtep" and includes it in
a new "libvtep.la".

Signed-off-by: Justin Pettit <jpettit@nicira.com>

vtep/automake.mk
@@ -1,3 +1,27 @@
+# vtep IDL
+OVSIDL_BUILT += \
+	vtep/vtep-idl.c \
+	vtep/vtep-idl.h \
+	vtep/vtep.ovsidl
+EXTRA_DIST += vtep/vtep-idl.ann
+VTEP_IDL_FILES = \
+	$(srcdir)/vtep/vtep.ovsschema \
+	$(srcdir)/vtep/vtep-idl.ann
+vtep/vtep-idl.ovsidl: $(VTEP_IDL_FILES)
+	$(AM_V_GEN)$(OVSDB_IDLC) annotate $(VTEP_IDL_FILES) > $@.tmp && \
+	mv $@.tmp $@
+CLEANFILES += vtep/vtep-idl.c vtep/vtep-idl.h
+
+# libvtep
+lib_LTLIBRARIES += vtep/libvtep.la
+vtep_libvtep_la_LDFLAGS = \
+	-version-info $(LT_CURRENT):$(LT_REVISION):$(LT_AGE) \
+	-Wl,--version-script=$(top_builddir)/vtep/libvtep.sym \
+	$(AM_LDFLAGS)
+vtep_libvtep_la_SOURCES = \
+	vtep/vtep-idl.c \
+	vtep/vtep-idl.h
+
 bin_PROGRAMS += \
    vtep/vtep-ctl
 
@@ -11,7 +35,7 @@ man_MANS += \
    vtep/vtep-ctl.8
 
 vtep_vtep_ctl_SOURCES = vtep/vtep-ctl.c
-vtep_vtep_ctl_LDADD = lib/libopenvswitch.la
+vtep_vtep_ctl_LDADD = vtep/libvtep.la lib/libopenvswitch.la
 
 # ovs-vtep
 scripts_SCRIPTS += \
@@ -46,7 +70,7 @@ EXTRA_DIST += vtep/vtep.xml
 DISTCLEANFILES += vtep/vtep.5
 man_MANS += vtep/vtep.5
 vtep/vtep.5: \
-	ovsdb/ovsdb-doc vtep/vtep.xml vtep/vtep.ovsschema $(VTEP_PIC)
+	ovsdb/ovsdb-doc vtep/vtep.xml $(srcdir)/vtep/vtep.ovsschema $(VTEP_PIC)
 	$(AM_V_GEN)$(OVSDB_DOC) \
 		$(VTEP_DOT_DIAGRAM_ARG) \
 		--version=$(VERSION) \

lib/automake.mk
@@ -268,8 +268,6 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/vlog.c \
 	lib/vswitch-idl.c \
 	lib/vswitch-idl.h \
-	lib/vtep-idl.c \
-	lib/vtep-idl.h \
 	lib/lldp/aa-structs.h \
 	lib/lldp/lldp.c \
 	lib/lldp/lldp-const.h \
@@ -436,10 +434,7 @@ MAN_FRAGMENTS += \
 OVSIDL_BUILT += \
 	$(srcdir)/lib/vswitch-idl.c \
 	$(srcdir)/lib/vswitch-idl.h \
-	$(srcdir)/lib/vswitch-idl.ovsidl \
-	$(srcdir)/lib/vtep-idl.c \
-	$(srcdir)/lib/vtep-idl.h \
-	$(srcdir)/lib/vtep-idl.ovsidl
+	$(srcdir)/lib/vswitch-idl.ovsidl
 
 EXTRA_DIST += $(srcdir)/lib/vswitch-idl.ann
 VSWITCH_IDL_FILES = \
@@ -447,14 +442,6 @@ VSWITCH_IDL_FILES = \
 	$(srcdir)/lib/vswitch-idl.ann
 $(srcdir)/lib/vswitch-idl.ovsidl: $(VSWITCH_IDL_FILES)
 	$(AM_V_GEN)$(OVSDB_IDLC) annotate $(VSWITCH_IDL_FILES) > $@.tmp && \
-	mv $@.tmp $@
-
-EXTRA_DIST += $(srcdir)/lib/vtep-idl.ann
-VTEP_IDL_FILES = \
-	$(srcdir)/vtep/vtep.ovsschema \
-	$(srcdir)/lib/vtep-idl.ann
-$(srcdir)/lib/vtep-idl.ovsidl: $(VTEP_IDL_FILES)
-	$(AM_V_GEN)$(OVSDB_IDLC) annotate $(VTEP_IDL_FILES) > $@.tmp && \
 	mv $@.tmp $@
 
 lib/dirs.c: lib/dirs.c.in Makefile

lib/.gitignore
@@ -14,8 +14,5 @@
 /vswitch-idl.c
 /vswitch-idl.h
 /vswitch-idl.ovsidl
-/vtep-idl.c
-/vtep-idl.h
-/vtep-idl.ovsidl
 /libopenvswitch.pc
 /libsflow.pc

vtep/.gitignore
@@ -2,6 +2,9 @@
 /Makefile.in
 /vtep-ctl
 /vtep-ctl.8
+/vtep-idl.c
+/vtep-idl.h
+/vtep-idl.ovsidl
 /vtep.5
 /vtep.gv
 /vtep.ovsschema.stamp

vtep/vtep-ctl.c
@@ -43,7 +43,7 @@
 #include "smap.h"
 #include "sset.h"
 #include "svec.h"
-#include "lib/vtep-idl.h"
+#include "vtep/vtep-idl.h"
 #include "table.h"
 #include "timeval.h"
 #include "util.h"

vtep/vtep-idl.ann
@@ -6,4 +6,4 @@
 # it can generate more programmer-friendly data structures.
 
 s["idlPrefix"] = "vteprec_"
-s["idlHeader"] = "\"lib/vtep-idl.h\""
+s["idlHeader"] = "\"vtep/vtep-idl.h\""

commit 973edd6eafb05d7a27c99b6efb7e410e96d8cb96

vtep: fix '--enable-shared' build and 'make distcheck'.
Found by travis.  Broken by commit 9d8215698 ('vtep: Move vtep IDL files
into new "libvtep.la".')

Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>
Signed-off-by: Ben Pfaff <blp@nicira.com>

configure.ac
@@ -135,7 +135,8 @@ AC_CONFIG_FILES([
     ovsdb/libovsdb.sym
     ofproto/libofproto.sym
     lib/libsflow.sym
-    lib/libopenvswitch.sym])
+    lib/libopenvswitch.sym
+    vtep/libvtep.sym])
 
 OVS_ENABLE_OPTION([-Wall])
 OVS_ENABLE_OPTION([-Wextra])

vtep/automake.mk
@@ -2,7 +2,7 @@
 OVSIDL_BUILT += \
 	vtep/vtep-idl.c \
 	vtep/vtep-idl.h \
-	vtep/vtep.ovsidl
+	vtep/vtep-idl.ovsidl
 EXTRA_DIST += vtep/vtep-idl.ann
 VTEP_IDL_FILES = \
 	$(srcdir)/vtep/vtep.ovsschema \

vtep/libvtep.sym.in
@@ -1,0 +1,4 @@
+libvtep_@LT_CURRENT@ {
+global:
+        *;
+};