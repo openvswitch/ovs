e0840f11b05204c318a894df983e7ab20a3597da
Ben Pfaff <blp@nicira.com>
2015-04-15 16:49:50 -0700

    expr: New module for Boolean expressions on fields, for use in OVN.
    Known weaknesses:
    
        * String fields can't be converted to flows yet.  A subsequent commit
          will fix this.
    
        * Flows aren't optimal in some situations likely to be common.
          I don't think either of those is a reason not to commit this;
          this is a solid base to build on.
    
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    Acked-by: Russell Bryant <rbryant@redhat.com>

--- ovn/lib/automake.mk
+++ ovn/lib/automake.mk
@@ -1,2 +1,6 @@
 lib_LTLIBRARIES += ovn/lib/libovn.la
-ovn_lib_libovn_la_SOURCES = ovn/lib/lex.c ovn/lib/lex.h
+ovn_lib_libovn_la_SOURCES = \
+	ovn/lib/expr.c \
+	ovn/lib/expr.h \
+	ovn/lib/lex.c \
+	ovn/lib/lex.h
