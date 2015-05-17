f046804891635d497e5b91a3b90340951132c1d9
Ethan Jackson <ethan@nicira.com>
2015-05-17 06:06:01 -0700

    sparse: Fix sparse when compiling DPDK.
    Sparse doesn't like several of the DPDK header files.  This patch
    works around it so we can get analysis when compiling DPDK.
    
    Signed-off-by: Ethan Jackson <ethan@nicira.com>
    Acked-by: Daniele Di Proietto <diproiettod@vmware.com>

--- include/sparse/automake.mk
+++ include/sparse/automake.mk
@@ -1,10 +1,15 @@
 noinst_HEADERS += \
         include/sparse/arpa/inet.h \
         include/sparse/assert.h \
+        include/sparse/bmi2intrin.h \
+        include/sparse/emmintrin.h \
         include/sparse/math.h \
         include/sparse/netinet/in.h \
         include/sparse/netinet/ip6.h \
         include/sparse/netpacket/packet.h \
         include/sparse/pthread.h \
+        include/sparse/rte_atomic.h \
+        include/sparse/rte_lcore.h \
+        include/sparse/rte_vect.h \
         include/sparse/sys/socket.h \
         include/sparse/sys/wait.h
