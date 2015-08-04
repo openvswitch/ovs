5b5c922b0ca64abb4ac63f4467e02fcd2221a51a
Alex Wang <alexw@nicira.com>
2015-08-04 11:31:22 -0700

    ovn-nbctl: Move ovn-nbctl to utilities directory.
    Signed-off-by: Alex Wang <alexw@nicira.com>
    Acked-by: Russell Bryant <rbryant@redhat.com>

--- ovn/utilities/automake.mk
+++ ovn/utilities/automake.mk
@@ -2,11 +2,19 @@ scripts_SCRIPTS += \
     ovn/utilities/ovn-ctl
 
 man_MANS += \
-    ovn/utilities/ovn-ctl.8
+    ovn/utilities/ovn-ctl.8 \
+    ovn/utilities/ovn-nbctl.8
 
 EXTRA_DIST += \
     ovn/utilities/ovn-ctl \
-    ovn/utilities/ovn-ctl.8.xml
+    ovn/utilities/ovn-ctl.8.xml \
+    ovn/utilities/ovn-nbctl.8.xml
 
 DISTCLEANFILES += \
-    ovn/utilities/ovn-ctl.8
+    ovn/utilities/ovn-ctl.8 \
+    ovn/utilities/ovn-nbctl.8
+
+# ovn-nbctl
+bin_PROGRAMS += ovn/utilities/ovn-nbctl
+ovn_utilities_ovn_nbctl_SOURCES = ovn/utilities/ovn-nbctl.c
+ovn_utilities_ovn_nbctl_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenvswitch.la
