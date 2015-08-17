7148fd9c1c3d4e0300cd9b90d00c15b263ba144b
Chris J Arges <chris.j.arges@canonical.com>
2015-08-17 07:40:43 -0700

    datapath: allocate nr_node_ids flow_stats instead of num_possible_nodes
    Some architectures like POWER can have a NUMA node_possible_map that
    contains sparse entries. This causes memory corruption with openvswitch
    since it allocates flow_cache with a multiple of num_possible_nodes() and
    assumes the node variable returned by for_each_node will index into
    flow->stats[node].
    
    Use nr_node_ids to allocate a maximal sparse array instead of
    num_possible_nodes().
    
    The crash was noticed after 3af229f2 was applied as it changed the
    node_possible_map to match node_online_map on boot.
    Fixes: 3af229f2071f5b5cb31664be6109561fbe19c861
    
    Signed-off-by: Chris J Arges <chris.j.arges@canonical.com>
    Acked-by: Pravin B Shelar <pshelar@nicira.com>
    Acked-by: Nishanth Aravamudan <nacc@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Upstream: bac541e4631 ("openvswitch: allocate nr_node_ids flow_stats
    instead of num_possible_nodes")
    
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>

--- datapath/flow_table.c
+++ datapath/flow_table.c
@@ -988,7 +988,7 @@ int ovs_flow_init(void)
 	BUILD_BUG_ON(sizeof(struct sw_flow_key) % sizeof(long));
 
 	flow_cache = kmem_cache_create("sw_flow", sizeof(struct sw_flow)
-				       + (nr_node_ids
+				       + (num_possible_nodes()
 					  * sizeof(struct flow_stats *)),
 				       0, 0, NULL);
 	if (flow_cache == NULL)
