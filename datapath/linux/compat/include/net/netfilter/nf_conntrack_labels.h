openvswitch/ovs

commit b8cce81fa9a1f558c72f47f8080da3b81f606ea8

compat: Backport nf_connlabels_{get, put}().
This is a partial backport of Linux commit 86ca02e77408
"netfilter: connlabels: Export setting connlabel length".

Signed-off-by: Joe Stringer <joestringer@nicira.com>
Acked-by: Pravin B Shelar <pshelar@nicira.com>

datapath/linux/compat/include/net/netfilter/nf_conntrack_labels.h
@@ -1,0 +1,41 @@
+#ifndef _NF_CONNTRACK_LABELS_WRAPPER_H
+#define _NF_CONNTRACK_LABELS_WRAPPER_H
+
+#include <linux/kconfig.h>
+#include <linux/version.h>
+#include_next <net/netfilter/nf_conntrack_labels.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0) && \
+    IS_ENABLED(CONFIG_NF_CONNTRACK_LABELS)
+
+#ifndef NF_CT_LABELS_MAX_SIZE
+#define NF_CT_LABELS_MAX_SIZE ((XT_CONNLABEL_MAXBIT + 1) / BITS_PER_BYTE)
+#endif
+
+/* XXX: This doesn't lock others out from doing the same configuration
+ *	simultaneously. */
+static inline int nf_connlabels_get(struct net *net, unsigned int n_bits)
+{
+	size_t words;
+
+	if (n_bits > (NF_CT_LABELS_MAX_SIZE * BITS_PER_BYTE))
+		return -ERANGE;
+
+	words = BITS_TO_LONGS(n_bits);
+
+	net->ct.labels_used++;
+	if (words > net->ct.label_words)
+		net->ct.label_words = words;
+
+	return 0;
+}
+
+static inline void nf_connlabels_put(struct net *net)
+{
+	net->ct.labels_used--;
+	if (net->ct.labels_used == 0)
+		net->ct.label_words = 0;
+}
+
+#endif
+#endif /* _NF_CONNTRACK_LABELS_WRAPPER_H */

datapath/linux/Modules.mk
@@ -93,6 +93,7 @@ openvswitch_headers += \
 	linux/compat/include/net/vxlan.h \
 	linux/compat/include/net/netfilter/nf_conntrack_core.h \
 	linux/compat/include/net/netfilter/nf_conntrack_expect.h \
+	linux/compat/include/net/netfilter/nf_conntrack_labels.h \
 	linux/compat/include/net/netfilter/nf_conntrack_zones.h \
 	linux/compat/include/net/sctp/checksum.h
 EXTRA_DIST += linux/compat/build-aux/export-check-whitelist

commit 41d238bb545c59fc8e5ade1a5ee9373b0be2fd81

datapath: Define nf_connlabels_{put,get}.
Previously this was only done when connlabels were enabled in the kernel
config, even if the functions didn't exist. Fix the compile error.

Fixes: b8cce81fa9a1 ("compat: Backport nf_connlabels_{get, put}().")
Reported-by: Simon Horman <simon.horman@netronome.com>
Signed-off-by: Joe Stringer <joe@ovn.org>
Acked-by: Pravin B Shelar <pshelar@nicira.com>

datapath/linux/compat/include/net/netfilter/nf_conntrack_labels.h
@@ -5,8 +5,8 @@
 #include <linux/version.h>
 #include_next <net/netfilter/nf_conntrack_labels.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0) && \
-    IS_ENABLED(CONFIG_NF_CONNTRACK_LABELS)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+#if IS_ENABLED(CONFIG_NF_CONNTRACK_LABELS)
 
 #ifndef NF_CT_LABELS_MAX_SIZE
 #define NF_CT_LABELS_MAX_SIZE ((XT_CONNLABEL_MAXBIT + 1) / BITS_PER_BYTE)
@@ -37,5 +37,13 @@ static inline void nf_connlabels_put(struct net *net)
 		net->ct.label_words = 0;
 }
 
-#endif
+#else /* CONFIG_NF_CONNTRACK_LABELS */
+static inline int nf_connlabels_get(struct net *net, unsigned int n_bits)
+{
+	return -ERANGE;
+}
+
+static inline void nf_connlabels_put(struct net *net) { }
+#endif /* CONFIG_NF_CONNTRACK_LABELS */
+#endif /* 4.3 */
 #endif /* _NF_CONNTRACK_LABELS_WRAPPER_H */