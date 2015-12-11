0caae084f9d3139dec1b6c8dd9cc315de13f2eb0
William Tu <u9012063@gmail.com>
2015-12-10 17:58:15 -0800

    ofp-actions: Add padding in ofpacts_pull_openflow_instructions()
    ofpacts_pull_openflow_instructions() should fill 'ofpacts' with a list
    of OpenFlow actions and each action (including the last one) should be
    padded to OFP_ACTION_ALIGN(8) bytes.
    
    In most of the cases this is taken care of (e.g. by ofpacts_decode), but
    for the Goto-Table instruction (and Clear-Actions, based on a quick code
    inspection), this wasn't the case.
    
    This caused the copy operation in recirc_unroll_actions() to read two
    extra bytes after an allocated area (not a big deal, but enough to
    displease the AddressSanitizer).
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>
    Co-authored-by: Daniele Di Proietto <diproiettod@vmware.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -5358,6 +5358,8 @@ ofpacts_pull_openflow_instructions(struct ofpbuf *openflow,
         ogt->table_id = oigt->table_id;
     }
 
+    ofpact_pad(ofpacts);
+
     error = ofpacts_verify(ofpacts->data, ofpacts->size,
                            (1u << N_OVS_INSTRUCTIONS) - 1);
 exit:
