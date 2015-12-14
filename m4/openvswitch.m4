openvswitch/ovs

commit 43000bc793cb977cca8c7251c05639705d9a9895

openvswitch.m4: Portability improvement
${parameter/pattern/string} substituion is not portable.
Replace them with $SED equivalent.

Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
Acked-by: Ben Pfaff <blp@ovn.org>

m4/openvswitch.m4
@@ -109,11 +109,11 @@ AC_DEFUN([OVS_CHECK_WIN32],
             fi
             if test "$cl_cv_x64" = yes; then
                 PTHREAD_WIN32_DIR=$withval/lib/x64
-                PTHREAD_WIN32_DIR_DLL=/${withval/:/}/dll/x64
+                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e '/://')/dll/x64
                 PTHREAD_WIN32_DIR_DLL_WIN_FORM=$withval/dll/x64
             else
                 PTHREAD_WIN32_DIR=$withval/lib/x86
-                PTHREAD_WIN32_DIR_DLL=/${withval/:/}/dll/x86
+                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e '/://')/dll/x64
                 PTHREAD_WIN32_DIR_DLL_WIN_FORM=$withval/dll/x86
             fi
             PTHREAD_INCLUDES=-I$withval/include

commit aca8be36a2aba3dccd48c1ad91ff8ea7e9249761

confifugre: Fix broken sed calls in shell code.
Commit 43000bc (openvswitch.m4: Portability improvement), which introduced
a portability improvement, also introduces two bugs.  This commit fixes
both bug, by adding the command for $SED 's' and changes to x86 for 32 bit
instead of x64.

Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

m4/openvswitch.m4
@@ -109,11 +109,11 @@ AC_DEFUN([OVS_CHECK_WIN32],
             fi
             if test "$cl_cv_x64" = yes; then
                 PTHREAD_WIN32_DIR=$withval/lib/x64
-                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e '/://')/dll/x64
+                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e 's/://')/dll/x64
                 PTHREAD_WIN32_DIR_DLL_WIN_FORM=$withval/dll/x64
             else
                 PTHREAD_WIN32_DIR=$withval/lib/x86
-                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e '/://')/dll/x64
+                PTHREAD_WIN32_DIR_DLL=/$(echo ${withval} | ${SED} -e 's/://')/dll/x86
                 PTHREAD_WIN32_DIR_DLL_WIN_FORM=$withval/dll/x86
             fi
             PTHREAD_INCLUDES=-I$withval/include