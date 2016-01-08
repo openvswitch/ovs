3001f7199693da0fad76718c7c0ca7572ff0846a
Joe Stringer <joe@ovn.org>
2016-01-07 17:47:23 -0800

    compat: Detect and use nf_ct_frag6_gather().
    This function is a likely candidate for backporting, and currently
    relies on version checks to include the source or not. Grep for the
    appropriate functions instead, and include the backport based on that.
    
    Signed-off-by: Joe Stringer <joe@ovn.org>
    Acked-by: Pravin B Shelar <pshelar@ovn.org>

--- datapath/linux/compat/include/net/netfilter/ipv6/nf_defrag_ipv6.h
+++ datapath/linux/compat/include/net/netfilter/ipv6/nf_defrag_ipv6.h
@@ -7,8 +7,14 @@
 #include_next <net/netfilter/ipv6/nf_defrag_ipv6.h>
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+/* Upstream commit 029f7f3b8701 ("netfilter: ipv6: nf_defrag: avoid/free clone
+ * operations") changed the semantics of nf_ct_frag6_gather(), so we backport
+ * it for all prior kernels.
+ */
+#if defined(HAVE_NF_CT_FRAG6_CONSUME_ORIG) || \
+    defined(HAVE_NF_CT_FRAG6_OUTPUT)
 #if defined(OVS_FRAGMENT_BACKPORT)
+#define OVS_NF_DEFRAG6_BACKPORT 1
 struct sk_buff *rpl_nf_ct_frag6_gather(struct sk_buff *skb, u32 user);
 int __init rpl_nf_ct_frag6_init(void);
 void rpl_nf_ct_frag6_cleanup(void);
@@ -24,9 +30,13 @@ static inline void rpl_nf_ct_frag6_cleanup(void) { }
 static inline void rpl_nf_ct_frag6_consume_orig(struct sk_buff *skb) { }
 #endif /* OVS_FRAGMENT_BACKPORT */
 #define nf_ct_frag6_gather rpl_nf_ct_frag6_gather
+#else /* HAVE_NF_CT_FRAG6_CONSUME_ORIG */
+static inline int __init rpl_nf_ct_frag6_init(void) { return 0; }
+static inline void rpl_nf_ct_frag6_cleanup(void) { }
+static inline void rpl_nf_ct_frag6_consume_orig(struct sk_buff *skb) { }
+#endif /* HAVE_NF_CT_FRAG6_CONSUME_ORIG */
 #define nf_ct_frag6_init rpl_nf_ct_frag6_init
 #define nf_ct_frag6_cleanup rpl_nf_ct_frag6_cleanup
 #define nf_ct_frag6_consume_orig rpl_nf_ct_frag6_consume_orig
-#endif /* < 4.3 */
 
 #endif /* __NF_DEFRAG_IPV6_WRAPPER_H */
