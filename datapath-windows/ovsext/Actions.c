7245461698265895ff99de7b2b9a106bb77e8199
Alin Serdean <aserdean@cloudbasesolutions.com>
2016-02-11 01:38:54 +0000

    datapath-windows: Refactor sofware offloads and mss
    The purpose of this patch is to refactor the software offloads found in
    the VXLAN and GRE code and also to refactor how the maximmum segment
    size for a given NBL is obtained.
    
    This patch introduces two functions OvsApplySWChecksumOnNB and OVSGetTcpMSS.
    
    OVSGetTcpMSS - will return the mss found in a given NBL.
    
    OvsApplySWChecksumOnNB - will compute and set software offloads for a given
                             NBL.
    
    Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
    Acked-by: Sorin Vinturis <svinturis at cloudbasesolutions.com>
    Acked-by: Nithin Raju <nithin@vmware.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- datapath-windows/ovsext/Actions.c
+++ datapath-windows/ovsext/Actions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 VMware, Inc.
+ * Copyright (c) 2014, 2016 VMware, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,13 @@
 
 #include "precomp.h"
 
-#include "Checksum.h"
+#include "Debug.h"
 #include "Event.h"
 #include "Flow.h"
 #include "Gre.h"
 #include "Mpls.h"
 #include "NetProto.h"
+#include "Offload.h"
 #include "PacketIO.h"
 #include "Stt.h"
 #include "Switch.h"
@@ -33,7 +34,6 @@
 #undef OVS_DBG_MOD
 #endif
 #define OVS_DBG_MOD OVS_DBG_ACTION
-#include "Debug.h"
 
 typedef struct _OVS_ACTION_STATS {
     UINT64 rxGre;
