2946574fb0723f8080526b1f339e4234e2669a04
William Tu <u9012063@gmail.com>
2016-02-11 09:45:08 -0800

    expr: Fix memory leak reported by valgrind.
    Testcase 1728: ovn -- 5-term mixed expression normalization.
        expr_clone_cmp (expr.c:1259)
        expr_clone (expr.c:1284)
        expr_clone_andor (expr.c:1271)
        expr_clone (expr.c:1288)
        expr_normalize_and (expr.c:2137)
        test_tree_shape_exhaustively (test-ovn.c:926)
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>
    Co-authored-by: Daniele Di Proietto <diproiettod@vmware.com>
    Signed-off-by: Russell Bryant <russell@ovn.org>

--- ovn/lib/expr.c
+++ ovn/lib/expr.c
@@ -1789,7 +1789,7 @@ crush_and_numeric(struct expr *expr, const struct expr_symbol *symbol)
                 expr_destroy(expr);
                 return new;
             }
-            free(new);
+            expr_destroy(new);
             break;
         }
     }
@@ -1827,7 +1827,7 @@ crush_and_numeric(struct expr *expr, const struct expr_symbol *symbol)
                                       &sub->cmp.value, &sub->cmp.mask)) {
                 list_push_back(&or->andor, &sub->node);
             } else {
-                free(sub);
+                expr_destroy(sub);
             }
         }
         free(disjuncts);
@@ -1974,7 +1974,7 @@ crush_or(struct expr *expr, const struct expr_symbol *symbol)
         if (compare_cmps_3way(a, b)) {
             list_push_back(&expr->andor, &b->node);
         } else {
-            free(b);
+            expr_destroy(b);
         }
     }
     free(subs);
