88a427742488e436c700705b00bdb997fd0e3307
Joe Stringer <joe@ovn.org>
2016-03-03 21:22:50 +1300

    ofp-actions: Prevent integer overflow in decode.
    When decoding a variable-length action, if the length of the action
    exceeds the length storable in a uint16_t then something has gone
    terribly wrong. Assert that this is not the case.
    
    Signed-off-by: Joe Stringer <joe@ovn.org>
    Acked-by: Jarno Rajahalme <jarno@ovn.org>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -7401,8 +7401,12 @@ ofpact_init(struct ofpact *ofpact, enum ofpact_type type, size_t len)
 void
 ofpact_finish(struct ofpbuf *ofpacts, struct ofpact *ofpact)
 {
+    ptrdiff_t len;
+
     ovs_assert(ofpact == ofpacts->header);
-    ofpact->len = (char *) ofpbuf_tail(ofpacts) - (char *) ofpact;
+    len = (char *) ofpbuf_tail(ofpacts) - (char *) ofpact;
+    ovs_assert(len <= UINT16_MAX);
+    ofpact->len = len;
     ofpbuf_padto(ofpacts, OFPACT_ALIGN(ofpacts->size));
 }
 
