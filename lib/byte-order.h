38324aa7d09b886ffe272fc08ba0c32057b72ca0
Lance Richardson <lrichard@redhat.com>
2016-03-15 11:52:59 -0400

    byte-order: use system ntohll() and htonll() for OS X
    Avoid collision with system headers when building under OS X.
    
    Signed-off-by: Lance Richardson <lrichard@redhat.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/byte-order.h
+++ lib/byte-order.h
@@ -22,7 +22,7 @@
 #include "openvswitch/types.h"
 
 #ifndef __CHECKER__
-#ifndef _WIN32
+#if !(defined(_WIN32) || defined(__MACH__))
 static inline ovs_be64
 htonll(uint64_t n)
 {
@@ -34,7 +34,7 @@ ntohll(ovs_be64 n)
 {
     return htonl(1) == 1 ? n : ((uint64_t) ntohl(n) << 32) | ntohl(n >> 32);
 }
-#endif /* _WIN32 */
+#endif /* !(defined(_WIN32) || defined(__MACH__)) */
 #else
 /* Making sparse happy with these functions also makes them unreadable, so
  * don't bother to show it their implementations. */
