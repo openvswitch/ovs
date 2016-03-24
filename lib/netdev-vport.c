80c4589aa499995a6527d15cfc7a544a841dfde6
Pravin B Shelar <pshelar@ovn.org>
2016-03-24 09:30:57 -0700

    tunneling: Enable IPv6 tuneling.
    There is check to disable IPv6 tunneling. Following patch
    removes it and reintroduces the tunneling automake tests.
    
    This reverts mostly commit 250bd94d1e500a89c76cac944e660bd9c07ac364.
    There are couple of new autotests and updated documentation
    related to ipv6 tunneling added in this patch.
    
    Signed-off-by: Pravin B Shelar <pshelar@ovn.org>
    Acked-by: Ben Pfaff <blp@ovn.org>

--- lib/netdev-vport.c
+++ lib/netdev-vport.c
@@ -504,10 +504,6 @@ set_tunnel_config(struct netdev *dev_, const struct smap *args)
                           name, node->value);
                 return EINVAL;
             }
-            if (dst_proto == ETH_TYPE_IPV6) {
-                VLOG_WARN("%s: IPv6 'remote_ip' is not supported", name);
-                return EOPNOTSUPP;
-            }
         } else if (!strcmp(node->key, "local_ip")) {
             int err;
             err = parse_tunnel_ip(node->value, true, &tnl_cfg.ip_src_flow,
@@ -516,10 +512,6 @@ set_tunnel_config(struct netdev *dev_, const struct smap *args)
             case ENOENT:
                 VLOG_WARN("%s: bad %s 'local_ip'", name, type);
                 break;
-            }
-            if (src_proto == ETH_TYPE_IPV6) {
-                VLOG_WARN("%s: IPv6 'local_ip' is not supported", name);
-                return EOPNOTSUPP;
             }
         } else if (!strcmp(node->key, "tos")) {
             if (!strcmp(node->value, "inherit")) {
