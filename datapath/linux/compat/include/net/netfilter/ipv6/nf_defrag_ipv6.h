9323abed99507ee18ae73e3dad57ea1fd0cf2cbb
Joe Stringer <joe@ovn.org>
2016-05-02 11:19:18 -0700

    compat: Document nf_defrag_ipv[46] backport.
    Document how the IP(6) defrag backport works, and do minor style cleanups.
    
    Signed-off-by: Joe Stringer <joe@ovn.org>
    Acked-by: Jesse Gross <jesse@kernel.org>

--- datapath/linux/compat/include/net/netfilter/ipv6/nf_defrag_ipv6.h
+++ datapath/linux/compat/include/net/netfilter/ipv6/nf_defrag_ipv6.h
@@ -16,9 +16,16 @@
 #define OVS_NF_DEFRAG6_BACKPORT 1
 int rpl_nf_ct_frag6_gather(struct net *net, struct sk_buff *skb, u32 user);
 #define nf_ct_frag6_gather rpl_nf_ct_frag6_gather
-#endif /* HAVE_NF_CT_FRAG6_CONSUME_ORIG */
 
-#ifdef OVS_NF_DEFRAG6_BACKPORT
+/* If backporting IPv6 defrag, then init/exit functions need to be called from
+ * compat_{in,ex}it() to prepare the backported fragmentation cache. In this
+ * case we declare the functions which are defined in
+ * datapath/linux/compat/nf_conntrack_reasm.c.
+ *
+ * Otherwise, if we can use upstream defrag then we can rely on the upstream
+ * nf_defrag_ipv6 module to init/exit correctly. In this case the calls in
+ * compat_{in,ex}it() can be no-ops.
+ */
 int __init rpl_nf_ct_frag6_init(void);
 void rpl_nf_ct_frag6_cleanup(void);
 #else /* !OVS_NF_DEFRAG6_BACKPORT */
