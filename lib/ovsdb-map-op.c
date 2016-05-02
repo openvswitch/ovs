2ec2d45b25efe51213bfcd533994368bdfee08c0
Edward Aymerich <edward.aymerich@hpe.com>
2016-05-02 10:26:51 -0600

    Fix non-ANSI declaration
    Fix non-ANSI declaration of a function inside 'ovsdb-map-op.c'.
    
    Signed-off-by: Edward Aymerich <edward.aymerich@hpe.com>
    Signed-off-by: Arnoldo Lutz <arnoldo.lutz.guevara@hpe.com>
    Co-authored-by: Arnoldo Lutz <arnoldo.lutz.guevara@hpe.com>

--- lib/ovsdb-map-op.c
+++ lib/ovsdb-map-op.c
@@ -80,7 +80,7 @@ map_op_type(const struct map_op *map_op)
 }
 
 struct map_op_list*
-map_op_list_create()
+map_op_list_create(void)
 {
     struct map_op_list *list = xmalloc(sizeof *list);
     hmap_init(&list->hmap);
