2f3b13ccbd6f98d10d52f4a9ecc3375e4f40d0fe
Joe Stringer <joe@ovn.org>
2016-05-02 17:47:32 -0700

    compat: Remove unused ipv[46] backports.
    These pieces #if on kernel versions which are not supported since commit
    f2ab1536ddbc ("compat: Backport conntrack strictly to v3.10+.")
    
    Signed-off-by: Joe Stringer <joe@ovn.org>
    Acked-by: Jesse Gross <jesse@kernel.org>

--- datapath/linux/compat/include/net/ip.h
+++ datapath/linux/compat/include/net/ip.h
@@ -6,13 +6,6 @@
 #include <net/route.h>
 #include <linux/version.h>
 
-#ifndef HAVE_IP_IS_FRAGMENT
-static inline bool ip_is_fragment(const struct iphdr *iph)
-{
-	return (iph->frag_off & htons(IP_MF | IP_OFFSET)) != 0;
-}
-#endif
-
 #ifndef HAVE_INET_GET_LOCAL_PORT_RANGE_USING_NET
 static inline void rpl_inet_get_local_port_range(struct net *net, int *low,
 					     int *high)
