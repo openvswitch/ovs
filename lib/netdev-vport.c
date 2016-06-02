68da36feee8ad2eccb9371542018c6b08b5a553d
Thadeu Lima de Souza Cascardo <cascardo@redhat.com>
2016-06-02 07:18:49 -0300

    netdev-vport: Update copyright headers
    Red Hat has contributed to the original code that has moved to netdev-native-tnl
    module and to code that has been kept in netdev-vport as well.
    
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@redhat.com>
    Signed-off-by: Jesse Gross <jesse@kernel.org>

--- lib/netdev-vport.c
+++ lib/netdev-vport.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2010, 2011, 2012, 2013, 2014 Nicira, Inc.
+ * Copyright (c) 2016 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
