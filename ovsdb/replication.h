a2bba75ba12afd614ee3dfa0280f6173e5dd72f8
Mario Cabrera <mario.cabrera@hpe.com>
2016-06-28 15:14:53 -0600

    ovsdb: Fix OVSDB disconnect replication bug
    Currently disconnecting from the replicator server means closing the jsonrpc
    connection and destroying the monitored table names and blacklisted table
    names.
    
    This patch makes a distition between disconnecting from the remote server,
    applicable when the replication incurs in an error, and destroying the
    remote server info, applicable when ovsdb-server exits gracefully.
    
    Signed-off-by: Mario Cabrera <mario.cabrera@hpe.com>

--- ovsdb/replication.h
+++ ovsdb/replication.h
@@ -34,6 +34,7 @@ void replication_run(struct shash *dbs);
 void set_remote_ovsdb_server(const char *remote_server);
 void set_tables_blacklist(const char *blacklist);
 void disconnect_remote_server(void);
+void destroy_remote_server(void);
 const struct db *find_db(const struct shash *all_dbs, const char *db_name);
 void replication_usage(void);
 
