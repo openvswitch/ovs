1e25f5caef5c1b6378bfee3f6ed0d1cb04e61e0c
Russell Bryant <russell@ovn.org>
2016-07-10 17:46:26 -0400

    ovn-sbctl: Adjust formatting of lflow-list.
    Adjust the field width for a couple of elements in the output of the
    lflow-list sub-command of ovn-sbctl. We now have a table ID of 10, so
    set a field width of 2 to keep output aligned. Table names have grown to
    a max of 19 characters, so update the field width to keep output
    aligned.  Finally, left justify values in fields, as I find that to look
    a little nicer.
    
    Also update sample output in OVN-Tutorial.md to match the current state.
    
    Signed-off-by: Russell Bryant <russell@ovn.org>
    Acked-by: Ben Pfaff <blp@ovn.org>

--- ovn/utilities/ovn-sbctl.c
+++ ovn/utilities/ovn-sbctl.c
@@ -733,7 +733,7 @@ cmd_lflow_list(struct ctl_context *ctx)
         }
 
         const char *table_name = smap_get(&lflow->external_ids, "stage-name");
-        printf("  table=%" PRId64 "(%16s), priority=%5" PRId64
+        printf("  table=%-2" PRId64 "(%-19s), priority=%-5" PRId64
                ", match=(%s), action=(%s)\n",
                lflow->table_id, table_name ? table_name : "",
                lflow->priority, lflow->match, lflow->actions);
