56a91749be54757a86cfd4363c63f6123276d14a
Ben Pfaff <blp@ovn.org>
2016-07-13 16:41:00 -0700

    ofp-actions: Translate OF1.1+ mod_nw_ttl action to OF1.0 properly.
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Jarno Rajahalme <jarno@ovn.org>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -1970,7 +1970,9 @@ encode_SET_IP_TTL(const struct ofpact_ip_ttl *ttl,
     if (ofp_version >= OFP11_VERSION) {
         put_OFPAT11_SET_NW_TTL(out, ttl->ttl);
     } else {
-        /* XXX */
+        struct mf_subfield dst = { .field = mf_from_id(MFF_IP_TTL),
+                                   .ofs = 0, .n_bits = 8 };
+        put_reg_load(out, &dst, ttl->ttl);
     }
 }
 
