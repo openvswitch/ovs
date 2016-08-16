openvswitch/ovs

commit 4acd1e87a8bf931a30bb0df9411be039ae1ecad8

ovn-trace: New utility.
This new utility is intended to fulfill for OVN the purpose that
"ofproto/trace" has for Open vSwitch.  First, it's meant to be a useful
tool for troubleshooting and diagnosis and in general for improving one's
understanding of the emergent properties of a flow table.  Second, it
simplifies and increases the practical scope of testing, as well as making
testing more reliable and repeatable and failures easier to interpret.

This commit adds only a single test that uses the new utility, based on the
oldest OVN end-to-end test "ovn -- 3 HVs, 1 LS, 3 lports/HV".  The
differences between the old and the new test illustrate properties of
tracing.  First, the new test does not start any ovn-controller processes
or simulate any hypervisors in a nontrivial way.  This is because ovn-trace
does not actually forward packets or rely on the physical structure of the
system.  Second, whereas the old test tested not just the logical but also
the physical structure of the system, it needed to have several logical
ports, a total of 9 (3 on each of 3 HVs), whereas since this test only
tests the logical network implementation it can use a smaller number.  This
property also means that the new test runs signicantly faster than the old
one (less than a second on my laptop).

In my opinion this approach points the way toward the future of OVN
testing.  Certainly, we need end-to-end tests.  However, I believe that the
bulk of our tests can be broken into ones that test the logical network
implementation (using tracing) and ones that test physical/logical
translation.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Ryan Moats <rmoats@us.ibm.com>

ovn/utilities/automake.mk
@@ -4,7 +4,8 @@ scripts_SCRIPTS += \
 man_MANS += \
     ovn/utilities/ovn-ctl.8 \
     ovn/utilities/ovn-nbctl.8 \
-    ovn/utilities/ovn-sbctl.8
+    ovn/utilities/ovn-sbctl.8 \
+    ovn/utilities/ovn-trace.8
 
 MAN_ROOTS += ovn/utilities/ovn-sbctl.8.in
 
@@ -18,7 +19,8 @@ EXTRA_DIST += \
     ovn/utilities/ovn-ctl.8.xml \
     ovn/utilities/ovn-docker-overlay-driver \
     ovn/utilities/ovn-docker-underlay-driver \
-    ovn/utilities/ovn-nbctl.8.xml
+    ovn/utilities/ovn-nbctl.8.xml \
+    ovn/utilities/ovn-trace.8.xml
 
 DISTCLEANFILES += \
     ovn/utilities/ovn-ctl.8 \
@@ -34,5 +36,10 @@ ovn_utilities_ovn_nbctl_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenv
 bin_PROGRAMS += ovn/utilities/ovn-sbctl
 ovn_utilities_ovn_sbctl_SOURCES = ovn/utilities/ovn-sbctl.c
 ovn_utilities_ovn_sbctl_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenvswitch.la
+
+# ovn-trace
+bin_PROGRAMS += ovn/utilities/ovn-trace
+ovn_utilities_ovn_trace_SOURCES = ovn/utilities/ovn-trace.c
+ovn_utilities_ovn_trace_LDADD = ovn/lib/libovn.la ovsdb/libovsdb.la lib/libopenvswitch.la
 
 include ovn/utilities/bugtool/automake.mk

lib/automake.mk
@@ -436,6 +436,7 @@ EXTRA_DIST += \
 	lib/dh2048.pem \
 	lib/dh4096.pem \
 	lib/common.xml \
+	lib/daemon.xml \
 	lib/dirs.c.in \
 	lib/db-ctl-base.xml \
 	lib/ssl.xml \

include/openvswitch/meta-flow.h
@@ -1808,6 +1808,14 @@ struct mf_bitmap {
 #error "Need to update CASE_MFF_XXREGS to match FLOW_N_XXREGS"
 #endif
 
+static inline bool
+mf_is_register(enum mf_field_id id)
+{
+    return ((id >= MFF_REG0   && id < MFF_REG0   + FLOW_N_REGS) ||
+            (id >= MFF_XREG0  && id < MFF_XREG0  + FLOW_N_XREGS) ||
+            (id >= MFF_XXREG0 && id < MFF_XXREG0 + FLOW_N_XXREGS));
+}
+
 /* Use this macro as CASE_MFF_TUN_METADATA: in a switch statement to choose
  * all of the MFF_TUN_METADATAn cases. */
 #define CASE_MFF_TUN_METADATA                         \

include/ovn/actions.h
@@ -27,6 +27,7 @@
 #include "util.h"
 
 struct lexer;
+struct ofpact_set_field;
 struct ofpbuf;
 struct shash;
 struct simap;
@@ -149,6 +150,13 @@ struct ovnact_load {
     struct expr_field dst;
     union expr_constant imm;
 };
+
+void ovnact_load_to_ofpact_set_field(const struct ovnact_load *,
+                                     bool (*lookup_port)(const void *aux,
+                                                         const char *port_name,
+                                                         unsigned int *portp),
+                                     const void *aux,
+                                     struct ofpact_set_field *);
 
 /* OVNACT_MOVE, OVNACT_EXCHANGE. */
 struct ovnact_move {

lib/daemon.xml
@@ -1,0 +1,121 @@
+<?xml version="1.0" encoding="utf-8"?>
+<dl>
+  <dt><code>--pidfile</code>[<code>=</code><var>pidfile</var>]</dt>
+  <dd>
+    <p>
+      Causes a file (by default, <code><var>program</var>.pid</code>) to be
+      created indicating the PID of the running process.  If the
+      <var>pidfile</var> argument is not specified, or if it does not begin
+      with <code>/</code>, then it is created in <code>@RUNDIR@</code>.
+    </p>
+
+    <p>
+      If <code>--pidfile</code> is not specified, no pidfile is created.
+    </p>
+  </dd>
+
+  <dt><code>--overwrite-pidfile</code></dt>
+  <dd>
+    <p>
+      By default, when <code>--pidfile</code> is specified and the specified
+      pidfile already exists and is locked by a running process, the daemon
+      refuses to start.  Specify <code>--overwrite-pidfile</code> to cause it
+      to instead overwrite the pidfile.
+    </p>
+
+    <p>
+      When <code>--pidfile</code> is not specified, this option has no effect.
+    </p>
+  </dd>
+
+  <dt><code>--detach</code></dt>
+  <dd>
+    Runs this program as a background process.  The process forks, and in the
+    child it starts a new session, closes the standard file descriptors (which
+    has the side effect of disabling logging to the console), and changes its
+    current directory to the root (unless <code>--no-chdir</code> is
+    specified).  After the child completes its initialization, the parent
+    exits.
+  </dd>
+
+  <dt><code>--monitor</code></dt>
+  <dd>
+    <p>
+      Creates an additional process to monitor this program.  If it dies due to
+      a signal that indicates a programming error (<code>SIGABRT</code>,
+      <code>SIGALRM</code>, <code>SIGBUS</code>, <code>SIGFPE</code>,
+      <code>SIGILL</code>, <code>SIGPIPE</code>, <code>SIGSEGV</code>,
+      <code>SIGXCPU</code>, or <code>SIGXFSZ</code>) then the monitor process
+      starts a new copy of it.  If the daemon dies or exits for another reason,
+      the monitor process exits.
+    </p>
+
+    <p>
+      This option is normally used with <code>--detach</code>, but it also
+      functions without it.
+    </p>
+  </dd>
+
+  <dt><code>--no-chdir</code></dt>
+  <dd>
+    <p>
+      By default, when <code>--detach</code> is specified, the deamon changes
+      its current working directory to the root directory after it detaches.
+      Otherwise, invoking the daemon from a carelessly chosen directory would
+      prevent the administrator from unmounting the file system that holds that
+      directory.
+    </p>
+
+    <p>
+      Specifying <code>--no-chdir</code> suppresses this behavior, preventing
+      the daemon from changing its current working directory.  This may be
+      useful for collecting core files, since it is common behavior to write
+      core dumps into the current working directory and the root directory is
+      not a good directory to use.
+    </p>
+
+    <p>
+      This option has no effect when <code>--detach</code> is not specified.
+    </p>
+  </dd>
+
+  <dt><code>--no-self-confinement</code></dt>
+  <dd>
+    By default this daemon will try to self-confine itself to work with files
+    under well-known directories whitelisted at build time.  It is better to
+    stick with this default behavior and not to use this flag unless some other
+    Access Control is used to confine daemon.  Note that in contrast to other
+    access control implementations that are typically enforced from
+    kernel-space (e.g. DAC or MAC), self-confinement is imposed from the
+    user-space daemon itself and hence should not be considered as a full
+    confinement strategy, but instead should be viewed as an additional layer
+    of security.
+  </dd>
+
+  <dt><code>--user=</code><var>user</var><code>:</code><var>group</var></dt>
+  <dd>
+    <p>
+      Causes this program to run as a different user specified in
+      <var>user</var><code>:</code><var>group</var>, thus dropping most of the
+      root privileges. Short forms <var>user</var> and
+      <code>:</code><var>group</var> are also allowed, with current user or
+      group assumed, respectively.  Only daemons started by the root user
+      accepts this argument.
+    </p>
+
+    <p>
+      On Linux, daemons will be granted <code>CAP_IPC_LOCK</code> and
+      <code>CAP_NET_BIND_SERVICES</code> before dropping root privileges.
+      Daemons interact with datapath, such as <code>ovs-vswitchd</code>, will
+      be granted two additional capabilities, namely <code>CAP_NET_ADMIN</code>
+      and <code>CAP_NET_RAW</code>.  The capability change will apply even if
+      the new user is root.
+    </p>
+
+    <p>
+      On Windows, this option is not currently supported.  For security
+      reasons, specifying this option will cause the daemon process not to
+      start.
+    </p>
+  </dd>
+</dl>

ovn/lib/actions.c
@@ -339,15 +339,17 @@ format_LOAD(const struct ovnact_load *load, struct ds *s)
     ds_put_char(s, ';');
 }
 
-static void
-encode_LOAD(const struct ovnact_load *load,
-            const struct ovnact_encode_params *ep,
-            struct ofpbuf *ofpacts)
+void
+ovnact_load_to_ofpact_set_field(const struct ovnact_load *load,
+                                bool (*lookup_port)(const void *aux,
+                                                    const char *port_name,
+                                                    unsigned int *portp),
+                                const void *aux,
+                                struct ofpact_set_field *sf)
 {
     const union expr_constant *c = &load->imm;
     struct mf_subfield dst = expr_resolve_field(&load->dst);
 
-    struct ofpact_set_field *sf = ofpact_put_SET_FIELD(ofpacts);
     sf->field = dst.field;
 
     if (load->dst.symbol->width) {
@@ -364,13 +366,22 @@ encode_LOAD(const struct ovnact_load *load,
         }
     } else {
         uint32_t port;
-        if (!ep->lookup_port(ep->aux, load->imm.string, &port)) {
+        if (!lookup_port(aux, load->imm.string, &port)) {
             port = 0;
         }
         bitwise_put(port, &sf->value,
                     sf->field->n_bytes, 0, sf->field->n_bits);
         bitwise_one(&sf->mask, sf->field->n_bytes, 0, sf->field->n_bits);
     }
+}
+
+static void
+encode_LOAD(const struct ovnact_load *load,
+            const struct ovnact_encode_params *ep,
+            struct ofpbuf *ofpacts)
+{
+    ovnact_load_to_ofpact_set_field(load, ep->lookup_port, ep->aux,
+                                    ofpact_put_SET_FIELD(ofpacts));
 }
 
 static void

ovn/utilities/ovn-trace.8.xml
@@ -1,0 +1,287 @@
+<?xml version="1.0" encoding="utf-8"?>
+<manpage program="ovn-trace" section="8" title="ovn-trace">
+  <h1>Name</h1>
+  <p>ovn-trace -- Open Virtual Network logical network tracing utility</p>
+
+  <h1>Synopsis</h1>
+  <p><code>ovn-trace</code> [<var>options</var>] <var>datapath</var> <var>microflow</var></p>
+  <p><code>ovn-trace</code> [<var>options</var>] <code>--detach</code></p>
+  
+  <h1>Description</h1>
+  <p>
+    This utility simulates packet forwarding within an OVN logical network.
+    It can be used to run through ``what-if'' scenarios: if a packet
+    originates at a logical port, what will happen to it and where will it
+    ultimately end up?  Users already familiar with the Open vSwitch
+    <code>ofproto/trace</code> command described in
+    <code>ovs-vswitch</code>(8) will find <code>ovn-trace</code> to be a
+    similar tool for logical networks.
+  </p>
+
+  <p>
+    <code>ovn-trace</code> works by reading the <code>Logical_Flow</code> and
+    other tables from the OVN southbound database (see
+    <code>ovn-sb</code>(5)).  It simulates a packet's path through logical
+    networks by repeatedly looking it up in the logical flow table, following
+    the entire tree of possibilities.
+  </p>
+
+  <p>
+    <code>ovn-trace</code> simulates only the OVN logical network.  It does
+    not simulate the physical elements on which the logical network is
+    layered.  This means that, for example, it is unimportant how VMs are
+    distributed among hypervisors, or whether their hypervisors are
+    functioning and reachable, so <code>ovn-trace</code> will yield the same
+    results regardless.  There is one important exception:
+    <code>ovn-northd</code>, the daemon that generates the logical flows that
+    <code>ovn-trace</code> simulates, treats logical ports differently based
+    on whether they are up or down.  Thus, if you see surprising results,
+    ensure that the ports involved in a simulation are up.
+  </p>
+
+  <p>
+    The simplest way to use <code>ovn-trace</code> is to provide
+    <var>datapath</var> and <var>microflow</var> arguments on the command
+    line.  In this case, it simulates the behavior of a single packet and
+    exits.  For an alternate usage model, see <code>Daemon Mode</code> below.
+  </p>
+
+  <p>
+    The <var>datapath</var> argument specifies the name of a logical
+    datapath.  Acceptable names are the <code>name</code> from the northbound
+    <code>Logical_Switch</code> or <code>Logical_Router</code> table, the
+    UUID of a record from one of those tables, or the UUID of a record from
+    the southbound <code>Datapath_Binding</code> table.
+  </p>
+
+  <p>
+    The <var>microflow</var> argument describes the packet whose forwarding
+    is to be simulated, in the syntax of an OVN logical expression, as
+    described in <code>ovn-sb</code>(5), to express constraints.  The parser
+    understands prerequisites; for example, if the expression refers to
+    <code>ip4.src</code>, there is no need to explicitly state
+    <code>ip4</code> or <code>eth.type == 0x800</code>.
+  </p>
+
+  <p>
+    For reasonable L2 behavior, the microflow should include at least
+    <code>inport</code> and <code>eth.dst</code>, plus <code>eth.src</code>
+    if port security is enabled.  For example:
+  </p>
+  <pre>
+    inport == "lp11" &amp;&amp; eth.src == 00:01:02:03:04:05 &amp;&amp; eth.dst == ff:ff:ff:ff:ff:ff
+  </pre>
+
+  <p>
+    For reasonable L3 behavior, <var>microflow</var> should also include
+    <code>ip4.src</code> and <code>ip4.dst</code> (or <code>ip6.src</code>
+    and <code>ip6.dst</code>) and <code>ip.ttl</code>.  For example:
+  </p>
+  <pre>
+    inport == "lp111" &amp;&amp; eth.src == f0:00:00:00:01:11 &amp;&amp; eth.dst == 00:00:00:00:ff:11
+    &amp;&amp; ip4.src == 192.168.11.1 &amp;&amp; ip4.dst == 192.168.22.2 &amp;&amp; ip.ttl == 64
+  </pre>
+
+  <p>Here's an ARP microflow example:</p>
+  <pre>
+    inport == "lp123"
+    &amp;&amp; eth.dst == ff:ff:ff:ff:ff:ff &amp;&amp; eth.src == f0:00:00:00:01:11
+    &amp;&amp; arp.op == 1 &amp;&amp; arp.sha == f0:00:00:00:01:11 &amp;&amp; arp.spa == 192.168.1.11
+    &amp;&amp; arp.tha == ff:ff:ff:ff:ff:ff &amp;&amp; arp.tpa == 192.168.2.22
+  </pre>
+
+  <p>
+    <code>ovn-trace</code> will reject erroneous microflow expressions, which
+    beyond syntax errors fall into two categories.  First, they can be
+    ambiguous.  For example, <code>tcp.src == 80</code> is ambiguous because
+    it does not state IPv4 or IPv6 as the Ethernet type.  <code>ip4
+    &amp;&amp; tcp.src > 1024</code> is also ambiguous because it does not
+    constrain bits of <code>tcp.src</code> to particular values.  Second,
+    they can be contradictory, e.g. <code>ip4 &amp;&amp; ip6</code>.
+  </p>
+
+  <h1>Output</h1>
+
+  <p>
+    <code>ovn-trace</code> supports the three different forms of output, each
+    described in a separate section below.  Regardless of the selected output
+    format, <code>ovn-trace</code> starts the output with a line that shows
+    the microflow being traced in OpenFlow syntax.
+  </p>
+
+  <h2>Detailed Output</h2>
+
+  <p>
+    The detailed form of output is also the default form.  This form groups
+    output into sections headed up by the ingress or egress pipeline being
+    traversed.  Each pipeline lists each table that was visited (by number
+    and name), the match expression and priority of the logical flow that was
+    matched, and the actions that were executed.
+  </p>
+
+  <p>
+    The execution of OVN logical actions naturally forms a ``control stack''
+    that resembles that of a program in conventional programming languages
+    such as C or Java.  Because the <code>next</code> action that calls into
+    another logical flow table for a lookup is a recursive construct, OVN
+    ``programs'' in practice tend to form deep control stacks that, displayed
+    in the obvious way using additional indentation for each level, quickly
+    use up the horizontal space on all but the widest displays.  To make
+    detailed output more readable, without loss of generality,
+    <code>ovn-trace</code> omits indentation for ``tail recursion,'' that is,
+    when <code>next</code> is the last action in a logical flow, it does not
+    indent details of the next table lookup more deeply.  Output still uses
+    indentation when it is needed for clarity.
+  </p>
+
+  <p>
+    OVN ``programs'' traces also tend to encounter long strings of logical
+    flows with match expression <code>1</code> (which matches every packet)
+    the single action <code>next;</code>.  These are uninteresting and merely
+    clutter output, so <code>ovn-trace</code> omits them entirely even from
+    detailed output.
+  </p>
+
+  <p>
+    The following excerpt from detailed <code>ovn-trace</code> output shows a
+    section for a packet traversing the ingress pipeline of logical datapath
+    <code>ls1</code> with ingress logical port <code>lp111</code>.  The
+    packet matches a logical flow in table 0 (aka
+    <code>ls_in_port_sec_l2</code>) with priority 50 and executes
+    <code>next(1);</code> to pass to table 1.  Tables 1 through 11 are
+    trivial and omitted.  In table 12 (aka <code>ls_in_l2_lkup</code>), the
+    packet matches a flow with priority 50 based on its Ethernet destination
+    address and the flow's actions output the packet to the
+    <code>lrp11-attachement</code> logical port.
+  </p>
+
+  <pre fixed="yes">
+    ingress(dp="ls1", inport="lp111")
+    ---------------------------------
+    0. ls_in_port_sec_l2: inport == "lp111", priority 50
+    next(1);
+    12. ls_in_l2_lkup: eth.dst == 00:00:00:00:ff:11, priority 50
+    outport = "lrp11-attachment";
+    output;
+  </pre>
+
+  <h2>Summary Output</h2>
+
+  <p>
+    Summary output includes the logical pipelines visited by a packet and the
+    logical actions executed on it.  Compared to the detailed output,
+    however, it removes details of tables and logical flows traversed by a
+    packet.  It uses a format closer to that of a programming language and
+    does not attempt to avoid indentation.  The summary output equivalent to
+    the above detailed output fragment is:
+  </p>
+
+  <pre fixed="yes">
+    ingress(dp="ls1", inport="lp111") {
+    outport = "lrp11-attachment";
+    output;
+    ...
+    };
+  </pre>
+
+  <h2>Minimal Output</h2>
+
+  <p>
+    Minimal output includes only actions that modify packet data (not
+    including OVN registers or metadata such as <code>outport</code>) and
+    <code>output</code> actions that actually deliver a packet to a logical
+    port (excluding patch ports).  The operands of actions that modify packet
+    data are displayed reduced to constants, e.g. <code>ip4.dst =
+    reg0;</code> might be show as <code>ip4.dst = 192.168.0.1;</code> if that
+    was the value actually loaded.  This yields output even simpler than the
+    summary format.  (Users familiar with Open vSwitch may recognize this as
+    similar in spirit to the datapath actions listed at the bottom of
+    <code>ofproto/trace</code> output.)
+  </p>
+
+  <p>
+    The minimal output format reflects the externally seen behavior of the
+    logical networks more than it does the implementation.  This makes this
+    output format the most suitable for use in regression tests, because it
+    is least likely to change when logical flow tables are rearranged without
+    semantic change.
+  </p>
+
+  <h1>Daemon Mode</h1>
+
+  <p>
+    If <code>ovn-trace</code> is invoked with the <code>--detach</code> option
+    (see <code>Daemon Options</code>, below), it runs in the background as a
+    daemon and accepts commands from <code>ovs-appctl</code> (or another
+    JSON-RPC client) indefinitely.  The currently supported commands are
+    described below.
+  </p>
+
+  <p>
+    
+  </p>
+
+  <dl>
+    <dt><code>trace</code> [<var>options</var>] <var>datapath</var> <var>microflow</var></dt>
+    <dd>
+      Traces <var>microflow</var> through <var>datapath</var> and replies with
+      the results of the trace.  Accepts the <var>options</var> described under
+      <code>Trace Options</code> below.
+    </dd>
+
+    <dt><code>exit</code></dt>
+    <dd>Causes <code>ovn-trace</code> to gracefully terminate.</dd>
+  </dl>
+
+  <h1>Options</h1>
+  
+  <h2>Trace Options</h2>
+
+  <dl>
+    <dt><code>--detailed</code></dt>
+    <dt><code>--summary</code></dt>
+    <dt><code>--minimal</code></dt>
+    <dd>
+      These options control the form and level of detail in
+      <code>ovn-trace</code> output.  If more than one of these options is
+      specified, all of the selected forms are output, in the order listed
+      above, each headed by a banner line.  If none of these options is
+      given, <code>--detailed</code> is the default.  See
+      <code>Output</code>, above, for a description of each kind of output.
+    </dd>
+
+    <dt><code>--all</code></dt>
+    <dd>
+      Selects all three forms of output.
+    </dd>
+  </dl>
+
+  <h2>Daemon Options</h2>
+  <xi:include href="lib/daemon.xml" xmlns:xi="http://www.w3.org/2003/XInclude"/>
+
+  <h2>Logging Options</h2>
+  <xi:include href="lib/vlog.xml" xmlns:xi="http://www.w3.org/2003/XInclude"/>
+
+  <h2>PKI Options</h2>
+  <p>
+    PKI configuration is required to use SSL for the connection to the
+    database.
+  </p>
+  <xi:include href="lib/ssl.xml" xmlns:xi="http://www.w3.org/2003/XInclude"/>
+
+  <h2>Other Options</h2>
+
+  <dl>
+    <dt><code>--db</code> <var>database</var></dt>
+    <dd>
+      The OVSDB database remote to contact.  If the <env>OVN_SB_DB</env>
+      environment variable is set, its value is used as the default.
+      Otherwise, the default is <code>unix:@RUNDIR@/db.sock</code>, but this
+      default is unlikely to be useful outside of single-machine OVN test
+      environments.
+    </dd>
+  </dl>
+  
+  <xi:include href="lib/common.xml" xmlns:xi="http://www.w3.org/2003/XInclude"/>
+
+</manpage>

commit 62b219f21e8d68bde7f8eeb71fe2d36bd5044bfe

ovn: include ovn-trace.8 in DISTCLEANFILES
Found by 'make distcheck'.

Fixes: 4acd1e87a8bf ("ovn-trace: New utility.")
Signed-off-by: Lance Richardson <lrichard@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ovn/utilities/automake.mk
@@ -25,7 +25,8 @@ EXTRA_DIST += \
 DISTCLEANFILES += \
     ovn/utilities/ovn-ctl.8 \
     ovn/utilities/ovn-nbctl.8 \
-    ovn/utilities/ovn-sbctl.8
+    ovn/utilities/ovn-sbctl.8 \
+    ovn/utilities/ovn-trace.8
 
 # ovn-nbctl
 bin_PROGRAMS += ovn/utilities/ovn-nbctl