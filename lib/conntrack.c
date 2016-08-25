e0e2fdf01254af58c99397b16e8946688f0ac144
Wei <realbaseball2008@gmail.com>
2016-08-25 20:47:49 +0800

    conntrack:fix address offset bug
    Signed-off-by: Wei <realbaseball2008@gmail.com>

--- lib/conntrack.c
+++ lib/conntrack.c
@@ -1003,7 +1003,7 @@ conn_key_hash(const struct conn_key *key, uint32_t basis)
     hash = hsrc ^ hdst;
 
     /* Hash the rest of the key(L3 and L4 types and zone). */
-    hash = hash_words((uint32_t *) &key->dst + 1,
+    hash = hash_words((uint32_t *) (&key->dst + 1),
                       (uint32_t *) (key + 1) - (uint32_t *) (&key->dst + 1),
                       hash);
 
