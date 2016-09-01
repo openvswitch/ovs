6b49a29809f687e3919886bb41ea60daf88368c7
Nithin Raju <nithin@vmware.com>
2016-08-31 23:45:09 -0700

    datapath-windows: add assert in OvsHashFlow()
    Signed-off-by: Nithin Raju <nithin@vmware.com>
    Acked-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
    Signed-off-by: Gurucharan Shetty <guru@ovn.org>

--- datapath-windows/ovsext/Flow.c
+++ datapath-windows/ovsext/Flow.c
@@ -2595,6 +2595,7 @@ OvsHashFlow(const OvsFlowKey *key)
     UINT8 *start;
 
     ASSERT(key->tunKey.dst || offset == sizeof(OvsIPv4TunnelKey));
+    ASSERT(!key->tunKey.dst || offset == OvsGetFlowL2Offset(&key->tunKey));
     start = (UINT8 *)key + offset;
     return OvsJhashBytes(start, size, 0);
 }
