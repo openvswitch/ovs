openvswitch/ovs

commit 60e0cd0419588f520d88a71fa70df3b130e18526

ovsdb: Replication usability improvements
Based on feedbacks from initial HA manager integration, added the
'--active' command line option and appctl command
"ovsdb-server/sync-status. See man page updates for details.

Added the RPL_S_INIT state in the state machine. This state is
not strictly necessary for the replication state machine, but is
introduced to make sure the state is update immediately when
the state machine is reset, via replication_init(). Without it
ovsdb/sync-status may display "replicating" or crash, if the command
is issued between after replication_init() is called, but before
the state variable is updated from replication_run().

Added a test to simulate the integration of HA manager with OVSDB
server using replication.

Other documentation and API improvements.

Tested-by: Numan Siddique <nusiddiq@redhat.com>
Signed-off-by: Andy Zhou <azhou@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

ovsdb/replication.c
@@ -37,7 +37,7 @@
 
 VLOG_DEFINE_THIS_MODULE(replication);
 
-static char *active_ovsdb_server;
+static char *sync_from;
 static struct jsonrpc_session *session = NULL;
 static unsigned int session_seqno = UINT_MAX;
 
@@ -87,6 +87,7 @@ static void request_ids_destroy(void);
 void request_ids_clear(void);
 
 enum ovsdb_replication_state {
+    RPL_S_INIT,
     RPL_S_DB_REQUESTED,
     RPL_S_SCHEMA_REQUESTED,
     RPL_S_MONITOR_REQUESTED,
@@ -108,8 +109,15 @@ static struct ovsdb* find_db(const char *db_name);
 
 
 void
-replication_init(void)
+replication_init(const char *sync_from_, const char *exclude_tables)
 {
+    free(sync_from);
+    sync_from = xstrdup(sync_from_);
+    char *err = set_blacklist_tables(exclude_tables, false);
+    /* Caller should have verified that the 'exclude_tables' is
+     * parseable. An error here is unexpected. */
+    ovs_assert(!err);
+
     shash_destroy(replication_dbs);
     replication_dbs = NULL;
 
@@ -118,8 +126,9 @@ replication_init(void)
         jsonrpc_session_close(session);
     }
 
-    session = jsonrpc_session_open(active_ovsdb_server, true);
+    session = jsonrpc_session_open(sync_from, true);
     session_seqno = UINT_MAX;
+    state = RPL_S_INIT;
 }
 
 void
@@ -143,7 +152,7 @@ replication_run(void)
         unsigned int seqno;
 
         seqno = jsonrpc_session_get_seqno(session);
-        if (seqno != session_seqno) {
+        if (seqno != session_seqno || state == RPL_S_INIT) {
             session_seqno = seqno;
             request_ids_clear();
             struct jsonrpc_msg *request;
@@ -156,6 +165,7 @@ replication_run(void)
             replication_dbs = replication_db_clone(&local_dbs);
 
             state = RPL_S_DB_REQUESTED;
+            VLOG_DBG("Send list_dbs request");
         }
 
         msg = jsonrpc_session_recv(session);
@@ -216,6 +226,7 @@ replication_run(void)
                             }
                         }
                     }
+                    VLOG_DBG("Send schema requests");
                     state = RPL_S_SCHEMA_REQUESTED;
                 }
                 break;
@@ -271,6 +282,7 @@ replication_run(void)
 
                             request_ids_add(request->id, db);
                             jsonrpc_session_send(session, request);
+                            VLOG_DBG("Send monitor requests");
                             state = RPL_S_MONITOR_REQUESTED;
                         }
                     }
@@ -289,6 +301,7 @@ replication_run(void)
                     /* Transition to replicating state after receiving
                      * all replies of "monitor" requests. */
                     if (hmap_is_empty(&request_ids)) {
+                        VLOG_DBG("Listening to monitor updates");
                         state = RPL_S_REPLICATING;
                     }
                 }
@@ -299,6 +312,7 @@ replication_run(void)
                 /* Ignore all messages */
                 break;
 
+            case RPL_S_INIT:
             case RPL_S_REPLICATING:
             default:
                 OVS_NOT_REACHED();
@@ -316,18 +330,6 @@ replication_wait(void)
         jsonrpc_session_wait(session);
         jsonrpc_session_recv_wait(session);
     }
-}
-
-void
-set_active_ovsdb_server(const char *active_server)
-{
-    active_ovsdb_server = nullable_xstrdup(active_server);
-}
-
-const char *
-get_active_ovsdb_server(void)
-{
-    return active_ovsdb_server;
 }
 
 /* Parse 'blacklist' to rebuild 'blacklist_tables'.  If 'dryrun' is false, the
@@ -457,9 +459,9 @@ replication_destroy(void)
     blacklist_tables_clear();
     shash_destroy(&blacklist_tables);
 
-    if (active_ovsdb_server) {
-        free(active_ovsdb_server);
-        active_ovsdb_server = NULL;
+    if (sync_from) {
+        free(sync_from);
+        sync_from = NULL;
     }
 
     request_ids_destroy();
@@ -855,12 +857,58 @@ replication_get_last_error(void)
     return err;
 }
 
+char *
+replication_status(void)
+{
+    bool alive = session && jsonrpc_session_is_alive(session);
+    struct ds ds = DS_EMPTY_INITIALIZER;
+
+    if (alive) {
+        switch(state) {
+        case RPL_S_INIT:
+        case RPL_S_DB_REQUESTED:
+        case RPL_S_SCHEMA_REQUESTED:
+        case RPL_S_MONITOR_REQUESTED:
+            ds_put_format(&ds, "connecting: %s", sync_from);
+            break;
+        case RPL_S_REPLICATING: {
+            struct shash_node *node;
+
+            ds_put_format(&ds, "replicating: %s\n", sync_from);
+            ds_put_cstr(&ds, "database:");
+            SHASH_FOR_EACH (node, replication_dbs) {
+                ds_put_format(&ds, " %s,", node->name);
+            }
+            ds_chomp(&ds, ',');
+
+            if (!shash_is_empty(&blacklist_tables)) {
+                ds_put_char(&ds, '\n');
+                ds_put_cstr(&ds, "exclude: ");
+                ds_put_and_free_cstr(&ds, get_blacklist_tables());
+            }
+            break;
+        }
+        case RPL_S_ERR:
+            ds_put_format(&ds, "Replication to (%s) failed\n", sync_from);
+            break;
+        default:
+            OVS_NOT_REACHED();
+            break;
+        }
+    } else {
+        ds_put_format(&ds, "not connected to %s", sync_from);
+    }
+    return ds_steal_cstr(&ds);
+}
+
 void
 replication_usage(void)
 {
     printf("\n\
 Syncing options:\n\
-  --sync-from=SERVER      sync DATABASE from active SERVER\n\
+  --sync-from=SERVER      sync DATABASE from active SERVER and start in\n\
+                          backup mode (except with --active)\n\
   --sync-exclude-tables=DB:TABLE,...\n\
-                          exclude the TABLE in DB from syncing\n");
+                          exclude the TABLE in DB from syncing\n\
+  --active                with --sync-from, start in active mode\n");
 }

ovsdb/jsonrpc-server.c
@@ -343,6 +343,12 @@ ovsdb_jsonrpc_server_reconnect(struct ovsdb_jsonrpc_server *svr, bool read_only)
     }
 }
 
+bool
+ovsdb_jsonrpc_server_is_read_only(struct ovsdb_jsonrpc_server *svr)
+{
+    return svr->read_only;
+}
+
 void
 ovsdb_jsonrpc_server_run(struct ovsdb_jsonrpc_server *svr)
 {

ovsdb/jsonrpc-server.h
@@ -65,6 +65,7 @@ void ovsdb_jsonrpc_server_reconnect(struct ovsdb_jsonrpc_server *, bool read_onl
 
 void ovsdb_jsonrpc_server_run(struct ovsdb_jsonrpc_server *);
 void ovsdb_jsonrpc_server_wait(struct ovsdb_jsonrpc_server *);
+bool ovsdb_jsonrpc_server_is_read_only(struct ovsdb_jsonrpc_server *);
 
 void ovsdb_jsonrpc_server_get_memory_usage(const struct ovsdb_jsonrpc_server *,
                                            struct simap *usage);

ovsdb/ovsdb-server.1.in
@@ -36,7 +36,7 @@ If none is specified, the default is \fB@DBDIR@/conf.db\fR.  The database
 files must already have been created and initialized using, for
 example, \fBovsdb\-tool create\fR.
 .
-.SH "ACTIVE and BACKUP "
+.SH "ACTIVE and BACKUP"
 \fBovsdb\-server\fR runs either as a backup server, or as an active server.
 When  \fBovsdb\-server\fR is running as a backup server, all transactions that
 can modify the database content, including the lock commands are rejected.
@@ -45,12 +45,12 @@ When \fBovsdb\-server\fR role changes, all existing client connection are
 reset, requiring clients to reconnect to the server.
 .PP
 By default, \fBovsdb\-server\fR runs as an active server, except when the
-\fB\-\-sync\-from=\fIserver\fR command line option is specified.  During
-runtime, \fBovsdb\-server\fR role can be switch by using appctl commands.
+\fB\-\-sync\-from=\fIserver\fR command line option is specified and without
+the \fB\-\-no\-sync option\fR.  During runtime, \fBovsdb\-server\fR role can be switch by using appctl commands.
 .PP
 \fBovsdb-server/connect\-active\-ovsdb\-server\fR switches
-\fBovsdb\-server\fR role into a backup server, Conversely,
-\fBovsdb-server/disconnect\-active\-ovsdb\-server\fR changes server into
+\fBovsdb\-server\fR into a backup server, Conversely,
+\fBovsdb-server/disconnect\-active\-ovsdb\-server\fR switches server into
 an active one.
 .
 .SH OPTIONS
@@ -220,11 +220,21 @@ specified by \fBovsdb\-server/set\-active\-ovsdb\-server\fR.
 .IP "\fBovsdb\-server/disconnect\-active\-ovsdb\-server"
 Causes \fBovsdb\-server\fR to  stop  synchronizing  its  databases with a active server.
 .
-.IP "\fBovsdb\-server/set\-sync\-excluded\-tables \fIdb\fB:\fItable\fR[\fB,\fIdb\fB:\fItable\fR]..."
+.IP "\fBovsdb\-server/set\-sync\-exclude\-tables \fIdb\fB:\fItable\fR[\fB,\fIdb\fB:\fItable\fR]..."
 Sets the \fItable\fR whitin \fIdb\fR that will be excluded from synchronization.
 .
-.IP "\fBovsdb\-server/get\-sync\-excluded\-tables"
+.IP "\fBovsdb\-server/get\-sync\-exclude\-tables"
 Gets  the  tables  that are currently excluded from synchronization.
+.
+.IP "\fBovsdb\-server/sync\-status"
+Prints a summary of replication run time information. The \fBstate\fR
+information is always provided, indicating whether the server is running
+in the \fIactive\fR or the \fIbackup\fR mode.
+When running in backup mode, replication connection status, which
+can be either \fIconnecting\fR, \fIreplicating\fR or \fIerror\fR, are shown.
+When the connection is in \fIreplicating\fR state, further output shows
+the list of databases currently replicating, and the tables that are
+excluded.
 .
 .so lib/vlog-unixctl.man
 .so lib/memory-unixctl.man

ovsdb/replication.h
@@ -21,7 +21,30 @@
 #include <stdbool.h>
 struct ovsdb;
 
-void replication_init(void);
+/* Replication module runs when OVSDB server runs in the backup mode.
+ *
+ * API Usage
+ *===========
+ *
+ * - replication_init() needs to be called whenever OVSDB server switches into
+ *   the backup mode.
+ *
+ * - replication_add_local_db() should be called immediately after to add all
+ *   known database that OVSDB server owns, one at a time.
+ *
+ * - replication_destroy() should be called when OVSDB server shutdown to
+ *   reclaim resources.
+ *
+ * - replication_run(), replication_wait(), replication_is_alive() and
+ *   replication_get_last_error() should be call within the main loop
+ *   whenever OVSDB server runs in the backup mode.
+ *
+ *  - set_blacklist_tables(), get_blacklist_tables(),
+ *    disconnect_active_server() and replication_usage() are support functions
+ *    used mainly by uinxctl commands.
+ */
+
+void replication_init(const char *sync_from, const char *exclude_tables);
 void replication_run(void);
 void replication_wait(void);
 void replication_destroy(void);
@@ -29,9 +52,8 @@ void replication_usage(void);
 void replication_add_local_db(const char *databse, struct ovsdb *db);
 bool replication_is_alive(void);
 int replication_get_last_error(void);
+char *replication_status(void);
 
-void set_active_ovsdb_server(const char *remote_server);
-const char *get_active_ovsdb_server(void);
 char *set_blacklist_tables(const char *blacklist, bool dryrun)
     OVS_WARN_UNUSED_RESULT;
 char *get_blacklist_tables(void) OVS_WARN_UNUSED_RESULT;

ovsdb/replication.man
@@ -2,7 +2,22 @@ The following options allow \fBovsdb\-server\fR to synchronize  its  databases
 with another running \fBovsdb\-server\fR.
 .TP
 \fB\-\-sync\-from=\fIserver\fR
-Causes  \fBovsdb\-server\fR to synchronize its databases with the databases in
-\fIserver\fR.  Every transaction committed by \fIserver\fR  will  be  replicated
-to \fBovsdb\-server\fR. \fIserver\fR is an active connection method in one of
-the forms documented in \fBovsdb\-client(1).
+Sets up \fBovsdb\-server\fR to synchronize its databases with the
+databases in \fIserver\fR, which must be an active connection method
+in one of the forms documented in \fBovsdb\-client\fR(1).  Every
+transaction committed by \fIserver\fR will be replicated to
+\fBovsdb\-server\fR.  This option makes \fBovsdb\-server\fR start
+as a backup server; add \fB\-\-active\fR to make it start as an
+active server.
+.TP
+\fB\-\-sync\-exclude-tables=\fIdb:table[,db:table]...\fR
+Causes the specified tables to be excluded from replication.
+.TP
+\fB\-\-active\fR
+By default, \fB\-\-sync\-from\fR makes \fBovsdb\-server\fR start up as
+a backup for \fIserver\fR.  With \fB\-\-active\fR, however,
+\fBovsdb\-server\fR starts as an active server.  Use this option to
+allow the syncing options to be specified using command line options,
+yet start the server, as the default, active server.  To switch the
+running server to backup mode, use \fBovs-appctl(1)\fR to execute the
+\fBovsdb\-server/connect\-active\-ovsdb\-server\fR command.

commit 1cb0f53cc00db18d7403a479d226ed8c88f14d76

replication: Be more careful about JSON parsing and simplify code.
The code here wasn't careful about parsing JSON received from the remote
OVSDB server.  It assumed, for example, that a row that the remote server
implied was new was actually new, without looking to see whether there was
already a row with that UUID.  This commit improves this validation.  It
also rewrites code that translated updates locally into calls into the
query engine, via JSON, into simple lookups by UUID.

For me, this fixes a test failure in test 1866
(ovsdb-server/active-backup-role-switching), which caused the following
valgrind report:

==18725== Process terminating with default action of signal 11 (SIGSEGV): dumping core
==18725==  Access not within mapped region at address 0x0
==18725==    at 0x43937E: ovsdb_datum_compare_3way (ovsdb-data.c:1626)
==18725==    by 0x439344: ovsdb_datum_equals (ovsdb-data.c:1616)
==18725==    by 0x4166CC: update_monitor_row_data (monitor.c:310)
==18725==    by 0x414A90: ovsdb_monitor_changes_update (monitor.c:1255)
==18725==    by 0x417009: ovsdb_monitor_change_cb (monitor.c:1339)
==18725==    by 0x41DB52: ovsdb_txn_for_each_change (transaction.c:906)
==18725==    by 0x416CC9: ovsdb_monitor_commit (monitor.c:1553)
==18725==    by 0x41D993: ovsdb_txn_commit_ (transaction.c:868)
==18725==    by 0x41D6F5: ovsdb_txn_commit (transaction.c:893)
==18725==    by 0x418185: process_notification (replication.c:576)
==18725==    by 0x417705: replication_run (replication.c:185)
==18725==    by 0x408240: main_loop (ovsdb-server.c:198)
==18725==    by 0x406432: main (ovsdb-server.c:429)

I don't know the exact cause of the problem, but this new implementation
leaves me more confident due to its simplicity.

Reported-by: Joe Stringer <joe@ovn.org>
Reported-at: http://openvswitch.org/pipermail/dev/2016-September/079315.html
Fixes: 60e0cd041958 ("ovsdb: Replication usability improvements")
Signed-off-by: Ben Pfaff <blp@ovn.org>
Tested-by: Joe Stringer <joe@ovn.org>

ovsdb/replication.c
@@ -54,14 +54,14 @@ static struct ovsdb_error *process_table_update(struct json *table_update,
                                                 struct ovsdb_txn *txn);
 
 static struct ovsdb_error *execute_insert(struct ovsdb_txn *txn,
-                                          const char *uuid,
+                                          const struct uuid *row_uuid,
                                           struct ovsdb_table *table,
                                           struct json *new);
 static struct ovsdb_error *execute_delete(struct ovsdb_txn *txn,
-                                          const char *uuid,
+                                          const struct uuid *row_uuid,
                                           struct ovsdb_table *table);
 static struct ovsdb_error *execute_update(struct ovsdb_txn *txn,
-                                          const char *uuid,
+                                          const struct uuid *row_uuid,
                                           struct ovsdb_table *table,
                                           struct json *new);
 
@@ -583,174 +583,104 @@ static struct ovsdb_error *
 process_table_update(struct json *table_update, const char *table_name,
                      struct ovsdb *database, struct ovsdb_txn *txn)
 {
-    struct shash_node *node;
-    struct ovsdb_table *table;
-    struct ovsdb_error *error;
+    struct ovsdb_table *table = ovsdb_get_table(database, table_name);
+    if (!table) {
+        return ovsdb_error("unknown table", "unknown table %s", table_name);
+    }
 
     if (table_update->type != JSON_OBJECT) {
         return ovsdb_error("Not a JSON object",
                            "<table-update> for table is not object");
     }
 
-    table = ovsdb_get_table(database, table_name);
-    error = NULL;
-
+    struct shash_node *node;
     SHASH_FOR_EACH (node, json_object(table_update)) {
         struct json *row_update = node->data;
         struct json *old, *new;
 
         if (row_update->type != JSON_OBJECT) {
-            error = ovsdb_error("NOt a JSON object",
-                                "<row-update> is not object");
-            break;
+            return ovsdb_error("Not a JSON object",
+                               "<row-update> is not object");
         }
+
+        struct uuid uuid;
+        if (!uuid_from_string(&uuid, node->name)) {
+            return ovsdb_syntax_error(table_update, "bad row UUID",
+                                      "<table-update> names must be UUIDs");
+        }
+
         old = shash_find_data(json_object(row_update), "old");
         new = shash_find_data(json_object(row_update), "new");
 
-        if (!old) {
-            error = execute_insert(txn, node->name, table, new);
-        } else{
-            if (!new) {
-                error = execute_delete(txn, node->name, table);
-            } else {
-                error = execute_update(txn, node->name, table, new);
-            }
-        }
+        struct ovsdb_error *error;
+        error = (!new ? execute_delete(txn, &uuid, table)
+                 : !old ? execute_insert(txn, &uuid, table, new)
+                 : execute_update(txn, &uuid, table, new));
         if (error) {
-            break;
+            return error;
         }
     }
-    return error;
+    return NULL;
 }
 
 static struct ovsdb_error *
-execute_insert(struct ovsdb_txn *txn, const char *uuid,
+execute_insert(struct ovsdb_txn *txn, const struct uuid *row_uuid,
                struct ovsdb_table *table, struct json *json_row)
 {
-    struct ovsdb_row *row = NULL;
-    struct uuid row_uuid;
-    struct ovsdb_error *error;
-
-    row = ovsdb_row_create(table);
-    error = ovsdb_row_from_json(row, json_row, NULL, NULL);
+    struct ovsdb_row *row = ovsdb_row_create(table);
+    struct ovsdb_error *error = ovsdb_row_from_json(row, json_row, NULL, NULL);
     if (!error) {
-        /* Add UUID to row. */
-        uuid_from_string(&row_uuid, uuid);
-        *ovsdb_row_get_uuid_rw(row) = row_uuid;
+        *ovsdb_row_get_uuid_rw(row) = *row_uuid;
         ovsdb_txn_row_insert(txn, row);
     } else {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+        VLOG_WARN_RL(&rl, "cannot add existing row "UUID_FMT" to table %s",
+                     UUID_ARGS(row_uuid), table->schema->name);
         ovsdb_row_destroy(row);
     }
 
     return error;
 }
 
-struct delete_row_cbdata {
-    size_t n_matches;
-    const struct ovsdb_table *table;
-    struct ovsdb_txn *txn;
-};
-
-static bool
-delete_row_cb(const struct ovsdb_row *row, void *dr_)
-{
-    struct delete_row_cbdata *dr = dr_;
-
-    dr->n_matches++;
-    ovsdb_txn_row_delete(dr->txn, row);
-
-    return true;
-}
-
 static struct ovsdb_error *
-execute_delete(struct ovsdb_txn *txn, const char *uuid,
+execute_delete(struct ovsdb_txn *txn, const struct uuid *row_uuid,
                struct ovsdb_table *table)
 {
-    const struct json *where;
-    struct ovsdb_error *error;
-    struct ovsdb_condition condition = OVSDB_CONDITION_INITIALIZER(&condition);
-    char where_string[UUID_LEN+29];
-
-    if (!table) {
-        return OVSDB_BUG("null table");
+    const struct ovsdb_row *row = ovsdb_table_get_row(table, row_uuid);
+    if (row) {
+        ovsdb_txn_row_delete(txn, row);
+    } else {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+        VLOG_WARN_RL(&rl, "cannot delete missing row "UUID_FMT" from table %s",
+                     UUID_ARGS(row_uuid), table->schema->name);
     }
-
-    snprintf(where_string, sizeof where_string, "%s%s%s",
-             "[[\"_uuid\",\"==\",[\"uuid\",\"",uuid,"\"]]]");
-
-    where = json_from_string(where_string);
-    error = ovsdb_condition_from_json(table->schema, where, NULL, &condition);
-    if (!error) {
-        struct delete_row_cbdata dr;
-
-        dr.n_matches = 0;
-        dr.table = table;
-        dr.txn = txn;
-        ovsdb_query(table, &condition, delete_row_cb, &dr);
-    }
-
-    ovsdb_condition_destroy(&condition);
-    json_destroy(CONST_CAST(struct json *, where));
-
-    return error;
-}
-
-struct update_row_cbdata {
-    size_t n_matches;
-    struct ovsdb_txn *txn;
-    const struct ovsdb_row *row;
-    const struct ovsdb_column_set *columns;
-};
-
-static bool
-update_row_cb(const struct ovsdb_row *row, void *ur_)
-{
-    struct update_row_cbdata *ur = ur_;
-
-    ur->n_matches++;
-    if (!ovsdb_row_equal_columns(row, ur->row, ur->columns)) {
-        ovsdb_row_update_columns(ovsdb_txn_row_modify(ur->txn, row),
-                                 ur->row, ur->columns);
-    }
-
-    return true;
+    return NULL;
 }
 
 static struct ovsdb_error *
-execute_update(struct ovsdb_txn *txn, const char *uuid,
+execute_update(struct ovsdb_txn *txn, const struct uuid *row_uuid,
                struct ovsdb_table *table, struct json *json_row)
 {
+    const struct ovsdb_row *row = ovsdb_table_get_row(table, row_uuid);
+    if (!row) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+        VLOG_WARN_RL(&rl, "cannot modify missing row "UUID_FMT" in table %s",
+                     UUID_ARGS(row_uuid), table->schema->name);
+        return NULL;
+    }
+
     struct ovsdb_column_set columns = OVSDB_COLUMN_SET_INITIALIZER;
-    struct ovsdb_condition condition = OVSDB_CONDITION_INITIALIZER(&condition);
-    struct update_row_cbdata ur;
-    struct ovsdb_row *row;
-    struct ovsdb_error *error;
-    const struct json *where;
-    char where_string[UUID_LEN+29];
+    struct ovsdb_row *update = ovsdb_row_create(table);
+    struct ovsdb_error *error = ovsdb_row_from_json(update, json_row,
+                                                    NULL, &columns);
 
-    snprintf(where_string, sizeof where_string, "%s%s%s",
-             "[[\"_uuid\",\"==\",[\"uuid\",\"",uuid,"\"]]]");
-    where = json_from_string(where_string);
-
-    row = ovsdb_row_create(table);
-    error = ovsdb_row_from_json(row, json_row, NULL, &columns);
-    if (!error) {
-        error = ovsdb_condition_from_json(table->schema, where, NULL,
-                                          &condition);
-    }
-    if (!error) {
-        ur.n_matches = 0;
-        ur.txn = txn;
-        ur.row = row;
-        ur.columns = &columns;
-        ovsdb_query(table, &condition, update_row_cb, &ur);
+    if (!error && !ovsdb_row_equal_columns(row, update, &columns)) {
+        ovsdb_row_update_columns(ovsdb_txn_row_modify(txn, row),
+                                 update, &columns);
     }
 
-    ovsdb_row_destroy(row);
     ovsdb_column_set_destroy(&columns);
-    ovsdb_condition_destroy(&condition);
-    json_destroy(CONST_CAST(struct json *, where));
-
+    ovsdb_row_destroy(update);
     return error;
 }
 