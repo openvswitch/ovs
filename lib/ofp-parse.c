openvswitch/ovs

commit 6dd3c787f5914ea27b239b6852d8fc0815033128

ofproto: Support packet_outs in bundles.
Add support for OFPT_PACKET_OUT messages in bundles.

While ovs-ofctl already has a packet-out command, we did not have a
string parser for it, as the parsing was done directly from command
line arguments.

This patch adds the string parser for packet-out messages, adds
support for it into the 'ovs-ofctl packet-out' command, and adds a new
ofctl/packet-out ovs-appctl command that can be used when ovs-ofctl is
used as a flow monitor.  The old 'ovs-ofctl packet-out syntax is
deprecated' and will be removed in a later OVS release.

The new packet-out parser is further supported with the ovs-ofctl
bundle command, which allows bundles to mix flow mods, group mods and
packet-out messages.  Also the packet-outs in bundles are only
executed if the whole bundle is successful.  A failing packet-out
translation may also make the whole bundle to fail.

Signed-off-by: Jarno Rajahalme <jarno@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

lib/ofp-parse.c
@@ -22,6 +22,7 @@
 #include <netinet/in.h>
 
 #include "byte-order.h"
+#include "dp-packet.h"
 #include "learn.h"
 #include "multipath.h"
 #include "netdev.h"
@@ -544,6 +545,107 @@ parse_ofp_str(struct ofputil_flow_mod *fm, int command, const char *str_,
     if (error) {
         fm->ofpacts = NULL;
         fm->ofpacts_len = 0;
+    }
+
+    free(string);
+    return error;
+}
+
+/* Parse a string representation of a OFPT_PACKET_OUT to '*po'.  If successful,
+ * both 'po->ofpacts' and 'po->packet' must be free()d by the caller. */
+static char * OVS_WARN_UNUSED_RESULT
+parse_ofp_packet_out_str__(struct ofputil_packet_out *po, char *string,
+                           enum ofputil_protocol *usable_protocols)
+{
+    enum ofputil_protocol action_usable_protocols;
+    uint64_t stub[256 / 8];
+    struct ofpbuf ofpacts = OFPBUF_STUB_INITIALIZER(stub);
+    struct dp_packet *packet = NULL;
+    char *act_str = NULL;
+    char *name, *value;
+    char *error = NULL;
+
+    *usable_protocols = OFPUTIL_P_ANY;
+
+    *po = (struct ofputil_packet_out) {
+        .buffer_id = UINT32_MAX,
+        .in_port = OFPP_CONTROLLER,
+    };
+
+    act_str = extract_actions(string);
+
+    while (ofputil_parse_key_value(&string, &name, &value)) {
+        if (!*value) {
+            error = xasprintf("field %s missing value", name);
+            goto out;
+        }
+
+        if (!strcmp(name, "in_port")) {
+            if (!ofputil_port_from_string(value, &po->in_port)) {
+                error = xasprintf("%s is not a valid OpenFlow port", value);
+                goto out;
+            }
+            if (po->in_port > OFPP_MAX && po->in_port != OFPP_LOCAL
+                && po->in_port != OFPP_NONE
+                && po->in_port != OFPP_CONTROLLER) {
+                error = xasprintf(
+                              "%s is not a valid OpenFlow port for PACKET_OUT",
+                              value);
+                goto out;
+            }
+        } else if (!strcmp(name, "packet")) {
+            const char *error_msg = eth_from_hex(value, &packet);
+            if (error_msg) {
+                error = xasprintf("%s: %s", name, error_msg);
+                goto out;
+            }
+        } else {
+            error = xasprintf("unknown keyword %s", name);
+            goto out;
+        }
+    }
+
+    if (!packet || !dp_packet_size(packet)) {
+        error = xstrdup("must specify packet");
+        goto out;
+    }
+
+    if (act_str) {
+        error = ofpacts_parse_actions(act_str, &ofpacts,
+                                      &action_usable_protocols);
+        *usable_protocols &= action_usable_protocols;
+        if (error) {
+            goto out;
+        }
+    }
+    po->ofpacts_len = ofpacts.size;
+    po->ofpacts = ofpbuf_steal_data(&ofpacts);
+
+    po->packet_len = dp_packet_size(packet);
+    po->packet = dp_packet_steal_data(packet);
+out:
+    ofpbuf_uninit(&ofpacts);
+    dp_packet_delete(packet);
+    return error;
+}
+
+/* Convert 'str_' (as described in the Packet-Out Syntax section of the
+ * ovs-ofctl man page) into 'po' for sending a OFPT_PACKET_OUT message to a
+ * switch.  Returns the set of usable protocols in '*usable_protocols'.
+ *
+ * Returns NULL if successful, otherwise a malloc()'d string describing the
+ * error.  The caller is responsible for freeing the returned string. */
+char * OVS_WARN_UNUSED_RESULT
+parse_ofp_packet_out_str(struct ofputil_packet_out *po, const char *str_,
+                         enum ofputil_protocol *usable_protocols)
+{
+    char *string = xstrdup(str_);
+    char *error;
+
+    error = parse_ofp_packet_out_str__(po, string, usable_protocols);
+    if (error) {
+        po->ofpacts = NULL;
+        po->ofpacts_len = 0;
     }
 
     free(string);
@@ -1709,26 +1811,6 @@ parse_ofp_group_mod_file(const char *file_name, int command,
     return NULL;
 }
 
-static void
-free_bundle_msgs(struct ofputil_bundle_msg **bms, size_t *n_bms)
-{
-    for (size_t i = 0; i < *n_bms; i++) {
-        switch ((int)(*bms)[i].type) {
-        case OFPTYPE_FLOW_MOD:
-            free(CONST_CAST(struct ofpact *, (*bms)[i].fm.ofpacts));
-            break;
-        case OFPTYPE_GROUP_MOD:
-            ofputil_uninit_group_mod(&(*bms)[i].gm);
-            break;
-        default:
-            break;
-        }
-    }
-    free(*bms);
-    *bms = NULL;
-    *n_bms = 0;
-}
-
 /* Opens file 'file_name' and reads each line as a flow_mod or a group_mod,
  * depending on the first keyword on each line.  Stores each flow and group
  * mods in '*bms', an array allocated on the caller's behalf, and the number of
@@ -1797,6 +1879,13 @@ parse_ofp_bundle_file(const char *file_name,
                 break;
             }
             (*bms)[*n_bms].type = OFPTYPE_GROUP_MOD;
+        } else if (!strncmp(s, "packet-out", len)) {
+            s += len;
+            error = parse_ofp_packet_out_str(&(*bms)[*n_bms].po, s, &usable);
+            if (error) {
+                break;
+            }
+            (*bms)[*n_bms].type = OFPTYPE_PACKET_OUT;
         } else {
             error = xasprintf("Unsupported bundle message type: %.*s",
                               (int)len, s);
@@ -1816,7 +1905,9 @@ parse_ofp_bundle_file(const char *file_name,
         char *err_msg = xasprintf("%s:%d: %s", file_name, line_number, error);
         free(error);
 
-        free_bundle_msgs(bms, n_bms);
+        ofputil_free_bundle_msgs(*bms, *n_bms);
+        *bms = NULL;
+        *n_bms = 0;
         return err_msg;
     }
     return NULL;

include/openvswitch/ofp-parse.h
@@ -28,6 +28,7 @@
 struct flow;
 struct ofpbuf;
 struct ofputil_flow_mod;
+struct ofputil_packet_out;
 struct ofputil_flow_monitor_request;
 struct ofputil_flow_stats_request;
 struct ofputil_group_mod;
@@ -45,6 +46,10 @@ char *parse_ofp_str(struct ofputil_flow_mod *, int command, const char *str_,
 char *parse_ofp_flow_mod_str(struct ofputil_flow_mod *, const char *string,
                              int command,
                              enum ofputil_protocol *usable_protocols)
+    OVS_WARN_UNUSED_RESULT;
+
+char *parse_ofp_packet_out_str(struct ofputil_packet_out *po, const char *str_,
+                               enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_table_mod(struct ofputil_table_mod *,

include/openvswitch/ofp-util.h
@@ -1338,18 +1338,20 @@ enum ofperr ofputil_decode_bundle_add(const struct ofp_header *,
                                       struct ofputil_bundle_add_msg *,
                                       enum ofptype *type);
 
+/* Bundle message as produced by ofp-parse. */
 struct ofputil_bundle_msg {
     enum ofptype type;
     union {
         struct ofputil_flow_mod fm;
         struct ofputil_group_mod gm;
+        struct ofputil_packet_out po;
     };
 };
 
-/* Destroys 'bms'. */
-void ofputil_encode_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms,
-                                struct ovs_list *requests,
+void ofputil_encode_bundle_msgs(const struct ofputil_bundle_msg *bms,
+                                size_t n_bms, struct ovs_list *requests,
                                 enum ofputil_protocol);
+void ofputil_free_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms);
 
 struct ofputil_tlv_map {
     struct ovs_list list_node;

lib/ofp-util.c
@@ -4131,6 +4131,9 @@ ofputil_packet_in_private_destroy(struct ofputil_packet_in_private *pin)
  * message's actions.  The caller must initialize 'ofpacts' and retains
  * ownership of it.  'po->ofpacts' will point into the 'ofpacts' buffer.
  *
+ * 'po->packet' refers to the packet data in 'oh', so the buffer containing
+ * 'oh' must not be destroyed while 'po' is being used.
+ *
  * Returns 0 if successful, otherwise an OFPERR_* value. */
 enum ofperr
 ofputil_decode_packet_out(struct ofputil_packet_out *po,
@@ -9462,8 +9465,30 @@ ofputil_decode_group_mod(const struct ofp_header *oh,
 
 /* Destroys 'bms'. */
 void
-ofputil_encode_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms,
-                           struct ovs_list *requests,
+ofputil_free_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms)
+{
+    for (size_t i = 0; i < n_bms; i++) {
+        switch ((int)bms[i].type) {
+        case OFPTYPE_FLOW_MOD:
+            free(CONST_CAST(struct ofpact *, bms[i].fm.ofpacts));
+            break;
+        case OFPTYPE_GROUP_MOD:
+            ofputil_uninit_group_mod(&bms[i].gm);
+            break;
+        case OFPTYPE_PACKET_OUT:
+            free(bms[i].po.ofpacts);
+            free(CONST_CAST(void *, bms[i].po.packet));
+            break;
+        default:
+            break;
+        }
+    }
+    free(bms);
+}
+
+void
+ofputil_encode_bundle_msgs(const struct ofputil_bundle_msg *bms,
+                           size_t n_bms, struct ovs_list *requests,
                            enum ofputil_protocol protocol)
 {
     enum ofp_version version = ofputil_protocol_to_ofp_version(protocol);
@@ -9474,11 +9499,12 @@ ofputil_encode_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms,
         switch ((int)bms[i].type) {
         case OFPTYPE_FLOW_MOD:
             request = ofputil_encode_flow_mod(&bms[i].fm, protocol);
-            free(CONST_CAST(struct ofpact *, bms[i].fm.ofpacts));
             break;
         case OFPTYPE_GROUP_MOD:
             request = ofputil_encode_group_mod(version, &bms[i].gm);
-            ofputil_uninit_group_mod(&bms[i].gm);
+            break;
+        case OFPTYPE_PACKET_OUT:
+            request = ofputil_encode_packet_out(&bms[i].po, protocol);
             break;
         default:
             break;
@@ -9487,7 +9513,6 @@ ofputil_encode_bundle_msgs(struct ofputil_bundle_msg *bms, size_t n_bms,
             ovs_list_push_back(requests, &request->list_node);
         }
     }
-    free(bms);
 }
 
 /* Parse a queue status request message into 'oqsr'.
@@ -9875,13 +9900,13 @@ ofputil_is_bundlable(enum ofptype type)
     case OFPTYPE_FLOW_MOD:
         /* Other supported types. */
     case OFPTYPE_GROUP_MOD:
+    case OFPTYPE_PACKET_OUT:
         return true;
 
         /* Nice to have later. */
     case OFPTYPE_FLOW_MOD_TABLE_ID:
     case OFPTYPE_TABLE_MOD:
     case OFPTYPE_METER_MOD:
-    case OFPTYPE_PACKET_OUT:
     case OFPTYPE_NXT_TLV_TABLE_MOD:
 
         /* Not to be bundlable. */

ofproto/bundles.h
@@ -33,12 +33,13 @@ extern "C" {
 
 struct ofp_bundle_entry {
     struct ovs_list   node;
-    enum ofptype      type;  /* OFPTYPE_FLOW_MOD, OFPTYPE_PORT_MOD, or
-                              * OFPTYPE_GROUP_MOD. */
+    enum ofptype      type;  /* OFPTYPE_FLOW_MOD, OFPTYPE_PORT_MOD,
+                              * OFPTYPE_GROUP_MOD, OFPTYPE_PACKET_OUT. */
     union {
         struct ofproto_flow_mod ofm;
         struct ofproto_port_mod opm;
         struct ofproto_group_mod ogm;
+        struct ofproto_packet_out opo;
     };
 
     /* OpenFlow header and some of the message contents for error reporting. */
@@ -106,6 +107,8 @@ ofp_bundle_entry_free(struct ofp_bundle_entry *entry)
             ofproto_flow_mod_uninit(&entry->ofm);
         } else if (entry->type == OFPTYPE_GROUP_MOD) {
             ofputil_uninit_group_mod(&entry->ogm.gm);
+        } else if (entry->type == OFPTYPE_PACKET_OUT) {
+            ofproto_packet_out_uninit(&entry->opo);
         }
         free(entry);
     }

ofproto/ofproto-dpif.c
@@ -4370,6 +4370,28 @@ error_out:
     return error;
 }
 
+static void
+packet_xlate_revert(struct ofproto *ofproto OVS_UNUSED,
+                    struct ofproto_packet_out *opo)
+    OVS_REQUIRES(ofproto_mutex)
+{
+    struct ofproto_dpif_packet_out *aux = opo->aux;
+    ovs_assert(aux);
+
+    /* Revert the learned flows. */
+    struct xc_entry *entry;
+    struct ofpbuf entries = aux->xcache.entries;
+
+    XC_ENTRY_FOR_EACH (entry, &entries) {
+        if (entry->type == XC_LEARN && entry->learn.ofm->learn_adds_rule) {
+            ofproto_flow_mod_learn_revert(entry->learn.ofm);
+        }
+    }
+
+    ofproto_dpif_packet_out_delete(aux);
+    opo->aux = NULL;
+}
+
 /* Push stats and perform side effects of flow translation. */
 static void
 ofproto_dpif_xcache_execute(struct ofproto_dpif *ofproto,
@@ -5824,6 +5846,7 @@ const struct ofproto_class ofproto_dpif_class = {
     rule_dealloc,
     rule_get_stats,
     packet_xlate,
+    packet_xlate_revert,
     packet_execute,
     set_frag_handling,
     nxt_resume,

ofproto/ofproto-provider.h
@@ -1313,6 +1313,11 @@ struct ofproto_class {
     enum ofperr (*packet_xlate)(struct ofproto *,
                                 struct ofproto_packet_out *opo);
 
+    /* Free resources taken by a successful packet_xlate().  If multiple
+     * packet_xlate() calls have been made in sequence, the corresponding
+     * packet_xlate_revert() calls have to be made in reverse order. */
+    void (*packet_xlate_revert)(struct ofproto *, struct ofproto_packet_out *);
+
     /* Executes the datapath actions, translation side-effects, and stats as
      * produced by ->packet_xlate().  The caller retains ownership of 'opo'.
      */
@@ -1906,6 +1911,8 @@ enum ofperr ofproto_flow_mod_learn(struct ofproto_flow_mod *, bool keep_ref)
     OVS_EXCLUDED(ofproto_mutex);
 enum ofperr ofproto_flow_mod_learn_refresh(struct ofproto_flow_mod *ofm);
 enum ofperr ofproto_flow_mod_learn_start(struct ofproto_flow_mod *ofm)
+    OVS_REQUIRES(ofproto_mutex);
+void ofproto_flow_mod_learn_revert(struct ofproto_flow_mod *ofm)
     OVS_REQUIRES(ofproto_mutex);
 void ofproto_flow_mod_learn_finish(struct ofproto_flow_mod *ofm,
                                           struct ofproto *orig_ofproto)

ofproto/ofproto.c
@@ -259,6 +259,9 @@ static enum ofperr ofproto_flow_mod_init(struct ofproto *,
 static enum ofperr ofproto_flow_mod_start(struct ofproto *,
                                           struct ofproto_flow_mod *)
     OVS_REQUIRES(ofproto_mutex);
+static void ofproto_flow_mod_revert(struct ofproto *,
+                                    struct ofproto_flow_mod *)
+    OVS_REQUIRES(ofproto_mutex);
 static void ofproto_flow_mod_finish(struct ofproto *,
                                     struct ofproto_flow_mod *,
                                     const struct openflow_mod_requester *)
@@ -3465,6 +3468,14 @@ ofproto_packet_out_start(struct ofproto *ofproto,
 }
 
 static void
+ofproto_packet_out_revert(struct ofproto *ofproto,
+                          struct ofproto_packet_out *opo)
+    OVS_REQUIRES(ofproto_mutex)
+{
+    ofproto->ofproto_class->packet_xlate_revert(ofproto, opo);
+}
+
+static void
 ofproto_packet_out_finish(struct ofproto *ofproto,
                           struct ofproto_packet_out *opo)
     OVS_REQUIRES(ofproto_mutex)
@@ -4974,6 +4985,14 @@ ofproto_flow_mod_learn_start(struct ofproto_flow_mod *ofm)
     ofm->temp_rule = rule;
 
     return error;
+}
+
+void
+ofproto_flow_mod_learn_revert(struct ofproto_flow_mod *ofm)
+    OVS_REQUIRES(ofproto_mutex)
+{
+    struct rule *rule = rule_collection_rules(&ofm->new_rules)[0];
+    ofproto_flow_mod_revert(rule->ofproto, ofm);
 }
 
 void
@@ -7495,6 +7514,9 @@ do_bundle_commit(struct ofconn *ofconn, uint32_t id, uint16_t flags)
                      * effect. */
                     be->ogm.version = version;
                     error = ofproto_group_mod_start(ofproto, &be->ogm);
+                } else if (be->type == OFPTYPE_PACKET_OUT) {
+                    be->opo.version = version;
+                    error = ofproto_packet_out_start(ofproto, &be->opo);
                 } else {
                     OVS_NOT_REACHED();
                 }
@@ -7517,8 +7539,9 @@ do_bundle_commit(struct ofconn *ofconn, uint32_t id, uint16_t flags)
                     ofproto_flow_mod_revert(ofproto, &be->ofm);
                 } else if (be->type == OFPTYPE_GROUP_MOD) {
                     ofproto_group_mod_revert(ofproto, &be->ogm);
+                } else if (be->type == OFPTYPE_PACKET_OUT) {
+                    ofproto_packet_out_revert(ofproto, &be->opo);
                 }
-
                 /* Nothing needs to be reverted for a port mod. */
             }
         } else {
@@ -7533,30 +7556,30 @@ do_bundle_commit(struct ofconn *ofconn, uint32_t id, uint16_t flags)
                      * processing. */
                     port_mod_finish(ofconn, &be->opm.pm, be->opm.port);
                 } else {
+                    version =
+                        (be->type == OFPTYPE_FLOW_MOD) ? be->ofm.version :
+                        (be->type == OFPTYPE_GROUP_MOD) ? be->ogm.version :
+                        (be->type == OFPTYPE_PACKET_OUT) ? be->opo.version :
+                        version;
+
+                    /* Bump the lookup version to the one of the current
+                     * message.  This makes all the changes in the bundle at
+                     * this version visible to lookups at once. */
+                    if (ofproto->tables_version < version) {
+                        ofproto->tables_version = version;
+                        ofproto->ofproto_class->set_tables_version(
+                            ofproto, ofproto->tables_version);
+                    }
+
                     struct openflow_mod_requester req = { ofconn,
                                                           &be->ofp_msg };
-                    if (be->type == OFPTYPE_FLOW_MOD) {
-                        /* Bump the lookup version to the one of the current
-                         * message.  This makes all the changes in the bundle
-                         * at this version visible to lookups at once. */
-                        if (ofproto->tables_version < be->ofm.version) {
-                            ofproto->tables_version = be->ofm.version;
-                            ofproto->ofproto_class->set_tables_version(
-                                ofproto, ofproto->tables_version);
-                        }
 
+                    if (be->type == OFPTYPE_FLOW_MOD) {
                         ofproto_flow_mod_finish(ofproto, &be->ofm, &req);
                     } else if (be->type == OFPTYPE_GROUP_MOD) {
-                        /* Bump the lookup version to the one of the current
-                         * message.  This makes all the changes in the bundle
-                         * at this version visible to lookups at once. */
-                        if (ofproto->tables_version < be->ogm.version) {
-                            ofproto->tables_version = be->ogm.version;
-                            ofproto->ofproto_class->set_tables_version(
-                                ofproto, ofproto->tables_version);
-                        }
-
                         ofproto_group_mod_finish(ofproto, &be->ogm, &req);
+                    } else if (be->type == OFPTYPE_PACKET_OUT) {
+                        ofproto_packet_out_finish(ofproto, &be->opo);
                     }
                 }
             }
@@ -7646,14 +7669,15 @@ handle_bundle_add(struct ofconn *ofconn, const struct ofp_header *oh)
 
     bmsg = ofp_bundle_entry_alloc(type, badd.msg);
 
+    struct ofpbuf ofpacts;
+    uint64_t ofpacts_stub[1024 / 8];
+    ofpbuf_use_stub(&ofpacts, ofpacts_stub, sizeof ofpacts_stub);
+
     if (type == OFPTYPE_PORT_MOD) {
         error = ofputil_decode_port_mod(badd.msg, &bmsg->opm.pm, false);
     } else if (type == OFPTYPE_FLOW_MOD) {
         struct ofputil_flow_mod fm;
-        struct ofpbuf ofpacts;
-        uint64_t ofpacts_stub[1024 / 8];
 
-        ofpbuf_use_stub(&ofpacts, ofpacts_stub, sizeof ofpacts_stub);
         error = ofputil_decode_flow_mod(&fm, badd.msg,
                                         ofconn_get_protocol(ofconn),
                                         &ofpacts,
@@ -7662,12 +7686,24 @@ handle_bundle_add(struct ofconn *ofconn, const struct ofp_header *oh)
         if (!error) {
             error = ofproto_flow_mod_init(ofproto, &bmsg->ofm, &fm, NULL);
         }
-        ofpbuf_uninit(&ofpacts);
     } else if (type == OFPTYPE_GROUP_MOD) {
         error = ofputil_decode_group_mod(badd.msg, &bmsg->ogm.gm);
+    } else if (type == OFPTYPE_PACKET_OUT) {
+        struct ofputil_packet_out po;
+
+        COVERAGE_INC(ofproto_packet_out);
+
+        /* Decode message. */
+        error = ofputil_decode_packet_out(&po, badd.msg, &ofpacts);
+        if (!error) {
+            po.ofpacts = ofpbuf_steal_data(&ofpacts);   /* Move to heap. */
+            error = ofproto_packet_out_init(ofproto, ofconn, &bmsg->opo, &po);
+        }
     } else {
         OVS_NOT_REACHED();
     }
+
+    ofpbuf_uninit(&ofpacts);
 
     if (!error) {
         error = ofp_bundle_add_message(ofconn, badd.bundle_id, badd.flags,

commit 16cf531e2e35d62331378c52751bfb85f0e75e86

ofp-parse: Fix sparse warnings about comparing ofp_port_ts.
Without this, sparse complains:

    lib/ofp-parse.c:588:19: warning: restricted ofp_port_t degrades to integer
    lib/ofp-parse.c:588:31: warning: restricted ofp_port_t degrades to integer

This is one of the irritating bits of using sparse, but on the whole I
think it saves us pretty often.

CC: Jarno Rajahalme <jarno@ovn.org>
Fixes: 6dd3c787f591 ("ofproto: Support packet_outs in bundles.")
Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Joe Stringer <joe@ovn.org>

lib/ofp-parse.c
@@ -585,7 +585,8 @@ parse_ofp_packet_out_str__(struct ofputil_packet_out *po, char *string,
                 error = xasprintf("%s is not a valid OpenFlow port", value);
                 goto out;
             }
-            if (po->in_port > OFPP_MAX && po->in_port != OFPP_LOCAL
+            if (ofp_to_u16(po->in_port) > ofp_to_u16(OFPP_MAX)
+                && po->in_port != OFPP_LOCAL
                 && po->in_port != OFPP_NONE
                 && po->in_port != OFPP_CONTROLLER) {
                 error = xasprintf(