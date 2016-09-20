openvswitch/ovs

commit df70a7731c6d50458e46c865975d569dd3909212

ofproto-dpif-xlate: Allow translating without side-effects.
Extend 'may_learn' attribute to also control the treatment of
FIN_TIMEOUT action and asynchronous messages (packet ins,
continuations), so that when 'may_learn' is 'false' and
'resubmit_stats' is 'NULL', no OpenFlow-visible side effects are
generated by the translation.

Correspondingly, add support for one-time asynchronous messages to
xlate cache, so that all side-effects of the translation may be
executed at a later stage.  This will be useful for bundle commits.

Signed-off-by: Jarno Rajahalme <jarno@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

ofproto/ofproto-dpif-xlate.c
@@ -2311,7 +2311,7 @@ xlate_normal(struct xlate_ctx *ctx)
 
     /* Learn source MAC. */
     bool is_grat_arp = is_gratuitous_arp(flow, wc);
-    if (ctx->xin->may_learn) {
+    if (ctx->xin->allow_side_effects) {
         update_learning_table(ctx->xbridge, in_xbundle, flow->dl_src, vlan,
                               is_grat_arp);
     }
@@ -2339,7 +2339,7 @@ xlate_normal(struct xlate_ctx *ctx)
             memset(&wc->masks.tp_src, 0xff, sizeof wc->masks.tp_src);
             if (mcast_snooping_is_membership(flow->tp_src) ||
                 mcast_snooping_is_query(flow->tp_src)) {
-                if (ctx->xin->may_learn && ctx->xin->packet) {
+                if (ctx->xin->allow_side_effects && ctx->xin->packet) {
                     update_mcast_snooping_table(ctx->xbridge, flow, vlan,
                                                 in_xbundle, ctx->xin->packet);
                 }
@@ -2371,7 +2371,7 @@ xlate_normal(struct xlate_ctx *ctx)
             return;
         } else if (is_mld(flow, wc)) {
             ctx->xout->slow |= SLOW_ACTION;
-            if (ctx->xin->may_learn && ctx->xin->packet) {
+            if (ctx->xin->allow_side_effects && ctx->xin->packet) {
                 update_mcast_snooping_table(ctx->xbridge, flow, vlan,
                                             in_xbundle, ctx->xin->packet);
             }
@@ -3508,6 +3508,10 @@ execute_controller_action(struct xlate_ctx *ctx, int len,
         return;
     }
 
+    if (!ctx->xin->allow_side_effects && !ctx->xin->xcache) {
+        return;
+    }
+
     packet = dp_packet_clone(ctx->xin->packet);
     packet_batch_init_packet(&batch, packet);
     odp_execute_actions(NULL, &batch, false,
@@ -3546,13 +3550,26 @@ execute_controller_action(struct xlate_ctx *ctx, int len,
     };
     flow_get_metadata(&ctx->xin->flow, &am->pin.up.public.flow_metadata);
 
-    ofproto_dpif_send_async_msg(ctx->xbridge->ofproto, am);
-    dp_packet_delete(packet);
+    /* Async messages are only sent once, so if we send one now, no
+     * xlate cache entry is created.  */
+    if (ctx->xin->allow_side_effects) {
+        ofproto_dpif_send_async_msg(ctx->xbridge->ofproto, am);
+    } else /* xcache */ {
+        struct xc_entry *entry;
+
+        entry = xlate_cache_add_entry(ctx->xin->xcache, XC_CONTROLLER);
+        entry->controller.ofproto = ctx->xbridge->ofproto;
+        entry->controller.am = am;
+    }
 }
 
 static void
 emit_continuation(struct xlate_ctx *ctx, const struct frozen_state *state)
 {
+    if (!ctx->xin->allow_side_effects && !ctx->xin->xcache) {
+        return;
+    }
+
     struct ofproto_async_msg *am = xmalloc(sizeof *am);
     *am = (struct ofproto_async_msg) {
         .controller_id = ctx->pause->controller_id,
@@ -3584,7 +3601,18 @@ emit_continuation(struct xlate_ctx *ctx, const struct frozen_state *state)
         },
     };
     flow_get_metadata(&ctx->xin->flow, &am->pin.up.public.flow_metadata);
-    ofproto_dpif_send_async_msg(ctx->xbridge->ofproto, am);
+
+    /* Async messages are only sent once, so if we send one now, no
+     * xlate cache entry is created.  */
+    if (ctx->xin->allow_side_effects) {
+        ofproto_dpif_send_async_msg(ctx->xbridge->ofproto, am);
+    } else /* xcache */ {
+        struct xc_entry *entry;
+
+        entry = xlate_cache_add_entry(ctx->xin->xcache, XC_CONTROLLER);
+        entry->controller.ofproto = ctx->xbridge->ofproto;
+        entry->controller.am = am;
+    }
 }
 
 static void
@@ -3985,7 +4013,7 @@ xlate_learn_action(struct xlate_ctx *ctx, const struct ofpact_learn *learn)
 {
     learn_mask(learn, ctx->wc);
 
-    if (ctx->xin->xcache || ctx->xin->may_learn) {
+    if (ctx->xin->xcache || ctx->xin->allow_side_effects) {
         uint64_t ofpacts_stub[1024 / 8];
         struct ofputil_flow_mod fm;
         struct ofproto_flow_mod ofm__, *ofm;
@@ -4008,7 +4036,7 @@ xlate_learn_action(struct xlate_ctx *ctx, const struct ofpact_learn *learn)
                                                      &fm, ofm);
         ofpbuf_uninit(&ofpacts);
 
-        if (!error && ctx->xin->may_learn) {
+        if (!error && ctx->xin->allow_side_effects) {
             error = ofproto_flow_mod_learn(ofm, ctx->xin->xcache != NULL);
         }
 
@@ -4034,8 +4062,10 @@ xlate_fin_timeout(struct xlate_ctx *ctx,
                   const struct ofpact_fin_timeout *oft)
 {
     if (ctx->rule) {
-        xlate_fin_timeout__(ctx->rule, ctx->xin->tcp_flags,
-                            oft->fin_idle_timeout, oft->fin_hard_timeout);
+        if (ctx->xin->allow_side_effects) {
+            xlate_fin_timeout__(ctx->rule, ctx->xin->tcp_flags,
+                                oft->fin_idle_timeout, oft->fin_hard_timeout);
+        }
         if (ctx->xin->xcache) {
             struct xc_entry *entry;
 
@@ -4936,7 +4966,7 @@ xlate_in_init(struct xlate_in *xin, struct ofproto_dpif *ofproto,
     xin->flow.in_port.ofp_port = in_port;
     xin->flow.actset_output = OFPP_UNSET;
     xin->packet = packet;
-    xin->may_learn = packet != NULL;
+    xin->allow_side_effects = packet != NULL;
     xin->rule = rule;
     xin->xcache = NULL;
     xin->ofpacts = NULL;

ofproto/ofproto-dpif-upcall.c
@@ -1912,7 +1912,7 @@ revalidate_ukey(struct udpif *udpif, struct udpif_key *ukey,
                   NULL, need_revalidate ? &wc : NULL, odp_actions);
     if (push.n_packets) {
         xin.resubmit_stats = &push;
-        xin.may_learn = true;
+        xin.allow_side_effects = true;
     }
     xin.xcache = ukey->xcache;
     xlate_actions(&xin, &xout);
@@ -2089,7 +2089,7 @@ push_dp_ops(struct udpif *udpif, struct ukey_op *ops, size_t n_ops)
                 xlate_in_init(&xin, ofproto, &flow, ofp_in_port, NULL,
                               push->tcp_flags, NULL, NULL, NULL);
                 xin.resubmit_stats = push->n_packets ? push : NULL;
-                xin.may_learn = push->n_packets > 0;
+                xin.allow_side_effects = push->n_packets > 0;
                 xlate_actions_for_side_effects(&xin);
 
                 if (netflow) {

ofproto/ofproto-dpif-xlate-cache.c
@@ -27,6 +27,7 @@
 #include "bond.h"
 #include "bundle.h"
 #include "byte-order.h"
+#include "connmgr.h"
 #include "coverage.h"
 #include "dp-packet.h"
 #include "dpif.h"
@@ -146,6 +147,13 @@ xlate_push_stats_entry(struct xc_entry *entry,
         tnl_neigh_lookup(entry->tnl_neigh_cache.br_name,
                          &entry->tnl_neigh_cache.d_ipv6, &dmac);
         break;
+    case XC_CONTROLLER:
+        if (entry->controller.am) {
+            ofproto_dpif_send_async_msg(entry->controller.ofproto,
+                                        entry->controller.am);
+            entry->controller.am = NULL; /* One time only. */
+        }
+        break;
     default:
         OVS_NOT_REACHED();
     }
@@ -224,6 +232,12 @@ xlate_cache_clear_entry(struct xc_entry *entry)
         group_dpif_unref(entry->group.group);
         break;
     case XC_TNL_NEIGH:
+        break;
+    case XC_CONTROLLER:
+        if (entry->controller.am) {
+            ofproto_async_msg_free(entry->controller.am);
+            entry->controller.am = NULL;
+        }
         break;
     default:
         OVS_NOT_REACHED();

ofproto/ofproto-dpif-xlate-cache.h
@@ -46,11 +46,12 @@ enum xc_type {
     XC_NETDEV,
     XC_NETFLOW,
     XC_MIRROR,
-    XC_LEARN,
+    XC_LEARN,            /* Calls back to ofproto. */
     XC_NORMAL,
-    XC_FIN_TIMEOUT,
+    XC_FIN_TIMEOUT,      /* Calls back to ofproto. */
     XC_GROUP,
     XC_TNL_NEIGH,
+    XC_CONTROLLER,
 };
 
 /* xlate_cache entries hold enough information to perform the side effects of
@@ -113,6 +114,10 @@ struct xc_entry {
             char br_name[IFNAMSIZ];
             struct in6_addr d_ipv6;
         } tnl_neigh_cache;
+        struct {
+            struct ofproto_dpif *ofproto;
+            struct ofproto_async_msg *am;
+        } controller;
     };
 };
 

ofproto/ofproto-dpif-xlate.h
@@ -56,12 +56,14 @@ struct xlate_in {
     const struct dp_packet *packet;
 
     /* Should OFPP_NORMAL update the MAC learning table?  Should "learn"
-     * actions update the flow table?
+     * actions update the flow table? Should FIN_TIMEOUT change the
+     * timeouts? Or should controller action send packet to the controller?
      *
      * We want to update these tables if we are actually processing a packet,
      * or if we are accounting for packets that the datapath has processed, but
-     * not if we are just revalidating. */
-    bool may_learn;
+     * not if we are just revalidating, or if we want to execute the
+     * side-effects later via the xlate cache. */
+    bool allow_side_effects;
 
     /* The rule initiating translation or NULL. If both 'rule' and 'ofpacts'
      * are NULL, xlate_actions() will do the initial rule lookup itself. */

commit 3b4fff438cea524ae421269abfc5197430b65cb9

ofproto-dpif-xlate: Fix memory leak in execute_controller_action.
commit df70a7731 ("ofproto-dpif-xlate: Allow translating
without side-effects.") created a memory leak by removing the
dp_packet_delete statement in execute_controller_action that
freed the earlier cloned packet.  This commit restores this
statement to the end of the method.

Fixes: df70a7731 ("ofproto-dpif-xlate: Allow translating without side-effects.")
Signed-off-by: Ryan Moats <rmoats@us.ibm.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ofproto/ofproto-dpif-xlate.c
@@ -3648,6 +3648,8 @@ execute_controller_action(struct xlate_ctx *ctx, int len,
         entry->controller.ofproto = ctx->xbridge->ofproto;
         entry->controller.am = am;
     }
+
+    dp_packet_delete(packet);
 }
 
 static void