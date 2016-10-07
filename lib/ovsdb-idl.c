01928c964f572fb0551c999437557458f326be3e
Ben Pfaff <blp@ovn.org>
2016-10-07 09:47:43 -0700

    ovsdb-idlc: Implement synthetic columns.
    A synthetic column is one that is not present in the actual database but
    instead calculated by code in the client based on columns in the row.  This
    can be useful to avoid repeatedly calculating the same function of a row.
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/ovsdb-idl.c
+++ lib/ovsdb-idl.c
@@ -1560,9 +1560,16 @@ ovsdb_idl_send_monitor_request__(struct ovsdb_idl *idl,
         columns = table->need_table ? json_array_create_empty() : NULL;
         for (j = 0; j < tc->n_columns; j++) {
             const struct ovsdb_idl_column *column = &tc->columns[j];
-            if (table->modes[j] & OVSDB_IDL_MONITOR) {
-                if (table_schema
-                    && !sset_contains(table_schema, column->name)) {
+            bool db_has_column = (table_schema &&
+                                  sset_contains(table_schema, column->name));
+            if (column->is_synthetic) {
+                if (db_has_column) {
+                    VLOG_WARN("%s table in %s database has synthetic "
+                              "column %s", table->class_->name,
+                              idl->class_->database, column->name);
+                }
+            } else if (table->modes[j] & OVSDB_IDL_MONITOR) {
+                if (table_schema && !db_has_column) {
                     VLOG_WARN("%s table in %s database lacks %s column "
                               "(database needs upgrade?)",
                               table->class_->name, idl->class_->database,
@@ -3815,6 +3822,7 @@ ovsdb_idl_txn_write__(const struct ovsdb_idl_row *row_,
     size_t column_idx;
     bool write_only;
 
+    ovs_assert(!column->is_synthetic);
     if (ovsdb_idl_row_is_synthetic(row)) {
         goto discard_datum;
     }
