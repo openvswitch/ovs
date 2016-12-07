1dea14357f9b9c1ce273538dd0cb36ba2a1993dd
nickcooper-zhangtonghao <nic@opencloud.tech>
2016-12-07 10:04:04 -0800

    ovs-vswitchd: Avoid segfault for "netdev" datapath.
    When the datapath, whose type is "netdev", processes packets
    in userspce action, it may cause a segmentation fault. In the
    dp_execute_userspace_action(), we pass the "wc" argument to
    dp_netdev_upcall() using NULL. In the dp_netdev_upcall() call tree,
    the "wc" will be used. For example, dp_netdev_upcall() uses the
    &wc->masks for debugging, and flow_wildcards_init_for_packet()
    uses the  "wc" if we disable megaflow, which is described in
    more detail below.
    
    Segmentation fault in flow_wildcards_init_for_packet:
    
        #0  0x0000000000468fe8 flow_wildcards_init_for_packet lib/flow.c:1275
        #1  0x0000000000436c0b upcall_cb ofproto/ofproto-dpif-upcall.c:1231
        #2  0x000000000045bd96 dp_netdev_upcall lib/dpif-netdev.c:3857
        #3  0x0000000000461bf3 dp_execute_userspace_action lib/dpif-netdev.c:4388
        #4  dp_execute_cb lib/dpif-netdev.c:4521
        #5  0x0000000000486ae2 odp_execute_actions lib/odp-execute.c:538
        #6  0x00000000004607f9 dp_netdev_execute_actions lib/dpif-netdev.c:4627
        #7  packet_batch_per_flow_execute lib/dpif-netdev.c:3927
        #8  dp_netdev_input__ lib/dpif-netdev.c:4229
        #9  0x0000000000460ba8 dp_netdev_input lib/dpif-netdev.c:4238
        #10 dp_netdev_process_rxq_port lib/dpif-netdev.c:2873
        #11 0x000000000046126e dpif_netdev_run lib/dpif-netdev.c:3000
        #12 0x000000000042baf5 type_run ofproto/ofproto-dpif.c:504
        #13 0x00000000004192bf ofproto_type_run ofproto/ofproto.c:1687
        #14 0x0000000000409965 bridge_run__ vswitchd/bridge.c:2875
        #15 0x000000000040f145 bridge_run vswitchd/bridge.c:2938
        #16 0x00000000004062e5 main vswitchd/ovs-vswitchd.c:111
    
    Signed-off-by: nickcooper-zhangtonghao <nic@opencloud.tech>
    Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>

--- ofproto/ofproto-dpif-upcall.c
+++ ofproto/ofproto-dpif-upcall.c
@@ -1227,7 +1227,7 @@ upcall_cb(const struct dp_packet *packet, const struct flow *flow, ovs_u128 *ufi
                    upcall.put_actions.size);
     }
 
-    if (OVS_UNLIKELY(!megaflow)) {
+    if (OVS_UNLIKELY(!megaflow && wc)) {
         flow_wildcards_init_for_packet(wc, flow);
     }
 
@@ -1504,7 +1504,7 @@ ukey_create_from_upcall(struct upcall *upcall, struct flow_wildcards *wc)
     bool megaflow;
     struct odp_flow_key_parms odp_parms = {
         .flow = upcall->flow,
-        .mask = &wc->masks,
+        .mask = wc ? &wc->masks : NULL,
     };
 
     odp_parms.support = ofproto_dpif_get_support(upcall->ofproto)->odp;
@@ -1519,7 +1519,7 @@ ukey_create_from_upcall(struct upcall *upcall, struct flow_wildcards *wc)
 
     atomic_read_relaxed(&enable_megaflows, &megaflow);
     ofpbuf_use_stack(&maskbuf, &maskstub, sizeof maskstub);
-    if (megaflow) {
+    if (megaflow && wc) {
         odp_parms.key_buf = &keybuf;
         odp_flow_key_from_mask(&odp_parms, &maskbuf);
     }
