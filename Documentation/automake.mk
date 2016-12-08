daf5079e73550f868f0f7ea7c296d395eaff77ad
Stephen Finucane <stephen@that.guru>
2016-12-08 12:55:20 +0000

    Add initial sphinx configuration
    This is essentially the output of 'sphinx-quickstart' but with the
    following changes:
    
    - Parts of the generated Makefile are merged into the existing
      Documentation/automake.mk Makefile
    - A license is added to the index.rst file
    - The OVS logo is added
    - A 'contents' page is added, so we don't need to include a TOC on the
      home page
    - The theme is switched to 'bizstyle', which makes better use of
      horizontal real estate than the default 'alabaster' theme
    
    Copyright is assigned to "The Open vSwitch Development Community".
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- Documentation/automake.mk
+++ Documentation/automake.mk
@@ -4,3 +4,26 @@ docs += \
 	Documentation/group-selection-method-property.txt \
 	Documentation/OVSDB-replication.rst \
 	Documentation/release-process.rst
+
+EXTRA_DIST += \
+	Documentation/_static/logo.png \
+	Documentation/conf.py \
+	Documentation/index.rst \
+	Documentation/contents.rst
+
+# You can set these variables from the command line.
+SPHINXOPTS =
+SPHINXBUILD = sphinx-build
+SPHINXSRCDIR = $(srcdir)/Documentation
+SPHINXBUILDDIR = $(srcdir)/Documentation/_build
+
+# Internal variables.
+PAPEROPT_a4 = -D latex_paper_size=a4
+PAPEROPT_letter = -D latex_paper_size=letter
+# TODO(stephenfin): Add '-W' flag here once we've integrated required docs
+ALLSPHINXOPTS = -d $(SPHINXBUILDDIR)/doctrees $(PAPEROPT_$(PAPER)) $(SPHINXOPTS) $(SPHINXSRCDIR)
+
+.PHONY: htmldocs
+htmldocs:
+	rm -rf $(SPHINXBUILDDIR)/*
+	$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(SPHINXBUILDDIR)/html
