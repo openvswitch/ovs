3dada1724c0e909bb0514f5fe2191030bbb87574
Lance Richardson <lrichard@redhat.com>
2016-12-14 11:16:53 -0500

    ovn-northd: fix monitor process naming
    Currently the ovn-northd monitor process and the ovn-northd process
    have the same name, e.g. ps -ef | grep northd shows (edited for space):
    
        ... ovn-northd --detach --monitor --log-file=ovn-northd.log --pidfile
        ... ovn-northd --detach --monitor --log-file=ovn-northd.log --pidfile
    
    With the call to ovs_cmdl_proctitle_init() added, we have:
    
        ... ovn-northd: monitoring pid 15662 (healthy)
        ... ovn-northd --detach --monitor --log-file=ovn-northd.log --pidfile
    
    Signed-off-by: Lance Richardson <lrichard@redhat.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ovn/northd/ovn-northd.c
+++ ovn/northd/ovn-northd.c
@@ -4913,6 +4913,7 @@ main(int argc, char *argv[])
     bool exiting;
 
     fatal_ignore_sigpipe();
+    ovs_cmdl_proctitle_init(argc, argv);
     set_program_name(argv[0]);
     service_start(&argc, &argv);
     parse_options(argc, argv);
