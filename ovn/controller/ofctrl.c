55a80797574286495438d2017d9a22a6c0c783f5
Ben Pfaff <blp@ovn.org>
2017-01-05 17:03:05 -0800

    ofctrl: Fix version check in ofctrl_inject_packet().
    "enum ofp_version" is unsigned in the System V ABI used by Linux, so
    it will never be less than 0, so an rconn with an unnegotiated version will
    never be found properly.  This fixes the problem.
    
    Fixes: 714651c7db6a ("ovn-controller: Introduce "inject-pkt" ovs-appctl command.")
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Justin Pettit <jpettit@ovn.org>

--- ovn/controller/ofctrl.c
+++ ovn/controller/ofctrl.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, 2016 Nicira, Inc.
+/* Copyright (c) 2015, 2016, 2017 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1119,7 +1119,7 @@ char *
 ofctrl_inject_pkt(const struct ovsrec_bridge *br_int, const char *flow_s,
                   const struct shash *addr_sets)
 {
-    enum ofp_version version = rconn_get_version(swconn);
+    int version = rconn_get_version(swconn);
     if (version < 0) {
         return xstrdup("OpenFlow channel not ready.");
     }
