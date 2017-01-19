c09220bf8c466632c7007f4c9b4dc3a749797958
Russell Bryant <russell@ovn.org>
2017-01-19 14:11:48 -0500

    doc: Remove tutorials/ovn-basics.
    The only thing worse than a lack of documentation is incorrect or
    out-of-date documentation.  Over time, this document has not kept up with
    the pace of OVN and is no longer a good current resource.
    
    For a sandbox based tutorial like this, I'd like to start over using
    ovn-trace as the basis.
    
    An even more important type of tutorial would be something along the lines
    of: http://blog.spinhirne.com/p/blog-series.html
    
    That blog series was fantastic and has been the primary tutorial reference
    I have been sending people to since it was written.
    
    Signed-off-by: Russell Bryant <russell@ovn.org>
    Acked-by: Ben Pfaff <blp@ovn.org>

--- tutorial/automake.mk
+++ tutorial/automake.mk
@@ -5,37 +5,6 @@ EXTRA_DIST += \
 	tutorial/t-stage1 \
 	tutorial/t-stage2 \
 	tutorial/t-stage3 \
-	tutorial/t-stage4 \
-	tutorial/ovn/env1/setup.sh \
-	tutorial/ovn/env1/packet1.sh \
-	tutorial/ovn/env1/packet2.sh \
-	tutorial/ovn/env1/packet3.sh \
-	tutorial/ovn/env1/packet4.sh \
-	tutorial/ovn/env1/add-third-port.sh \
-	tutorial/ovn/env1/add-unknown-ports.sh \
-	tutorial/ovn/env1/add-security-ip-ports.sh \
-	tutorial/ovn/env2/setup.sh \
-	tutorial/ovn/env2/packet1.sh \
-	tutorial/ovn/env2/packet2.sh \
-	tutorial/ovn/env3/setup.sh \
-	tutorial/ovn/env3/packet1.sh \
-	tutorial/ovn/env3/packet2.sh \
-	tutorial/ovn/env4/setup.sh \
-	tutorial/ovn/env4/packet1.sh \
-	tutorial/ovn/env4/packet2.sh \
-	tutorial/ovn/env4/packet3.sh \
-	tutorial/ovn/env4/packet4.sh \
-	tutorial/ovn/env5/setup.sh \
-	tutorial/ovn/env5/packet1.sh \
-	tutorial/ovn/env5/packet2.sh \
-	tutorial/ovn/env6/setup.sh \
-	tutorial/ovn/env6/add-acls.sh \
-	tutorial/ovn/env7/add-container-ports.sh \
-	tutorial/ovn/env7/packet1.sh \
-	tutorial/ovn/env7/packet2.sh \
-	tutorial/ovn/env7/setup.sh \
-	tutorial/ovn/env8/packet1.sh \
-	tutorial/ovn/env8/packet2.sh \
-	tutorial/ovn/env8/setup.sh
+	tutorial/t-stage4
 sandbox: all
 	cd $(srcdir)/tutorial && MAKE=$(MAKE) ./ovs-sandbox -b $(abs_builddir) $(SANDBOXFLAGS)
