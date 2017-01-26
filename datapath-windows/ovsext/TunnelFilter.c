df3487c4b29d1902935ba1fe1900c276c370cc7c
Alin Serdean <aserdean@cloudbasesolutions.com>
2017-01-26 22:49:40 +0000

    windows: Change driver and MSI company name to LF
    Until now we used 'Open vSwitch' as the company/organization name.
    
    The project is now under The Linux Foundation ownership.
    
    This patch updates the MSI and driver attributes to reflect that ownership.
    
    Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
    Acked-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Sairam Venugopal <vsairam@vmware.com>
    Signed-off-by: Gurucharan Shetty <guru@ovn.org>

--- datapath-windows/ovsext/TunnelFilter.c
+++ datapath-windows/ovsext/TunnelFilter.c
@@ -50,7 +50,7 @@
 
 /* The provider name should always match the provider string from the install
  * file. */
-#define OVS_TUNNEL_PROVIDER_NAME        L"Open vSwitch"
+#define OVS_TUNNEL_PROVIDER_NAME        L"The Linux Foundation (R)"
 
 /* The provider description should always contain the OVS service description
  * string from the install file. */
