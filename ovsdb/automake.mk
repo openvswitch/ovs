1e78e308519d4ba7d4df87627ab4d96c699fed3d
Aaron Conole <aconole@redhat.com>
2017-01-26 14:07:00 -0500

    libX.pc: use the correct output directory
    When the ovsdb library pkgconfig changes were introduced, they placed
    generated output in the src directory.  This is incorrect, however, as
    the output files should actually be placed in the build directory.  It
    is only seen when running `make distcheck` after enabling shared
    libraries (ex: `./configure --enable-shared`).
    
    Fixes: commit e72e07a97e95 ("lib: Add support for pkgconfig for libovsdb.")
    Signed-off-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ovsdb/automake.mk
+++ ovsdb/automake.mk
@@ -40,7 +40,7 @@ ovsdb_libovsdb_la_CFLAGS = $(AM_CFLAGS)
 ovsdb_libovsdb_la_CPPFLAGS = $(AM_CPPFLAGS)
 
 pkgconfig_DATA += \
-	$(srcdir)/ovsdb/libovsdb.pc
+	ovsdb/libovsdb.pc
 
 MAN_FRAGMENTS += \
 	ovsdb/remote-active.man \

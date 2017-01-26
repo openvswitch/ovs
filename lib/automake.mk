openvswitch/ovs

commit e72e07a97e9577141b1222cd382e20872777e7c1

lib: Add support for pkgconfig for libsflow.
Signed-off-by: Thomas Graf <tgraf@noironetworks.com>
Acked-by: Flavio Leitner <fbl@redhat.com>
Signed-off-by: Ben Pfaff <blp@nicira.com>

lib/automake.mk
@@ -391,7 +391,8 @@ lib_libopenvswitch_la_SOURCES += lib/stream-nossl.c
 endif
 
 pkgconfig_DATA += \
-	$(srcdir)/lib/libopenvswitch.pc
+	$(srcdir)/lib/libopenvswitch.pc \
+	$(srcdir)/lib/libsflow.pc
 
 EXTRA_DIST += \
 	lib/dh1024.pem \

configure.ac
@@ -173,6 +173,7 @@ AC_CONFIG_FILES(datapath/linux/Makefile)
 AC_CONFIG_FILES(datapath/linux/Makefile.main)
 AC_CONFIG_FILES(tests/atlocal)
 AC_CONFIG_FILES(lib/libopenvswitch.pc)
+AC_CONFIG_FILES(lib/libsflow.pc)
 AC_CONFIG_FILES(ofproto/libofproto.pc)
 AC_CONFIG_FILES(ovsdb/libovsdb.pc)
 

lib/.gitignore
@@ -18,3 +18,4 @@
 /vtep-idl.h
 /vtep-idl.ovsidl
 /libopenvswitch.pc
+/libsflow.pc

lib/libsflow.pc.in
@@ -1,0 +1,11 @@
+prefix=@prefix@
+exec_prefix=@exec_prefix@
+libdir=@libdir@
+includedir=@includedir@
+
+Name: libofproto
+Description: sFlow library of Open vSwitch
+Version: @VERSION@
+Libs: -L${libdir} -lsflow
+Libs.private: @LIBS@
+Cflags: -I${includedir}/openvswitch

commit 1e78e308519d4ba7d4df87627ab4d96c699fed3d

libX.pc: use the correct output directory
When the ovsdb library pkgconfig changes were introduced, they placed
generated output in the src directory.  This is incorrect, however, as
the output files should actually be placed in the build directory.  It
is only seen when running `make distcheck` after enabling shared
libraries (ex: `./configure --enable-shared`).

Fixes: commit e72e07a97e95 ("lib: Add support for pkgconfig for libovsdb.")
Signed-off-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/automake.mk
@@ -429,8 +429,8 @@ lib_libopenvswitch_la_SOURCES += lib/stream-nossl.c
 endif
 
 pkgconfig_DATA += \
-	$(srcdir)/lib/libopenvswitch.pc \
-	$(srcdir)/lib/libsflow.pc
+	lib/libopenvswitch.pc \
+	lib/libsflow.pc
 
 EXTRA_DIST += \
 	lib/dh1024.pem \

ofproto/automake.mk
@@ -62,7 +62,7 @@ ofproto_libofproto_la_LIBADD += ${PTHREAD_LIBS}
 endif
 
 pkgconfig_DATA += \
-	$(srcdir)/ofproto/libofproto.pc
+	ofproto/libofproto.pc
 
 # Distribute this generated file in order not to require Python at
 # build time if ofproto/ipfix.xml is not modified.

ovsdb/automake.mk
@@ -40,7 +40,7 @@ ovsdb_libovsdb_la_CFLAGS = $(AM_CFLAGS)
 ovsdb_libovsdb_la_CPPFLAGS = $(AM_CPPFLAGS)
 
 pkgconfig_DATA += \
-	$(srcdir)/ovsdb/libovsdb.pc
+	ovsdb/libovsdb.pc
 
 MAN_FRAGMENTS += \
 	ovsdb/remote-active.man \