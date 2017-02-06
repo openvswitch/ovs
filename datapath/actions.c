f0b0bdd20d9b9c247400266b7126cc4505362c1e
Yang, Yi Y <yi.y.yang@intel.com>
2017-02-06 21:04:35 +0800

    datapath: use hard_header_len instead of hardcoded ETH_HLEN
    Upstream commit:
        commit 738314a084aae5f76ff760279034b39d52c42e8b
        Author: Jiri Benc <jbenc@redhat.com>
        Date:   Thu Nov 10 16:28:17 2016 +0100
    
        openvswitch: use hard_header_len instead of hardcoded ETH_HLEN
    
        On tx, use hard_header_len while deciding whether to refragment or drop the
        packet. That way, all combinations are calculated correctly:
    
        * L2 packet going to L2 interface (the L2 header len is subtracted),
        * L2 packet going to L3 interface (the L2 header is included in the packet
          lenght),
        * L3 packet going to L3 interface.
    
        Signed-off-by: Jiri Benc <jbenc@redhat.com>
        Acked-by: Pravin B Shelar <pshelar@ovn.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Yi Yang <yi.y.yang@intel.com>
    Signed-off-by: Joe Stringer <joe@ovn.org>

--- datapath/actions.c
+++ datapath/actions.c
@@ -773,7 +773,8 @@ static void do_output(struct datapath *dp, struct sk_buff *skb, int out_port,
 				pskb_trim(skb, ETH_HLEN);
 		}
 
-		if (likely(!mru || (skb->len <= mru + ETH_HLEN))) {
+		if (likely(!mru ||
+		           (skb->len <= mru + vport->dev->hard_header_len))) {
 			ovs_vport_send(vport, skb);
 		} else if (mru <= vport->dev->mtu) {
 			struct net *net = ovs_dp_get_net(dp);
