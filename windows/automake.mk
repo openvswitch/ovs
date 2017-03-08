openvswitch/ovs

commit d183efc22b2b0b84c341500a73f567f98f4d00f4

This commit adds the windows installer to the OVS tree.
Requirements are the following:
Visual Studio Community 2013
WiX Toolset 3.9
Microsoft_VC120_CRT_x86.msm

More detailed information on the requirements and build instructions
can be found under:
https://github.com/cloudbase/ovs-windows-installer/blob/master/README.rst

To run and make the installer issue the following:

./boot.sh
./configure CC=./build-aux/cccl LD="`which link`" \
LIBS="-lws2_32 -liphlpapi" --prefix="C:/openvswitch/usr" \
--localstatedir="C:/openvswitch/var" --sysconfdir="C:/openvswitch/etc" \
--with-pthread="C:/pthread" --with-vstudiotarget="Release"

make clean && make -j16 windows_installer

To uninstall one could use the following Powershell commandlets:
$app = Get-WmiObject -Class Win32_Product | Where-Object `
 { $_.Name -match "Open Vswitch" }

$app.Uninstall()

Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
Co-authored-by: Alessandro Pilotti <apilotti@cloudbasesolutions.com>
Signed-off-by: Alessandro Pilotti <apilotti@cloudbasesolutions.com>
Signed-off-by: Gurucharan Shetty <gshetty@nicira.com>

windows/automake.mk
@@ -1,0 +1,57 @@
+# Copyright 2015 Cloudbase Solutions Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+PTHREAD_TEMP_DIR=`echo "$(PTHREAD_LDFLAGS)" | sed 's|^.\(.*\).$:\1||'`
+windows_installer: all
+#Userspace files needed for the installer
+	cp -f $(top_srcdir)/datapath-windows/misc/OVS.psm1 windows/ovs-windows-installer/Services/OVS.psm1
+	cp -f $(top_srcdir)/vswitchd/vswitch.ovsschema windows/ovs-windows-installer/Services/vswitch.ovsschema
+	cp -f $(top_srcdir)/vswitchd/ovs-vswitchd.exe windows/ovs-windows-installer/Services/ovs-vswitchd.exe
+	cp -f $(top_srcdir)/ovsdb/ovsdb-server.exe windows/ovs-windows-installer/Services/ovsdb-server.exe
+	cp -f $(top_srcdir)/utilities/*.exe windows/ovs-windows-installer/Binaries/
+	cp -f $(top_srcdir)/utilities/*.pdb windows/ovs-windows-installer/Symbols/
+	cp -f $(top_srcdir)/ovsdb/ovsdb-client.exe windows/ovs-windows-installer/Binaries/ovsdb-client.exe
+	cp -f $(top_srcdir)/ovsdb/ovsdb-tool.exe windows/ovs-windows-installer/Binaries/ovsdb-tool.exe
+	cp -f $(top_srcdir)/ovsdb/ovsdb-client.pdb windows/ovs-windows-installer/Symbols/
+	cp -f $(top_srcdir)/ovsdb/ovsdb-tool.pdb windows/ovs-windows-installer/Symbols/
+#Third party files needed by the installer
+	cp -f $(PTHREAD_TEMP_DIR)/../../dll/x86/*.dll windows/ovs-windows-installer/Binaries/
+	cp -f "/c/Program Files (x86)/Common Files/Merge Modules/Microsoft_VC120_CRT_x86.msm" windows/ovs-windows-installer/Redist/Microsoft_VC120_CRT_x86.msm
+#Forwarding extension files needed for the installer
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8$(VSTUDIO_CONFIG)/package/ovsext.cat windows/ovs-windows-installer/Driver/Win8/ovsext.cat
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8$(VSTUDIO_CONFIG)/package/ovsext.inf windows/ovs-windows-installer/Driver/Win8/ovsext.inf
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8$(VSTUDIO_CONFIG)/package/OVSExt.sys windows/ovs-windows-installer/Driver/Win8/OVSExt.sys
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8.1$(VSTUDIO_CONFIG)/package/ovsext.cat windows/ovs-windows-installer/Driver/Win8.1/ovsext.cat
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8.1$(VSTUDIO_CONFIG)/package/ovsext.inf windows/ovs-windows-installer/Driver/Win8.1/ovsext.inf
+	cp -f $(top_srcdir)/datapath-windows/x64/Win8.1$(VSTUDIO_CONFIG)/package/ovsext.sys windows/ovs-windows-installer/Driver/Win8.1/ovsext.sys
+	MSBuild.exe windows/ovs-windows-installer.sln /target:Build /property:Configuration="Release"
+
+EXTRA_DIST += \
+	windows/.gitignore \
+	windows/automake.mk \
+	windows/README.rst \
+	windows/ovs-windows-installer.sln \
+	windows/ovs-windows-installer/Actions/OVSActions.js \
+	windows/ovs-windows-installer/CustomActions.wxs \
+	windows/ovs-windows-installer/Dialogs/BeginningDialog.wxs \
+	windows/ovs-windows-installer/Dialogs/MyEndDialog.wxs \
+	windows/ovs-windows-installer/Dialogs/MyTroubleshootDialog.wxs \
+	windows/ovs-windows-installer/Dialogs/UserFinishDialog.wxs \
+	windows/ovs-windows-installer/License.rtf \
+	windows/ovs-windows-installer/Product.wxs \
+	windows/ovs-windows-installer/UI.wxs \
+	windows/ovs-windows-installer/images/bannrbmp.bmp \
+	windows/ovs-windows-installer/images/dlgbmp.bmp \
+	windows/ovs-windows-installer/ovs-windows-installer.wixproj
+

Makefile.am
@@ -335,11 +335,13 @@ CLEANFILES += manpage-dep-check
 if VSTUDIO_DDK
 ALL_LOCAL += ovsext_make
 ovsext_make: datapath-windows/ovsext.sln
-	MSBuild.exe datapath-windows/ovsext.sln /target:Build /property:Configuration="$(VSTUDIO_CONFIG)"
+	MSBuild.exe datapath-windows/ovsext.sln /target:Build /property:Configuration="Win8$(VSTUDIO_CONFIG)"
+	MSBuild.exe datapath-windows/ovsext.sln /target:Build /property:Configuration="Win8.1$(VSTUDIO_CONFIG)"
 
 CLEAN_LOCAL += ovsext_clean
 ovsext_clean: datapath-windows/ovsext.sln
-	MSBuild.exe datapath-windows/ovsext.sln /target:Clean /property:Configuration="$(VSTUDIO_CONFIG)"
+	MSBuild.exe datapath-windows/ovsext.sln /target:Clean /property:Configuration="Win8$(VSTUDIO_CONFIG)"
+	MSBuild.exe datapath-windows/ovsext.sln /target:Clean /property:Configuration="Win8.1$(VSTUDIO_CONFIG)"
 endif
 
 dist-hook: $(DIST_HOOKS)
@@ -377,3 +379,4 @@ include tutorial/automake.mk
 include vtep/automake.mk
 include datapath-windows/automake.mk
 include datapath-windows/include/automake.mk
+include windows/automake.mk

appveyor.yml
@@ -39,5 +39,5 @@ build_script:
 - C:\MinGW\msys\1.0\bin\bash -lc "cp /c/pthreads-win32/Pre-built.2/dll/x86/*.dll /c/openvswitch/."
 - C:\MinGW\msys\1.0\bin\bash -lc "mv /bin/link.exe /bin/link_copy.exe"
 - C:\MinGW\msys\1.0\bin\bash -lc "cd /c/openvswitch && ./boot.sh"
-- C:\MinGW\msys\1.0\bin\bash -lc "cd /c/openvswitch && ./configure CC=build-aux/cccl LD=\"`which link`\" LIBS=\"-lws2_32 -liphlpapi\" --with-pthread=C:/pthreads-win32/Pre-built.2 --with-openssl=C:/OpenSSL-Win32 --with-vstudioddk=\"Win8.1 Debug\""
+- C:\MinGW\msys\1.0\bin\bash -lc "cd /c/openvswitch && ./configure CC=build-aux/cccl LD=\"`which link`\" LIBS=\"-lws2_32 -liphlpapi\" --with-pthread=C:/pthreads-win32/Pre-built.2 --with-openssl=C:/OpenSSL-Win32 --with-vstudiotarget=\"Debug\""
 - C:\MinGW\msys\1.0\bin\bash -lc "cd /c/openvswitch && make"

datapath-windows/ovsext.sln
@@ -1,6 +1,6 @@
 Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 2013
-VisualStudioVersion = 12.0.21005.1
+VisualStudioVersion = 12.0.31101.0
 MinimumVisualStudioVersion = 10.0.40219.1
 Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Package", "Package", "{6BA8554E-AE50-49B0-9C98-4592447FEF8D}"
 EndProject
@@ -12,32 +12,32 @@ Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "ovsext", "ovsext\ovsext.vcx
 EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Win8 Debug|x64 = Win8 Debug|x64
-		Win8 Release|x64 = Win8 Release|x64
-		Win8.1 Debug|x64 = Win8.1 Debug|x64
-		Win8.1 Release|x64 = Win8.1 Release|x64
+		Win8.1Debug|x64 = Win8.1Debug|x64
+		Win8.1Release|x64 = Win8.1Release|x64
+		Win8Debug|x64 = Win8Debug|x64
+		Win8Release|x64 = Win8Release|x64
 	EndGlobalSection
 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8 Debug|x64.ActiveCfg = Win8 Debug|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8 Debug|x64.Build.0 = Win8 Debug|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8 Release|x64.ActiveCfg = Win8 Release|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8 Release|x64.Build.0 = Win8 Release|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1 Debug|x64.ActiveCfg = Win8.1 Debug|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1 Debug|x64.Build.0 = Win8.1 Debug|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1 Release|x64.ActiveCfg = Win8.1 Release|x64
-		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1 Release|x64.Build.0 = Win8.1 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Debug|x64.ActiveCfg = Win8 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Debug|x64.Build.0 = Win8 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Debug|x64.Deploy.0 = Win8 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Release|x64.ActiveCfg = Win8 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Release|x64.Build.0 = Win8 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8 Release|x64.Deploy.0 = Win8 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Debug|x64.ActiveCfg = Win8.1 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Debug|x64.Build.0 = Win8.1 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Debug|x64.Deploy.0 = Win8.1 Debug|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Release|x64.ActiveCfg = Win8.1 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Release|x64.Build.0 = Win8.1 Release|x64
-		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1 Release|x64.Deploy.0 = Win8.1 Release|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1Debug|x64.ActiveCfg = Win8.1 Debug|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1Debug|x64.Build.0 = Win8.1 Debug|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1Release|x64.ActiveCfg = Win8.1 Release|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8.1Release|x64.Build.0 = Win8.1 Release|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8Debug|x64.ActiveCfg = Win8 Debug|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8Debug|x64.Build.0 = Win8 Debug|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8Release|x64.ActiveCfg = Win8 Release|x64
+		{911D7389-3E61-449F-B8F3-14AD7EE9A0F2}.Win8Release|x64.Build.0 = Win8 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Debug|x64.ActiveCfg = Win8.1 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Debug|x64.Build.0 = Win8.1 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Debug|x64.Deploy.0 = Win8.1 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Release|x64.ActiveCfg = Win8.1 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Release|x64.Build.0 = Win8.1 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8.1Release|x64.Deploy.0 = Win8.1 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Debug|x64.ActiveCfg = Win8 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Debug|x64.Build.0 = Win8 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Debug|x64.Deploy.0 = Win8 Debug|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Release|x64.ActiveCfg = Win8 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Release|x64.Build.0 = Win8 Release|x64
+		{63FE215D-98BE-4440-8081-C6160EFB80FA}.Win8Release|x64.Deploy.0 = Win8 Release|x64
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE

m4/openvswitch.m4
@@ -120,16 +120,14 @@ dnl OVS_CHECK_WINDOWS
 dnl
 dnl Configure Visual Studio solution build
 AC_DEFUN([OVS_CHECK_VISUAL_STUDIO_DDK], [
-AC_ARG_WITH([vstudioddk],
-         [AS_HELP_STRING([--with-vstudioddk=version_type],
-            [Visual Studio DDK version type e.g. Win8.1 Release])],
+AC_ARG_WITH([vstudiotarget],
+         [AS_HELP_STRING([--with-vstudiotarget=target_type],
+            [Target type: Debug/Release])],
          [
             case "$withval" in
-            "Win8.1 Release") ;;
-            "Win8.1 Debug") ;;
-            "Win8 Release") ;;
-            "Win8 Debug") ;;
-            *) AC_MSG_ERROR([No good Visual Studio configuration found]) ;;
+            "Release") ;;
+            "Debug") ;;
+            *) AC_MSG_ERROR([No valid Visual Studio configuration found]) ;;
             esac
 
             VSTUDIO_CONFIG=$withval
@@ -139,7 +137,7 @@ AC_ARG_WITH([vstudioddk],
       )
 
   AC_SUBST([VSTUDIO_CONFIG])
-  AC_DEFINE([VSTUDIO_DDK], [1], [System uses the Visual Studio DDK version module.])
+  AC_DEFINE([VSTUDIO_DDK], [1], [System uses the Visual Studio build target.])
   AM_CONDITIONAL([VSTUDIO_DDK], [test -n "$VSTUDIO_CONFIG"])
 ])
 

windows/.gitignore
@@ -1,0 +1,191 @@
+## Ignore Visual Studio temporary files, build results, and
+## files generated by popular Visual Studio add-ons.
+
+# User-specific files
+*.suo
+*.user
+*.sln.docstates
+
+ovs-windows-installer/Binaries.wxs
+ovs-windows-installer/Symbols.wxs
+
+# Build results
+[Dd]ebug/
+[Dd]ebugPublic/
+[Rr]elease/
+x64/
+build/
+bld/
+[Bb]in/
+[Oo]bj/
+
+# MSTest test Results
+[Tt]est[Rr]esult*/
+[Bb]uild[Ll]og.*
+
+#NUNIT
+*.VisualState.xml
+TestResult.xml
+
+# Build Results of an ATL Project
+[Dd]ebugPS/
+[Rr]eleasePS/
+dlldata.c
+
+*_i.c
+*_p.c
+*_i.h
+*.ilk
+*.meta
+*.obj
+*.pch
+*.pdb
+*.pgc
+*.pgd
+*.rsp
+*.sbr
+*.tlb
+*.tli
+*.tlh
+*.tmp
+*.tmp_proj
+*.log
+*.vspscc
+*.vssscc
+.builds
+*.pidb
+*.svclog
+*.scc
+
+# Chutzpah Test files
+_Chutzpah*
+
+# Visual C++ cache files
+ipch/
+*.aps
+*.ncb
+*.opensdf
+*.sdf
+*.cachefile
+
+# Visual Studio profiler
+*.psess
+*.vsp
+*.vspx
+
+# TFS 2012 Local Workspace
+$tf/
+
+# Guidance Automation Toolkit
+*.gpState
+
+# ReSharper is a .NET coding add-in
+_ReSharper*/
+*.[Rr]e[Ss]harper
+*.DotSettings.user
+
+# JustCode is a .NET coding addin-in
+.JustCode
+
+# TeamCity is a build add-in
+_TeamCity*
+
+# DotCover is a Code Coverage Tool
+*.dotCover
+
+# NCrunch
+*.ncrunch*
+_NCrunch_*
+.*crunch*.local.xml
+
+# MightyMoose
+*.mm.*
+AutoTest.Net/
+
+# Web workbench (sass)
+.sass-cache/
+
+# Installshield output folder
+[Ee]xpress/
+
+# DocProject is a documentation generator add-in
+DocProject/buildhelp/
+DocProject/Help/*.HxT
+DocProject/Help/*.HxC
+DocProject/Help/*.hhc
+DocProject/Help/*.hhk
+DocProject/Help/*.hhp
+DocProject/Help/Html2
+DocProject/Help/html
+
+# Click-Once directory
+publish/
+
+# Publish Web Output
+*.[Pp]ublish.xml
+*.azurePubxml
+
+# NuGet Packages Directory
+packages/*
+## TODO: If the tool you use requires repositories.config uncomment the next line
+#!packages/repositories.config
+
+# Enable "build/" folder in the NuGet Packages folder since NuGet packages use it for MSBuild targets
+# This line needs to be after the ignore of the build folder (and the packages folder if the line above has been uncommented)
+!packages/build/
+
+# Windows Azure Build Output
+csx/
+*.build.csdef
+
+# Windows Store app package directory
+AppPackages/
+
+# Others
+sql/
+*.Cache
+ClientBin/
+[Ss]tyle[Cc]op.*
+~$*
+*~
+*.dbmdl
+*.dbproj.schemaview
+*.pfx
+*.publishsettings
+node_modules/
+
+# RIA/Silverlight projects
+Generated_Code/
+
+# Backup & report files from converting an old project file to a newer
+# Visual Studio version. Backup files are not needed, because we have git ;-)
+_UpgradeReport_Files/
+Backup*/
+UpgradeLog*.XML
+UpgradeLog*.htm
+
+# SQL Server files
+*.mdf
+*.ldf
+
+# Business Intelligence projects
+*.rdl.data
+*.bim.layout
+*.bim_*.settings
+
+# Microsoft Fakes
+FakesAssemblies/
+
+# =========================
+# Windows detritus
+# =========================
+
+# Windows image file caches
+Thumbs.db
+ehthumbs.db
+
+# Folder config file
+Desktop.ini
+
+# Recycle Bin used on file shares
+$RECYCLE.BIN/

windows/ovs-windows-installer.sln
@@ -1,0 +1,19 @@
+?
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 2013
+VisualStudioVersion = 12.0.31101.0
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{930C7802-8A8C-48F9-8165-68863BCCD9DD}") = "ovs-windows-installer", "ovs-windows-installer\ovs-windows-installer.wixproj", "{259905A2-7434-4190-8A33-8FBA67171DD6}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{259905A2-7434-4190-8A33-8FBA67171DD6}.Release|x86.ActiveCfg = Release|x86
+		{259905A2-7434-4190-8A33-8FBA67171DD6}.Release|x86.Build.0 = Release|x86
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal

windows/ovs-windows-installer/Actions/OVSActions.js
@@ -1,0 +1,258 @@
+/*
+Copyright 2015 Cloudbase Solutions Srl
+All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License"); you may
+   not use this file except in compliance with the License. You may obtain
+   a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+   License for the specific language governing permissions and limitations
+   under the License.
+*/
+
+// http://msdn.microsoft.com/en-us/library/sfw6660x(VS.85).aspx
+var Buttons =
+{
+    OkOnly: 0,
+    OkCancel: 1,
+    AbortRetryIgnore: 2,
+    YesNoCancel: 3
+};
+
+var Icons =
+{
+    Critical: 16,
+    Question: 32,
+    Exclamation: 48,
+    Information: 64
+}
+
+var MsgKind =
+{
+    Error: 0x01000000,
+    Warning: 0x02000000,
+    User: 0x03000000,
+    Log: 0x04000000
+};
+
+// http://msdn.microsoft.com/en-us/library/aa371254(VS.85).aspx
+var MsiActionStatus =
+{
+    None: 0,
+    Ok: 1, // success
+    Cancel: 2,
+    Abort: 3,
+    Retry: 4, // aka suspend?
+    Ignore: 5  // skip remaining actions; this is not an error.
+};
+
+var ServiceStartAction = {
+    Stop: "Stop",
+    Start: "Start",
+    Restart: "Restart"
+};
+
+var ServiceStartMode = {
+    Boot: "Boot",
+    System: "System",
+    Auto: "Auto",
+    Manual: "Manual",
+    Disabled: "Disabled"
+};
+
+function throwException(num, msg) {
+    throw {
+        number: num,
+        message: msg
+    };
+}
+
+function decimalToHexString(number) {
+    if (number < 0)
+        number = 0xFFFFFFFF + number + 1;
+    return number.toString(16).toUpperCase();
+}
+
+function logMessage(msg) {
+    var record = Session.Installer.CreateRecord(0);
+    record.StringData(0) = "CustomActions: " + msg;
+    Session.Message(MsgKind.Log, record);
+}
+
+function logMessageEx(msg, type) {
+    var record = Session.Installer.CreateRecord(0);
+    record.StringData(0) = msg;
+    Session.Message(type, record);
+}
+
+function logException(exc) {
+    var record = Session.Installer.CreateRecord(0);
+    record.StringData(0) = exc.message == "" ? "An exception occurred: 0x" + decimalToHexString(exc.number) : exc.message;
+    Session.Message(MsgKind.Error + Icons.Critical + Buttons.OkOnly, record);
+
+    // Log the full exception as well
+    record.StringData(0) = "CustomAction exception details: 0x" + decimalToHexString(exc.number) + " : " + exc.message;
+    Session.Message(MsgKind.Log, record);
+}
+
+function runCommand(cmd, expectedReturnValue, envVars, windowStyle, waitOnReturn, workingDir) {
+    var shell = new ActiveXObject("WScript.Shell");
+    logMessage("Running command: " + cmd);
+
+    if (envVars) {
+        var env = shell.Environment("Process");
+        for (var k in envVars)
+            env(k) = envVars[k];
+    }
+
+    if (typeof windowStyle == 'undefined')
+        windowStyle = 0;
+
+    if (typeof waitOnReturn == 'undefined')
+        waitOnReturn = true;
+
+    if (typeof workingDir == 'undefined')
+        workingDir = null;
+
+    if (workingDir) {
+        shell.CurrentDirectory = workingDir;
+    }
+
+    var retVal = shell.run(cmd, windowStyle, waitOnReturn);
+
+    if (waitOnReturn && expectedReturnValue != undefined && expectedReturnValue != null && retVal != expectedReturnValue)
+        throwException(-1, "Command failed. Return value: " + retVal.toString());
+
+    logMessage("Command completed. Return value: " + retVal);
+
+    return retVal;
+}
+
+function getWmiCimV2Svc() {
+    return GetObject("winmgmts:\\\\.\\root\\cimv2");
+}
+
+function getSafeArray(jsArr) {
+    var dict = new ActiveXObject("Scripting.Dictionary");
+    for (var i = 0; i < jsArr.length; i++)
+        dict.add(i, jsArr[i]);
+    return dict.Items();
+}
+
+function invokeWMIMethod(svc, methodName, inParamsValues, wmiSvc, jobOutParamName) {
+    logMessage("Invoking " + methodName);
+
+    var inParams = null;
+    if (inParamsValues) {
+        for (var k in inParamsValues) {
+            if (!inParams)
+                inParams = svc.Methods_(methodName).InParameters.SpawnInstance_();
+            var val = inParamsValues[k];
+            if (val instanceof Array)
+                inParams[k] = getSafeArray(val);
+            else
+                inParams[k] = val;
+        }
+    }
+
+    var outParams = svc.ExecMethod_(methodName, inParams);
+    if (outParams.ReturnValue == 4096) {
+        var job = wmiSvc.Get(outParams[jobOutParamName]);
+        waitForJob(wmiSvc, job);
+    }
+    else if (outParams.ReturnValue != 0)
+        throwException(-1, methodName + " failed. Return value: " + outParams.ReturnValue.toString());
+
+    return outParams;
+}
+
+function sleep(interval) {
+    // WScript.Sleep is not supported in MSI's WSH. Here's a workaround for the moment.
+
+    // interval is ignored
+    var numPings = 2;
+    cmd = "ping -n " + numPings + " 127.0.0.1";
+
+    var shell = new ActiveXObject("WScript.Shell");
+    shell.run(cmd, 0, true);
+}
+
+function getService(serviceName) {
+    var wmiSvc = getWmiCimV2Svc();
+    return wmiSvc.ExecQuery("SELECT * FROM Win32_Service WHERE Name='" + serviceName + "'").ItemIndex(0);
+}
+
+function changeService(serviceName, startMode, startAction) {
+    var svc = getService(serviceName);
+
+    if ((startAction == ServiceStartAction.Stop || startAction == ServiceStartAction.Restart) && svc.Started)
+        invokeWMIMethod(svc, "StopService");
+
+    if (startMode && svc.StartMode != startMode)
+        invokeWMIMethod(svc, "ChangeStartMode",
+                {
+                    "StartMode": (startMode == ServiceStartMode.Auto ? "Automatic" : startMode)
+                });
+
+    if (startAction == ServiceStartAction.Restart && svc.Started) {
+        var wmiSvc = getWmiCimV2Svc();
+        do {
+            sleep(200);
+            svc = wmiSvc.Get(svc.Path_);
+        } while (svc.Started);
+    }
+
+    if ((startAction == ServiceStartAction.Start || startAction == ServiceStartAction.Restart) && !svc.Started)
+        invokeWMIMethod(svc, "StartService");
+}
+
+function runCommandAction() {
+    var exceptionMsg = null;
+
+    try {
+        var data = Session.Property("CustomActionData").split('|');
+        var i = 0;
+        var cmd = data[i++];
+        var expectedRetValue = data.length > i ? data[i++] : 0;
+        var exceptionMsg = data.length > i ? data[i++] : null;
+        var workingDir = data.length > i ? data[i++] : null;
+
+        runCommand(cmd, expectedRetValue, null, 0, true, workingDir);
+        return MsiActionStatus.Ok;
+    }
+    catch (ex) {
+        if (exceptionMsg) {
+            logMessageEx(exceptionMsg, MsgKind.Error + Icons.Critical + Buttons.OkOnly);
+            // log also the original exception
+            logMessage(ex.message);
+        }
+        else
+            logException(ex);
+
+        return MsiActionStatus.Abort;
+    }
+}
+
+function changeServiceAction() {
+    try {
+        var data = Session.Property("CustomActionData").split('|');
+        var serviceName = data[0];
+        var startMode = data[1];
+        var startAction = data[2];
+
+        logMessage("Changing service " + serviceName + ", startMode: " + startMode + ", startAction: " + startAction);
+
+        changeService(serviceName, startMode, startAction);
+
+        return MsiActionStatus.Ok;
+    }
+    catch (ex) {
+        logMessage(ex.message);
+        return MsiActionStatus.Abort;
+    }
+}

commit 5d3f609b65e0adc3761ca9c71fce8a9a9b6b32d7

windows: automake.mk: Remove the .gitignore file from distributed files
Commit d183efc22b2b ("This commit adds the windows installer to the
OVS tree.) added the .gitignore file to the distributed files but this
file shouldn't be part of the distributed archive.

CC: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
Fixes: d183efc22b2b ("This commit adds the windows installer to the OVS tree.")
Signed-off-by: Markos Chandras <mchandras@suse.de>
Signed-off-by: Ben Pfaff <blp@ovn.org>

windows/automake.mk
@@ -38,7 +38,6 @@ windows_installer: all
 	MSBuild.exe windows/ovs-windows-installer.sln /target:Build /property:Configuration="Release" /property:Version="$(PACKAGE_VERSION)"
 
 EXTRA_DIST += \
-	windows/.gitignore \
 	windows/automake.mk \
 	windows/README.rst \
 	windows/ovs-windows-installer.sln \