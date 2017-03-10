openvswitch/ovs

commit 2ae3d5421b40f9054d61a4bdb9daf06fd2623f54

netdev-dpdk: Refactor dpdk_mp_get().
The error handling path in dpdk_mp_get() is getting complicated, it
even requires a boolean variable.

Simplify it by extracting the function dpdk_mp_create().

CC: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>
Acked-by: Ben Pfaff <blp@ovn.org>
Acked-by: Aaron Conole <aconole@redhat.com>
Tested-by: Aaron Conole <aconole@redhat.com>

lib/netdev-dpdk.c
@@ -478,21 +478,12 @@ ovs_rte_pktmbuf_init(struct rte_mempool *mp,
 }
 
 static struct dpdk_mp *
-dpdk_mp_get(int socket_id, int mtu)
+dpdk_mp_create(int socket_id, int mtu)
 {
-    struct dpdk_mp *dmp = NULL;
-    char mp_name[RTE_MEMPOOL_NAMESIZE];
-    unsigned mp_size;
     struct rte_pktmbuf_pool_private mbp_priv;
-    bool failed = false;
-
-    ovs_mutex_lock(&dpdk_mp_mutex);
-    LIST_FOR_EACH (dmp, list_node, &dpdk_mp_list) {
-        if (dmp->socket_id == socket_id && dmp->mtu == mtu) {
-            dmp->refcount++;
-            goto out;
-        }
-    }
+    char mp_name[RTE_MEMPOOL_NAMESIZE];
+    struct dpdk_mp *dmp;
+    unsigned mp_size;
 
     dmp = dpdk_rte_mzalloc(sizeof *dmp);
     dmp->socket_id = socket_id;
@@ -502,7 +493,7 @@ dpdk_mp_get(int socket_id, int mtu)
     mbp_priv.mbuf_priv_size = sizeof(struct dp_packet)
                               - sizeof(struct rte_mbuf);
     /* XXX: this is a really rough method of provisioning memory.
-     * It's impossible to determine what the exact memory requirements are when
+     * It's impossible to determine what the exact memory requirements are
      * when the number of ports and rxqs that utilize a particular mempool can
      * change dynamically at runtime. For now, use this rough heurisitic.
      */
@@ -515,7 +506,6 @@ dpdk_mp_get(int socket_id, int mtu)
     do {
         if (snprintf(mp_name, RTE_MEMPOOL_NAMESIZE, "ovs_mp_%d_%d_%u",
                      dmp->mtu, dmp->socket_id, mp_size) < 0) {
-            failed = true;
             goto out;
         }
 
@@ -528,21 +518,37 @@ dpdk_mp_get(int socket_id, int mtu)
     } while (!dmp->mp && rte_errno == ENOMEM && (mp_size /= 2) >= MIN_NB_MBUF);
 
     if (dmp->mp == NULL) {
-        failed = true;
         goto out;
     } else {
-        VLOG_DBG("Allocated \"%s\" mempool with %u mbufs", mp_name, mp_size );
+        VLOG_DBG("Allocated \"%s\" mempool with %u mbufs", mp_name, mp_size);
     }
 
+    return dmp;
+
+out:
+    rte_free(dmp);
+    return NULL;
+}
+
+static struct dpdk_mp *
+dpdk_mp_get(int socket_id, int mtu)
+{
+    struct dpdk_mp *dmp;
+
+    ovs_mutex_lock(&dpdk_mp_mutex);
+    LIST_FOR_EACH (dmp, list_node, &dpdk_mp_list) {
+        if (dmp->socket_id == socket_id && dmp->mtu == mtu) {
+            dmp->refcount++;
+            goto out;
+        }
+    }
+
+    dmp = dpdk_mp_create(socket_id, mtu);
     ovs_list_push_back(&dpdk_mp_list, &dmp->list_node);
 
 out:
     ovs_mutex_unlock(&dpdk_mp_mutex);
 
-    if (failed) {
-        rte_free(dmp);
-        return NULL;
-    }
     return dmp;
 }
 

commit 96e9b168e0ed45823616eddbe879e06976ecfb98

netdev-dpdk: Fix mempool segfault.
The dpdk_mp_get() function can return a NULL pointer which leads to a
segfault when a mempool cannot be created. The lack of a return value
check for the function netdev_dpdk_mempool_configure() when called in
netdev_dpdk_reconfigure() can result in a segfault also as
a NULL pointer for the mempool will be passed to rte_eth_rx_queue_setup().

Fix this by adding appropriate NULL pointer and return value checks to
dpdk_mp_get(), netdev_dpdk_reconfigure() and dpdk_vhost_reconfigure_helper().

Signed-off-by: Ian Stokes <ian.stokes@intel.com>
Fixes: 2ae3d542 ("netdev-dpdk: Refactor dpdk_mp_get().")
Fixes: 0072e931 ("netdev-dpdk: add support for jumbo frames")
CC: Daniele Di Proietto <diproiettod@vmware.com>
CC: Mark Kavanagh <mark.b.kavanagh@intel.com>
Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>

lib/netdev-dpdk.c
@@ -530,7 +530,9 @@ dpdk_mp_get(int socket_id, int mtu)
     }
 
     dmp = dpdk_mp_create(socket_id, mtu);
-    ovs_list_push_back(&dpdk_mp_list, &dmp->list_node);
+    if (dmp) {
+        ovs_list_push_back(&dpdk_mp_list, &dmp->list_node);
+    }
 
 out:
     ovs_mutex_unlock(&dpdk_mp_mutex);
@@ -3131,7 +3133,10 @@ netdev_dpdk_reconfigure(struct netdev *netdev)
 
     if (dev->mtu != dev->requested_mtu
         || dev->socket_id != dev->requested_socket_id) {
-        netdev_dpdk_mempool_configure(dev);
+        err = netdev_dpdk_mempool_configure(dev);
+        if (err) {
+            goto out;
+        }
     }
 
     netdev->n_txq = dev->requested_n_txq;
@@ -3160,6 +3165,7 @@ dpdk_vhost_reconfigure_helper(struct netdev_dpdk *dev)
 {
     dev->up.n_txq = dev->requested_n_txq;
     dev->up.n_rxq = dev->requested_n_rxq;
+    int err;
 
     /* Enable TX queue 0 by default if it wasn't disabled. */
     if (dev->tx_q[0].map == OVS_VHOST_QUEUE_MAP_UNKNOWN) {
@@ -3170,13 +3176,12 @@ dpdk_vhost_reconfigure_helper(struct netdev_dpdk *dev)
 
     if (dev->requested_socket_id != dev->socket_id
         || dev->requested_mtu != dev->mtu) {
-        if (!netdev_dpdk_mempool_configure(dev)) {
+        err = netdev_dpdk_mempool_configure(dev);
+        if (err) {
+            return err;
+        } else {
             netdev_change_seq_changed(&dev->up);
         }
-    }
-
-    if (!dev->dpdk_mp) {
-        return ENOMEM;
     }
 
     if (netdev_dpdk_get_vid(dev) >= 0) {