882072d77a2213c28555d39b95259980622cd8e6
Ben Pfaff <blp@ovn.org>
2017-04-14 12:38:12 -0700

    doc: Avoid need to generate conf.py.
    It's awkward to have to at the same time generate conf.py from conf.py.in
    and to keep both versions in the repository.  This avoids the issue.
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Stephen Finucane <stephen@that.guru>

--- Documentation/automake.mk
+++ Documentation/automake.mk
@@ -3,7 +3,6 @@ DOC_SOURCE = \
 	Documentation/_static/logo.png \
 	Documentation/_static/overview.png \
 	Documentation/conf.py \
-	Documentation/conf.py.in \
 	Documentation/index.rst \
 	Documentation/contents.rst \
 	Documentation/intro/index.rst \
@@ -125,15 +124,6 @@ clean-docs:
 	rm -rf $(SPHINXBUILDDIR)/linkcheck
 	rm -f docs-check
 CLEAN_LOCAL += clean-docs
-
-ALL_LOCAL += $(srcdir)/Documentation/conf.py
-$(srcdir)/Documentation/conf.py: $(srcdir)/Documentation/conf.py.in
-	$(AM_V_GEN)($(ro_shell) && sed -e 's,[@]VERSION[@],$(VERSION),g' \
-		-e 's,[@]OVS_MAJOR[@],$(shell echo $(VERSION) | cut -f1 -d.),g' \
-		-e 's,[@]OVS_MINOR[@],$(shell echo $(VERSION) | cut -f2 -d.),g') \
-		< $(srcdir)/Documentation/$(@F).in > $(@F).tmp || exit 1; \
-	if cmp -s $(@F).tmp $@; then touch $@; rm $(@F).tmp; else mv $(@F).tmp $@; fi
-
 endif
 .PHONY: check-docs
 .PHONY: clean-docs
