bf2526d7e31a0a7a798a5c831c1c0b2be7e87725
Stephen Finucane <stephen@that.guru>
2017-04-18 11:26:49 +0100

    doc: Don't override default theme
    Sphinx 1.3 renamed the 'default' theme to 'classic' and configured the
    'alabaster' theme as the new default. To prevent breaking existing
    builds, the 'default' name was reserved as an alias for 'classic' [1].
    However, initially this raised a warning [1] with a message to use
    'classic' instead. This warning was removed in 1.3.2 [2], but it will
    result in errors (due to the use of the '-W' flag) for Sphinx 1.3.0 and
    1.3.1 users.
    
    Mitigate the issue by not setting a theme if the 'ovs_sphinx_theme'
    package is absent. This will result in Sphinx using its default theme,
    be that 'classic' (Sphinx < 1.3) or 'alabaster'.
    
    [1] https://github.com/sphinx-doc/sphinx/commit/68021b0bd
    [2] https://github.com/sphinx-doc/sphinx/commit/034c4e942
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Cc: Matthew Thode <mthode@mthode.org>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -126,8 +126,6 @@ linkcheck_anchors = False
 #
 if use_ovs_theme:
     html_theme = 'ovs'
-else:
-    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
