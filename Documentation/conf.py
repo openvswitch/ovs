openvswitch/ovs

commit f15010f6631fe6a7f960ab4a8fca6f777d92cf92

doc: Reduce duplication in 'man_pages'
All these entries are going to be roughly the same, with only two key
differences. Clarify things by focusing on those differences.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Signed-off-by: Ben Pfaff <blp@ovn.org>

Documentation/conf.py
@@ -112,11 +112,15 @@ html_static_path = ['_static']
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('ref/ovs-test.8', 'ovs-test',
-     u'Check Linux drivers for performance, vlan and L3 tunneling problems',
-     [author], 8),
-    ('ref/ovs-vlan-test.8', 'ovs-vlan-test',
-     u'Check Linux drivers for problems with vlan traffic',
-     [author], 8)
+_man_pages = [
+    ('ovs-test.8',
+     u'Check Linux drivers for performance, vlan and L3 tunneling problems'),
+    ('ovs-vlan-test.8',
+     u'Check Linux drivers for problems with vlan traffic'),
 ]
+
+# Generate list of (path, name, description, [author, ...], section)
+man_pages = [
+    ('ref/%s' % filename, filename.split('.', 1)[0],
+     description, [author], filename.split('.', 1)[1])
+    for filename, description in _man_pages]

commit 56cb734293acc46a2976c67f49107126669be52f

doc: Resolve pep8 warnings in conf.py
flake8 doesn't like us redefining variables in loops.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Reported-by: Bhanuprakash Bodireddy <bhanuprakash.bodireddy@intel.com>
Fixes: f15010f ("doc: Reduce duplication in 'man_pages'")
Acked-by: Bhanuprakash Bodireddy <Bhanuprakash.bodireddy@intel.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

Documentation/conf.py
@@ -121,6 +121,6 @@ _man_pages = [
 
 # Generate list of (path, name, description, [author, ...], section)
 man_pages = [
-    ('ref/%s' % filename, filename.split('.', 1)[0],
-     description, [author], filename.split('.', 1)[1])
-    for filename, description in _man_pages]
+    ('ref/%s' % file_name, file_name.split('.', 1)[0],
+     description, [author], file_name.split('.', 1)[1])
+    for file_name, description in _man_pages]