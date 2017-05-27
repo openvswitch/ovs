5978c2e31574cb5a97931e36e6725ce3ab7d7df6
Ben Pfaff <blp@ovn.org>
2017-05-26 22:53:19 -0700

    ofproto: Use shash_find_and_delete() in ofport_destroy__().
    This is shorter and slightly safer than combining calls to shash_find()
    and shash_delete().
    
    Found by Coverity.
    
    Reported-at: https://scan3.coverity.com/reports.htm#v16889/p10449/fileInstanceId=14762983&defectInstanceId=4305289&mergedDefectId=179850
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Justin Pettit <jpettit@ovn.org>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -2511,8 +2511,7 @@ ofport_destroy__(struct ofport *port)
     const char *name = netdev_get_name(port->netdev);
 
     hmap_remove(&ofproto->ports, &port->hmap_node);
-    shash_delete(&ofproto->port_by_name,
-                 shash_find(&ofproto->port_by_name, name));
+    shash_find_and_delete(&ofproto->port_by_name, name);
 
     netdev_close(port->netdev);
     ofproto->ofproto_class->port_dealloc(port);
