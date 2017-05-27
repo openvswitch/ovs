7d170821184c578e0146e3796ffc03e9bf0a0eba
Ben Pfaff <blp@ovn.org>
2017-05-26 22:39:02 -0700

    ovn-northd: Avoid null deref for missing outport in build_static_route_flow().
    Found by Coverity.
    
    Reported-at: https://scan3.coverity.com/reports.htm#v16889/p10449/fileInstanceId=14763080&defectInstanceId=4305186&mergedDefectId=179788
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Miguel Angel Ajo <majopela@redhat.com>

--- ovn/northd/ovn-northd.c
+++ ovn/northd/ovn-northd.c
@@ -3966,7 +3966,7 @@ build_static_route_flow(struct hmap *lflows, struct ovn_datapath *od,
         }
     }
 
-     if (!lrp_addr_s) {
+    if (!out_port || !lrp_addr_s) {
         /* There is no matched out port. */
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
         VLOG_WARN_RL(&rl, "No path for static route %s; next hop %s",
