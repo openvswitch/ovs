openvswitch/ovs

commit 69876ed78611844d6db4b6b62c9a229f185e3fec

netdev-dpdk: Add support for virtual DPDK PMDs (vdevs)
Prior to this commit, the 'dpdk' port type could only be used for
physical DPDK devices. Now, virtual devices (or 'vdevs') are supported.
'vdev' devices are those which use virtual DPDK Poll Mode Drivers eg.
null, pcap. To add a DPDK vdev, a valid 'dpdk-devargs' must be set for
the given dpdk port. The format expected is 'eth_<driver_name><x>' where
'x' is a number between 0 and RTE_MAX_ETHPORTS -1.

For example to add a port that uses the 'null' DPDK PMD driver:

ovs-vsctl set Interface null0 options:dpdk-devargs=eth_null0

Not all DPDK vdevs have been verified to work at this point in time.

Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>
Acked-by: Stephen Finucane <stephen@that.guru>  # docs only
Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>

lib/netdev-dpdk.c
@@ -1134,25 +1134,19 @@ netdev_dpdk_lookup_by_port_id(int port_id)
 static int
 netdev_dpdk_process_devargs(const char *devargs)
 {
-    struct rte_pci_addr addr;
     uint8_t new_port_id = UINT8_MAX;
 
-    if (!eal_parse_pci_DomBDF(devargs, &addr)) {
-        /* Valid PCI address format detected - configure physical device */
-        if (!rte_eth_dev_count()
-                || rte_eth_dev_get_port_by_name(devargs, &new_port_id)
-                || !rte_eth_dev_is_valid_port(new_port_id)) {
-            /* PCI device not found in DPDK, attempt to attach it */
-            if (!rte_eth_dev_attach(devargs, &new_port_id)) {
-                /* Attach successful */
-                VLOG_INFO("Device "PCI_PRI_FMT" has been attached to DPDK",
-                          addr.domain, addr.bus, addr.devid, addr.function);
-            } else {
-                /* Attach unsuccessful */
-                VLOG_INFO("Error attaching device "PCI_PRI_FMT" to DPDK",
-                          addr.domain, addr.bus, addr.devid, addr.function);
-                return -1;
-            }
+    if (!rte_eth_dev_count()
+            || rte_eth_dev_get_port_by_name(devargs, &new_port_id)
+            || !rte_eth_dev_is_valid_port(new_port_id)) {
+        /* Device not found in DPDK, attempt to attach it */
+        if (!rte_eth_dev_attach(devargs, &new_port_id)) {
+            /* Attach successful */
+            VLOG_INFO("Device '%s' attached to DPDK", devargs);
+        } else {
+            /* Attach unsuccessful */
+            VLOG_INFO("Error attaching device '%s' to DPDK", devargs);
+            return -1;
         }
     }
 
@@ -2460,16 +2454,9 @@ netdev_dpdk_detach(struct unixctl_conn *conn, int argc OVS_UNUSED,
     char *response;
     uint8_t port_id;
     char devname[RTE_ETH_NAME_MAX_LEN];
-    struct rte_pci_addr addr;
     struct netdev_dpdk *dev;
 
     ovs_mutex_lock(&dpdk_mutex);
-
-    if (eal_parse_pci_DomBDF(argv[1], &addr)) {
-        response = xasprintf("Invalid PCI address '%s'. Cannot detach.",
-                             argv[1]);
-        goto error;
-    }
 
     if (!rte_eth_dev_count() || rte_eth_dev_get_port_by_name(argv[1],
                                                              &port_id)) {

vswitchd/vswitch.xml
@@ -2306,8 +2306,13 @@
       <column name="options" key="dpdk-devargs"
               type='{"type": "string"}'>
         <p>
-          Specifies the PCI address of a physical dpdk device.
-          Only supported by 'dpdk' devices.
+          Specifies the PCI address associated with the port for physical
+          devices, or the virtual driver to be used for the port when a virtual
+          PMD is intended to be used. For the latter, the argument string
+          typically takes the form of eth_&lt;driver_name&gt;&lt;x&gt; where
+          'driver_name' is a valid virtual DPDK PMD driver name and where 'x'
+          lies in the range of 0 to RTE_MAX_ETHPORTS-1.
+          Only supported by the dpdk port type.
         </p>
       </column>
 

commit 03d6399e618e4136c5da0be2b6f18f0b7d75b2bb

netdev-dpdk: Fix double attaching of virtual devices.
'devargs' for virtual devices contains not only name but
also a list of arguments like this:

	'net_pcap0,rx_pcap=file_rx.pcap,tx_pcap=file_tx.pcap'
	or
	'eth_af_packet0,iface=eth0'

We must cut off the arguments from this string before calling
'rte_eth_dev_get_port_by_name()' to avoid double attaching of
the same device.

CC: Ciara Loftus <ciara.loftus@intel.com>
Fixes: 69876ed78611 ("netdev-dpdk: Add support for virtual DPDK PMDs (vdevs)")
Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Tested-by: Billy O'Mahony <billy.o.mahony@intel.com>
Acked-by: Billy O'Mahony <billy.o.mahony@intel.com>

lib/netdev-dpdk.c
@@ -1115,10 +1115,12 @@ netdev_dpdk_lookup_by_port_id(int port_id)
 static int
 netdev_dpdk_process_devargs(const char *devargs, char **errp)
 {
+    /* Get the name up to the first comma. */
+    char *name = xmemdup0(devargs, strcspn(devargs, ","));
     uint8_t new_port_id = UINT8_MAX;
 
     if (!rte_eth_dev_count()
-            || rte_eth_dev_get_port_by_name(devargs, &new_port_id)
+            || rte_eth_dev_get_port_by_name(name, &new_port_id)
             || !rte_eth_dev_is_valid_port(new_port_id)) {
         /* Device not found in DPDK, attempt to attach it */
         if (!rte_eth_dev_attach(devargs, &new_port_id)) {
@@ -1128,10 +1130,11 @@ netdev_dpdk_process_devargs(const char *devargs, char **errp)
             /* Attach unsuccessful */
             VLOG_WARN_BUF(errp, "Error attaching device '%s' to DPDK",
                           devargs);
-            return -1;
+            new_port_id = UINT8_MAX;
         }
     }
 
+    free(name);
     return new_port_id;
 }
 