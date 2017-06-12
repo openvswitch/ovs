fb1922532f4ec72d0572e8b8f1745f645839294e
Ben Pfaff <blp@ovn.org>
2017-06-12 08:35:48 -0700

    ofp-actions: Properly interpret "output:in_port".
    It was being misinterpreted as output:NXM_OF_IN_PORT[].  This
    interpretation is incorrect because of OpenFlow rules that say that only
    the special form generated by output:in_port (or "in_port" on its own)
    actually outputs to the input port.  The interpretation here was a no-op.
    
    Fixes: 21b2fa617126 ("ofp-parse: Allow match field names in actions and brackets in matches.")
    Reported-by: nickcooper-zhangtonghao <nic@opencloud.tech>
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Jarno Rajahalme <jarno@ovn.org>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/ofp-actions.c
+++ lib/ofp-actions.c
@@ -626,27 +626,29 @@ parse_OUTPUT(const char *arg, struct ofpbuf *ofpacts,
 
         output_trunc = ofpact_put_OUTPUT_TRUNC(ofpacts);
         return parse_truncate_subfield(output_trunc, arg);
-    } else {
-        struct mf_subfield src;
-        char *error = mf_parse_subfield(&src, arg);
-        if (!error) {
-            struct ofpact_output_reg *output_reg;
+    }
 
-            output_reg = ofpact_put_OUTPUT_REG(ofpacts);
-            output_reg->max_len = UINT16_MAX;
-            output_reg->src = src;
-        } else {
-            free(error);
-            struct ofpact_output *output;
-
-            output = ofpact_put_OUTPUT(ofpacts);
-            if (!ofputil_port_from_string(arg, &output->port)) {
-                return xasprintf("%s: output to unknown port", arg);
-            }
-            output->max_len = output->port == OFPP_CONTROLLER ? UINT16_MAX : 0;
-        }
+    ofp_port_t port;
+    if (ofputil_port_from_string(arg, &port)) {
+        struct ofpact_output *output = ofpact_put_OUTPUT(ofpacts);
+        output->port = port;
+        output->max_len = output->port == OFPP_CONTROLLER ? UINT16_MAX : 0;
         return NULL;
     }
+
+    struct mf_subfield src;
+    char *error = mf_parse_subfield(&src, arg);
+    if (!error) {
+        struct ofpact_output_reg *output_reg;
+
+        output_reg = ofpact_put_OUTPUT_REG(ofpacts);
+        output_reg->max_len = UINT16_MAX;
+        output_reg->src = src;
+        return NULL;
+    }
+    free(error);
+
+    return xasprintf("%s: output to unknown port", arg);
 }
 
 static void
