8140a5ff687489f6c35bf3b5c00d39db18c8ed73
Paul Blakey <paulb@mellanox.com>
2017-06-13 18:03:33 +0300

    netdev-tc-offloads: Implement netdev flow flush using tc interface
    Signed-off-by: Paul Blakey <paulb@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Acked-by: Flavio Leitner <fbl@sysclose.org>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- lib/netdev-tc-offloads.c
+++ lib/netdev-tc-offloads.c
@@ -36,10 +36,20 @@
 
 VLOG_DEFINE_THIS_MODULE(netdev_tc_offloads);
 
+static struct vlog_rate_limit error_rl = VLOG_RATE_LIMIT_INIT(60, 5);
+
 int
-netdev_tc_flow_flush(struct netdev *netdev OVS_UNUSED)
+netdev_tc_flow_flush(struct netdev *netdev)
 {
-    return EOPNOTSUPP;
+    int ifindex = netdev_get_ifindex(netdev);
+
+    if (ifindex < 0) {
+        VLOG_ERR_RL(&error_rl, "failed to get ifindex for %s: %s",
+                    netdev_get_name(netdev), ovs_strerror(-ifindex));
+        return -ifindex;
+    }
+
+    return tc_flush(ifindex);
 }
 
 int
