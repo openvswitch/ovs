5ca89127382d3b0651383330e616b358a34a0dcf
Russell Bryant <russell@ovn.org>
2017-07-20 20:23:21 -0400

    docs: Refer to correct package name for sphinx theme.
    Update the log message emitted when the OVS sphinx theme is not found
    to reference the name of the package to be installed via pip:
    ovs-sphinx-theme.
    
    Signed-off-by: Russell Bryant <russell@ovn.org>
    Acked-by: Lance Richardson <lrichard@redhat.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -19,7 +19,7 @@ try:
     import ovs_sphinx_theme
     use_ovs_theme = True
 except ImportError:
-    print("Cannot find 'ovs_sphinx' package. Falling back to default theme.")
+    print("Cannot find 'ovs-sphinx-theme' package. Falling back to default theme.")
     use_ovs_theme = False
 
 # -- General configuration ------------------------------------------------
