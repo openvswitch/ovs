openvswitch/ovs

commit 5ca89127382d3b0651383330e616b358a34a0dcf

docs: Refer to correct package name for sphinx theme.
Update the log message emitted when the OVS sphinx theme is not found
to reference the name of the package to be installed via pip:
ovs-sphinx-theme.

Signed-off-by: Russell Bryant <russell@ovn.org>
Acked-by: Lance Richardson <lrichard@redhat.com>

Documentation/conf.py
@@ -19,7 +19,7 @@ try:
     import ovs_sphinx_theme
     use_ovs_theme = True
 except ImportError:
-    print("Cannot find 'ovs_sphinx' package. Falling back to default theme.")
+    print("Cannot find 'ovs-sphinx-theme' package. Falling back to default theme.")
     use_ovs_theme = False
 
 # -- General configuration ------------------------------------------------

commit c302537fb428252b8051f0b24dc029a6260bf458

Documentation/conf.py: Fix line length.
A previous commit introduced a line that was greater than 79
characters long, causing a flake8 warning to be emitted.

Reported-by: Joe Stringer <joe@ovn.org>
Fixes: 5ca89127382d ("docs: Refer to correct package name for sphinx theme.")
Signed-off-by: Russell Bryant <russell@ovn.org>

Documentation/conf.py
@@ -19,7 +19,8 @@ try:
     import ovs_sphinx_theme
     use_ovs_theme = True
 except ImportError:
-    print("Cannot find 'ovs-sphinx-theme' package. Falling back to default theme.")
+    print("Cannot find 'ovs-sphinx-theme' package. "
+          "Falling back to default theme.")
     use_ovs_theme = False
 
 # -- General configuration ------------------------------------------------