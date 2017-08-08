openvswitch/ovs

commit 50f96b10e1c87db9fbe4df297f9b2fea13436bc0

Support accepting and displaying port names in OVS tools.
Until now, most ovs-ofctl commands have not accepted names for ports, only
numbers, and have not been able to display port names either.  It's a lot
easier for users if they can use and see meaningful names instead of
arbitrary numbers.  This commit adds that support.

For backward compatibility, only interactive ovs-ofctl commands by default
display port names; to display them in scripts, use the new --names
option.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Tested-by: Aaron Conole <aconole@redhat.com>

include/openvswitch/ofp-print.h
@@ -37,25 +37,31 @@ struct dp_packet;
 extern "C" {
 #endif
 
-void ofp_print(FILE *, const void *, size_t, int verbosity);
+void ofp_print(FILE *, const void *, size_t, const struct ofputil_port_map *,
+               int verbosity);
 void ofp_print_packet(FILE *stream, const void *data,
                       size_t len, ovs_be32 packet_type);
 void ofp_print_dp_packet(FILE *stream, const struct dp_packet *packet);
 
-void ofp10_match_print(struct ds *, const struct ofp10_match *, int verbosity);
+void ofp10_match_print(struct ds *, const struct ofp10_match *,
+                       const struct ofputil_port_map *, int verbosity);
 
-char *ofp_to_string(const void *, size_t, int verbosity);
-char *ofp10_match_to_string(const struct ofp10_match *, int verbosity);
+char *ofp_to_string(const void *, size_t, const struct ofputil_port_map *,
+                    int verbosity);
+char *ofp10_match_to_string(const struct ofp10_match *,
+                            const struct ofputil_port_map *, int verbosity);
 char *ofp_packet_to_string(const void *data, size_t len, ovs_be32 packet_type);
 char *ofp_dp_packet_to_string(const struct dp_packet *packet);
 
-void ofp_print_flow_stats(struct ds *, const struct ofputil_flow_stats *);
 void ofp_print_version(const struct ofp_header *, struct ds *);
 void ofp_print_table_features(
     struct ds *, const struct ofputil_table_features *features,
     const struct ofputil_table_features *prev_features,
     const struct ofputil_table_stats *stats,
     const struct ofputil_table_stats *prev_stats);
+
+void ofp_print_flow_stats(struct ds *, const struct ofputil_flow_stats *,
+                          const struct ofputil_port_map *);
 
 #ifdef  __cplusplus
 }

include/openvswitch/match.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 Nicira, Inc.
+ * Copyright (c) 2009-2017 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@
 #include "openvswitch/tun-metadata.h"
 
 struct ds;
+struct ofputil_port_map;
 
 /* A flow classification match.
  *
@@ -186,9 +187,11 @@ uint32_t match_hash(const struct match *, uint32_t basis);
 void match_init_hidden_fields(struct match *);
 bool match_has_default_hidden_fields(const struct match *);
 
-void match_format(const struct match *, struct ds *, int priority);
-char *match_to_string(const struct match *, int priority);
-void match_print(const struct match *);
+void match_format(const struct match *, const struct ofputil_port_map *,
+                  struct ds *, int priority);
+char *match_to_string(const struct match *, const struct ofputil_port_map *,
+                      int priority);
+void match_print(const struct match *, const struct ofputil_port_map *);
 
 /* Compressed match. */
 
@@ -228,7 +231,9 @@ bool minimatch_equal(const struct minimatch *a, const struct minimatch *b);
 bool minimatch_matches_flow(const struct minimatch *, const struct flow *);
 
 void minimatch_format(const struct minimatch *, const struct tun_table *,
+                      const struct ofputil_port_map *,
                       struct ds *, int priority);
-char *minimatch_to_string(const struct minimatch *, int priority);
+char *minimatch_to_string(const struct minimatch *,
+                          const struct ofputil_port_map *, int priority);
 
 #endif /* match.h */

include/openvswitch/meta-flow.h
@@ -30,6 +30,7 @@
 
 struct ds;
 struct match;
+struct ofputil_port_map;
 struct ofputil_tlv_table_mod;
 
 /* Open vSwitch fields
@@ -2101,10 +2102,13 @@ enum ofperr mf_check_dst(const struct mf_subfield *, const struct match *);
 
 /* Parsing and formatting. */
 char *mf_parse(const struct mf_field *, const char *,
+               const struct ofputil_port_map *,
                union mf_value *value, union mf_value *mask);
-char *mf_parse_value(const struct mf_field *, const char *, union mf_value *);
+char *mf_parse_value(const struct mf_field *, const char *,
+                     const struct ofputil_port_map *, union mf_value *);
 void mf_format(const struct mf_field *,
                const union mf_value *value, const union mf_value *mask,
+               const struct ofputil_port_map *,
                struct ds *);
 void mf_format_subvalue(const union mf_subvalue *subvalue, struct ds *s);
 

include/openvswitch/ofp-actions.h
@@ -1017,11 +1017,14 @@ const struct mf_field *ofpact_get_mf_dst(const struct ofpact *ofpact);
 uint32_t ofpacts_get_meter(const struct ofpact[], size_t ofpacts_len);
 
 /* Formatting and parsing ofpacts. */
-void ofpacts_format(const struct ofpact[], size_t ofpacts_len, struct ds *);
-char *ofpacts_parse_actions(const char *, struct ofpbuf *ofpacts,
+void ofpacts_format(const struct ofpact[], size_t ofpacts_len,
+                    const struct ofputil_port_map *, struct ds *);
+char *ofpacts_parse_actions(const char *, const struct ofputil_port_map *,
+                            struct ofpbuf *ofpacts,
                             enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
-char *ofpacts_parse_instructions(const char *, struct ofpbuf *ofpacts,
+char *ofpacts_parse_instructions(const char *, const struct ofputil_port_map *,
+                                 struct ofpbuf *ofpacts,
                                  enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 const char *ofpact_name(enum ofpact_type);

include/openvswitch/ofp-parse.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, 2012, 2013, 2014, 2015, 2016 Nicira, Inc.
+ * Copyright (c) 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,15 +40,17 @@ struct simap;
 enum ofputil_protocol;
 
 char *parse_ofp_str(struct ofputil_flow_mod *, int command, const char *str_,
+                    const struct ofputil_port_map *,
                     enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_flow_mod_str(struct ofputil_flow_mod *, const char *string,
-                             int command,
+                             const struct ofputil_port_map *, int command,
                              enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_packet_out_str(struct ofputil_packet_out *po, const char *str_,
+                               const struct ofputil_port_map *,
                                enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
@@ -57,19 +59,21 @@ char *parse_ofp_table_mod(struct ofputil_table_mod *,
                           uint32_t *usable_versions)
     OVS_WARN_UNUSED_RESULT;
 
-char *parse_ofp_flow_mod_file(const char *file_name, int command,
+char *parse_ofp_flow_mod_file(const char *file_name,
+                              const struct ofputil_port_map *, int command,
                               struct ofputil_flow_mod **fms, size_t *n_fms,
                               enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_flow_stats_request_str(struct ofputil_flow_stats_request *,
                                        bool aggregate, const char *string,
+                                       const struct ofputil_port_map *,
                                        enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_exact_flow(struct flow *flow, struct flow_wildcards *wc,
                            const struct tun_table *tun_table, const char *s,
-                           const struct simap *portno_names);
+                           const struct ofputil_port_map *port_map);
 
 char *parse_ofp_meter_mod_str(struct ofputil_meter_mod *, const char *string,
                               int command,
@@ -78,20 +82,24 @@ char *parse_ofp_meter_mod_str(struct ofputil_meter_mod *, const char *string,
 
 char *parse_flow_monitor_request(struct ofputil_flow_monitor_request *,
                                  const char *,
+                                 const struct ofputil_port_map *port_map,
                                  enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
-char *parse_ofp_group_mod_file(const char *file_name, int command,
+char *parse_ofp_group_mod_file(const char *file_name,
+                               const struct ofputil_port_map *, int command,
                                struct ofputil_group_mod **gms, size_t *n_gms,
                                enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_group_mod_str(struct ofputil_group_mod *, int command,
                               const char *string,
+                              const struct ofputil_port_map *,
                               enum ofputil_protocol *usable_protocols)
     OVS_WARN_UNUSED_RESULT;
 
 char *parse_ofp_bundle_file(const char *file_name,
+                            const struct ofputil_port_map *,
                             struct ofputil_bundle_msg **, size_t *n_bms,
                             enum ofputil_protocol *)
     OVS_WARN_UNUSED_RESULT;

include/openvswitch/ofp-util.h
@@ -21,6 +21,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "openvswitch/flow.h"
+#include "openvswitch/hmap.h"
 #include "openvswitch/list.h"
 #include "openvswitch/match.h"
 #include "openvswitch/meta-flow.h"
@@ -38,15 +39,36 @@ union ofp_action;
 struct ofpact_set_field;
 struct vl_mff_map;
 
+/* Mapping between port numbers and names. */
+struct ofputil_port_map {
+    struct hmap by_name;
+    struct hmap by_number;
+};
+
+#define OFPUTIL_PORT_MAP_INITIALIZER(MAP)  \
+    { HMAP_INITIALIZER(&(MAP)->by_name), HMAP_INITIALIZER(&(MAP)->by_number) }
+
+void ofputil_port_map_init(struct ofputil_port_map *);
+const char *ofputil_port_map_get_name(const struct ofputil_port_map *,
+                                      ofp_port_t);
+ofp_port_t ofputil_port_map_get_number(const struct ofputil_port_map *,
+                                      const char *name);
+void ofputil_port_map_put(struct ofputil_port_map *,
+                          ofp_port_t, const char *name);
+void ofputil_port_map_destroy(struct ofputil_port_map *);
+
 /* Port numbers. */
 enum ofperr ofputil_port_from_ofp11(ovs_be32 ofp11_port,
                                     ofp_port_t *ofp10_port);
 ovs_be32 ofputil_port_to_ofp11(ofp_port_t ofp10_port);
 
-bool ofputil_port_from_string(const char *, ofp_port_t *portp);
-void ofputil_format_port(ofp_port_t port, struct ds *);
-void ofputil_port_to_string(ofp_port_t, char namebuf[OFP10_MAX_PORT_NAME_LEN],
-                            size_t bufsize);
+bool ofputil_port_from_string(const char *, const struct ofputil_port_map *,
+                              ofp_port_t *portp);
+const char *ofputil_port_get_reserved_name(ofp_port_t);
+void ofputil_format_port(ofp_port_t port, const struct ofputil_port_map *,
+                         struct ds *);
+void ofputil_port_to_string(ofp_port_t, const struct ofputil_port_map *,
+                            char *namebuf, size_t bufsize);
 
 /* Group numbers. */
 enum { MAX_GROUP_NAME_LEN = INT_STRLEN(uint32_t) };

lib/bundle.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, 2012, 2013, 2014, 2015, 2016 Nicira, Inc.
+/* Copyright (c) 2011, 2012, 2013, 2014, 2015, 2016, 2017 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -145,7 +145,8 @@ bundle_check(const struct ofpact_bundle *bundle, ofp_port_t max_ports,
  * Returns NULL if successful, otherwise a malloc()'d string describing the
  * error.  The caller is responsible for freeing the returned string.*/
 static char * OVS_WARN_UNUSED_RESULT
-bundle_parse__(const char *s, char **save_ptr,
+bundle_parse__(const char *s, const struct ofputil_port_map *port_map,
+               char **save_ptr,
                const char *fields, const char *basis, const char *algorithm,
                const char *slave_type, const char *dst,
                const char *slave_delim, struct ofpbuf *ofpacts)
@@ -172,7 +173,7 @@ bundle_parse__(const char *s, char **save_ptr,
             break;
         }
 
-        if (!ofputil_port_from_string(slave, &slave_port)) {
+        if (!ofputil_port_from_string(slave, port_map, &slave_port)) {
             return xasprintf("%s: bad port number", slave);
         }
         ofpbuf_put(ofpacts, &slave_port, sizeof slave_port);
@@ -232,7 +233,8 @@ bundle_parse__(const char *s, char **save_ptr,
  * Returns NULL if successful, otherwise a malloc()'d string describing the
  * error.  The caller is responsible for freeing the returned string. */
 char * OVS_WARN_UNUSED_RESULT
-bundle_parse(const char *s, struct ofpbuf *ofpacts)
+bundle_parse(const char *s, const struct ofputil_port_map *port_map,
+             struct ofpbuf *ofpacts)
 {
     char *fields, *basis, *algorithm, *slave_type, *slave_delim;
     char *tokstr, *save_ptr;
@@ -246,7 +248,8 @@ bundle_parse(const char *s, struct ofpbuf *ofpacts)
     slave_type = strtok_r(NULL, ", ", &save_ptr);
     slave_delim = strtok_r(NULL, ": ", &save_ptr);
 
-    error = bundle_parse__(s, &save_ptr, fields, basis, algorithm, slave_type,
+    error = bundle_parse__(s, port_map,
+                           &save_ptr, fields, basis, algorithm, slave_type,
                            NULL, slave_delim, ofpacts);
     free(tokstr);
 
@@ -259,7 +262,8 @@ bundle_parse(const char *s, struct ofpbuf *ofpacts)
  * Returns NULL if successful, otherwise a malloc()'d string describing the
  * error.  The caller is responsible for freeing the returned string.*/
 char * OVS_WARN_UNUSED_RESULT
-bundle_parse_load(const char *s, struct ofpbuf *ofpacts)
+bundle_parse_load(const char *s, const struct ofputil_port_map *port_map,
+                  struct ofpbuf *ofpacts)
 {
     char *fields, *basis, *algorithm, *slave_type, *dst, *slave_delim;
     char *tokstr, *save_ptr;
@@ -274,7 +278,8 @@ bundle_parse_load(const char *s, struct ofpbuf *ofpacts)
     dst = strtok_r(NULL, ", ", &save_ptr);
     slave_delim = strtok_r(NULL, ": ", &save_ptr);
 
-    error = bundle_parse__(s, &save_ptr, fields, basis, algorithm, slave_type,
+    error = bundle_parse__(s, port_map,
+                           &save_ptr, fields, basis, algorithm, slave_type,
                            dst, slave_delim, ofpacts);
 
     free(tokstr);
@@ -282,9 +287,11 @@ bundle_parse_load(const char *s, struct ofpbuf *ofpacts)
     return error;
 }
 
-/* Appends a human-readable representation of 'nab' to 's'. */
+/* Appends a human-readable representation of 'nab' to 's'.  If 'port_map' is
+ * nonnull, uses it to translate port numbers to names in output. */
 void
-bundle_format(const struct ofpact_bundle *bundle, struct ds *s)
+bundle_format(const struct ofpact_bundle *bundle,
+              const struct ofputil_port_map *port_map, struct ds *s)
 {
     const char *action, *fields, *algorithm;
     size_t i;
@@ -318,7 +325,7 @@ bundle_format(const struct ofpact_bundle *bundle, struct ds *s)
             ds_put_char(s, ',');
         }
 
-        ofputil_format_port(bundle->slaves[i], s);
+        ofputil_format_port(bundle->slaves[i], port_map, s);
     }
 
     ds_put_format(s, "%s)%s", colors.paren, colors.end);

lib/bundle.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, 2012, 2013, 2014 Nicira, Inc.
+/* Copyright (c) 2011, 2012, 2013, 2014, 2017 Nicira, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ struct flow_wildcards;
 struct match;
 struct ofpact_bundle;
 struct ofpbuf;
+struct ofputil_port_map;
 
 /* NXAST_BUNDLE helper functions.
  *
@@ -45,9 +46,12 @@ ofp_port_t bundle_execute(const struct ofpact_bundle *, const struct flow *,
                         void *aux);
 enum ofperr bundle_check(const struct ofpact_bundle *, ofp_port_t max_ports,
                          const struct match *);
-char *bundle_parse(const char *, struct ofpbuf *ofpacts) OVS_WARN_UNUSED_RESULT;
-char *bundle_parse_load(const char *, struct ofpbuf *ofpacts)
+char *bundle_parse(const char *, const struct ofputil_port_map *port_map,
+                   struct ofpbuf *ofpacts) OVS_WARN_UNUSED_RESULT;
+char *bundle_parse_load(const char *, const struct ofputil_port_map *port_map,
+                        struct ofpbuf *ofpacts)
     OVS_WARN_UNUSED_RESULT;
-void bundle_format(const struct ofpact_bundle *, struct ds *);
+void bundle_format(const struct ofpact_bundle *,
+                   const struct ofputil_port_map *, struct ds *);
 
 #endif /* bundle.h */

commit 08ba5f57f1a3305a4eab820c0d70ac7efd127b65

include: Add struct declaration to ofp-print.h.
If a libopenvswitch user includes ofp-print.h before ofp-util.h (which
is standard alphabetical order), and turns on -Werror, then they would
hit this compilation error in the include:

error: 'struct ofputil_port_map' declared inside parameter list will not
be visible outside of this definition or declaration [-Werror]
 void ofp_print(FILE *, const void *, size_t *, const struct ofputil_port_map *,

Fixes: 50f96b10e1c8 ("Support accepting and displaying port names in OVS tools.")
Signed-off-by: Joe Stringer <joe@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>

include/openvswitch/ofp-print.h
@@ -29,6 +29,7 @@ struct ofp10_match;
 struct ofp_flow_mod;
 struct ofp_header;
 struct ofputil_flow_stats;
+struct ofputil_port_map;
 struct ofputil_table_features;
 struct ofputil_table_stats;
 struct dp_packet;