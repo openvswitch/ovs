7b6ba92b1d38dd2114324448aae3b78b5a546c73
aaron conole <aconole@redhat.com>
2017-08-09 16:36:53 -0400

    redhat: add vfio udev rules
    This commit builds on the non-root ovs work and adds a udev rule which will
    automatically set the group permissions of vfio devices.
    
    Fixes: e3e738a3d058 ("redhat: allow dpdk to also run as non-root user")
    Signed-off-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Russell Bryant <russell@ovn.org>

--- rhel/automake.mk
+++ rhel/automake.mk
@@ -26,6 +26,7 @@ EXTRA_DIST += \
 	rhel/openvswitch-fedora.spec.in \
 	rhel/usr_share_openvswitch_scripts_sysconfig.template \
 	rhel/usr_share_openvswitch_scripts_systemd_sysconfig.template \
+	rhel/usr_lib_udev_rules.d_91-vfio.rules \
 	rhel/usr_lib_systemd_system_openvswitch.service \
 	rhel/usr_lib_systemd_system_ovsdb-server.service \
 	rhel/usr_lib_systemd_system_ovs-vswitchd.service.in \
