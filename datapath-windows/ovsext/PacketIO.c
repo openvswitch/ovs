bf4077e2cb571584cebcb1b506a34b5f31dc7be0
Shashank Ram <rams@vmware.com>
2017-08-21 14:45:23 -0700

    datapath-windows: Move OvsCreateNewNBLsFromMultipleNBs to BuggerMgmt
    Moves function OvsCreateNewNBLsFromMultipleNBs() to BufferMgmt.c
    to facilitate consumption from outside PacketIO.c.
    
    Signed-off-by: Shashank Ram <rams@vmware.com>
    Acked-by: Anand Kumar <kumaranand@vmware.com>
    Acked-by: Alin Gabriel Serdean <aserdean@ovn.org>
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>

--- datapath-windows/ovsext/PacketIO.c
+++ datapath-windows/ovsext/PacketIO.c
@@ -46,10 +46,6 @@ extern NDIS_STRING ovsExtFriendlyNameUC;
 static VOID OvsFinalizeCompletionList(OvsCompletionList *completionList);
 static VOID OvsCompleteNBLIngress(POVS_SWITCH_CONTEXT switchContext,
                     PNET_BUFFER_LIST netBufferLists, ULONG sendCompleteFlags);
-static NTSTATUS OvsCreateNewNBLsFromMultipleNBs(
-                    POVS_SWITCH_CONTEXT switchContext,
-                    PNET_BUFFER_LIST *curNbl,
-                    PNET_BUFFER_LIST *lastNbl);
 
 VOID
 OvsInitCompletionList(OvsCompletionList *completionList,
@@ -499,42 +495,4 @@ OvsExtCancelSendNBL(NDIS_HANDLE filterModuleContext,
 
     /* All send requests get completed synchronously, so there is no need to
      * implement this callback. */
-}
-
-static NTSTATUS
-OvsCreateNewNBLsFromMultipleNBs(POVS_SWITCH_CONTEXT switchContext,
-                                PNET_BUFFER_LIST *curNbl,
-                                PNET_BUFFER_LIST *lastNbl)
-{
-    NTSTATUS status = STATUS_SUCCESS;
-    PNET_BUFFER_LIST newNbls = NULL;
-    PNET_BUFFER_LIST nbl = NULL;
-    BOOLEAN error = TRUE;
-
-    do {
-        /* Create new NBLs from curNbl with multiple net buffers. */
-        newNbls = OvsPartialCopyToMultipleNBLs(switchContext,
-                                               *curNbl, 0, 0, TRUE);
-        if (NULL == newNbls) {
-            OVS_LOG_ERROR("Failed to allocate NBLs with single NB.");
-            status = NDIS_STATUS_RESOURCES;
-            break;
-        }
-
-        nbl = newNbls;
-        while (nbl) {
-            *lastNbl = nbl;
-            nbl = NET_BUFFER_LIST_NEXT_NBL(nbl);
-        }
-
-        (*curNbl)->Next = NULL;
-
-        OvsCompleteNBL(switchContext, *curNbl, TRUE);
-
-        *curNbl = newNbls;
-
-        error = FALSE;
-    } while (error);
-
-    return status;
 }
