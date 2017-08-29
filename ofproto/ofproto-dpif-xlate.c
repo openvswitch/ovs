5fbd02295be0dfad138b389cd13ddf48733566a2
Andy Zhou <azhou@ovn.org>
2017-08-28 20:02:13 -0700

    ofproto-dpif-xlate: Remove uncessary assignment
    Signed-off-by: Andy Zhou <azhou@ovn.org>
    Tested-by: Greg Rose <gvrose8192@gmail.com>
    Reviewed-by: Greg Rose <gvrose8192@gmail.com>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -5373,7 +5373,6 @@ compose_clone(struct xlate_ctx *ctx, const struct ofpact_nest *oc)
     if (ctx->xbridge->support.clone) { /* Use clone action */
         /* Use clone action as datapath clone. */
         offset = nl_msg_start_nested(ctx->odp_actions, OVS_ACTION_ATTR_CLONE);
-        ac_offset = ctx->odp_actions->size;
         do_xlate_actions(oc->actions, oc_actions_len, ctx);
         if (ctx->freezing) {
             finish_freezing(ctx);
