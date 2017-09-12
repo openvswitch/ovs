a0237d3c7207dfce9d021544ef3d8eb39ee402ed
Alin Gabriel Serdean <aserdean@ovn.org>
2017-09-12 04:27:28 +0300

    tests: Fix sparse error on test-ovn.c
    Commit d5c70d4bcc344ae10a644b83f1790a0235871efc fixed the MSVC issue
    however, introduced a sparse error:
    "tests/test-ovn.c:205:43: warning: Using plain integer as NULL pointer"
    
    Use 'NULL' instead of '0'.
    
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>
    Acked-by: Shashank Ram <rams@vmware.com>

--- tests/test-ovn.c
+++ tests/test-ovn.c
@@ -201,7 +201,7 @@ create_macros(struct shash *macros)
     static const char *const addrs3[] = {
         "00:00:00:00:00:01", "00:00:00:00:00:02", "00:00:00:00:00:03",
     };
-    static const char *const addrs4[] = { 0 };
+    static const char *const addrs4[] = { NULL };
 
     expr_macros_add(macros, "set1", addrs1, 3);
     expr_macros_add(macros, "set2", addrs2, 3);
