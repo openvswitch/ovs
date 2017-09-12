5e8bed8373ddacf34262e080a7570074a67b4e4b
Yi-Hung Wei <yihung.wei@gmail.com>
2017-09-12 15:43:03 -0700

    Fix C++ build issue when static_assert() is not available
    This patch prevents compile errors if the C++ compiler does not support
    C++11 or the support is not enabled.
    
    VMWare-BZ: #1953215
    Fixes: 994bfc298502 ("Automatically verify that OVS header files work OK in C++ also.")
    Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- include/openvswitch/compiler.h
+++ include/openvswitch/compiler.h
@@ -243,9 +243,13 @@
 #ifdef __CHECKER__
 #define BUILD_ASSERT(EXPR) ((void) 0)
 #define BUILD_ASSERT_DECL(EXPR) extern int (*build_assert(void))[1]
-#elif defined(__cplusplus)
+#elif defined(__cplusplus) && __cplusplus >= 201103L
 #define BUILD_ASSERT(EXPR) static_assert(EXPR, "assertion failed")
 #define BUILD_ASSERT_DECL(EXPR) static_assert(EXPR, "assertion failed")
+#elif defined(__cplusplus) && __cplusplus < 201103L
+#include <boost/static_assert.hpp>
+#define BUILD_ASSERT BOOST_STATIC_ASSERT
+#define BUILD_ASSERT_DECL BOOST_STATIC_ASSERT
 #elif (__GNUC__ * 256 + __GNUC_MINOR__ >= 0x403 \
        || __has_extension(c_static_assert))
 #define BUILD_ASSERT_DECL(EXPR) _Static_assert(EXPR, #EXPR)

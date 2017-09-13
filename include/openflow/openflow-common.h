openvswitch/ovs

commit 994bfc298502e6f9178dd231c569a6b305a8299f

Automatically verify that OVS header files work OK in C++ also.
This should help address a recurring problem.

This change makes the OVS header files, when parsed by a C++ compiler,
require C++11 or later.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Russell Bryant <russell@ovn.org>

include/openflow/openflow-common.h
@@ -60,8 +60,7 @@
         extern int (*build_assert(void))[ sizeof(struct {               \
                     unsigned int build_assert_failed : (EXPR) ? 1 : -1; })]
 #else /* __cplusplus */
-#include <boost/static_assert.hpp>
-#define OFP_ASSERT BOOST_STATIC_ASSERT
+#define OFP_ASSERT(EXPR) static_assert(EXPR, "assertion failed")
 #endif /* __cplusplus */
 
 /* Version number:

include/openvswitch/compiler.h
@@ -244,9 +244,8 @@
 #define BUILD_ASSERT(EXPR) ((void) 0)
 #define BUILD_ASSERT_DECL(EXPR) extern int (*build_assert(void))[1]
 #elif defined(__cplusplus)
-#include <boost/static_assert.hpp>
-#define BUILD_ASSERT BOOST_STATIC_ASSERT
-#define BUILD_ASSERT_DECL BOOST_STATIC_ASSERT
+#define BUILD_ASSERT(EXPR) static_assert(EXPR, "assertion failed")
+#define BUILD_ASSERT_DECL(EXPR) static_assert(EXPR, "assertion failed")
 #elif (__GNUC__ * 256 + __GNUC_MINOR__ >= 0x403 \
        || __has_extension(c_static_assert))
 #define BUILD_ASSERT_DECL(EXPR) _Static_assert(EXPR, #EXPR)

Makefile.am
@@ -108,6 +108,7 @@ MAN_ROOTS =
 noinst_DATA =
 noinst_HEADERS =
 lib_LTLIBRARIES =
+noinst_LTLIBRARIES =
 noinst_man_MANS =
 noinst_PROGRAMS =
 noinst_SCRIPTS =

configure.ac
@@ -23,6 +23,7 @@ AM_INIT_AUTOMAKE([tar-pax])
 
 AC_PROG_CC_C99
 AM_PROG_CC_C_O
+AC_PROG_CXX
 AC_PROG_CPP
 AC_PROG_MKDIR_P
 AC_PROG_FGREP
@@ -135,6 +136,7 @@ OVS_CHECK_POSIX_AIO
 OVS_CHECK_PTHREAD_SET_NAME
 OVS_CHECK_LINUX_HOST
 OVS_LIBTOOL_VERSIONS
+OVS_CHECK_CXX
 AX_FUNC_POSIX_MEMALIGN
 
 OVS_CHECK_INCLUDE_NEXT([stdio.h string.h])

include/openvswitch/automake.mk
@@ -30,3 +30,17 @@ openvswitchinclude_HEADERS = \
 	include/openvswitch/version.h \
 	include/openvswitch/vconn.h \
 	include/openvswitch/vlog.h
+
+if HAVE_CXX
+# OVS does not use C++ itself, but it provides public header files
+# that a C++ compiler should accept, so when --enable-Werror is in
+# effect and a C++ compiler is available, we build a C++ source file
+# that #includes all the public headers, as a way to ensure that they
+# are acceptable as C++.
+noinst_LTLIBRARIES += include/openvswitch/libcxxtest.la
+nodist_include_openvswitch_libcxxtest_la_SOURCES = include/openvswitch/cxxtest.cc
+include/openvswitch/cxxtest.cc: include/openvswitch/automake.mk
+	$(AM_V_GEN)for header in $(openvswitchinclude_HEADERS); do	\
+	  echo $$header;						\
+	done | sed 's,^include/\(.*\)$$,#include <\1>,' > $@
+endif

m4/openvswitch.m4
@@ -620,3 +620,19 @@ AC_DEFUN([OVS_LIBTOOL_VERSIONS],
   AC_MSG_RESULT([libX-$OVS_MAJOR.$OVS_MINOR.so.$LT_CURRENT.0.$OVS_MICRO)])
   AC_SUBST(OVS_LTINFO)
     ])
+
+dnl OVS does not use C++ itself, but it provides public header files
+dnl that a C++ compiler should accept, so when --enable-Werror is in
+dnl effect and a C++ compiler is available, we enable building a C++
+dnl source file that #includes all the public headers, as a way to
+dnl ensure that they are acceptable as C++.
+AC_DEFUN([OVS_CHECK_CXX],
+  [AC_REQUIRE([AC_PROG_CXX])
+   AC_REQUIRE([OVS_ENABLE_WERROR])
+   AX_CXX_COMPILE_STDCXX([11], [], [optional])
+   if test $enable_Werror = yes && test $HAVE_CXX11 = 1; then
+     enable_cxx=:
+   else
+     enable_cxx=false
+   fi
+   AM_CONDITIONAL([HAVE_CXX], [$enable_cxx])])

commit 5e8bed8373ddacf34262e080a7570074a67b4e4b

Fix C++ build issue when static_assert() is not available
This patch prevents compile errors if the C++ compiler does not support
C++11 or the support is not enabled.

VMWare-BZ: #1953215
Fixes: 994bfc298502 ("Automatically verify that OVS header files work OK in C++ also.")
Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

include/openflow/openflow-common.h
@@ -59,9 +59,12 @@
 #define OFP_ASSERT(EXPR)                                                \
         extern int (*build_assert(void))[ sizeof(struct {               \
                     unsigned int build_assert_failed : (EXPR) ? 1 : -1; })]
-#else /* __cplusplus */
+#elif __cplusplus >= 201103L
 #define OFP_ASSERT(EXPR) static_assert(EXPR, "assertion failed")
-#endif /* __cplusplus */
+#else  /* __cplusplus < 201103L */
+#include <boost/static_assert.hpp>
+#define OFP_ASSERT BOOST_STATIC_ASSERT
+#endif /* __cplusplus < 201103L */
 
 /* Version number:
  * Non-experimental versions released: 0x01 0x02

include/openvswitch/compiler.h
@@ -243,9 +243,13 @@
 #ifdef __CHECKER__
 #define BUILD_ASSERT(EXPR) ((void) 0)
 #define BUILD_ASSERT_DECL(EXPR) extern int (*build_assert(void))[1]
-#elif defined(__cplusplus)
+#elif defined(__cplusplus) && __cplusplus >= 201103L
 #define BUILD_ASSERT(EXPR) static_assert(EXPR, "assertion failed")
 #define BUILD_ASSERT_DECL(EXPR) static_assert(EXPR, "assertion failed")
+#elif defined(__cplusplus) && __cplusplus < 201103L
+#include <boost/static_assert.hpp>
+#define BUILD_ASSERT BOOST_STATIC_ASSERT
+#define BUILD_ASSERT_DECL BOOST_STATIC_ASSERT
 #elif (__GNUC__ * 256 + __GNUC_MINOR__ >= 0x403 \
        || __has_extension(c_static_assert))
 #define BUILD_ASSERT_DECL(EXPR) _Static_assert(EXPR, #EXPR)