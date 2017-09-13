71a5fac10f7281e02ab14a249a3b08e7b96215da
Ian Stokes <ian.stokes@intel.com>
2017-09-13 14:57:26 +0100

    docs: Use DPDK 16.11.3 stable release.
    Modify docs and travis linux build script to use DPDK 16.11.3 stable
    branch to benefit from most recent bug fixes.
    
    There are no new features introduced in the DPDK release, only back
    ported bug fixes. For completeness these bug fixes have been
    documented under the 16.11.3 section in the link below.
    
    http://dpdk.org/doc/guides-16.11/rel_notes/release_16_11.html
    
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- .travis/linux-build.sh
+++ .travis/linux-build.sh
@@ -80,7 +80,7 @@ fi
 
 if [ "$DPDK" ]; then
     if [ -z "$DPDK_VER" ]; then
-        DPDK_VER="16.11.2"
+        DPDK_VER="16.11.3"
     fi
     install_dpdk $DPDK_VER
     if [ "$CC" = "clang" ]; then
