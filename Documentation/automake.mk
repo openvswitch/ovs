d366c02b9f1dd443587d74c52edf4dd35273daf4
Ben Pfaff <blp@ovn.org>
2017-09-25 11:26:56 -0700

    Documentation: Also define install-man-rst when Sphinx is not available.
    "make sandbox" wants to install the ReST manpages, but it failed when
    Sphinx wasn't available.  This fixes the problem.
    
    Fixes: 986311be550e ("ovs-sandbox: Install .rst manpages into the sandbox as well.")
    Reported-by: Justin Pettit <jpettit@ovn.org>
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Justin Pettit <jpettit@ovn.org>

--- Documentation/automake.mk
+++ Documentation/automake.mk
@@ -178,8 +178,8 @@ extract_stem_and_section = \
 	eval "mandir=\$$man$${section}dir"; \
 	test -n "$$mandir" || { echo "unknown directory for manpage section $$section"; continue; }
 
-if HAVE_SPHINX
 INSTALL_DATA_LOCAL += install-man-rst
+if HAVE_SPHINX
 install-man-rst: docs-check
 	@$(set_mandirs); \
 	for rst in $(RST_MANPAGES); do \
@@ -189,6 +189,9 @@ install-man-rst: docs-check
 	    echo " $(INSTALL_DATA) $(SPHINXBUILDDIR)/man/$$stem.$$section '$(DESTDIR)'\"$$mandir/$$stem.$$section\""; \
 	    $(INSTALL_DATA) $(SPHINXBUILDDIR)/man/$$stem.$$section '$(DESTDIR)'"$$mandir/$$stem.$$section"; \
 	done
+else
+install-man-rst:
+	@:
 endif
 
 UNINSTALL_LOCAL += uninstall-man-rst
