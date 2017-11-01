36e6714054534b552413ba3b8e7eab49f965dc29
Yi-Hung Wei <yihung.wei@gmail.com>
2017-11-01 16:59:52 -0700

    ofproto/trace: Fix memory leak in oftrace_push_ct_state()
    Free the allocated memory in the pop function.
    
    Fixes: 0f2f05bbcf743 ("ofproto/trace: Add --ct-next option to ofproto/trace")
    Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ofproto/ofproto-dpif-trace.c
+++ ofproto/ofproto-dpif-trace.c
@@ -135,7 +135,9 @@ oftrace_pop_ct_state(struct ovs_list *next_ct_states)
 {
     struct oftrace_next_ct_state *s;
     LIST_FOR_EACH_POP (s, node, next_ct_states) {
-        return s->state;
+        uint32_t state = s->state;
+        free(s);
+        return state;
     }
     OVS_NOT_REACHED();
 }
