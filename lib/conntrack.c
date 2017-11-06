5b183f1182445b1955d16b232a5e3e41bb6dbab9
borjam <borjam@gmail.com>
2017-11-06 10:32:12 +0100

    Modified includes order. sys/types.h should go before the inet stuff.

--- lib/conntrack.c
+++ lib/conntrack.c
@@ -17,10 +17,10 @@
 #include <config.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/icmp6.h>
 #include <string.h>
-#include <sys/types.h>
 
 #include "bitmap.h"
 #include "conntrack.h"
