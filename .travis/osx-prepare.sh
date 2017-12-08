60edd619b49f95fdd75b103c17d84378bf3b8a3b
Ilya Maximets <i.maximets@samsung.com>
2017-12-08 15:53:10 +0300

    travis: Use pip2 instead of pip for OSX build.
    xcode8.3 is a new default image for OS X on Travis-CI, but
    it does not have 'pip':
    
        pip install --user six
        ./.travis/osx-prepare.sh: line 3: pip: command not found
    
    'pip2' or 'pip3' should be used explicitly instead:
    
        https://github.com/travis-ci/travis-ci/issues/8829
    
    Signed-off-by: Ilya Maximets <i.maximets@samsung.com>
    Acked-by: Alin Gabriel Serdean <aserdean@ovn.org>
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>

--- .travis/osx-prepare.sh
+++ .travis/osx-prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-pip install --user six
-pip install --user --upgrade docutils
+pip2 install --user six
+pip2 install --user --upgrade docutils
 
 brew update || true
 brew uninstall libtool && brew install libtool || true
