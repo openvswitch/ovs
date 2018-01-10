81f9ef5a3dd7da6f93ab64980b3d5eaee5de7058
William Tu <u9012063@gmail.com>
2018-01-10 13:13:11 -0800

    tunnel: fix tunnel flags set/clear.
    Existing code only set these tunnel flags (df, csum, and key) when the
    flag is set in the output tunnel port, but did not clear when the flag
    is unset.  The patch fixes it by setting and clearing it accordingly.
    
    Two existing testcases need to fix:
    'tunnel - Geneve option present' has no key set up, so we should match
    'flags(df)' instead of 'flags(df|key)'.  The second case
    'tunnel - concomitant IPv6 and IPv4 tunnels' follows the same pattern.
    One additional test case 'tunnel - Mix Geneve/GRE options' is added.
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    VMWare-BZ: #2019012
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ofproto/tunnel.c
+++ ofproto/tunnel.c
@@ -464,6 +464,7 @@ tnl_port_send(const struct ofport_dpif *ofport, struct flow *flow,
         }
     }
 
+    flow->tunnel.flags &= ~(FLOW_TNL_F_MASK & ~FLOW_TNL_PUB_F_MASK);
     flow->tunnel.flags |= (cfg->dont_fragment ? FLOW_TNL_F_DONT_FRAGMENT : 0)
         | (cfg->csum ? FLOW_TNL_F_CSUM : 0)
         | (cfg->out_key_present ? FLOW_TNL_F_KEY : 0);
