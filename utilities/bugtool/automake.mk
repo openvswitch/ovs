97cb2cffc0d9ed9aad4aaa02a4a407d55ae7a6bc
Dávid Patrik Pintér <david.patrik.pinter@ericsson.com>
2018-01-10 10:36:54 +0000

    ovs-bugtool: Bugfix for bugtool plugin queries.
    Introducing 'ovs-bugtool-ovs-ofctl-loop-over-bridges' bash script.
    It calls ovs-ofctl commands given as first parameter for each
    bridge with the corresponding open flow protocol option.
    Usage:
    ovs-bugtool-ovs-ofctl-loop-over-bridges "param1" "param2"
     - param1: ovs-ofctl command to call (show, dump-flows, etc...)
     - param2: optional, add further parts for the shell command
       (like "| grep something")
    
    Calling ovs-ofctl commands 'show' and 'dump-flows' with the
    'ovs-bugtool-ovs-ofctl-loop-over-bridges' bash script.
    
    Signed-off-by: David Patrik Pinter <david.patrik.pinter@ericsson.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- utilities/bugtool/automake.mk
+++ utilities/bugtool/automake.mk
@@ -25,8 +25,7 @@ bugtool_scripts = \
 	utilities/bugtool/ovs-bugtool-vsctl-show \
 	utilities/bugtool/ovs-bugtool-ovsdb-dump \
 	utilities/bugtool/ovs-bugtool-daemons-ver \
-	utilities/bugtool/ovs-bugtool-ovs-ofctl-show \
-	utilities/bugtool/ovs-bugtool-ovs-ofctl-dump-flows \
+	utilities/bugtool/ovs-bugtool-ovs-ofctl-loop-over-bridges \
 	utilities/bugtool/ovs-bugtool-ovs-appctl-dpif \
 	utilities/bugtool/ovs-bugtool-bond-show \
 	utilities/bugtool/ovs-bugtool-conntrack-dump
