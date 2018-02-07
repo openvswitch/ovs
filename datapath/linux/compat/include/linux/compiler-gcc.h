da4908001bdc4c677fe084ad8506f986cac878f4
Greg Rose <gvrose8192@gmail.com>
2018-02-07 07:30:06 -0800

    compat: Fix compiler headers
    Since Linux kernel upstream commit d15155824c50
    ("linux/compiler.h: Split into compiler.h and compiler_types.h") this
    error check for the gcc compiler header is no longer valid.  Remove
    so that openvswitch builds for linux kernels 4.14.8 and since.
    
    Signed-off-by: Greg Rose <gvrose8192@gmail.com>
    Acked-by: Pravin B Shelar <pshelar@ovn.org>

--- datapath/linux/compat/include/linux/compiler-gcc.h
+++ datapath/linux/compat/include/linux/compiler-gcc.h
@@ -1,5 +1,7 @@
 #ifndef __LINUX_COMPILER_H
+#ifndef HAVE_LINUX_COMPILER_TYPES_H
 #error "Please don't include <linux/compiler-gcc.h> directly, include <linux/compiler.h> instead."
+#endif
 #endif
 
 #include_next <linux/compiler-gcc.h>
