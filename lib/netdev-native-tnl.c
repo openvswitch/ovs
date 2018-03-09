3456684ed04ea8f443e672a2e600fa5f1c42a9af
William Tu <u9012063@gmail.com>
2018-03-09 13:02:22 -0800

    userspace: return correct ipv6 header len.
    The ipv6 header len might have extension header, but current
    code simply returns fixed ipv6 header length 40-byte.
    
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/netdev-native-tnl.c
+++ lib/netdev-native-tnl.c
@@ -123,7 +123,7 @@ netdev_tnl_ip_extract_tnl_md(struct dp_packet *packet, struct flow_tnl *tnl,
         tnl->ip_tos = ntohl(tc_flow) >> 20;
         tnl->ip_ttl = ip6->ip6_hlim;
 
-        *hlen += IPV6_HEADER_LEN;
+        *hlen += packet->l4_ofs - packet->l3_ofs;
 
     } else {
         VLOG_WARN_RL(&err_rl, "ipv4 packet has invalid version (%d)",
