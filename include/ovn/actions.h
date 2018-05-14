d5176edf567470749b3270f46dee3c562dd386f8
Numan Siddique <nusiddiq@redhat.com>
2018-05-14 23:20:32 +0530

    ovn: Set proper Neighbour Adv flag when replying for NS request for router IP
    Presently when a VM's IPv6 stack sends a Neighbor Solicitation request for its
    router IP, (mostly when the ND cache entry for the router is in STALE state)
    ovn-controller responds with a Neighbor Adv packet (using the action nd_na).
    But it doesn't set 'ND_RSO_ROUTER' in the RSO flags (please see RFC4861 page 23).
    Because of which, the VM deletes the default route. The default route gets added
    again when the next RA is received (but would again gets deleted if its sends
    NS request). And this results in disruption of IPv6 traffic.
    
    This patch addresses this issue by adding a new action 'nd_na_router' which is
    same as 'nd_na' but it sets the 'ND_RSO_ROUTER' in the RSO flags. ovn-northd
    uses this action. A new action is added instead of modifying the existing 'nd_na'
    action. This is because
      - We cannot set the RSO flags in the "nd_na { ..actions .. }"
      - It would be ugly to have something like nd_na { router_flags, ...actions .. }
    
    Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1567735
    Signed-off-by: Numan Siddique <nusiddiq@redhat.com>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    
    (cherry-picked from c9756229ed818e4c354d8bfd7c65c656f92fc98b and resolved conflicts)
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- include/ovn/actions.h
+++ include/ovn/actions.h
@@ -65,6 +65,7 @@ struct ovn_extend_table;
     OVNACT(CLONE,             ovnact_nest)            \
     OVNACT(ARP,               ovnact_nest)            \
     OVNACT(ND_NA,             ovnact_nest)            \
+    OVNACT(ND_NA_ROUTER,      ovnact_nest)            \
     OVNACT(GET_ARP,           ovnact_get_mac_bind)    \
     OVNACT(PUT_ARP,           ovnact_put_mac_bind)    \
     OVNACT(GET_ND,            ovnact_get_mac_bind)    \
@@ -429,6 +430,12 @@ enum action_opcode {
      * The actions, in OpenFlow 1.3 format, follow the action_header.
      */
     ACTION_OPCODE_ND_NS,
+
+    /* "nd_na_router { ...actions... }" with rso flag 'ND_RSO_ROUTER' set.
+        *
+        * The actions, in OpenFlow 1.3 format, follow the action_header.
+        */
+    ACTION_OPCODE_ND_NA_ROUTER,
 };
 
 /* Header. */
