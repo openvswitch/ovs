7717d233cfdf0b3c59f62cd6bf86e3b4261dcbf8
Ben Pfaff <blp@ovn.org>
2018-06-04 14:16:40 -0700

    Makefile: Add build-time check for files with initial tabs.
    This should make it harder to reintroduce inappropriate indentation.
    
    Signed-off-by: Ben Pfaff <blp@ovn.org>
    Acked-by: Justin Pettit <jpettit@ovn.org>

--- build-aux/automake.mk
+++ build-aux/automake.mk
@@ -5,6 +5,7 @@ EXTRA_DIST += \
 	build-aux/dist-docs \
 	build-aux/dpdkstrip.py \
 	build-aux/generate-dhparams-c \
+	build-aux/initial-tab-whitelist \
 	build-aux/sodepends.py \
 	build-aux/soexpand.py \
 	build-aux/text2c \
