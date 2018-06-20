openvswitch/ovs

commit 1e40b541bc9bd0a768986172ec63e41708a3e1f6

datapath: Fix max MTU size on RHEL 7.5 kernel
Without the patch, in RHEL 7.5, the maximum configurable MTU of vport
internal device is 1500, which shall be 65535.  This patch fixes this
issue.

Fixes: 39ca338374ab ("datapath: compat: Fix build on RHEL 7.5")
Reported-by: Lucas Alvares Gomes <lucasagomes@gmail.com>
Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Reviewed-by: Greg Rose <gvrose8192@gmail.com>
Tested-by: Greg Rose <gvrose8192@gmail.com>

datapath/vport-internal_dev.c
@@ -169,6 +169,8 @@ static void do_setup(struct net_device *netdev)
 
 #ifdef HAVE_NET_DEVICE_WITH_MAX_MTU
 	netdev->max_mtu = ETH_MAX_MTU;
+#elif HAVE_RHEL7_MAX_MTU
+	netdev->extended->max_mtu = ETH_MAX_MTU;
 #endif
 	netdev->netdev_ops = &internal_dev_netdev_ops;
 

commit b59974806c2c84f1a37262aae23762020c66f8dc

datapath: Fix compiler warning for HAVE_RHEL7_MAX_MTU.
Fixes: 1e40b541bc ("datapath: Fix max MTU size on RHEL 7.5 kernel")
Signed-off-by: Justin Pettit <jpettit@ovn.org>
Reviewed-by: Greg Rose <gvrose8192@gmail.com>

datapath/vport-internal_dev.c
@@ -169,7 +169,7 @@ static void do_setup(struct net_device *netdev)
 
 #ifdef HAVE_NET_DEVICE_WITH_MAX_MTU
 	netdev->max_mtu = ETH_MAX_MTU;
-#elif HAVE_RHEL7_MAX_MTU
+#elif defined(HAVE_RHEL7_MAX_MTU)
 	netdev->extended->max_mtu = ETH_MAX_MTU;
 #endif
 	netdev->netdev_ops = &internal_dev_netdev_ops;