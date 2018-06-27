openvswitch/ovs

commit 61677bf976e909c6bfdc03804759767145bb804c

ofp-meter: Fix ds_put_format that treats enum type as short integer
Travis job fails because of the below error and this patch solves this issue.

lib/ofp-meter.c:340:48: error: format specifies type 'unsigned short'
but the argument has underlying type 'unsigned int' [-Werror,-Wformat]
        ds_put_format(s, "flags:0x%"PRIx16" ", flags);

Signed-off-by: Yifeng Sun <pkusunyifeng@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/ofp-meter.c
@@ -337,7 +337,7 @@ ofp_print_meter_flags(struct ds *s, enum ofp13_meter_flags flags)
 
     flags &= ~(OFPMF13_KBPS | OFPMF13_PKTPS | OFPMF13_BURST | OFPMF13_STATS);
     if (flags) {
-        ds_put_format(s, "flags:0x%"PRIx16" ", flags);
+        ds_put_format(s, "flags:0x%x", (unsigned)flags);
     }
 }
 

commit e4614813306778b24a609d8d68bba015aaee9dcd

ofp-meter: Fix ofp_print_meter_flags() output.
It had a missing space.

CC: Yifeng Sun <pkusunyifeng@gmail.com>
Fixes: 61677bf976e9 ("ofp-meter: Fix ds_put_format that treats enum type as short integer")
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/ofp-meter.c
@@ -337,7 +337,7 @@ ofp_print_meter_flags(struct ds *s, enum ofp13_meter_flags flags)
 
     flags &= ~(OFPMF13_KBPS | OFPMF13_PKTPS | OFPMF13_BURST | OFPMF13_STATS);
     if (flags) {
-        ds_put_format(s, "flags:0x%x", (unsigned)flags);
+        ds_put_format(s, "flags:0x%x ", (unsigned)flags);
     }
 }
 