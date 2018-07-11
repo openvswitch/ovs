61b1c7acb9a209ec973c2a2a0d1126e5944845e8
Olivier Cochard-Labbé <olivier@cochard.me>
2018-07-11 11:02:28 +0200

    netdev-bsd: Fix crash on FreeBSD.
    Working on bug https://github.com/openvswitch/ovs-issues/issues/152, I've
    found wrong mapping of netdev functions on FreeBSD.
    
    Signed-off-by: Olivier Cochard <olivier@FreeBSD.org>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/netdev-bsd.c
+++ lib/netdev-bsd.c
@@ -65,7 +65,6 @@
 
 VLOG_DEFINE_THIS_MODULE(netdev_bsd);
 
-
 struct netdev_rxq_bsd {
     struct netdev_rxq up;
 
@@ -1517,7 +1516,7 @@ netdev_bsd_update_flags(struct netdev *netdev_, enum netdev_flags off,
     NULL, /* get_carrier_resets */                   \
     NULL, /* set_miimon_interval */                  \
     netdev_bsd_get_stats,                            \
-                                                     \
+    NULL, /* get_custom_stats */                     \
     GET_FEATURES,                                    \
     NULL, /* set_advertisement */                    \
     NULL, /* get_pt_mode */                          \
