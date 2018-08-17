cb2a5486a3a3756ee3868da0050d737c8989770c
Yi-Hung Wei <yihung.wei@gmail.com>
2018-08-17 02:05:06 -0700

    datapath: conntrack: Support conntrack zone limit
    Upstream commit:
        commit 11efd5cb04a184eea4f57b68ea63dddd463158d1
        Author: Yi-Hung Wei <yihung.wei@gmail.com>
        Date:   Thu May 24 17:56:43 2018 -0700
    
        openvswitch: Support conntrack zone limit
    
        Currently, nf_conntrack_max is used to limit the maximum number of
        conntrack entries in the conntrack table for every network namespace.
        For the VMs and containers that reside in the same namespace,
        they share the same conntrack table, and the total # of conntrack entries
        for all the VMs and containers are limited by nf_conntrack_max.  In this
        case, if one of the VM/container abuses the usage the conntrack entries,
        it blocks the others from committing valid conntrack entries into the
        conntrack table.  Even if we can possibly put the VM in different network
        namespace, the current nf_conntrack_max configuration is kind of rigid
        that we cannot limit different VM/container to have different # conntrack
        entries.
    
        To address the aforementioned issue, this patch proposes to have a
        fine-grained mechanism that could further limit the # of conntrack entries
        per-zone.  For example, we can designate different zone to different VM,
        and set conntrack limit to each zone.  By providing this isolation, a
        mis-behaved VM only consumes the conntrack entries in its own zone, and
        it will not influence other well-behaved VMs.  Moreover, the users can
        set various conntrack limit to different zone based on their preference.
    
        The proposed implementation utilizes Netfilter's nf_conncount backend
        to count the number of connections in a particular zone.  If the number of
        connection is above a configured limitation, ovs will return ENOMEM to the
        userspace.  If userspace does not configure the zone limit, the limit
        defaults to zero that is no limitation, which is backward compatible to
        the behavior without this patch.
    
        The following high leve APIs are provided to the userspace:
          - OVS_CT_LIMIT_CMD_SET:
            * set default connection limit for all zones
            * set the connection limit for a particular zone
          - OVS_CT_LIMIT_CMD_DEL:
            * remove the connection limit for a particular zone
          - OVS_CT_LIMIT_CMD_GET:
            * get the default connection limit for all zones
            * get the connection limit for a particular zone
    
        Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
        Acked-by: Pravin B Shelar <pshelar@ovn.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Yi-Hung Wei <yihung.wei@gmail.com>
    Signed-off-by: Justin Pettit <jpettit@ovn.org>

--- datapath/datapath.c
+++ datapath/datapath.c
@@ -2305,6 +2305,9 @@ static struct genl_family *dp_genl_families[] = {
 	&dp_flow_genl_family,
 	&dp_packet_genl_family,
 	&dp_meter_genl_family,
+#if	IS_ENABLED(CONFIG_NETFILTER_CONNCOUNT)
+	&dp_ct_limit_genl_family,
+#endif
 };
 
 static void dp_unregister_genl(int n_families)
@@ -2340,10 +2343,9 @@ static int __net_init ovs_init_net(struct net *net)
 
 	INIT_LIST_HEAD(&ovs_net->dps);
 	INIT_WORK(&ovs_net->dp_notify_work, ovs_dp_notify_wq);
-	ovs_ct_init(net);
 	ovs_netns_frags_init(net);
 	ovs_netns_frags6_init(net);
-	return 0;
+	return ovs_ct_init(net);
 }
 
 static void __net_exit list_vports_from_net(struct net *net, struct net *dnet,
@@ -2510,3 +2512,4 @@ MODULE_ALIAS_GENL_FAMILY(OVS_VPORT_FAMILY);
 MODULE_ALIAS_GENL_FAMILY(OVS_FLOW_FAMILY);
 MODULE_ALIAS_GENL_FAMILY(OVS_PACKET_FAMILY);
 MODULE_ALIAS_GENL_FAMILY(OVS_METER_FAMILY);
+MODULE_ALIAS_GENL_FAMILY(OVS_CT_LIMIT_FAMILY);
