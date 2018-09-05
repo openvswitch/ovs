71972fcaa2aa25d546f47078d9257797eb79eb86
Bhargava Shastry <bshastry@sect.tu-berlin.de>
2018-09-05 16:41:49 +0200

    ossfuzz: Improve coverage of flow_extract_target
    This commit improves test coverage of the ossfuzz flow extract test harness
    by extending the harness with additional API calls from lib/flow.c
    
    An additional minor change is adding a config option to
    flow_extract_target.options file in `tests/ossfuzz/config` to suppress
    debug output while fuzzing.
    
    A cursory evaluation shows that the patch covers 8 additional files and
    improves line coverage of lib/flow.c from 23% to 37%.
    
    Signed-off-by: Bhargava Shastry <bshastry at sect.tu-berlin.de>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- tests/oss-fuzz/flow_extract_target.c
+++ tests/oss-fuzz/flow_extract_target.c
@@ -2,14 +2,48 @@
 #include "fuzzer.h"
 #include "dp-packet.h"
 #include "flow.h"
+#include "openvswitch/ofp-match.h"
+#include "openvswitch/ofp-print.h"
+#include "openvswitch/match.h"
 
 int
 LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct dp_packet packet;
     struct flow flow;
-
     dp_packet_use_const(&packet, data, size);
     flow_extract(&packet, &flow);
+
+    /* Extract flowmap. */
+    struct flowmap fmap;
+    flow_wc_map(&flow, &fmap);
+
+    /* Extract metadata. */
+    struct match flow_metadata;
+    flow_get_metadata(&flow, &flow_metadata);
+
+    /* Hashing functions. */
+    uint32_t hash = flow_hash_5tuple(&flow, 0);
+    hash = flow_hash_symmetric_l4(&flow, 0);
+    hash = flow_hash_symmetric_l2(&flow, 0);
+    hash = flow_hash_symmetric_l3l4(&flow, 0, NULL);
+    ignore(hash);
+
+    /* Convert flow to match. */
+    struct match match;
+    match_wc_init(&match, &flow);
+
+    struct ofp10_match ext_match;
+    ofputil_match_to_ofp10_match(&match, &ext_match);
+
+    /* Print match and packet. */
+    ofp_print_packet(stdout, dp_packet_data(&packet), dp_packet_size(&packet),
+                     htonl(PT_ETH));
+    ovs_hex_dump(stdout, dp_packet_data(&packet), dp_packet_size(&packet), 0,
+                 true);
+    match_print(&match, NULL);
+
+    ovs_hex_dump(stdout, &ext_match, sizeof ext_match, 0, false);
+
     return 0;
 }
