2c022949efff7f135686bd9007fffaaa6147f935
Yifeng Sun <pkusunyifeng@gmail.com>
2018-10-05 14:50:39 -0700

    odp-util: Fix a use-after-free bug.
    After ofpbuf_put, actions may have been reallocated and
    key will point to invalid memory address.
    
    Reported-at: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=10796
    Reported-at: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=10802
    Signed-off-by: Yifeng Sun <pkusunyifeng@gmail.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/odp-util.c
+++ lib/odp-util.c
@@ -2242,13 +2242,14 @@ parse_odp_action(const char *s, const struct simap *port_names,
                 key->nla_len += size;
                 ofpbuf_put(actions, mask + 1, size);
 
-                /* Add new padding as needed */
-                ofpbuf_put_zeros(actions, NLA_ALIGN(key->nla_len) -
-                                          key->nla_len);
-
                 /* 'actions' may have been reallocated by ofpbuf_put(). */
                 nested = ofpbuf_at_assert(actions, start_ofs, sizeof *nested);
                 nested->nla_type = OVS_ACTION_ATTR_SET_MASKED;
+
+                key = nested + 1;
+                /* Add new padding as needed */
+                ofpbuf_put_zeros(actions, NLA_ALIGN(key->nla_len) -
+                                          key->nla_len);
             }
         }
         ofpbuf_uninit(&maskbuf);
