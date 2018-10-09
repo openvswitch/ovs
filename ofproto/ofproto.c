49d6aafb9cc644e358c231350f9723ba01de8f55
Ben Pfaff <blp@ovn.org>
2018-10-09 11:15:00 -0700

    ofproto: Don't always treat passive controllers as "equal".
    If a passive controller chooses to configure itself as a slave controller,
    I don't know a reason why it should be considered "equal" for some
    purposes.
    
    Acked-by: Justin Pettit <jpettit@ovn.org>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -3425,8 +3425,7 @@ handle_set_config(struct ofconn *ofconn, const struct ofp_header *oh)
 static enum ofperr
 reject_slave_controller(struct ofconn *ofconn)
 {
-    if (ofconn_get_type(ofconn) == OFCONN_PRIMARY
-        && ofconn_get_role(ofconn) == OFPCR12_ROLE_SLAVE) {
+    if (ofconn_get_role(ofconn) == OFPCR12_ROLE_SLAVE) {
         return OFPERR_OFPBRC_IS_SLAVE;
     } else {
         return 0;
