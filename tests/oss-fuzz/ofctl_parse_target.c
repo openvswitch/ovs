ead2252b2b717f52728a6b8c78a2e9346863ccb8
Yifeng Sun <pkusunyifeng@gmail.com>
2018-10-25 16:17:23 -0700

    ofctl_parse_target: Only parse complete ofputil_flow_mod data.
    When parse_ofp_flow_mod_str returns error, `fm` is incomplete and pointers
    in it may be null, e.g. fm.match.flow. In this case, passing it to
    ofctl_parse_flows__ may cause pointer errors because ofctl_parse_flows__
    expects a valid input of type struct ofputil_flow_mod.
    
    Reported-at: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11110
    Signed-off-by: Yifeng Sun <pkusunyifeng@gmail.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- tests/oss-fuzz/ofctl_parse_target.c
+++ tests/oss-fuzz/ofctl_parse_target.c
@@ -58,8 +58,9 @@ ofctl_parse_flow(const char *input, int command)
                                    command, &usable_protocols);
     if (error) {
         printf("Error encountered: %s\n", error);
+    } else {
+        ofctl_parse_flows__(&fm, 1, usable_protocols);
     }
-    ofctl_parse_flows__(&fm, 1, usable_protocols);
 }
 
 int
