openvswitch/ovs

commit d5cc46e3d185dcd32e2ef099b2ce38e1a7f95eee

ipsec: Use @PYTHON@ directly instead of "/usr/bin/env python"
Using "/usr/bin/env" is against Fedora Packaging Guidelines [1].

Moreover, in this specific case, it also prevent "make rpm-fedora" to
successfully complete on "Fedora Rawhide" since "#!/usr/bin/env python"
must not be used anymore [2].

[1] https://fedoraproject.org/wiki/Packaging:Guidelines#Shebang_lines
[2] https://fedoraproject.org/wiki/Changes/Make_ambiguous_python_shebangs_error

CC: Qiuyu Xiao <qiuyu.xiao.qyx@gmail.com>
Fixes: 22c5eafb6efa ("ipsec: reintroduce IPsec support for tunneling")
Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ipsec/automake.mk
@@ -5,6 +5,6 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without warranty of any kind.
 
-EXTRA_DIST += \
-        ipsec/ovs-monitor-ipsec
-FLAKE8_PYFILES += ipsec/ovs-monitor-ipsec
+bin_SCRIPTS += ipsec/ovs-monitor-ipsec
+EXTRA_DIST += ipsec/ovs-monitor-ipsec.in
+FLAKE8_PYFILES += ipsec/ovs-monitor-ipsec.in

configure.ac
@@ -208,6 +208,7 @@ AC_CONFIG_COMMANDS([include/openflow/openflow.h.stamp])
 AC_CONFIG_COMMANDS([utilities/bugtool/dummy], [:])
 AC_CONFIG_COMMANDS([ovn/dummy], [:])
 AC_CONFIG_COMMANDS([ovn/utilities/dummy], [:])
+AC_CONFIG_COMMANDS([ipsec/dummy], [:])
 
 m4_ifdef([AM_SILENT_RULES], [AM_SILENT_RULES])
 

ipsec/ovs-monitor-ipsec.in
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! @PYTHON@
 # Copyright (c) 2017 Nicira, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");

commit 81b286609543a9feacf58371ba5edf5402281674

ipsec: Install ovs-monitor-ipsec in script directory
In commit d5cc46e3d185 ("ipsec: Use @PYTHON@ directly instead of
"/usr/bin/env python"") ovs-monitor-ipsec is installed in bin directory,
but it's supposed to be installed in script directory.

This commit removes also the manual copy of "ovs-monitor-ipsec" in spec file
since it's installed directly in "make install".

Fixes: d5cc46e3d185 ("ipsec: Use @PYTHON@ directly instead of "/usr/bin/env python"")
Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ipsec/automake.mk
@@ -5,7 +5,7 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without warranty of any kind.
 
-bin_SCRIPTS += ipsec/ovs-monitor-ipsec
+scripts_SCRIPTS += ipsec/ovs-monitor-ipsec
 EXTRA_DIST += ipsec/ovs-monitor-ipsec.in
 FLAKE8_PYFILES += ipsec/ovs-monitor-ipsec.in
 CLEANFILES += ipsec/ovs-monitor-ipsec

rhel/openvswitch-fedora.spec.in
@@ -341,10 +341,6 @@ install -p -D -m 0755 \
         rhel/usr_share_openvswitch_scripts_ovs-systemd-reload \
         $RPM_BUILD_ROOT%{_datadir}/openvswitch/scripts/ovs-systemd-reload
 
-install -m 0755 \
-        ipsec/ovs-monitor-ipsec \
-        $RPM_BUILD_ROOT%{_datadir}/openvswitch/scripts/ovs-monitor-ipsec
-
 # remove unpackaged files
 rm -f $RPM_BUILD_ROOT%{_bindir}/ovs-parse-backtrace \
         $RPM_BUILD_ROOT%{_sbindir}/ovs-vlan-bug-workaround \
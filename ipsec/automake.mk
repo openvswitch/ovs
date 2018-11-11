81b286609543a9feacf58371ba5edf5402281674
Timothy Redaelli <tredaelli@redhat.com>
2018-11-11 11:04:17 +0100

    ipsec: Install ovs-monitor-ipsec in script directory
    In commit d5cc46e3d185 ("ipsec: Use @PYTHON@ directly instead of
    "/usr/bin/env python"") ovs-monitor-ipsec is installed in bin directory,
    but it's supposed to be installed in script directory.
    
    This commit removes also the manual copy of "ovs-monitor-ipsec" in spec file
    since it's installed directly in "make install".
    
    Fixes: d5cc46e3d185 ("ipsec: Use @PYTHON@ directly instead of "/usr/bin/env python"")
    Signed-off-by: Timothy Redaelli <tredaelli@redhat.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ipsec/automake.mk
+++ ipsec/automake.mk
@@ -5,7 +5,7 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without warranty of any kind.
 
-bin_SCRIPTS += ipsec/ovs-monitor-ipsec
+scripts_SCRIPTS += ipsec/ovs-monitor-ipsec
 EXTRA_DIST += ipsec/ovs-monitor-ipsec.in
 FLAKE8_PYFILES += ipsec/ovs-monitor-ipsec.in
 CLEANFILES += ipsec/ovs-monitor-ipsec
