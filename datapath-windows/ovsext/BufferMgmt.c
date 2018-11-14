26624604036330ce7cb91ebba2ea895e3d544f51
Sairam Venugopal <vsairam@vmware.com>
2018-11-14 12:07:28 -0800

    datapath-windows: Fix invalid reference in Buffermgmt.c
    OVS_BUFFER_CONTEXT gets cleared as part of NdisFreeNetBufferListContext
    function call. This causes an invalid reference error.
    
    Found while testing with driver verifier enabled.
    
    Signed-off-by: Sairam Venugopal <vsairam@vmware.com>
    Acked-by: Anand Kumar <kumaranand@vmware.com>
    Acked-by: Alin Gabriel Serdean <aserdean@ovn.org>
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>

--- datapath-windows/ovsext/BufferMgmt.c
+++ datapath-windows/ovsext/BufferMgmt.c
@@ -1611,6 +1611,7 @@ OvsCompleteNBL(PVOID switch_ctx,
 {
     POVS_BUFFER_CONTEXT ctx;
     UINT16 flags;
+    UINT32 dataOffsetDelta;
     PNET_BUFFER_LIST parent;
     NDIS_STATUS status;
     NDIS_HANDLE poolHandle;
@@ -1642,6 +1643,7 @@ OvsCompleteNBL(PVOID switch_ctx,
     nb = NET_BUFFER_LIST_FIRST_NB(nbl);
 
     flags = ctx->flags;
+    dataOffsetDelta = ctx->dataOffsetDelta;
     if (!(flags & OVS_BUFFER_FRAGMENT) &&
         NET_BUFFER_DATA_LENGTH(nb) != ctx->origDataLength) {
         UINT32 diff;
@@ -1656,7 +1658,7 @@ OvsCompleteNBL(PVOID switch_ctx,
         }
     }
 
-    if (ctx->flags & OVS_BUFFER_PRIVATE_CONTEXT) {
+    if (flags & OVS_BUFFER_PRIVATE_CONTEXT) {
         NdisFreeNetBufferListContext(nbl, sizeof (OVS_BUFFER_CONTEXT));
     }
 
@@ -1729,7 +1731,7 @@ OvsCompleteNBL(PVOID switch_ctx,
 #ifdef DBG
         InterlockedDecrement((LONG volatile *)&ovsPool->fragNBLCount);
 #endif
-        NdisFreeFragmentNetBufferList(nbl, ctx->dataOffsetDelta, 0);
+        NdisFreeFragmentNetBufferList(nbl, dataOffsetDelta, 0);
     }
 
     if (parent != NULL) {
