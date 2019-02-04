a5837eacaa6677e7556632452da646f3f5b0709b
Numan Siddique <nusiddiq@redhat.com>
2019-02-04 22:20:31 +0530

    ovn-controller: Fix chassisredirect port flapping when ovs-vswitchd crashes
    On a chassis when ovs-vswitchd crashes for some reason, the BFD status doesn't
    get updated in the ovs db. ovn-controller will be reading the old BFD status
    even though ovs-vswitchd is crashed. This results in the chassiredirect port
    claim flapping between the master chassis and the chasiss with the next higher
    priority if ovs-vswitchd crashes in the master chassis.
    
    All the other chassis notices the BFD status down with the master chassis
    and hence the next higher priority claims the port. But according to
    the master chassis, the BFD status is fine and it again claims back the
    chassisredirect port. And this results in flapping. The issue gets resolved
    when ovs-vswitchd comes back but until then it leads to lot of SB DB
    transactions and high CPU usage in ovn-controller's.
    
    This patch fixes the issue by checking the OF connection status of the
    ovn-controller with ovs-vswitchd and calculates the active bfd tunnels
    only if it's connected.
    
    Signed-off-by: Numan Siddique <nusiddiq@redhat.com>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ovn/controller/ofctrl.c
+++ ovn/controller/ofctrl.c
@@ -1265,3 +1265,9 @@ ofctrl_inject_pkt(const struct ovsrec_bridge *br_int, const char *flow_s,
 
     return NULL;
 }
+
+bool
+ofctrl_is_connected(void)
+{
+    return rconn_is_connected(swconn);
+}
