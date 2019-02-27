228355c5390f42ea8e90fb4b943f4da553400cb7
Alin Gabriel Serdean <aserdean@ovn.org>
2019-02-27 19:26:46 +0200

    datapath-windows: Add annotations to find vport functions
    Add annotations to find vport functions to check if the dispatch lock is
    held.
    
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>
    Acked-by: Anand Kumar <kumaranand@vmware.com>
    Acked-by: Sairam Venugopal <vsairam@vmware.com>

--- datapath-windows/ovsext/Vport.c
+++ datapath-windows/ovsext/Vport.c
@@ -135,7 +135,7 @@ HvCreatePort(POVS_SWITCH_CONTEXT switchContext,
      * Lookup by port name to see if this port with this name had been added
      * (and deleted) previously.
      */
-    vport = OvsFindVportByHvNameW(gOvsSwitchContext,
+    vport = OvsFindVportByHvNameW(switchContext,
                                   portParam->PortFriendlyName.String,
                                   portParam->PortFriendlyName.Length);
     if (vport && vport->isAbsentOnHv == FALSE) {
@@ -692,6 +692,7 @@ done:
 /*
  * OVS Vport related functionality.
  */
+_Use_decl_annotations_
 POVS_VPORT_ENTRY
 OvsFindVportByPortNo(POVS_SWITCH_CONTEXT switchContext,
                      UINT32 portNo)
@@ -786,6 +787,7 @@ OvsFindTunnelVportByPortType(POVS_SWITCH_CONTEXT switchContext,
     return NULL;
 }
 
+_Use_decl_annotations_
 POVS_VPORT_ENTRY
 OvsFindVportByOvsName(POVS_SWITCH_CONTEXT switchContext,
                       PSTR name)
@@ -809,6 +811,7 @@ OvsFindVportByOvsName(POVS_SWITCH_CONTEXT switchContext,
 }
 
 /* OvsFindVportByHvName: "name" is assumed to be null-terminated */
+_Use_decl_annotations_
 POVS_VPORT_ENTRY
 OvsFindVportByHvNameW(POVS_SWITCH_CONTEXT switchContext,
                       PWSTR wsName, SIZE_T wstrSize)
@@ -861,6 +864,7 @@ Cleanup:
     return vport;
 }
 
+_Use_decl_annotations_
 POVS_VPORT_ENTRY
 OvsFindVportByHvNameA(POVS_SWITCH_CONTEXT switchContext,
                       PSTR name)
@@ -883,6 +887,7 @@ OvsFindVportByHvNameA(POVS_SWITCH_CONTEXT switchContext,
     return vport;
 }
 
+_Use_decl_annotations_
 POVS_VPORT_ENTRY
 OvsFindVportByPortIdAndNicIndex(POVS_SWITCH_CONTEXT switchContext,
                                 NDIS_SWITCH_PORT_ID portId,
