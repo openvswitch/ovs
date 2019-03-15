openvswitch/ovs

commit 29718ad49d61e1ab32d23d27225ec7368f1824bb

Remove support for OpenFlow 1.6 (draft).
ONF abandoned the OpenFlow specification, so that OpenFlow 1.6 will never
be completed.  It did not contain much in the way of useful features, so
remove what support Open vSwitch already had.

Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Justin Pettit <jpettit@ovn.org>

include/openvswitch/ofp-protocol.h
@@ -79,12 +79,10 @@ enum ofputil_protocol {
     OFPUTIL_P_OF13_OXM      = 1 << 6,
     OFPUTIL_P_OF14_OXM      = 1 << 7,
     OFPUTIL_P_OF15_OXM      = 1 << 8,
-    OFPUTIL_P_OF16_OXM      = 1 << 9,
 #define OFPUTIL_P_ANY_OXM (OFPUTIL_P_OF12_OXM | \
                            OFPUTIL_P_OF13_OXM | \
                            OFPUTIL_P_OF14_OXM | \
-                           OFPUTIL_P_OF15_OXM | \
-                           OFPUTIL_P_OF16_OXM)
+                           OFPUTIL_P_OF15_OXM)
 
 #define OFPUTIL_P_NXM_OF11_UP (OFPUTIL_P_OF10_NXM_ANY | OFPUTIL_P_OF11_STD | \
                                OFPUTIL_P_ANY_OXM)
@@ -96,8 +94,7 @@ enum ofputil_protocol {
 #define OFPUTIL_P_OF12_UP (OFPUTIL_P_OF12_OXM | OFPUTIL_P_OF13_UP)
 #define OFPUTIL_P_OF13_UP (OFPUTIL_P_OF13_OXM | OFPUTIL_P_OF14_UP)
 #define OFPUTIL_P_OF14_UP (OFPUTIL_P_OF14_OXM | OFPUTIL_P_OF15_UP)
-#define OFPUTIL_P_OF15_UP (OFPUTIL_P_OF15_OXM | OFPUTIL_P_OF16_UP)
-#define OFPUTIL_P_OF16_UP OFPUTIL_P_OF16_OXM
+#define OFPUTIL_P_OF15_UP OFPUTIL_P_OF15_OXM
 
     /* All protocols. */
 #define OFPUTIL_P_ANY ((1 << 10) - 1)

build-aux/extract-ofp-errors
@@ -12,8 +12,7 @@ version_map = {"1.0": 0x01,
                "1.2": 0x03,
                "1.3": 0x04,
                "1.4": 0x05,
-               "1.5": 0x06,
-               "1.6": 0x07}
+               "1.5": 0x06}
 version_reverse_map = dict((v, k) for (k, v) in version_map.items())
 
 token = None

build-aux/extract-ofp-msgs
@@ -12,8 +12,7 @@ VERSION = {"1.0": 0x01,
            "1.2": 0x03,
            "1.3": 0x04,
            "1.4": 0x05,
-           "1.5": 0x06,
-           "1.6": 0x07}
+           "1.5": 0x06}
 
 NX_VENDOR_ID = 0x00002320
 ONF_VENDOR_ID = 0x4f4e4600

include/openflow/automake.mk
@@ -9,7 +9,6 @@ openflowinclude_HEADERS = \
 	include/openflow/openflow-1.3.h \
 	include/openflow/openflow-1.4.h \
 	include/openflow/openflow-1.5.h \
-	include/openflow/openflow-1.6.h \
 	include/openflow/openflow-common.h \
 	include/openflow/openflow.h
 

include/openflow/openflow-1.0.h
@@ -21,11 +21,6 @@
 
 #include <openflow/openflow-common.h>
 
-/* Maximum name of a port.
- *
- * OpenFlow 1.6 (draft) increases this to 64. */
-#define OFP10_MAX_PORT_NAME_LEN  16
-
 /* Port number(s)   meaning
  * ---------------  --------------------------------------
  * 0x0000           not assigned a meaning by OpenFlow 1.0
@@ -102,7 +97,7 @@ enum ofp10_port_features {
 struct ofp10_phy_port {
     ovs_be16 port_no;
     struct eth_addr hw_addr;
-    char name[OFP10_MAX_PORT_NAME_LEN]; /* Null-terminated */
+    char name[OFP_MAX_PORT_NAME_LEN]; /* Null-terminated */
 
     ovs_be32 config;        /* Bitmap of OFPPC_* and OFPPC10_* flags. */
     ovs_be32 state;         /* Bitmap of OFPPS_* and OFPPS10_* flags. */

include/openflow/openflow-1.1.h
@@ -113,7 +113,7 @@ struct ofp11_port {
     uint8_t pad[4];
     struct eth_addr hw_addr;
     uint8_t pad2[2];                  /* Align to 64 bits. */
-    char name[OFP10_MAX_PORT_NAME_LEN]; /* Null-terminated */
+    char name[OFP_MAX_PORT_NAME_LEN]; /* Null-terminated */
 
     ovs_be32 config;        /* Bitmap of OFPPC_* flags. */
     ovs_be32 state;         /* Bitmap of OFPPS_* and OFPPS11_* flags. */

include/openflow/openflow-1.4.h
@@ -80,7 +80,7 @@ struct ofp14_port {
     uint8_t pad[2];
     struct eth_addr hw_addr;
     uint8_t pad2[2];                  /* Align to 64 bits. */
-    char name[OFP10_MAX_PORT_NAME_LEN]; /* Null-terminated */
+    char name[OFP_MAX_PORT_NAME_LEN]; /* Null-terminated */
 
     ovs_be32 config;        /* Bitmap of OFPPC_* flags. */
     ovs_be32 state;         /* Bitmap of OFPPS_* flags. */

include/openflow/openflow-1.6.h
@@ -1,91 +1,0 @@
-/* Copyright (c) 2008 The Board of Trustees of The Leland Stanford
- * Junior University
- * Copyright (c) 2011, 2013, 2014 Open Networking Foundation
- *
- * We are making the OpenFlow specification and associated documentation
- * (Software) available for public use and benefit with the expectation
- * that others will use, modify and enhance the Software and contribute
- * those enhancements back to the community. However, since we would
- * like to make the Software available for broadest use, with as few
- * restrictions as possible permission is hereby granted, free of
- * charge, to any person obtaining a copy of this Software to deal in
- * the Software under the copyrights without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * The name and trademarks of copyright holder(s) may NOT be used in
- * advertising or publicity pertaining to the Software or any
- * derivatives without specific, written prior permission.
- */
-
-/*
- * Copyright (c) 2017 Nicira, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef OPENFLOW_16_H
-#define OPENFLOW_16_H 1
-
-#include <openflow/openflow-common.h>
-
-#define OFP16_MAX_PORT_NAME_LEN  64
-
-struct ofp16_port {
-    ovs_be32 port_no;
-    ovs_be16 length;
-    uint8_t pad[2];                   /* Align to 64 bits. */
-    struct eth_addr hw_addr;          /* EUI-48 hardware address. */
-    uint8_t pad2[2];                  /* Align to 64 bits. */
-    struct eth_addr64 hw_addr64;      /* EUI-64 hardware address */
-    char name[OFP16_MAX_PORT_NAME_LEN]; /* Null-terminated */
-
-    ovs_be32 config;        /* Bitmap of OFPPC_* flags. */
-    ovs_be32 state;         /* Bitmap of OFPPS_* flags. */
-
-    /* Followed by 0 or more OFPPDPT14_* properties.  (OpenFlow 1.6 (draft)
-     * defines the same properties as OpenFlow 1.4.) */
-};
-OFP_ASSERT(sizeof(struct ofp16_port) == 96);
-
-struct ofp16_port_mod {
-    ovs_be32 port_no;
-    uint8_t pad[4];              /* Align to 64 bits. */
-    struct eth_addr hw_addr;
-    uint8_t pad2[2];
-    struct eth_addr64 hw_addr64; /* EUI-64 hardware address */
-
-    ovs_be32 config;        /* Bitmap of OFPPC_* flags. */
-    ovs_be32 mask;          /* Bitmap of OFPPC_* flags to be changed. */
-
-    /* Followed by 0 or more OFPPMPT14_* properties.  (OpenFlow 1.6 (draft)
-     * defines the same properties as OpenFlow 1.4.) */
-};
-OFP_ASSERT(sizeof(struct ofp16_port_mod) == 32);
-
-
-#endif /* openflow/openflow-1.6.h */

commit 2115f115158ad1c46952186bcfa6de10d1e06632

ofp-protocol: Changed the number of bits in OFPUTIL_P_ANY from 10 to 9.
The removal of support for OpenFlow 1.6 (draft) resulted in the removal of
"OFPUTIL_P_OF16_OXM 1 << 9". OFPUTIL_P_ANY which represets all protocols will
now have only 9 valid bits.

Fixes: 29718ad49d61 ("Remove support for OpenFlow 1.6 (draft).")
Signed-off-by: Ashish Varma <ashishvarma.ovs@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

include/openvswitch/ofp-protocol.h
@@ -97,7 +97,7 @@ enum ofputil_protocol {
 #define OFPUTIL_P_OF15_UP OFPUTIL_P_OF15_OXM
 
     /* All protocols. */
-#define OFPUTIL_P_ANY ((1 << 10) - 1)
+#define OFPUTIL_P_ANY ((1 << 9) - 1)
 
     /* Protocols in which a specific table may be specified in flow_mods. */
 #define OFPUTIL_P_TID (OFPUTIL_P_OF10_STD_TID | \