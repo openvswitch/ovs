e14a9e15f0e93b9654883e8ec8c05273d43e9c8e
Chris Mi <chrism@mellanox.com>
2019-10-16 11:37:14 +0300

    tc: Limit the max action number to 16
    Currently, ovs supports to offload max TCA_ACT_MAX_PRIO(32) actions.
    But net sched api has a limit of 4K message size which is not enough
    for 32 actions when echo flag is set.
    
    After a lot of testing, we find that 16 actions is a reasonable number.
    So in this commit, we introduced a new define to limit the max actions.
    
    Fixes: 0c70132cd288("tc: Make the actions order consistent")
    Signed-off-by: Chris Mi <chrism@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- lib/tc.h
+++ lib/tc.h
@@ -174,6 +174,8 @@ enum tc_offloaded_state {
     TC_OFFLOADED_STATE_NOT_IN_HW,
 };
 
+#define TCA_ACT_MAX_NUM 16
+
 struct tc_flower {
     uint32_t handle;
     uint32_t prio;
@@ -182,7 +184,7 @@ struct tc_flower {
     struct tc_flower_key mask;
 
     int action_count;
-    struct tc_action actions[TCA_ACT_MAX_PRIO];
+    struct tc_action actions[TCA_ACT_MAX_NUM];
 
     struct ovs_flow_stats stats;
     uint64_t lastused;
