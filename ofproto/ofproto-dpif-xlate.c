84dd881bb5be74b16b6177889394e99a49f6be90
Dmytro Linkin <dmitrolin@mellanox.com>
2019-12-03 16:11:21 +0200

    ofproto-dpif-xlate: Prevent duplicating of traffic to a mirror port
    Currently ofproto design disallow duplicating output packet on forwarding
    and mirroring to/from same ovs port. Next scenario reveal lack of design:
    1. Send ping between regular ovs ports (VFs, for ex.), stop it.
    2. While rule still exist, make mirror for one of the ports.
    Prevent duplicating of traffic to a mirror port.
    
    Fixes: 86e2dcddce85 ("dpif-xlate: Snoop multicast packets and send them properly")
    Signed-off-by: Dmytro Linkin <dmitrolin@mellanox.com>
    Acked-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -3118,6 +3118,13 @@ xlate_normal(struct xlate_ctx *ctx)
 
         if (mac_port) {
             struct xbundle *mac_xbundle = xbundle_lookup(ctx->xcfg, mac_port);
+
+            if (mac_xbundle && xbundle_mirror_out(ctx->xbridge, mac_xbundle)) {
+                xlate_report(ctx, OFT_WARN,
+                             "learned port is a mirror port, dropping");
+                return;
+            }
+
             if (mac_xbundle
                 && mac_xbundle != in_xbundle
                 && mac_xbundle->ofbundle != in_xbundle->ofbundle) {
