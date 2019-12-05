3843208ee019b8559dc02a01039b8d43cc4dac0b
Malvika Gupta <malvika.gupta@arm.com>
2019-12-05 11:04:20 -0600

    dpif-netdev-perf: Accurate cycle counter update
    The accurate timing implementation in this patch gets the wall clock counter via
    cntvct_el0 register access. This call is portable to all aarch64 architectures
    and has been verified on an 64-bit arm server.
    
    Suggested-by: Yanqin Wei <yanqin.wei@arm.com>
    Reviewed-by: Ilya Maximets <i.maximets@ovn.org>
    Signed-off-by: Malvika Gupta <malvika.gupta@arm.com>
    Signed-off-by: Ben Pfaff <blp@ovn.org>

--- lib/dpif-netdev-perf.h
+++ lib/dpif-netdev-perf.h
@@ -220,6 +220,10 @@ cycles_counter_update(struct pmd_perf_stats *s)
     asm volatile("rdtsc" : "=a" (l), "=d" (h));
 
     return s->last_tsc = ((uint64_t) h << 32) | l;
+#elif !defined(_MSC_VER) && defined(__aarch64__)
+    asm volatile("mrs %0, cntvct_el0" : "=r" (s->last_tsc));
+
+    return s->last_tsc;
 #elif defined(__linux__)
     return rdtsc_syscall(s);
 #else
