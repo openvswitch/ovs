bdfab59371f8028d25e4943df20066c3055fa94e
Lance Yang <Lance.Yang@arm.com>
2019-12-06 11:26:12 +0800

    travis: Move x86-only add-on packages to linux-prepare script.
    To enable multiple CPU architectures support, it is necessary to move
    the x86-only add-on packages from .travis.yml file. Otherwise, the
    x86-only add-on packages will break the builds on some other CPU
    architectures.
    
    Reviewed-by: Yanqin Wei <Yanqin.Wei@arm.com>
    Reviewed-by: Malvika Gupta <Malvika.Gupta@arm.com>
    Reviewed-by: Gavin Hu <Gavin.Hu@arm.com>
    Reviewed-by: Ruifeng Wang <Ruifeng.Wang@arm.com>
    Acked-by: David Wilder <dwilder@us.ibm.com>
    Signed-off-by: Lance Yang <Lance.Yang@arm.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- .travis/linux-prepare.sh
+++ .travis/linux-prepare.sh
@@ -16,9 +16,11 @@ pip install --disable-pip-version-check --user six flake8 hacking
 pip install --user --upgrade docutils
 
 if [ "$M32" ]; then
+    # Installing 32-bit libraries.
     # 32-bit and 64-bit libunwind can not be installed at the same time.
     # This will remove the 64-bit libunwind and install 32-bit version.
-    sudo apt-get install -y libunwind-dev:i386
+    sudo apt-get install -y \
+        libunwind-dev:i386 libunbound-dev:i386 gcc-multilib
 fi
 
 # IPv6 is supported by kernel but disabled in TravisCI images:
