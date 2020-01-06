2dec526496a62c3aaf849f24ec1b5983a91827a9
Greg Rose <gvrose8192@gmail.com>
2020-01-06 13:36:34 -0800

    compat: Include confirm_neigh parameter if needed
    A change backported to the Linux 4.14.162 LTS kernel requires
    a boolean parameter.  Check for the presence of the parameter
    and adjust the caller in that case.
    
    Passes check-kmod test with no regressions.
    
    Passes Travis build here:
    https://travis-ci.org/gvrose8192/ovs-experimental/builds/633461320
    
    Signed-off-by: Greg Rose <gvrose8192@gmail.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- datapath/linux/compat/ip6_gre.c
+++ datapath/linux/compat/ip6_gre.c
@@ -1089,7 +1089,11 @@ static netdev_tx_t ip6erspan_tunnel_xmit(struct sk_buff *skb,
 
 	/* TooBig packet may have updated dst->dev's mtu */
 	if (!t->parms.collect_md && dst && dst_mtu(dst) > dst->dev->mtu)
+#ifndef HAVE_DST_OPS_CONFIRM_NEIGH
 		dst->ops->update_pmtu(dst, NULL, skb, dst->dev->mtu);
+#else
+		dst->ops->update_pmtu(dst, NULL, skb, dst->dev->mtu, false);
+#endif
 
 	err = ip6_tnl_xmit(skb, dev, dsfield, &fl6, encap_limit, &mtu,
 			   NEXTHDR_GRE);
