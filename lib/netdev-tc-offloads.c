c5eee1f9f166533c943f79c8a5606758c0a61a80
Ilya Maximets <i.maximets@ovn.org>
2020-01-14 21:18:14 +0100

    netdev-offload-tc: Fix crash if offloading is not configured on outdev.
    If output device is not yet added to netdev-offload, netdev_ports_get()
    will not find it leading to NULL pointer dereference inside
    netdev_get_ifindex().
    
    Fixes: 8f283af89298 ("netdev-tc-offloads: Implement netdev flow put using tc interface")
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
    Acked-by: Ben Pfaff <blp@ovn.org>

--- lib/netdev-tc-offloads.c
+++ lib/netdev-tc-offloads.c
@@ -1088,6 +1088,10 @@ netdev_tc_flow_put(struct netdev *netdev, struct match *match,
             odp_port_t port = nl_attr_get_odp_port(nla);
             struct netdev *outdev = netdev_ports_get(port, info->dpif_class);
 
+            if (!outdev) {
+                VLOG_DBG_RL(&rl, "Can't find netdev for output port %d", port);
+                return ENODEV;
+            }
             action->ifindex_out = netdev_get_ifindex(outdev);
             action->type = TC_ACT_OUTPUT;
             flower.action_count++;
