d69d4c05376661c8716374355ec1a84b8b123905
Ilya Maximets <i.maximets@ovn.org>
2020-03-12 10:57:44 +0100

    travis: Disable sindex build in sparse.
    Sparse introduced a new utility 'sindex' for semantic search,
    but unfortunately it fails to build in Travis environment.
    Disabling it explicitly as we don't need it anyway.
    
    Acked-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- .travis/linux-prepare.sh
+++ .travis/linux-prepare.sh
@@ -7,9 +7,11 @@ set -ev
 # Explicitly disable sparse support for llvm because some travis
 # environments claim to have LLVM (llvm-config exists and works) but
 # linking against it fails.
+# Disabling sqlite support because sindex build fails and we don't
+# really need this utility being installed.
 git clone git://git.kernel.org/pub/scm/devel/sparse/sparse.git
 cd sparse
-make -j4 HAVE_LLVM= install
+make -j4 HAVE_LLVM= HAVE_SQLITE= install
 cd ..
 
 pip3 install --disable-pip-version-check --user flake8 hacking
