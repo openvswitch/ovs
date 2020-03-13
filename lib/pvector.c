openvswitch/ovs

commit fe7cfa5c3f195b477f0d6ce499315415b2604b67

lib/pvector: Non-intrusive RCU priority vector.
Factor out the priority vector code from the classifier.

Making the classifier use RCU instead of locking requires parallel
access to the priority vector, pointing to subtables in descending
priority order.  When a new subtable is added, a new copy of the
priority vector is allocated, while the current readers can keep on
using the old copy they started with.  Adding and removing subtables
is usually less frequent than adding and removing rules, so this
should not have a visible performance implication.  As an optimization
for the userspace datapath use, where all the subtables have the same
priority, new subtables can be added to the end of the vector without
reallocation and without disturbing readers.

cls_subtables_reset() is now removed, as it served its purpose in bug
hunting.  Checks on the new pvector are now incorporated into
tests/test-classifier.c.

Signed-off-by: Jarno Rajahalme <jrajahalme@nicira.com>
Acked-by: Ben Pfaff <blp@nicira.com>

lib/pvector.c
@@ -1,0 +1,210 @@
+/*
+ * Copyright (c) 2014 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+#include "pvector.h"
+
+static struct pvector_impl *
+pvector_impl_get(const struct pvector *pvec)
+{
+    return ovsrcu_get(struct pvector_impl *, &pvec->impl);
+}
+
+static struct pvector_impl *
+pvector_impl_alloc(size_t size)
+{
+    struct pvector_impl *impl;
+
+    impl = xmalloc(sizeof *impl + size * sizeof impl->vector[0]);
+    impl->size = 0;
+    impl->allocated = size;
+
+    return impl;
+}
+
+static struct pvector_impl *
+pvector_impl_dup(struct pvector_impl *old)
+{
+    return xmemdup(old, sizeof *old + old->allocated * sizeof old->vector[0]);
+}
+
+/* Initializes 'pvec' as an empty concurrent priority vector. */
+void
+pvector_init(struct pvector *pvec)
+{
+    ovsrcu_set(&pvec->impl, pvector_impl_alloc(PVECTOR_EXTRA_ALLOC));
+}
+
+/* Destroys 'pvec'.
+ *
+ * The client is responsible for destroying any data previously held in
+ * 'pvec'. */
+void
+pvector_destroy(struct pvector *pvec)
+{
+    ovsrcu_postpone(free, pvector_impl_get(pvec));
+    ovsrcu_set(&pvec->impl, NULL); /* Poison. */
+}
+
+/* Iterators for callers that need the 'index' afterward. */
+#define PVECTOR_IMPL_FOR_EACH(ENTRY, INDEX, IMPL)          \
+    for ((INDEX) = 0;                                      \
+         (INDEX) < (IMPL)->size                            \
+             && ((ENTRY) = &(IMPL)->vector[INDEX], true);  \
+         (INDEX)++)
+
+static int
+pvector_entry_cmp(const void *a_, const void *b_)
+{
+    unsigned int a = ((const struct pvector_entry *)a_)->priority;
+    unsigned int b = ((const struct pvector_entry *)b_)->priority;
+
+    return a > b ? -1 : a < b;
+}
+
+static void
+pvector_impl_sort(struct pvector_impl *impl)
+{
+    qsort(impl->vector, impl->size, sizeof *impl->vector, pvector_entry_cmp);
+}
+
+/* Returns the index with priority equal or lower than 'target_priority',
+ * which will be one past the vector if none exists. */
+static int
+pvector_impl_find_priority(struct pvector_impl *impl,
+                           unsigned int target_priority)
+{
+    const struct pvector_entry *entry;
+    int index;
+
+    PVECTOR_IMPL_FOR_EACH (entry, index, impl) {
+        if (entry->priority <= target_priority) {
+            break;
+        }
+    }
+    return index;
+}
+
+/* Returns the index of the 'ptr' in the vector, or -1 if none is found. */
+static int
+pvector_impl_find(struct pvector_impl *impl, void *target)
+{
+    const struct pvector_entry *entry;
+    int index;
+
+    PVECTOR_IMPL_FOR_EACH (entry, index, impl) {
+        if (entry->ptr == target) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+void
+pvector_insert(struct pvector *pvec, void *ptr, unsigned int priority)
+{
+    struct pvector_impl *old, *new;
+    int index;
+
+    ovs_assert(ptr != NULL);
+
+    old = pvector_impl_get(pvec);
+
+    /* Check if can add to the end without reallocation. */
+    if (old->allocated > old->size &&
+        (!old->size || priority <= old->vector[old->size - 1].priority)) {
+        old->vector[old->size].ptr = ptr;
+        old->vector[old->size].priority = priority;
+        /* Size increment must not be visible to the readers before the new
+         * entry is stored. */
+        atomic_thread_fence(memory_order_release);
+        ++old->size;
+    } else {
+        new = pvector_impl_alloc(old->size + 1 + PVECTOR_EXTRA_ALLOC);
+
+        index = pvector_impl_find_priority(old, priority);
+        /* Now at the insertion index. */
+        memcpy(new->vector, old->vector, index * sizeof old->vector[0]);
+        new->vector[index].ptr = ptr;
+        new->vector[index].priority = priority;
+        memcpy(&new->vector[index + 1], &old->vector[index],
+               (old->size - index) * sizeof old->vector[0]);
+        new->size = old->size + 1;
+
+        ovsrcu_set(&pvec->impl, new);
+        ovsrcu_postpone(free, old);
+    }
+}
+
+void
+pvector_remove(struct pvector *pvec, void *ptr)
+{
+    struct pvector_impl *old, *new;
+    int index;
+
+    old = pvector_impl_get(pvec);
+
+    ovs_assert(old->size > 0);
+
+    index = pvector_impl_find(old, ptr);
+    ovs_assert(index >= 0);
+    /* Now at the index of the entry to be deleted. */
+
+    /* We do not try to delete the last entry without reallocation so that
+     * the readers can read the 'size' once in the beginning of each iteration.
+     */
+
+    /* Keep extra space for insertions to the end. */
+    new = pvector_impl_alloc(old->size - 1 + PVECTOR_EXTRA_ALLOC);
+
+    memcpy(new->vector, old->vector, index * sizeof old->vector[0]);
+    memcpy(&new->vector[index], &old->vector[index + 1],
+           (old->size - (index + 1)) * sizeof old->vector[0]);
+
+    new->size = old->size - 1;
+
+    ovsrcu_set(&pvec->impl, new);
+    ovsrcu_postpone(free, old);
+}
+
+/* Change entry's 'priority' and keep the vector ordered. */
+void
+pvector_change_priority(struct pvector *pvec, void *ptr, unsigned int priority)
+{
+    struct pvector_impl *old = pvector_impl_get(pvec);
+    int index = pvector_impl_find(old, ptr);
+
+    ovs_assert(index >= 0);
+    /* Now at the index of the entry to be updated. */
+
+    if ((priority > old->vector[index].priority && index > 0
+         && priority > old->vector[index - 1].priority)
+        || (priority < old->vector[index].priority && index < old->size - 1
+            && priority < old->vector[index + 1].priority)) {
+        /* Have to reallocate to reorder. */
+        struct pvector_impl *new = pvector_impl_dup(old);
+
+        new->vector[index].priority = priority;
+        pvector_impl_sort(new);
+
+        ovsrcu_set(&pvec->impl, new);
+        ovsrcu_postpone(free, old);
+    } else {
+        /* Can update in place. Readers are free to use either value,
+         * so we do not try to synchronize here. */
+        old->vector[index].priority = priority;
+    }
+}

lib/pvector.h
@@ -1,0 +1,210 @@
+/*
+ * Copyright (c) 2014 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PVECTOR_H
+#define PVECTOR_H 1
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "ovs-rcu.h"
+#include "util.h"
+
+/* Concurrent Priority Vector
+ * ==========================
+ *
+ * Concurrent priority vector holds non-NULL pointers to objects in an
+ * increasing priority order and allows readers to traverse the vector without
+ * being concerned about writers modifying the vector as they are traversing
+ * it.
+ *
+ * The priority order is maintained as a linear vector of elements to allow
+ * for efficient memory prefetching.
+ *
+ * Concurrency is implemented with OVS RCU so that the readers can assume
+ * that once they have taken a pointer to the vector with
+ * pvector_cursor_init(), the 'size' member will not decrease, so that
+ * they can safely read 'size' entries from 'vector', and find that each
+ * entry has a valid, non-NULL 'ptr', and the vector is in order from highest
+ * to lowest 'priority'.  The 'priority' values can change any time, but only
+ * so that the order of the entries does not change, so readers can use
+ * 'priority' values read at any time after acquisition of the vector pointer.
+ *
+ * Writers can concurrently add entries to the end of the vector, incrementing
+ * 'size', or update the 'priority' value of an entry, but only if that does
+ * not change the ordering of the entries.  Writers will never change the 'ptr'
+ * values, or decrement the 'size' on a copy that readers have access to.
+ */
+
+struct pvector_entry {
+    unsigned int priority;
+    void *ptr;
+};
+
+/* Writers will preallocate space for some entries at the end to avoid future
+ * reallocations. */
+enum { PVECTOR_EXTRA_ALLOC = 4 };
+
+struct pvector_impl {
+    size_t size;       /* Number of entries in the vector. */
+    size_t allocated;  /* Number of allocated entries. */
+    struct pvector_entry vector[];
+};
+
+/* Concurrent priority vector. */
+struct pvector {
+    OVSRCU_TYPE(struct pvector_impl *) impl;
+};
+
+/* Initialization. */
+void pvector_init(struct pvector *);
+void pvector_destroy(struct pvector *);
+
+/* Count. */
+static inline size_t pvector_count(const struct pvector *);
+static inline bool pvector_is_empty(const struct pvector *);
+
+/* Insertion and deletion. */
+void pvector_insert(struct pvector *, void *, unsigned int);
+void pvector_change_priority(struct pvector *, void *, unsigned int);
+void pvector_remove(struct pvector *, void *);
+
+/* Iteration.
+ *
+ *
+ * Thread-safety
+ * =============
+ *
+ * Iteration is safe even in a pvector that is changing concurrently.
+ * Multiple writers must exclude each other via e.g., a mutex.
+ *
+ * Example
+ * =======
+ *
+ *     struct my_node {
+ *         int data;
+ *     };
+ *
+ *     struct my_node elem1, elem2, *iter;
+ *     struct pvector my_pvector;
+ *
+ *     pvector_init(&my_pvector);
+ *     ...add data...
+ *     pvector_insert(&my_pvector, &elem1, 1);
+ *     pvector_insert(&my_pvector, &elem2, 2);
+ *     ...
+ *     PVECTOR_FOR_EACH (iter, &my_pvector) {
+ *         ...operate on '*iter'...
+ *         ...elem2 to be seen before elem1...
+ *     }
+ *     ...
+ *     pvector_destroy(&my_pvector);
+ *
+ * There is no PVECTOR_FOR_EACH_SAFE variant as iteration is performed on RCU
+ * protected instance of the priority vector.  Any concurrent modifications
+ * that would be disruptive for readers (such as deletions), will be performed
+ * on a new instance.  To see any of the modifications, a new iteration loop
+ * has to be started.
+ *
+ * The PVECTOR_FOR_EACH_PRIORITY limits the iteration to entries with higher
+ * than given priority and allows for object lookahead.
+ *
+ * The iteration loop must be completed without entering the OVS RCU quiescent
+ * period.  That is, an old iteration loop must not be continued after any
+ * blocking IO (VLOG is non-blocking, so that is OK).
+ */
+struct pvector_cursor {
+    size_t size;        /* Number of entries in the vector. */
+    size_t entry_idx;   /* Current index. */
+    const struct pvector_entry *vector;
+};
+
+static inline struct pvector_cursor pvector_cursor_init(const struct pvector *,
+                                                        size_t n_ahead,
+                                                        size_t obj_size);
+static inline void *pvector_cursor_next(struct pvector_cursor *,
+                                        int64_t stop_at_priority,
+                                        size_t n_ahead, size_t obj_size);
+static inline void pvector_cursor_lookahead(const struct pvector_cursor *,
+                                            int n, size_t size);
+
+#define PVECTOR_FOR_EACH(PTR, PVECTOR)                                  \
+    for (struct pvector_cursor cursor__ = pvector_cursor_init(PVECTOR, 0, 0); \
+         ((PTR) = pvector_cursor_next(&cursor__, -1, 0, 0)) != NULL; )
+
+/* Loop while priority is higher than 'PRIORITY' and prefetch objects
+ * of size 'SZ' 'N' objects ahead from the current object. */
+#define PVECTOR_FOR_EACH_PRIORITY(PTR, PRIORITY, N, SZ, PVECTOR)        \
+    for (struct pvector_cursor cursor__ = pvector_cursor_init(PVECTOR, N, SZ); \
+         ((PTR) = pvector_cursor_next(&cursor__, PRIORITY, N, SZ)) != NULL; )
+
+
+/* Inline implementations. */
+
+static inline struct pvector_cursor
+pvector_cursor_init(const struct pvector *pvec,
+                    size_t n_ahead, size_t obj_size)
+{
+    const struct pvector_impl *impl;
+    struct pvector_cursor cursor;
+
+    impl = ovsrcu_get(struct pvector_impl *, &pvec->impl);
+
+    ovs_prefetch_range(impl->vector, impl->size * sizeof impl->vector[0]);
+
+    cursor.size = impl->size;
+    cursor.vector = impl->vector;
+    cursor.entry_idx = -1;
+
+    for (size_t i = 0; i < n_ahead; i++) {
+        /* Prefetch the first objects. */
+        pvector_cursor_lookahead(&cursor, i, obj_size);
+    }
+    return cursor;
+}
+
+static inline void *pvector_cursor_next(struct pvector_cursor *cursor,
+                                        int64_t stop_at_priority,
+                                        size_t n_ahead, size_t obj_size)
+{
+    if (++cursor->entry_idx < cursor->size &&
+        cursor->vector[cursor->entry_idx].priority > stop_at_priority) {
+        if (n_ahead) {
+            pvector_cursor_lookahead(cursor, n_ahead, obj_size);
+        }
+        return cursor->vector[cursor->entry_idx].ptr;
+    }
+    return NULL;
+}
+
+static inline void pvector_cursor_lookahead(const struct pvector_cursor *cursor,
+                                            int n, size_t size)
+{
+    if (cursor->entry_idx + n < cursor->size) {
+        ovs_prefetch_range(cursor->vector[cursor->entry_idx + n].ptr, size);
+    }
+}
+
+static inline size_t pvector_count(const struct pvector *pvec)
+{
+    return ovsrcu_get(struct pvector_impl *, &pvec->impl)->size;
+}
+
+static inline bool pvector_is_empty(const struct pvector *pvec)
+{
+    return pvector_count(pvec) == 0;
+}
+
+#endif /* pvector.h */

lib/automake.mk
@@ -177,6 +177,8 @@ lib_libopenvswitch_la_SOURCES = \
 	lib/poll-loop.h \
 	lib/process.c \
 	lib/process.h \
+	lib/pvector.c \
+	lib/pvector.h \
 	lib/random.c \
 	lib/random.h \
 	lib/rconn.c \

tests/test-classifier.c
@@ -467,6 +467,22 @@ destroy_classifier(struct classifier *cls)
 }
 
 static void
+pvector_verify(struct pvector *pvec)
+{
+    void *ptr OVS_UNUSED;
+    unsigned int priority, prev_priority = UINT_MAX;
+
+    PVECTOR_FOR_EACH (ptr, pvec) {
+        priority = cursor__.vector[cursor__.entry_idx].priority;
+        if (priority > prev_priority) {
+            VLOG_ABORT("Priority vector is out of order (%u > %u)",
+                       priority, prev_priority);
+        }
+        prev_priority = priority;
+    }
+}
+
+static void
 check_tables(const struct classifier *cls, int n_tables, int n_rules,
              int n_dups) OVS_REQ_RDLOCK(cls->rwlock)
 {
@@ -478,10 +494,28 @@ check_tables(const struct classifier *cls, int n_tables, int n_rules,
     int found_dups = 0;
     int found_rules2 = 0;
 
+    pvector_verify(&cls->cls->subtables);
+
     HMAP_FOR_EACH (table, hmap_node, &cls->cls->subtables_map) {
         const struct cls_match *head;
         unsigned int max_priority = 0;
         unsigned int max_count = 0;
+        bool found = false;
+        const struct cls_subtable *iter;
+
+        /* Locate the subtable from 'subtables'. */
+        PVECTOR_FOR_EACH (iter, &cls->cls->subtables) {
+            if (iter == table) {
+                if (found) {
+                    VLOG_ABORT("Subtable %p duplicated in 'subtables'.",
+                               table);
+                }
+                found = true;
+            }
+        }
+        if (!found) {
+            VLOG_ABORT("Subtable %p not found from 'subtables'.", table);
+        }
 
         assert(!hmap_is_empty(&table->rules));
 
@@ -514,6 +548,7 @@ check_tables(const struct classifier *cls, int n_tables, int n_rules,
     }
 
     assert(found_tables == hmap_count(&cls->cls->subtables_map));
+    assert(found_tables == pvector_count(&cls->cls->subtables));
     assert(n_tables == -1 || n_tables == hmap_count(&cls->cls->subtables_map));
     assert(n_rules == -1 || found_rules == n_rules);
     assert(n_dups == -1 || found_dups == n_dups);

commit c50eaa3d0b9d8325cd3b53f52105465b5bf9bce9

pvector: Use acquire-release semantics for size.
Read/write concurrency of pvector library is implemented by a temp vector
and RCU protection. Considering performance reason, insertion does not
follow this scheme.
In insertion function, a thread fence ensures size increment is done
after new entry is stored. But there is no barrier in the iteration
fuction(pvector_cursor_init). Entry point access may be reordered before
loading vector size, so the invalid entry point may be loaded when vector
iteration.
This patch fixes it by acquire-release pair. It can guarantee new size is
observed by reader after new entry stored by writer. And this is
implemented by one-way barrier instead of two-way memory fence.

Fixes: fe7cfa5c3f19 ("lib/pvector: Non-intrusive RCU priority vector.")
Reviewed-by: Gavin Hu <Gavin.Hu@arm.com>
Reviewed-by: Lijian Zhang <Lijian.Zhang@arm.com>
Signed-off-by: Yanqin Wei <Yanqin.Wei@arm.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

lib/pvector.c
@@ -33,7 +33,7 @@ pvector_impl_alloc(size_t size)
     struct pvector_impl *impl;
 
     impl = xmalloc(sizeof *impl + size * sizeof impl->vector[0]);
-    impl->size = 0;
+    atomic_init(&impl->size, 0);
     impl->allocated = size;
 
     return impl;
@@ -117,18 +117,22 @@ pvector_insert(struct pvector *pvec, void *ptr, int priority)
 {
     struct pvector_impl *temp = pvec->temp;
     struct pvector_impl *old = pvector_impl_get(pvec);
+    size_t size;
 
     ovs_assert(ptr != NULL);
 
+    /* There is no possible concurrent writer. Insertions must be protected
+     * by mutex or be always excuted from the same thread. */
+    atomic_read_relaxed(&old->size, &size);
+
     /* Check if can add to the end without reallocation. */
-    if (!temp && old->allocated > old->size &&
-        (!old->size || priority <= old->vector[old->size - 1].priority)) {
-        old->vector[old->size].ptr = ptr;
-        old->vector[old->size].priority = priority;
+    if (!temp && old->allocated > size &&
+        (!size || priority <= old->vector[size - 1].priority)) {
+        old->vector[size].ptr = ptr;
+        old->vector[size].priority = priority;
         /* Size increment must not be visible to the readers before the new
          * entry is stored. */
-        atomic_thread_fence(memory_order_release);
-        ++old->size;
+        atomic_store_explicit(&old->size, size + 1, memory_order_release);
     } else {
         if (!temp) {
             temp = pvector_impl_dup(old);

lib/pvector.h
@@ -69,8 +69,8 @@ struct pvector_entry {
 };
 
 struct pvector_impl {
-    size_t size;       /* Number of entries in the vector. */
-    size_t allocated;  /* Number of allocated entries. */
+    atomic_size_t size;   /* Number of entries in the vector. */
+    size_t allocated;     /* Number of allocated entries. */
     struct pvector_entry vector[];
 };
 
@@ -181,12 +181,17 @@ pvector_cursor_init(const struct pvector *pvec,
 {
     const struct pvector_impl *impl;
     struct pvector_cursor cursor;
+    size_t size;
 
     impl = ovsrcu_get(struct pvector_impl *, &pvec->impl);
 
-    ovs_prefetch_range(impl->vector, impl->size * sizeof impl->vector[0]);
+    /* Use memory_order_acquire to ensure entry access can not be
+     * reordered to happen before size read. */
+    atomic_read_explicit(&CONST_CAST(struct pvector_impl *, impl)->size,
+                         &size, memory_order_acquire);
+    ovs_prefetch_range(impl->vector, size * sizeof impl->vector[0]);
 
-    cursor.size = impl->size;
+    cursor.size = size;
     cursor.vector = impl->vector;
     cursor.entry_idx = -1;
 