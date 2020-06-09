058b80d3de31b2c539d9e6f5f6687bde78ef08e9
Tonghao Zhang <xiangxia.m.yue@gmail.com>
2020-06-09 08:53:40 +0800

    dpif-netdev: Add check mark to avoid ovs-vswitchd crash.
    When changing the pmd interfaces attribute, ovs-vswitchd will
    reload pmd and flush offload flows. reload_affected_pmds may
    be invoked twice or more. In that case, the flows may been
    queued to "dp_netdev_flow_offload" thread again.
    
    For example:
    $ ovs-vsctl -- set interface <Interface> options:dpdk-lsc-interrupt=true
    
    ovs-vswitchd main       flow-offload thread
    append F to queue       ...
    ...
    append F to queue
    ...                     del F
    ...                     del F (crash [1])
    
    [1]:
    ovs_assert_failure          lib/cmap.c:922
    cmap_replace                lib/cmap.c:921
    cmap_remove                 lib/cmap.h:295
    mark_to_flow_disassociate   lib/dpif-netdev.c:2269
    dp_netdev_flow_offload_del  lib/dpif-netdev.c:2369
    dp_netdev_flow_offload_main lib/dpif-netdev.c:2492
    
    Fixes: 02bb2824e51d ("dpif-netdev: do hw flow offload in a thread")
    Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -2389,6 +2389,12 @@ mark_to_flow_disassociate(struct dp_netdev_pmd_thread *pmd,
     uint32_t mark = flow->mark;
     int ret = 0;
 
+    /* INVALID_FLOW_MARK may mean that the flow has been disassociated or
+     * never associated. */
+    if (OVS_UNLIKELY(mark == INVALID_FLOW_MARK)) {
+        return EINVAL;
+    }
+
     cmap_remove(&flow_mark.mark_to_flow, mark_node, hash_int(mark, 0));
     flow->mark = INVALID_FLOW_MARK;
 
