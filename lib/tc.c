d5659751f65ebc17d9aec40b60c1cff3a2d87162
Roi Dayan <roid@mellanox.com>
2020-08-04 09:37:21 +0300

    tc: Use skip_hw flag when probing tc features
    There is no need to pass tc rules to hw when just probing
    for tc features. this will avoid redundant errors from hw drivers
    that may happen.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>
    Acked-By: Vlad Buslov <vladbu@mellanox.com>
    Reviewed-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>
    Signed-off-by: Simon Horman <simon.horman@netronome.com>

--- lib/tc.c
+++ lib/tc.c
@@ -65,12 +65,6 @@ VLOG_DEFINE_THIS_MODULE(tc);
 
 static struct vlog_rate_limit error_rl = VLOG_RATE_LIMIT_INIT(60, 5);
 
-enum tc_offload_policy {
-    TC_POLICY_NONE,
-    TC_POLICY_SKIP_SW,
-    TC_POLICY_SKIP_HW
-};
-
 static enum tc_offload_policy tc_policy = TC_POLICY_NONE;
 
 struct tc_pedit_key_ex {
@@ -2757,6 +2751,7 @@ nl_msg_put_flower_options(struct ofpbuf *request, struct tc_flower *flower)
     bool is_vlan = eth_type_vlan(flower->key.eth_type);
     bool is_qinq = is_vlan && eth_type_vlan(flower->key.encap_eth_type[0]);
     bool is_mpls = eth_type_mpls(flower->key.eth_type);
+    enum tc_offload_policy policy = flower->tc_policy;
     int err;
 
     /* need to parse acts first as some acts require changing the matching
@@ -2882,7 +2877,11 @@ nl_msg_put_flower_options(struct ofpbuf *request, struct tc_flower *flower)
         }
     }
 
-    nl_msg_put_u32(request, TCA_FLOWER_FLAGS, tc_get_tc_cls_policy(tc_policy));
+    if (policy == TC_POLICY_NONE) {
+        policy = tc_policy;
+    }
+
+    nl_msg_put_u32(request, TCA_FLOWER_FLAGS, tc_get_tc_cls_policy(policy));
 
     if (flower->tunnel) {
         nl_msg_put_flower_tunnel(request, flower);
