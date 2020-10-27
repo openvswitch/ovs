d9f29982eefc05ff444dd5a385199b56d7f8be61
Thomas Neuman <thomas.neuman@nutanix.com>
2020-10-27 17:39:27 +0000

    python: Add 'six' to list of install requirements.
    Fixes: 73eb682edb67 ("python: Fix xmlrpclib imports.")
    Signed-off-by: Thomas Neuman <thomas.neuman@nutanix.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- python/setup.py
+++ python/setup.py
@@ -82,7 +82,7 @@ setup_args = dict(
     ext_modules=[setuptools.Extension("ovs._json", sources=["ovs/_json.c"],
                                       libraries=['openvswitch'])],
     cmdclass={'build_ext': try_build_ext},
-    install_requires=['sortedcontainers'],
+    install_requires=['six', 'sortedcontainers'],
     extras_require={':sys_platform == "win32"': ['pywin32 >= 1.0']},
 )
 
