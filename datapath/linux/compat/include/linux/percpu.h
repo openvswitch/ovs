9ca60e6b126742461d7e15e37b85149da7e2a8e0
Greg Rose <gvrose8192@gmail.com>
2020-11-12 15:10:37 -0800

    compat: Remove stale code.
    Remove stale and unused code left over after support for kernels
    older than 3.10 was removed.
    
    Fixes: 8063e0958780 ("datapath: Drop support for kernel older than 3.10")
    Signed-off-by: Greg Rose <gvrose8192@gmail.com>
    Acked-by: Yi-Hung Wei <yihung.wei@gmail.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- datapath/linux/compat/include/linux/percpu.h
+++ datapath/linux/compat/include/linux/percpu.h
@@ -7,12 +7,6 @@
 #define this_cpu_ptr(ptr) per_cpu_ptr(ptr, smp_processor_id())
 #endif
 
-#ifdef HAVE_RHEL6_PER_CPU
-#undef this_cpu_read
-#undef this_cpu_inc
-#undef this_cpu_dec
-#endif
-
 #if !defined this_cpu_read
 #define this_cpu_read(ptr) percpu_read(ptr)
 #endif
