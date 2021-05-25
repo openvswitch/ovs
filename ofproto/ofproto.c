fc23134227f4977f5e0df06f05d114e711f3e056
Dumitru Ceara <dceara@redhat.com>
2021-05-25 17:16:12 +0200

    ofproto: Fix potential NULL dereference in ofproto_get_datapath_cap().
    Reproducer:
      ovs-vsctl \
        -- add-br br \
        -- set bridge br datapath-type=foo \
        -- --id=@m create Datapath datapath_version=0 'capabilities={}' \
        -- set Open_vSwitch . datapaths:"foo"=@m
    
    Fixes: 27501802d09f ("ofproto-dpif: Expose datapath capability to ovsdb.")
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Acked-by: Paolo Valerio <pvalerio@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -961,7 +961,7 @@ ofproto_get_datapath_cap(const char *datapath_type, struct smap *dp_cap)
     datapath_type = ofproto_normalize_type(datapath_type);
     const struct ofproto_class *class = ofproto_class_find__(datapath_type);
 
-    if (class->get_datapath_cap) {
+    if (class && class->get_datapath_cap) {
         class->get_datapath_cap(datapath_type, dp_cap);
     }
 }
