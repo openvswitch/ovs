f36e7438fa42e7b3b9c9404ec775eb8e1c9f31c1
Eli Britstein <elibr@nvidia.com>
2021-06-23 15:52:40 +0000

    netdev-offload: Add HW miss packet state recover API.
    When the HW offload involves multiple flows, like in tunnel decap path,
    it is possible that not all flows in the path are offloaded, resulting
    in partial processing in HW. In order to proceed with rest of the
    processing in SW, the packet state has to be recovered as if it was
    processed in SW from the beginning. In the case of tunnel decap,
    potential state to recover could be the outer tunneling layer to
    metadata.
    Add an API for that.
    
    Signed-off-by: Eli Britstein <elibr@nvidia.com>
    Reviewed-by: Gaetan Rivet <gaetanr@nvidia.com>
    Acked-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
    Tested-by: Emma Finn <emma.finn@intel.com>
    Tested-by: Marko Kovacevic <marko.kovacevic@intel.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/netdev-offload.c
+++ lib/netdev-offload.c
@@ -254,6 +254,18 @@ netdev_flow_put(struct netdev *netdev, struct match *match,
 }
 
 int
+netdev_hw_miss_packet_recover(struct netdev *netdev,
+                              struct dp_packet *packet)
+{
+    const struct netdev_flow_api *flow_api =
+        ovsrcu_get(const struct netdev_flow_api *, &netdev->flow_api);
+
+    return (flow_api && flow_api->hw_miss_packet_recover)
+            ? flow_api->hw_miss_packet_recover(netdev, packet)
+            : EOPNOTSUPP;
+}
+
+int
 netdev_flow_get(struct netdev *netdev, struct match *match,
                 struct nlattr **actions, const ovs_u128 *ufid,
                 struct dpif_flow_stats *stats,
--- lib/netdev-offload.h
+++ lib/netdev-offload.h
@@ -89,6 +89,7 @@ bool netdev_flow_dump_next(struct netdev_flow_dump *, struct match *,
 int netdev_flow_put(struct netdev *, struct match *, struct nlattr *actions,
                     size_t actions_len, const ovs_u128 *,
                     struct offload_info *, struct dpif_flow_stats *);
+int netdev_hw_miss_packet_recover(struct netdev *, struct dp_packet *);
 int netdev_flow_get(struct netdev *, struct match *, struct nlattr **actions,
                     const ovs_u128 *, struct dpif_flow_stats *,
                     struct dpif_flow_attrs *, struct ofpbuf *wbuffer);
