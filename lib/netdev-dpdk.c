f20abde5a2f3e109fbf8acd1683800fd50edc469
Gaetan Rivet <grive@u256.net>
2021-09-08 11:47:51 +0200

    netdev-dpdk: Remove rte-flow API access locks.
    The rte_flow DPDK API was made thread-safe [1] in release 20.11.
    Now that the DPDK offload provider in OVS is thread safe, remove the
    locks.
    
    [1]: http://mails.dpdk.org/archives/dev/2020-October/184251.html
    
    Signed-off-by: Gaetan Rivet <grive@u256.net>
    Reviewed-by: Eli Britstein <elibr@nvidia.com>
    Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/netdev-dpdk.c
+++ lib/netdev-dpdk.c
@@ -5225,9 +5225,7 @@ netdev_dpdk_rte_flow_destroy(struct netdev *netdev,
     struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
     int ret;
 
-    ovs_mutex_lock(&dev->mutex);
     ret = rte_flow_destroy(dev->port_id, rte_flow, error);
-    ovs_mutex_unlock(&dev->mutex);
     return ret;
 }
 
@@ -5241,9 +5239,7 @@ netdev_dpdk_rte_flow_create(struct netdev *netdev,
     struct rte_flow *flow;
     struct netdev_dpdk *dev = netdev_dpdk_cast(netdev);
 
-    ovs_mutex_lock(&dev->mutex);
     flow = rte_flow_create(dev->port_id, attr, items, actions, error);
-    ovs_mutex_unlock(&dev->mutex);
     return flow;
 }
 
@@ -5271,9 +5267,7 @@ netdev_dpdk_rte_flow_query_count(struct netdev *netdev,
     }
 
     dev = netdev_dpdk_cast(netdev);
-    ovs_mutex_lock(&dev->mutex);
     ret = rte_flow_query(dev->port_id, rte_flow, actions, query, error);
-    ovs_mutex_unlock(&dev->mutex);
     return ret;
 }
 
