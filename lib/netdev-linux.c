105874d7d94942bfdf56f6f36c4f70ca8fe7f1db
Yunjian Wang <wangyunjian@huawei.com>
2021-09-11 14:34:47 +0800

    netdev-linux: Fix a null pointer dereference in netdev_linux_notify_sock().
    If nl_sock_join_mcgroup() returns an error, the 'sock' is freed and
    set to NULL. This issues will lead to null pointer deference in
    nl_sock_listen_all_nsid(). To fix it, we call nl_sock_listen_all_nsid()
    before joining the mcgroups.
    
    Fixes: cf114a7fce80 ("netlink linux: enable listening to all nsids")
    Signed-off-by: Yunjian Wang <wangyunjian@huawei.com>
    Acked-by: Flavio Leitner <fbl@sysclose.org>
    Reviewed-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/netdev-linux.c
+++ lib/netdev-linux.c
@@ -604,6 +604,7 @@ netdev_linux_notify_sock(void)
         if (!error) {
             size_t i;
 
+            nl_sock_listen_all_nsid(sock, true);
             for (i = 0; i < ARRAY_SIZE(mcgroups); i++) {
                 error = nl_sock_join_mcgroup(sock, mcgroups[i]);
                 if (error) {
@@ -613,7 +614,6 @@ netdev_linux_notify_sock(void)
                 }
             }
         }
-        nl_sock_listen_all_nsid(sock, true);
         ovsthread_once_done(&once);
     }
 
