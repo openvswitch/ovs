d438cc32ee77fd354bfe8362df6cf81a1eade3e7
Wilson Peng <pweisong@vmware.com>
2021-10-13 04:06:47 -0700

    datapath-windows: add layers when adding the deferred actions
    Currently the layers info propogated to ProcessDeferredActions may be
    incorrect. Because of this, any subsequent usage of layers might result
    in undesired behavior. Accordingly in this patch it will add the related
     layers in the deferred action to make sure the layers consistent with
    the related NBL.
    
    In the specified case 229, we have encountered one issue when doing
    the decap Geneve Packet and doing the twice NAT(via two flow tables)
    and found the HTTP packet will be changed the TCP sequence.
    
    After debugging, we found the issue is caused by the not-updated
    layers value isTcp and isUdp for Geneve decapping case.
    
    The related function call chains are listed below,
    
    OvsExecuteDpIoctl—>OvsActionsExecute—>OvsDoExecuteActions->OvsTunnelPortRx
    ——>OvsDoExecuteActions——〉nat ct action and recircle action
    ->OvsActionsExecute->defered_actions processing for nat and recircle action
    
    For the Geneve packet decaping, it will firstly set the layers for Udp packet.
    Then it will go on doing OVS flow extract to get the inner packet layers and
    Processing the first nat action and first recircle action. After that datapath
    Will do defered_actions processing on OvsActionsExecute. And it does inherit
    The incorrect geneve packet layers value( isTCP 0 and isUdp 1).So in the second
    Nat action processing it will get the wrong TCP Headers in OvsUpdateAddressAndPort
    And it will update  related TCP check field value but in this case it will change
    The packet Tcp seq value.
    
    Reported-at:https://github.com/openvswitch/ovs-issues/issues/229
    Signed-off-by: Wilson Peng <pweisong@vmware.com>
    Signed-off-by: Alin-Gabriel Serdean <aserdean@ovn.org>

--- datapath-windows/ovsext/Recirc.h
+++ datapath-windows/ovsext/Recirc.h
@@ -18,6 +18,7 @@
 #define __RECIRC_H_ 1
 
 #include "Actions.h"
+#include "NetProto.h"
 
 #define DEFERRED_ACTION_QUEUE_SIZE          10
 #define DEFERRED_ACTION_EXEC_LEVEL           4
@@ -26,6 +27,7 @@ typedef struct _OVS_DEFERRED_ACTION {
     PNET_BUFFER_LIST    nbl;
     PNL_ATTR            actions;
     OvsFlowKey          key;
+    OVS_PACKET_HDR_INFO layers;
 } OVS_DEFERRED_ACTION, *POVS_DEFERRED_ACTION;
 
 /*
@@ -52,6 +54,7 @@ OvsProcessDeferredActions(POVS_SWITCH_CONTEXT switchContext,
 POVS_DEFERRED_ACTION
 OvsAddDeferredActions(PNET_BUFFER_LIST packet,
                       OvsFlowKey *key,
+                      POVS_PACKET_HDR_INFO layers,
                       const PNL_ATTR actions);
 
 /*
