8081d280d3c3c3b88d067d6a7560660d97d28a86
n-sandeep <sandeep.nagapattinam@intel.com>
2021-12-09 17:31:50 +0530

    P4-OVS: Point few dependent packages to specific SHA
    Updating P4-OVS depenedent packages to specific SHA.
    
    Change-type: Other
    Title: P4-OVS: Point few dependent packages to specific SHA
    Signed-off-by: n-sandeep <sandeep.nagapattinam@intel.com>

--- install_dep_packages.sh
+++ install_dep_packages.sh
@@ -80,6 +80,8 @@ MODULE="gflags"
 echo "####  Cloning, Building and Installing the '$MODULE' module ####"
 mkdir -p ${SRC_DIR}/$MODULE
 git clone https://github.com/gflags/gflags.git  ${SRC_DIR}/$MODULE
+cd $SRC_DIR/$MODULE
+git checkout 827c769e5fc98e0f2a34c47cef953cc6328abced
 mkdir -p $SRC_DIR/$MODULE/build
 cd $SRC_DIR/$MODULE/build
 cmake -DBUILD_SHARED_LIBS=ON $CMAKE_PREFIX ..
@@ -92,6 +94,8 @@ MODULE="glog"
 echo "####  Cloning, Building and Installing the '$MODULE' module ####"
 mkdir -p ${SRC_DIR}/$MODULE
 git clone https://github.com/google/glog.git  ${SRC_DIR}/$MODULE
+cd $SRC_DIR/$MODULE
+git checkout 503e3dec8d1fe071376befc62119a837c26612a3
 mkdir -p $SRC_DIR/$MODULE/build
 cd $SRC_DIR/$MODULE/build
 cmake $CMAKE_PREFIX ..
@@ -104,6 +108,8 @@ MODULE="abseil-cpp"
 echo "####  Cloning, Building and Installing the '$MODULE' module ####"
 mkdir -p ${SRC_DIR}/$MODULE
 git clone https://github.com/abseil/abseil-cpp.git  ${SRC_DIR}/$MODULE
+cd $SRC_DIR/$MODULE
+git checkout ec0d76f1d012cc1a4b3b08dfafcfc5237f5ba2c9
 mkdir -p $SRC_DIR/$MODULE/build
 cd $SRC_DIR/$MODULE/build
 cmake -DBUILD_TESTING=OFF $CMAKE_PREFIX ..
@@ -116,6 +122,8 @@ MODULE="cctz"
 echo "####  Cloning, Building and Installing the '$MODULE' module ####"
 mkdir -p ${SRC_DIR}/$MODULE
 git clone https://github.com/google/cctz.git  ${SRC_DIR}/$MODULE
+cd $SRC_DIR/$MODULE
+git checkout 02918d62329ef440935862719829d061a5f4beba
 mkdir -p $SRC_DIR/$MODULE/build
 cd $SRC_DIR/$MODULE/build
 cmake -DBUILD_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON $CMAKE_PREFIX ..
@@ -157,6 +165,8 @@ MODULE="json"
 echo "####  Cloning, Building and Installing the '$MODULE' module ####"
 mkdir -p ${SRC_DIR}/$MODULE
 git clone https://github.com/nlohmann/json.git  ${SRC_DIR}/$MODULE
+cd $SRC_DIR/$MODULE
+git checkout 760304635dc74a5bf77903ad92446a6febb85acf
 mkdir -p $SRC_DIR/$MODULE/build
 cd $SRC_DIR/$MODULE/build
 cmake $CMAKE_PREFIX ..
