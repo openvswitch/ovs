2b69d6b49e5d11780605df61831e2768208595ef
Dumitru Ceara <dceara@redhat.com>
2022-01-24 15:18:36 +0100

    bfd: lldp: stp: Fix misaligned packet field access.
    UB Sanitizer reports:
      lib/bfd.c:748:16:
            runtime error: member access within misaligned address 0x000001f0d6ea
                           for type 'struct msg', which requires 4 byte alignment
      0x000001f0d6ea: note: pointer points here
       00 20  00 00 20 40 03 18 93 f9  0a 6e 00 00 00 00 00 0f 42 40 00 0f ...
                    ^
          #0 0x59008e in bfd_process_packet lib/bfd.c:748
          #1 0x52a240 in process_special ofproto/ofproto-dpif-xlate.c:3370
          #2 0x553452 in xlate_actions ofproto/ofproto-dpif-xlate.c:7766
          #3 0x4fc9e6 in upcall_xlate ofproto/ofproto-dpif-upcall.c:1237
          #4 0x4fdecc in process_upcall ofproto/ofproto-dpif-upcall.c:1456
          #5 0x4fd936 in upcall_cb ofproto/ofproto-dpif-upcall.c:1358
          [...]
      lib/stp.c:754:15:
            runtime error: member access within misaligned address 0x000002c4ea61
            for type 'const   struct stp_bpdu_header', which requires 2 byte alignment
      0x000002c4ea61: note: pointer points here
       26 42 42  03 00 00 00 00 00 80 00  aa 66 aa 66 00 01 00 00  00 00 80 ...
                    ^
          #0 0x8a2bce in stp_received_bpdu lib/stp.c:754
          #1 0x51e603 in stp_process_packet ofproto/ofproto-dpif-xlate.c:1788
          #2 0x52a96d in process_special ofproto/ofproto-dpif-xlate.c:3394
          #3 0x5534df in xlate_actions ofproto/ofproto-dpif-xlate.c:7766
          #4 0x4fcb49 in upcall_xlate ofproto/ofproto-dpif-upcall.c:1237
          [...]
      lib/lldp/lldp.c:149:10:
            runtime error: load of misaligned address 0x7ffcc0ae72bd for type
                           'ovs_be16', which requires 2 byte alignment
      0x7ffcc0ae72bd: note: pointer points here
       8e e7 84 ad 04 00 05  46 61 73 74 45 74 68 65  72 6e 65 74 20 31 2f 35 ...
                   ^
          #0 0x718d63 in lldp_tlv_end lib/lldp/lldp.c:149
          #1 0x7191de in lldp_send lib/lldp/lldp.c:184
          #2 0x484d6c in test_aa_send tests/test-aa.c:238
          [...]
    
    Acked-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/lldp/lldp.c
+++ lib/lldp/lldp.c
@@ -146,7 +146,9 @@ static void
 lldp_tlv_end(struct dp_packet *p, unsigned int start)
 {
     ovs_be16 *tlv = dp_packet_at_assert(p, start, 2);
-    *tlv |= htons((dp_packet_size(p) - (start + 2)) & 0x1ff);
+    put_unaligned_be16(tlv,
+                       get_unaligned_be16(tlv)
+                       | htons((dp_packet_size(p) - (start + 2)) & 0x1ff));
 }
 
 int
