dd4d8e34ba5d246112a267e913339d28fb984223
lic121 <lic121@chinatelecom.cn>
2022-04-29 07:32:44 +0000

    ofproto-dpif-xlate: Remove mirror assert.
    During the revalidation/upcall, mirror could be removed. Instead of crash
    the process, we can simply skip the deleted mirror.
    
    The issue had been triggered multiple times by ovs-tcpdump in my test.
    
    Fixes: ec7ceaed4f3e ("ofproto-dpif: Modularize mirror code.")
    Signed-off-by: lic121 <lic121@chinatelecom.cn>
    Tested-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -2142,9 +2142,14 @@ mirror_packet(struct xlate_ctx *ctx, struct xbundle *xbundle,
         int snaplen;
 
         /* Get the details of the mirror represented by the rightmost 1-bit. */
-        ovs_assert(mirror_get(xbridge->mbridge, raw_ctz(mirrors),
-                              &vlans, &dup_mirrors,
-                              &out, &snaplen, &out_vlan));
+        if (OVS_UNLIKELY(!mirror_get(xbridge->mbridge, raw_ctz(mirrors),
+                                     &vlans, &dup_mirrors,
+                                     &out, &snaplen, &out_vlan))) {
+            /* The mirror got reconfigured before we got to read it's
+             * configuration. */
+            mirrors = zero_rightmost_1bit(mirrors);
+            continue;
+        }
 
 
         /* If this mirror selects on the basis of VLAN, and it does not select
