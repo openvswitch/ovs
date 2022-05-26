89fe949a67e473d571a84983342da4fc1240efae
Peng He <hepeng.0320@bytedance.com>
2022-05-26 03:13:17 +0000

    ofproto-dpif: Fix meter use-after-free.
    Add a rcu_barrier before close_dpif_backer to ensure that
    all meters have been freed before id_pool_destory meter's
    id-pool.
    
    Signed-off-by: Peng He <hepeng.0320@bytedance.com>
    Tested-by: David Marchand <david.marchand@redhat.com>
    Reviewed-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Eelco Chaudron <echaudro@redhat.com>
    Acked-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ofproto/ofproto-dpif.c
+++ ofproto/ofproto-dpif.c
@@ -1804,6 +1804,8 @@ destruct(struct ofproto *ofproto_, bool del)
 
     seq_destroy(ofproto->ams_seq);
 
+    /* Wait for all the meter destroy work to finish. */
+    ovsrcu_barrier();
     close_dpif_backer(ofproto->backer, del);
 }
 
