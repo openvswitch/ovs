b0e8668f38f5a253b4f3d76251500e81fc122ecc
Sunil Pai G <sunil.pai.g@intel.com>
2022-07-04 12:27:00 +0000

    dpif-netdev: Simplify AVX512 build time checks to enhance readability.
    The preprocessor comparison string to check AVX512 capabilities are
    lengthy and effecting user readability. Simpify this by aliasing the checks.
    
    Suggested-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: Sunil Pai G <sunil.pai.g@intel.com>
    Acked-by: Cian Ferriter <cian.ferriter@intel.com>
    Signed-off-by: Ian Stokes <ian.stokes@intel.com>

--- lib/dpif-netdev-lookup.c
+++ lib/dpif-netdev-lookup.c
@@ -22,9 +22,10 @@
 #include "openvswitch/vlog.h"
 
 VLOG_DEFINE_THIS_MODULE(dpif_netdev_lookup);
+#define DPCLS_IMPL_AVX512_CHECK (__x86_64__ && HAVE_AVX512F \
+    && HAVE_LD_AVX512_GOOD && HAVE_AVX512BW && __SSE4_2__)
 
-#if (__x86_64__ && HAVE_AVX512F && HAVE_LD_AVX512_GOOD && HAVE_AVX512BW \
-     && __SSE4_2__)
+#if DPCLS_IMPL_AVX512_CHECK
 static dpcls_subtable_lookup_func
 dpcls_subtable_avx512_gather_probe(uint32_t u0_bits, uint32_t u1_bits)
 {
@@ -61,8 +62,7 @@ static struct dpcls_subtable_lookup_info_t subtable_lookups[] = {
       .name = "generic",
       .usage_cnt = ATOMIC_COUNT_INIT(0), },
 
-#if (__x86_64__ && HAVE_AVX512F && HAVE_LD_AVX512_GOOD && HAVE_AVX512BW \
-     && __SSE4_2__)
+#if DPCLS_IMPL_AVX512_CHECK
     /* Only available on x86_64 bit builds with SSE 4.2 used for OVS core. */
     { .prio = 0,
       .probe = dpcls_subtable_avx512_gather_probe,
