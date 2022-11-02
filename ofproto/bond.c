openvswitch/ovs

commit 423416f58749de8e498afccc15d290a656abdf72

lacp: report desync in ovs threads enabling slave
It is helpful in reporting main thread that is yet to enable bond slave,
but link state was brought up by lacp thread and capture this desync
between ovs threads for debugging.

Fixes: a8448cb170 ("lacp: Avoid packet drop on LACP bond after link up")
Signed-off-by: Gowrishankar Muthukrishnan <gmuthukr@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

ofproto/bond.c
@@ -817,6 +817,10 @@ bond_check_admissibility(struct bond *bond, const void *slave_,
          * When may_enable is TRUE, it means LACP is UP and waiting for the
          * main thread to run LACP state machine and enable the slave. */
         verdict = (slave->enabled || slave->may_enable) ? BV_ACCEPT : BV_DROP;
+        if (!slave->enabled && slave->may_enable) {
+            VLOG_DBG_RL(&rl, "bond %s: slave %s: main thread not yet enabled slave",
+                         bond->name, bond->active_slave->name);
+        }
         goto out;
     case LACP_CONFIGURED:
         if (!bond->lacp_fallback_ab) {

commit 46ab9d80c2ab8f13dfe2ba2a9700887cd4f7fc36

bond: Fix crash while logging not yet enabled member.
The log should be printed with the member name, not the active member
name, and the active member does not judge whether it is NULL.  If null,
OVS will crash with the following backtrace:

 (gdb) bt
  0 bond_check_admissibility (ofproto/bond.c:877)
  1 is_admissible (ofproto/ofproto-dpif-xlate.c:2574)
  2 xlate_normal (ofproto/ofproto-dpif-xlate.c:3027)
  3 xlate_output_action (ofproto/ofproto-dpif-xlate.c:5284)
  4 do_xlate_actions (ofproto/ofproto-dpif-xlate.c:6960)
  5 xlate_actions (ofproto/ofproto-dpif-xlate.c:7924)
  6 upcall_xlate (ofproto/ofproto-dpif-upcall.c:1237)
  7 process_upcall (ofproto/ofproto-dpif-upcall.c:1456)
  8 upcall_cb (ofproto/ofproto-dpif-upcall.c:1358)
  9 dp_netdev_upcall (lib/dpif-netdev.c:7793)
 10 handle_packet_upcall (lib/dpif-netdev.c:8255)
 11 fast_path_processing (lib/dpif-netdev.c:8374)
 12 dp_netdev_input__ (lib/dpif-netdev.c:8463)
 13 dp_netdev_input (lib/dpif-netdev.c:8501)
 14 dp_netdev_process_rxq_port (lib/dpif-netdev.c:5337)
 15 pmd_thread_main (lib/dpif-netdev.c:6944)
 16 ovsthread_wrapper (lib/ovs-thread.c:422)
 17 ?? (/lib64/libpthread.so.0)
 18 clone (/lib64/libc.so.6)

Fixes: 423416f58749 ("lacp: report desync in ovs threads enabling slave")
Signed-off-by: yangchang <yangchang@chinatelecom.cn>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

ofproto/bond.c
@@ -897,7 +897,7 @@ bond_check_admissibility(struct bond *bond, const void *member_,
         if (!member->enabled && member->may_enable) {
             VLOG_DBG_RL(&rl, "bond %s: member %s: "
                         "main thread has not yet enabled member",
-                        bond->name, bond->active_member->name);
+                        bond->name, member->name);
         }
         goto out;
     case LACP_CONFIGURED: