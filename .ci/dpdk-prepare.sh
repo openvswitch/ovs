bf9c834e645e8edbcd14692f5ad27c12ab6ba6bc
David Marchand <david.marchand@redhat.com>
2023-05-04 19:10:49 +0200

    ci: Separate DPDK from OVS build.
    Let's separate DPDK compilation from the rest of OVS build:
    - this avoids multiple jobs building DPDK in parallel, which especially
      affects builds in the dpdk-latest branch,
    - we separate concerns about DPDK build requirements from OVS build
      requirements, like python dependencies,
    - building DPDK does not depend on how we will link OVS against it, so we
      can use a single cache entry regardless of DPDK_SHARED option,
    
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

---
+++ .ci/dpdk-prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ev
+
+# Installing wheel separately because it may be needed to build some
+# of the packages during dependency backtracking and pip >= 22.0 will
+# abort backtracking on build failures:
+#     https://github.com/pypa/pip/issues/10655
+pip3 install --disable-pip-version-check --user wheel
+pip3 install --disable-pip-version-check --user pyelftools
+pip3 install --user  'meson==0.53.2'
