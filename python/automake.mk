3b3a7b50fd3ca4103a7277e522dbc3953adba4f1
Robin Jarry <rjarry@redhat.com>
2023-08-23 16:29:00 +0200

    python: Rename build related code to ovs_build_helpers.
    The python/build folder contents are completely unrelated to the ovs
    python bindings. These files are only used during the build for various
    subsystems (docs, man pages, code generation, etc.).
    
    Having that folder in that location prevents from running:
    
      cd python && python3 -m build
    
    Which is a way to generate PEP517 compatible source archives and binary
    wheel packages.
    
    Rename that folder to ovs_build_helpers which is more explicit. Update
    all imports accordingly.
    
    Link: https://peps.python.org/pep-0517/
    Link: https://pypi.org/project/build/
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- python/automake.mk
+++ python/automake.mk
@@ -66,10 +66,10 @@ ovs_pytests = \
 # These python files are used at build time but not runtime,
 # so they are not installed.
 EXTRA_DIST += \
-	python/build/__init__.py \
-	python/build/extract_ofp_fields.py \
-	python/build/nroff.py \
-	python/build/soutil.py
+	python/ovs_build_helpers/__init__.py \
+	python/ovs_build_helpers/extract_ofp_fields.py \
+	python/ovs_build_helpers/nroff.py \
+	python/ovs_build_helpers/soutil.py
 
 # PyPI support.
 EXTRA_DIST += \
@@ -88,10 +88,10 @@ PYCOV_CLEAN_FILES += $(PYFILES:.py=.py,cover)
 
 FLAKE8_PYFILES += \
 	$(filter-out python/ovs/compat/% python/ovs/dirs.py,$(PYFILES)) \
-	python/build/__init__.py \
-	python/build/extract_ofp_fields.py \
-	python/build/nroff.py \
-	python/build/soutil.py \
+	python/ovs_build_helpers/__init__.py \
+	python/ovs_build_helpers/extract_ofp_fields.py \
+	python/ovs_build_helpers/nroff.py \
+	python/ovs_build_helpers/soutil.py \
 	python/ovs/dirs.py.template \
 	python/setup.py
 
