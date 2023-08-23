465e1746ae9c87941ef7b69ef8abb0b166970d61
Robin Jarry <rjarry@redhat.com>
2023-08-23 16:29:00 +0200

    python: Rename build related code to ovs_build_helpers.
    The python/build folder contents are completely unrelated to the ovs
    python bindings. These files are only used during the build for various
    subsystems (docs, man pages, code generation, etc.).
    
    Having that folder in that location prevents from running:
    
      cd python && python3 -m build
    
    Which is a way to generate PEP517 compatible source archives and binary
    wheel packages.
    
    Rename that folder to ovs_build_helpers which is more explicit. Update
    all imports accordingly.
    
    Link: https://peps.python.org/pep-0517/
    Link: https://pypi.org/project/build/
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- build-aux/sodepends.py
+++ build-aux/sodepends.py
@@ -14,8 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from build import soutil
 import sys
+
+from ovs_build_helpers import soutil
 
 
 def sodepends(include_dirs, filenames, dst):
