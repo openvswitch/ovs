ad46807e9f7e7b1bf60fd3fdf6ce8d122e20d6c5
Eelco Chaudron <echaudro@redhat.com>
2023-10-23 16:22:47 +0200

    netdev-offload: Fix Clang's static analyzer 'Division by zero' warnings.
    When enabling DPDK with the configure the below, ovs-vswitchd will crash.
    
      ovs-vsctl set Open_vSwitch . other_config:n-offload-threads=0
      ovs-vsctl set Open_vSwitch . other_config:hw-offload=true
    
    This issue arises because setting the 'n-offload-threads' value to zero
    is not a supported configuration. This fix addresses this by implementing
    a check to ensure a valid 'n-offload-threads' value, both during
    configuration and statistics gathering.
    
    Fixes: 62c2d8a67543 ("netdev-offload: Add multi-thread API.")
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Acked-by: Ilya Maximets <i.maximets@ovn.org>
    Signed-off-by: Simon Horman <horms@ovn.org>

--- tests/test-id-fpool.c
+++ tests/test-id-fpool.c
@@ -237,7 +237,7 @@ print_result(const char *prefix)
     for (i = 0; i < n_threads; i++) {
         avg += thread_working_ms[i];
     }
-    avg /= n_threads;
+    avg /= n_threads ? n_threads : 1;
     printf("%s: ", prefix);
     for (i = 0; i < n_threads; i++) {
         if (thread_working_ms[i] >= TIMEOUT_MS) {
