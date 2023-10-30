be18e15562b35a2b422ead572728fae9c780780c
Ilya Maximets <i.maximets@ovn.org>
2023-10-30 21:10:44 +0100

    automake: Move build-aux EXTRA_DIST updates to their own file.
    Otherwise it's hard to keep track of all the scripts we have.
    
    Acked-by: Eelco Chaudron <echaudro@redhat.com>
    Reviewed-By: Ihar Hrachyshka <ihar@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- build-aux/automake.mk
+++ build-aux/automake.mk
@@ -1,11 +1,19 @@
 EXTRA_DIST += \
 	build-aux/calculate-schema-cksum \
 	build-aux/cccl \
+	build-aux/check-structs \
 	build-aux/cksum-schema-check \
 	build-aux/dist-docs \
 	build-aux/dpdkstrip.py \
-	build-aux/generate-dhparams-c \
+	build-aux/extract-odp-netlink-h \
+	build-aux/extract-odp-netlink-macros-h \
+	build-aux/extract-odp-netlink-windows-dp-h \
+	build-aux/extract-ofp-actions \
+	build-aux/extract-ofp-errors \
+	build-aux/extract-ofp-fields \
+	build-aux/extract-ofp-msgs \
 	build-aux/gen_ofp_field_decoders \
+	build-aux/generate-dhparams-c \
 	build-aux/initial-tab-allowed-files \
 	build-aux/sodepends.py \
 	build-aux/soexpand.py \
