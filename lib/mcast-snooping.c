e646b392b3113b9d1bbaa09b93da42d82d9906ef
David Marchand <david.marchand@redhat.com>
2023-11-16 12:42:45 +0100

    mcast-snooping: Flush flood and report ports when deleting interfaces.
    When a configuration change triggers an interface destruction/creation
    (like for example, setting ofport_request), a port object may still be
    referenced as a fport or a rport in the mdb.
    
    Before the fix, when flooding multicast traffic:
    bridge("br0")
    -------------
     0. priority 32768
        NORMAL
         -> forwarding to mcast group port
         >> mcast flood port is unknown, dropping
         -> mcast flood port is input port, dropping
         -> forwarding to mcast flood port
    
    Before the fix, when flooding igmp report traffic:
    bridge("br0")
    -------------
     0. priority 32768
        NORMAL
         >> mcast port is unknown, dropping the report
         -> forwarding report to mcast flagged port
         -> mcast port is input port, dropping the Report
         -> forwarding report to mcast flagged port
    
    Add relevant cleanup and update unit tests.
    
    Fixes: 4fbbf8624868 ("mcast-snooping: Flush ports mdb when VLAN configuration changed.")
    Acked-by: Paolo Valerio <pvalerio@redhat.com>
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Eelco Chaudron <echaudro@redhat.com>
    Signed-off-by: Simon Horman <horms@ovn.org>

--- lib/mcast-snooping.c
+++ lib/mcast-snooping.c
@@ -946,8 +946,9 @@ mcast_snooping_wait(struct mcast_snooping *ms)
 void
 mcast_snooping_flush_bundle(struct mcast_snooping *ms, void *port)
 {
-    struct mcast_group *g;
     struct mcast_mrouter_bundle *m;
+    struct mcast_port_bundle *p;
+    struct mcast_group *g;
 
     if (!mcast_snooping_enabled(ms)) {
         return;
@@ -967,6 +968,20 @@ mcast_snooping_flush_bundle(struct mcast_snooping *ms, void *port)
     LIST_FOR_EACH_SAFE (m, mrouter_node, &ms->mrouter_lru) {
         if (m->port == port) {
             mcast_snooping_flush_mrouter(m);
+            ms->need_revalidate = true;
+        }
+    }
+
+    LIST_FOR_EACH_SAFE (p, node, &ms->fport_list) {
+        if (p->port == port) {
+            mcast_snooping_flush_port(p);
+            ms->need_revalidate = true;
+        }
+    }
+
+    LIST_FOR_EACH_SAFE (p, node, &ms->rport_list) {
+        if (p->port == port) {
+            mcast_snooping_flush_port(p);
             ms->need_revalidate = true;
         }
     }
