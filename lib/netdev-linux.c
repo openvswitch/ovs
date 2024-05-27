6f955620faf6a86ea357042e4ecc78a9e06e5c27
Mike Pattrick <mkp@redhat.com>
2024-05-27 15:08:46 -0400

    netdev-linux: Initialize link speed in error conditions.
    Clang's static analyzer noted that the output from
    netdev_linux_get_speed_locked can be checked even if this function
    doesn't set any values.
    
    Now we always set those values to a sane default in all cases.
    
    Fixes: 19cffe30cfda ("netdev-linux: Avoid deadlock in netdev_get_speed.")
    Signed-off-by: Mike Pattrick <mkp@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/netdev-linux.c
+++ lib/netdev-linux.c
@@ -2570,6 +2570,7 @@ netdev_linux_get_speed_locked(struct netdev_linux *netdev,
                               uint32_t *current, uint32_t *max)
 {
     if (netdev_linux_netnsid_is_remote(netdev)) {
+        *current = *max = 0;
         return EOPNOTSUPP;
     }
 
@@ -2579,6 +2580,8 @@ netdev_linux_get_speed_locked(struct netdev_linux *netdev,
                    ? 0 : netdev->current_speed;
         *max = MIN(UINT32_MAX,
                    netdev_features_to_bps(netdev->supported, 0) / 1000000ULL);
+    } else {
+        *current = *max = 0;
     }
     return netdev->get_features_error;
 }
