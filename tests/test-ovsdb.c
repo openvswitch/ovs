openvswitch/ovs

commit 55b9507e6824b935ffa0205fc7c7bebfe4e54279

ovsdb-idl: Add the support to specify the uuid for row insert.
ovsdb-server allows the OVSDB clients to specify the uuid for
the row inserts [1].  Both the C IDL client library and Python
IDL are missing this feature.  This patch adds this support.

In C IDL, for each schema table, a new function is generated -
<schema_table>insert_persistent_uuid(txn, uuid) which can
be used the clients to persist the uuid.

ovs-vsctl and other derivatives of ctl now supports the same
in the generic 'create' command with the option "--id=<UUID>".

In Python IDL, the uuid to persist can be specified in
the Transaction.insert() function.

[1] - a529e3cd1f("ovsdb-server: Allow OVSDB clients to specify the UUID for inserted rows.:)

Acked-by: Adrian Moreno <amorenoz@redhat.com>
Acked-by: Han Zhou <hzhou@ovn.org>
Acked-by: Terry Wilson <twilson@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

python/ovs/db/idl.py
@@ -1223,7 +1223,7 @@ class Row(object):
         d["a"] = "b"
         row.mycolumn = d
 """
-    def __init__(self, idl, table, uuid, data):
+    def __init__(self, idl, table, uuid, data, persist_uuid=False):
         # All of the explicit references to self.__dict__ below are required
         # to set real attributes with invoking self.__getattr__().
         self.__dict__["uuid"] = uuid
@@ -1277,6 +1277,10 @@ class Row(object):
         # verified as prerequisites when the transaction commits.  The values
         # in the dictionary are all None.
         self.__dict__["_prereqs"] = {}
+
+        # Indicates if the specified 'uuid' should be used as the row uuid
+        # or let the server generate it.
+        self.__dict__["_persist_uuid"] = persist_uuid
 
     def __lt__(self, other):
         if not isinstance(other, Row):
@@ -1816,7 +1820,11 @@ class Transaction(object):
                 op = {"table": row._table.name}
                 if row._data is None:
                     op["op"] = "insert"
-                    op["uuid-name"] = _uuid_name_from_uuid(row.uuid)
+                    if row._persist_uuid:
+                        op["uuid"] = row.uuid
+                    else:
+                        op["uuid-name"] = _uuid_name_from_uuid(row.uuid)
+
                     any_updates = True
 
                     op_index = len(operations) - 1
@@ -2056,20 +2064,22 @@ class Transaction(object):
             row._mutations['_removes'].pop(column.name, None)
         row._changes[column.name] = datum.copy()
 
-    def insert(self, table, new_uuid=None):
+    def insert(self, table, new_uuid=None, persist_uuid=False):
         """Inserts and returns a new row in 'table', which must be one of the
         ovs.db.schema.TableSchema objects in the Idl's 'tables' dict.
 
         The new row is assigned a provisional UUID.  If 'uuid' is None then one
         is randomly generated; otherwise 'uuid' should specify a randomly
-        generated uuid.UUID not otherwise in use.  ovsdb-server will assign a
-        different UUID when 'txn' is committed, but the IDL will replace any
-        uses of the provisional UUID in the data to be to be committed by the
-        UUID assigned by ovsdb-server."""
+        generated uuid.UUID not otherwise in use.  If 'persist_uuid' is true
+        and 'new_uuid' is specified, IDL requests the ovsdb-server to assign
+        the same UUID, otherwise ovsdb-server will assign a different UUID when
+        'txn' is committed and the IDL will replace any uses of the provisional
+        UUID in the data to be committed by the UUID assigned by
+        ovsdb-server."""
         assert self._status == Transaction.UNCOMMITTED
         if new_uuid is None:
             new_uuid = uuid.uuid4()
-        row = Row(self.idl, table, new_uuid, None)
+        row = Row(self.idl, table, new_uuid, None, persist_uuid=persist_uuid)
         table.rows[row.uuid] = row
         self._txn_rows[row.uuid] = row
         return row

tests/ovsdb-idl.at
@@ -2555,3 +2555,61 @@ OVSDB_CHECK_IDL_TRACK([track, insert and delete, refs to link2],
 005: table link2: i=1 l1= uuid=<1>
 006: done
 ]])
+
+m4_define([OVSDB_CHECK_IDL_PERS_UUID_INSERT_C],
+  [AT_SETUP([$1 - C])
+   AT_KEYWORDS([idl persistent uuid insert])
+   AT_CHECK([ovsdb_start_idltest "" "$abs_srcdir/idltest.ovsschema"])
+   AT_CHECK([test-ovsdb '-vPATTERN:console:test-ovsdb|%c|%m' -vjsonrpc -t10 idl unix:socket $2],
+            [0], [stdout], [stderr])
+   AT_CHECK([sort stdout],
+            [0], [$3])
+   AT_CHECK([grep $4 stderr], [0], [ignore])
+   OVSDB_SERVER_SHUTDOWN
+   AT_CLEANUP])
+
+m4_define([OVSDB_CHECK_IDL_PERS_UUID_INSERT_PY],
+  [AT_SETUP([$1 - Python3])
+   AT_KEYWORDS([idl persistent uuid insert])
+   AT_CHECK([ovsdb_start_idltest "" "$abs_srcdir/idltest.ovsschema"])
+   AT_CHECK([$PYTHON3 $srcdir/test-ovsdb.py  -t10 idl $srcdir/idltest.ovsschema unix:socket $2],
+            [0], [stdout], [stderr])
+   AT_CHECK([sort stdout],
+            [0], [$3])
+   AT_CHECK([grep $4 stderr], [0], [ignore])
+   OVSDB_SERVER_SHUTDOWN
+   AT_CLEANUP])
+
+
+m4_define([OVSDB_CHECK_IDL_PERS_UUID_INSERT],
+  [OVSDB_CHECK_IDL_PERS_UUID_INSERT_C($@)
+   OVSDB_CHECK_IDL_PERS_UUID_INSERT_PY($@)])
+
+OVSDB_CHECK_IDL_PERS_UUID_INSERT([simple idl, persistent uuid insert],
+  [['insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df2222 2, insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df3333 3' \
+    'insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df4444 4, insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df2222 5' \
+    'insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df4444 4' \
+    'delete 2' \
+    'insert_uuid c5cc12f8-eaa1-43a7-8a73-bccd18df2222 5'
+    ]],
+  [[000: empty
+001: commit, status=success
+002: table simple: i=2 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df2222
+002: table simple: i=3 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df3333
+003: commit, status=error
+004: table simple: i=2 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df2222
+004: table simple: i=3 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df3333
+005: commit, status=success
+006: table simple: i=2 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df2222
+006: table simple: i=3 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df3333
+006: table simple: i=4 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df4444
+007: commit, status=success
+008: table simple: i=3 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df3333
+008: table simple: i=4 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df4444
+009: commit, status=success
+010: table simple: i=3 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df3333
+010: table simple: i=4 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df4444
+010: table simple: i=5 r=0 b=false s= u=00000000-0000-0000-0000-000000000000 ia=[] ra=[] ba=[] sa=[] ua=[] uuid=c5cc12f8-eaa1-43a7-8a73-bccd18df2222
+011: done
+]],
+  [['This UUID would duplicate a UUID already present within the table or deleted within the same transaction']])

tests/test-ovsdb.c
@@ -2400,7 +2400,7 @@ idltest_find_simple(struct ovsdb_idl *idl, int i)
     return NULL;
 }
 
-static void
+static bool
 idl_set(struct ovsdb_idl *idl, char *commands, int step)
 {
     char *cmd, *save_ptr1 = NULL;
@@ -2458,6 +2458,19 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
 
             s = idltest_simple_insert(txn);
             idltest_simple_set_i(s, atoi(arg1));
+        } else if (!strcmp(name, "insert_uuid")) {
+            struct idltest_simple *s;
+
+            if (!arg1 || !arg2) {
+                ovs_fatal(0, "\"insert\" command requires 2 arguments");
+            }
+
+            struct uuid s_uuid;
+            if (!uuid_from_string(&s_uuid, arg1)) {
+                 ovs_fatal(0, "\"insert_uuid\" command requires valid uuid");
+            }
+            s = idltest_simple_insert_persist_uuid(txn, &s_uuid);
+            idltest_simple_set_i(s, atoi(arg2));
         } else if (!strcmp(name, "delete")) {
             const struct idltest_simple *s;
 
@@ -2522,7 +2535,7 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
             print_and_log("%03d: destroy", step);
             ovsdb_idl_txn_destroy(txn);
             ovsdb_idl_check_consistency(idl);
-            return;
+            return true;
         } else {
             ovs_fatal(0, "unknown command %s", name);
         }
@@ -2543,6 +2556,8 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
 
     ovsdb_idl_txn_destroy(txn);
     ovsdb_idl_check_consistency(idl);
+
+    return (status != TXN_ERROR);
 }
 
 static const struct ovsdb_idl_table_class *
@@ -2777,7 +2792,14 @@ do_idl(struct ovs_cmdl_context *ctx)
             update_conditions(idl, arg + strlen(cond_s));
             print_and_log("%03d: change conditions", step++);
         } else if (arg[0] != '[') {
-            idl_set(idl, arg, step++);
+            if (!idl_set(idl, arg, step++)) {
+                /* If idl_set() returns false, then no transaction
+                 * was sent to the server and most likely 'seqno'
+                 * would remain the same.  And the above 'Wait for update'
+                 * for loop poll_block() would never return.
+                 * So set seqno to 0. */
+                seqno = 0;
+            }
         } else {
             struct json *json = parse_json(arg);
             substitute_uuids(json, symtab);

tests/test-ovsdb.py
@@ -429,6 +429,14 @@ def idl_set(idl, commands, step):
 
             s = txn.insert(idl.tables["simple"])
             s.i = int(args[0])
+        elif name == "insert_uuid":
+            if len(args) != 2:
+                sys.stderr.write('"set" command requires 2 argument\n')
+                sys.exit(1)
+
+            s = txn.insert(idl.tables["simple"], new_uuid=args[0],
+                           persist_uuid=True)
+            s.i = int(args[1])
         elif name == "delete":
             if len(args) != 1:
                 sys.stderr.write('"delete" command requires 1 argument\n')
@@ -491,7 +499,7 @@ def idl_set(idl, commands, step):
             print("%03d: destroy" % step)
             sys.stdout.flush()
             txn.abort()
-            return
+            return True
         elif name == "linktest":
             l1_0 = txn.insert(idl.tables["link1"])
             l1_0.i = 1
@@ -614,6 +622,8 @@ def idl_set(idl, commands, step):
         sys.stdout.write(", events=" + ", ".join(sorted(events)))
     sys.stdout.write("\n")
     sys.stdout.flush()
+
+    return status != ovs.db.idl.Transaction.ERROR
 
 
 def update_condition(idl, commands):
@@ -748,7 +758,13 @@ def do_idl(schema_file, remote, *commands):
             sys.stdout.flush()
             step += 1
         elif not command.startswith("["):
-            idl_set(idl, command, step)
+            if not idl_set(idl, command, step):
+                # If idl_set() returns false, then no transaction
+                # was sent to the server and most likely seqno
+                # would remain the same.  And the above 'Wait for update'
+                # for loop poller.block() would never return.
+                # So set seqno to 0.
+                seqno = 0
             step += 1
         else:
             json = ovs.json.from_string(command)

lib/db-ctl-base.c
@@ -1731,29 +1731,43 @@ cmd_create(struct ctl_context *ctx)
     const struct ovsdb_idl_table_class *table;
     const struct ovsdb_idl_row *row;
     const struct uuid *uuid = NULL;
+    bool persist_uuid = false;
+    struct uuid uuid_;
     int i;
 
     ctx->error = get_table(table_name, &table);
     if (ctx->error) {
         return;
     }
-    if (id) {
-        struct ovsdb_symbol *symbol = NULL;
 
-        ctx->error = create_symbol(ctx->symtab, id, &symbol, NULL);
-        if (ctx->error) {
-            return;
+    if (id) {
+        if (uuid_from_string(&uuid_, id)) {
+            uuid = &uuid_;
+            persist_uuid = true;
+        } else {
+            struct ovsdb_symbol *symbol = NULL;
+
+            ctx->error = create_symbol(ctx->symtab, id, &symbol, NULL);
+            if (ctx->error) {
+                return;
+            }
+            if (table->is_root) {
+                /* This table is in the root set, meaning that rows created in
+                 * it won't disappear even if they are unreferenced, so disable
+                 * warnings about that by pretending that there is a
+                 * reference. */
+                symbol->strong_ref = true;
+            }
+            uuid = &symbol->uuid;
         }
-        if (table->is_root) {
-            /* This table is in the root set, meaning that rows created in it
-             * won't disappear even if they are unreferenced, so disable
-             * warnings about that by pretending that there is a reference. */
-            symbol->strong_ref = true;
-        }
-        uuid = &symbol->uuid;
     }
 
-    row = ovsdb_idl_txn_insert(ctx->txn, table, uuid);
+    if (persist_uuid) {
+        row = ovsdb_idl_txn_insert_persist_uuid(ctx->txn, table, uuid);
+    } else {
+        row = ovsdb_idl_txn_insert(ctx->txn, table, uuid);
+    }
+
     for (i = 2; i < ctx->argc; i++) {
         ctx->error = set_column(table, row, ctx->argv[i], ctx->symtab);
         if (ctx->error) {

lib/db-ctl-base.man
@@ -203,7 +203,7 @@ Without \fB\-\-if-exists\fR, it is an error if \fIrecord\fR does not
 exist.  With \fB\-\-if-exists\fR, this command does nothing if
 \fIrecord\fR does not exist.
 .
-.IP "[\fB\-\-id=@\fIname\fR] \fBcreate\fR \fItable column\fR[\fB:\fIkey\fR]\fB=\fIvalue\fR..."
+.IP "[\fB\-\-id=(@\fIname\fR | \fIuuid\fR] \fBcreate\fR \fItable column\fR[\fB:\fIkey\fR]\fB=\fIvalue\fR..."
 Creates a new record in \fItable\fR and sets the initial values of
 each \fIcolumn\fR.  Columns not explicitly set will receive their
 default values.  Outputs the UUID of the new row.
@@ -212,6 +212,9 @@ If \fB@\fIname\fR is specified, then the UUID for the new row may be
 referred to by that name elsewhere in the same \fB\*(PN\fR
 invocation in contexts where a UUID is expected.  Such references may
 precede or follow the \fBcreate\fR command.
+.IP
+If a valid \fIuuid\fR is specified, then it is used as the UUID
+of the new row.
 .
 .RS
 .IP "Caution (ovs-vsctl as example)"

lib/db-ctl-base.xml
@@ -310,7 +310,7 @@
       </p>
     </dd>
 
-    <dt>[<code>--id=@</code><var>name</var>] <code>create</code> <var>table column</var>[<code>:</code><var>key</var>]<code>=</code><var>value</var>...</dt>
+    <dt>[<code>--id=(@</code><var>name</var>|<var>uuid</var>)] <code>create</code> <var>table column</var>[<code>:</code><var>key</var>]<code>=</code><var>value</var>...</dt>
     <dd>
       <p>
         Creates a new record in <var>table</var> and sets the initial values of
@@ -322,6 +322,10 @@
         referred to by that name elsewhere in the same <code>\*(PN</code>
         invocation in contexts where a UUID is expected.  Such references may
         precede or follow the <code>create</code> command.
+      </p>
+      <p>
+        If a valid <var>uuid</var> is specified, then it is used as the
+        UUID of the new row.
       </p>
       <dl>
         <dt>Caution (ovs-vsctl as example)</dt>

lib/ovsdb-idl-provider.h
@@ -74,6 +74,7 @@ struct ovsdb_idl_row {
     struct ovs_list dst_arcs;   /* Backward arcs (ovsdb_idl_arc.dst_node). */
     struct ovsdb_idl_table *table; /* Containing table. */
     struct ovsdb_datum *old_datum; /* Committed data (null if orphaned). */
+    bool persist_uuid;          /* Persist 'uuid' during insert txn if set. */
     bool parsed; /* Whether the row is parsed. */
     struct ovs_list reparse_node; /* Rows that needs to be re-parsed due to
                                    * insertion of a referenced row. */

commit d56a5bacda6cb9ee527d92a8a80c510d6e099d08

python: ovsdb-idl: Fix persist_uuid references.
When inserting a Row with persist_uuid=True, the Row will have
a 'uuid' element and not a 'named-uuid' element, so ensure that
other operations in the transaction refer to the row by 'uuid'.

Fixes: 55b9507e6824 ("ovsdb-idl: Add the support to specify the uuid for row insert.")
Signed-off-by: Terry Wilson <twilson@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

python/ovs/db/idl.py
@@ -1731,7 +1731,7 @@ class Transaction(object):
                     and ovs.ovsuuid.is_valid_string(json[1])):
                 uuid = ovs.ovsuuid.from_string(json[1])
                 row = self._txn_rows.get(uuid, None)
-                if row and row._data is None:
+                if row and row._data is None and not row._persist_uuid:
                     return ["named-uuid", _uuid_name_from_uuid(uuid)]
             else:
                 return [self._substitute_uuids(elem) for elem in json]

tests/ovsdb-idl.at
@@ -2826,7 +2826,7 @@ m4_define([OVSDB_CHECK_IDL_PERS_UUID_INSERT_C],
             [0], [stdout], [stderr])
    AT_CHECK([sort stdout],
             [0], [$3])
-   AT_CHECK([grep $4 stderr], [0], [ignore])
+   m4_if([$4], [], [], [AT_CHECK([grep $4 stderr], [0], [ignore])])
    OVSDB_SERVER_SHUTDOWN
    AT_CLEANUP])
 
@@ -2838,7 +2838,7 @@ m4_define([OVSDB_CHECK_IDL_PERS_UUID_INSERT_PY],
             [0], [stdout], [stderr])
    AT_CHECK([sort stdout],
             [0], [$3])
-   AT_CHECK([grep $4 stderr], [0], [ignore])
+   m4_if([$4], [], [], [AT_CHECK([grep $4 stderr], [0], [ignore])])
    OVSDB_SERVER_SHUTDOWN
    AT_CLEANUP])
 
@@ -2876,6 +2876,14 @@ OVSDB_CHECK_IDL_PERS_UUID_INSERT([simple idl, persistent uuid insert],
 ]],
   [['This UUID would duplicate a UUID already present within the table or deleted within the same transaction']])
 
+OVSDB_CHECK_IDL_PERS_UUID_INSERT([simple idl, persistent uuid insert uref],
+  [['insert_uuid_uref d7f2845f-2e8d-46a9-8330-f6d0b7d2ca36 689420a0-515b-4c0f-8eba-7ad59a344b54']],
+  [[000: empty
+001: commit, status=success
+002: table simple3: name= uset=[] uref=[689420a0-515b-4c0f-8eba-7ad59a344b54] uuid=d7f2845f-2e8d-46a9-8330-f6d0b7d2ca36
+002: table simple4: name= uuid=689420a0-515b-4c0f-8eba-7ad59a344b54
+003: done
+]])
 
 OVSDB_CHECK_IDL_PY([simple idl, python, add_op],
   [],

tests/test-ovsdb.c
@@ -2452,6 +2452,10 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
     struct ovsdb_idl_txn *txn;
     enum ovsdb_idl_txn_status status;
     bool increment = false;
+    /* FIXME: ovsdb_idl_check_consistency() doesn't currently handle refs added
+     * in the same txn, so if any op does this, we need to skip the check until
+     * that is fixed. */
+    bool skip_pre_commit_consistency_check = false;
 
     txn = ovsdb_idl_txn_create(idl);
     ovsdb_idl_check_consistency(idl);
@@ -2518,6 +2522,26 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
             }
             s = idltest_simple_insert_persist_uuid(txn, &s_uuid);
             idltest_simple_set_i(s, atoi(arg2));
+        } else if (!strcmp(name, "insert_uuid_uref")) {
+            struct idltest_simple3 *s3;
+            struct idltest_simple4 *s4;
+
+            if (!arg1 || !arg2) {
+                ovs_fatal(0,
+                          "\"insert_uuid_uref\" command requires 2 arguments");
+            }
+
+            struct uuid s3_uuid;
+            struct uuid s4_uuid;
+            if (!uuid_from_string(&s3_uuid, arg1) ||
+                !uuid_from_string(&s4_uuid, arg2)) {
+                 ovs_fatal(
+                    0, "\"insert_uuid_uref\" command requires 2 valid uuids");
+            }
+            s4 = idltest_simple4_insert_persist_uuid(txn, &s4_uuid);
+            s3 = idltest_simple3_insert_persist_uuid(txn, &s3_uuid);
+            idltest_simple3_set_uref(s3, &s4, 1);
+            skip_pre_commit_consistency_check = true;
         } else if (!strcmp(name, "delete")) {
             const struct idltest_simple *s;
 
@@ -2586,7 +2610,9 @@ idl_set(struct ovsdb_idl *idl, char *commands, int step)
         } else {
             ovs_fatal(0, "unknown command %s", name);
         }
-        ovsdb_idl_check_consistency(idl);
+        if (!skip_pre_commit_consistency_check) {
+            ovsdb_idl_check_consistency(idl);
+        }
     }
 
     status = ovsdb_idl_txn_commit_block(txn);

tests/test-ovsdb.py
@@ -518,6 +518,17 @@ def idl_set(idl, commands, step):
             s = txn.insert(idl.tables["simple"], new_uuid=uuid.UUID(args[0]),
                            persist_uuid=True)
             s.i = int(args[1])
+        elif name == "insert_uuid_uref":
+            if len(args) != 2:
+                sys.stderr.write('"set" command requires 2 argument\n')
+                sys.exit(1)
+
+            s4 = txn.insert(idl.tables["simple4"], new_uuid=uuid.UUID(args[1]),
+                            persist_uuid=True)
+            s3 = txn.insert(idl.tables["simple3"], new_uuid=uuid.UUID(args[0]),
+                            persist_uuid=True)
+            s3.uref = s4
+
         elif name == "add_op":
             if len(args) != 1:
                 sys.stderr.write('"add_op" command requires 1 argument\n')