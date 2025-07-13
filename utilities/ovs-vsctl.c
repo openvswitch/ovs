943f9096e2eab95b8abc4e6910001bada13cb88e
Flavio Leitner <fbl@sysclose.org>
2025-07-13 19:07:17 -0300

    ovs-vsctl: Exit with error if postdb checks report errors.
    Today the exit code refers to the execution of the change
    in the database. However, when not using parameter --no-wait
    (default), the ovs-vsctl also checks if OVSDB transactions
    are successfully recorded and reload by ovs-vswitchd. In this
    case, an error message is printed if there is a problem during
    the reload, like for example the one below:
    
     # ovs-vsctl add-port br0 gre0 -- \
        set interface gre0 type=ip6gre options:key=100 \
        options:remote_ip=2001::2
     ovs-vsctl: Error detected while setting up 'gre0': could not \
     add network device gre0 to ofproto (Address family not supported\
     by protocol).  See ovs-vswitchd log for details.
     ovs-vsctl: The default log directory is "/var/log/openvswitch".
     # echo $?
     0
    
    This patch changes to exit with specific error code 160
    (ERROR_BAD_ARGUMENTS) on Windows and 65 (EX_DATAERR) on
    Linux or BSD if errors were reported during the reload.
    
    This change may break existing scripts because ovs-vsctl will
    start to fail when before it was succeeding. However, if an
    error is printed, then it is likely that the change was not
    functional anyway.
    
    Reported-at: https://issues.redhat.com/browse/FDP-793
    Signed-off-by: Flavio Leitner <fbl@sysclose.org>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- utilities/ovs-vsctl.c
+++ utilities/ovs-vsctl.c
@@ -56,6 +56,15 @@
 
 VLOG_DEFINE_THIS_MODULE(vsctl);
 
+/* Post OVSDB reload error reported. */
+#ifdef _WIN32
+#include <WinError.h>
+#define EXIT_POSTDB_ERROR ERROR_BAD_ARGUMENTS
+#else
+#include <sysexits.h>
+#define EXIT_POSTDB_ERROR EX_DATAERR
+#endif
+
 struct vsctl_context;
 
 /* --db: The database server to contact. */
@@ -115,7 +124,7 @@ static void parse_options(int argc, char *argv[], struct shash *local_options);
 static void run_prerequisites(struct ctl_command[], size_t n_commands,
                               struct ovsdb_idl *);
 static bool do_vsctl(const char *args, struct ctl_command *, size_t n,
-                     struct ovsdb_idl *);
+                     struct ovsdb_idl *, bool *postdb_err);
 
 /* post_db_reload_check frame work is to allow ovs-vsctl to do additional
  * checks after OVSDB transactions are successfully recorded and reload by
@@ -134,11 +143,13 @@ static bool do_vsctl(const char *args, struct ctl_command *, size_t n,
  * Current implementation only check for Post OVSDB reload failures on new
  * interface additions with 'add-br' and 'add-port' commands.
  *
+ * post_db_reload_do_checks() returns 'true' if a failure is reported.
+ *
  * post_db_reload_expect_iface()
  *
  * keep track of interfaces to be checked post OVSDB reload. */
 static void post_db_reload_check_init(void);
-static void post_db_reload_do_checks(const struct vsctl_context *);
+static bool post_db_reload_do_checks(const struct vsctl_context *);
 static void post_db_reload_expect_iface(const struct ovsrec_interface *);
 
 static struct uuid *neoteric_ifaces;
@@ -200,9 +211,15 @@ main(int argc, char *argv[])
         }
 
         if (seqno != ovsdb_idl_get_seqno(idl)) {
+            bool postdb_err;
+
             seqno = ovsdb_idl_get_seqno(idl);
-            if (do_vsctl(args, commands, n_commands, idl)) {
+            if (do_vsctl(args, commands, n_commands, idl, &postdb_err)) {
                 free(args);
+                if (postdb_err) {
+                    exit(EXIT_POSTDB_ERROR);
+                }
+
                 exit(EXIT_SUCCESS);
             }
         }
@@ -2824,10 +2841,10 @@ post_db_reload_expect_iface(const struct ovsrec_interface *iface)
     neoteric_ifaces[n_neoteric_ifaces++] = iface->header_.uuid;
 }
 
-static void
+static bool
 post_db_reload_do_checks(const struct vsctl_context *vsctl_ctx)
 {
-    bool print_error = false;
+    bool reconfig_failed = false;
     size_t i;
 
     for (i = 0; i < n_neoteric_ifaces; i++) {
@@ -2849,14 +2866,16 @@ post_db_reload_do_checks(const struct vsctl_context *vsctl_ctx)
                                  "See ovs-vswitchd log for details.",
                               iface->name);
                 }
-                print_error = true;
+                reconfig_failed = true;
             }
         }
     }
 
-    if (print_error) {
+    if (reconfig_failed) {
         ovs_error(0, "The default log directory is \"%s\".", ovs_logdir());
     }
+
+    return reconfig_failed;
 }
 
 
@@ -2965,7 +2984,7 @@ vsctl_parent_process_info(void)
 
 static bool
 do_vsctl(const char *args, struct ctl_command *commands, size_t n_commands,
-         struct ovsdb_idl *idl)
+         struct ovsdb_idl *idl, bool *postdb_err)
 {
     struct ovsdb_idl_txn *txn;
     const struct ovsrec_open_vswitch *ovs;
@@ -2977,6 +2996,8 @@ do_vsctl(const char *args, struct ctl_command *commands, size_t n_commands,
     int64_t next_cfg = 0;
     char *ppid_info = NULL;
 
+    ovs_assert(postdb_err);
+    *postdb_err = false;
     txn = the_idl_txn = ovsdb_idl_txn_create(idl);
     if (dry_run) {
         ovsdb_idl_txn_set_dry_run(txn);
@@ -3139,7 +3160,9 @@ do_vsctl(const char *args, struct ctl_command *commands, size_t n_commands,
             ovsdb_idl_run(idl);
             OVSREC_OPEN_VSWITCH_FOR_EACH (ovs, idl) {
                 if (ovs->cur_cfg >= next_cfg) {
-                    post_db_reload_do_checks(&vsctl_ctx);
+                    if (post_db_reload_do_checks(&vsctl_ctx)) {
+                        *postdb_err = true;
+                    }
                     goto done;
                 }
             }
