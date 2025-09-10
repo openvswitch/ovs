927d767301f27fa0e82d4d579a293df3549b7787
David Marchand <david.marchand@redhat.com>
2025-09-10 16:46:36 +0200

    odp-execute: Fix packet length check for TSO packets.
    TSO packets were incorrectly treated as too big by the check_pkt_len
    action with the userspace datapath.
    Adjust the check by looking at the requested segment size.
    
    Fixes: 29cf9c1b3b9c ("userspace: Add TCP Segmentation Offload support")
    Reported-at: https://issues.redhat.com/browse/FDP-1631
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- lib/dp-packet.h
+++ lib/dp-packet.h
@@ -573,6 +573,22 @@ dp_packet_get_tcp_payload(const struct dp_packet *b)
     return NULL;
 }
 
+static inline const void *
+dp_packet_get_inner_tcp_payload(const struct dp_packet *b)
+{
+    size_t l4_size = dp_packet_inner_l4_size(b);
+
+    if (OVS_LIKELY(l4_size >= TCP_HEADER_LEN)) {
+        struct tcp_header *tcp = dp_packet_inner_l4(b);
+        int tcp_len = TCP_OFFSET(tcp->tcp_ctl) * 4;
+
+        if (OVS_LIKELY(tcp_len >= TCP_HEADER_LEN && tcp_len <= l4_size)) {
+            return (const char *) tcp + tcp_len;
+        }
+    }
+    return NULL;
+}
+
 static inline uint32_t
 dp_packet_get_tcp_payload_length(const struct dp_packet *pkt)
 {
