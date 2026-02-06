2a8c2d66d4a7e2a2806de85dd235bf169732714c
Jun Gu <jun.gu@easystack.cn>
2026-02-06 03:35:16 +0000

    ofproto-dpif-xlate: Avoid mirroring on recirculation.
    The current implementation triggers mirroring even when a packet is
    being recirculated (e.g., for bond member selection). This leads to
    incorrect mirroring behavior in the following ways:
    
    1. Redundant Mirrors: If a packet is mirrored during the recirculation
       phase and then mirrored again when it finally hits an actual output
       action, the mirror destination receives duplicate traffic.
    
    2. False Mirroring: If for some reason the post-recurculation rules are
       not available, we should not be mirroring the packet. For example,
       if a bond is removed while we have the first datapath flow installed,
       but the packet after recirculation goes to userspace via MISS upcall
       and gets dropped, then we should not mirror that packet.
    
    This patch refactors compose_output_action__() to ensure mirror_packet()
    is not invoked during recirculation and avoid mirroring on recirculation.
    
    Signed-off-by: Jun Gu <jun.gu@easystack.cn>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -4385,6 +4385,7 @@ compose_output_action__(struct xlate_ctx *ctx, ofp_port_t ofp_port,
     struct eth_addr flow_dl_src = flow->dl_src;
     ovs_be32 flow_packet_type = flow->packet_type;
     ovs_be16 flow_dl_type = flow->dl_type;
+    bool skip_mirrors = false;
 
     /* If 'struct flow' gets additional metadata, we'll need to zero it out
      * before traversing a patch port. */
@@ -4503,6 +4504,10 @@ compose_output_action__(struct xlate_ctx *ctx, ofp_port_t ofp_port,
             /* Recirc action. */
             nl_msg_put_u32(ctx->odp_actions, OVS_ACTION_ATTR_RECIRC,
                            xr->recirc_id);
+
+            /* Recirculation does not send the packet anywhere, so it
+             * should not trigger mirroring. */
+            skip_mirrors = true;
         } else if (is_native_tunnel) {
             /* Output to native tunnel port. */
             native_tunnel_output(ctx, xport, flow, odp_port, truncate,
@@ -4545,7 +4550,8 @@ compose_output_action__(struct xlate_ctx *ctx, ofp_port_t ofp_port,
         ctx->nf_output_iface = ofp_port;
     }
 
-    if (mbridge_has_mirrors(ctx->xbridge->mbridge) && xport->xbundle) {
+    if (!skip_mirrors && mbridge_has_mirrors(ctx->xbridge->mbridge)
+        && xport->xbundle) {
         mirror_packet(ctx, xport->xbundle,
                       xbundle_mirror_dst(xport->xbundle->xbridge,
                                          xport->xbundle));
