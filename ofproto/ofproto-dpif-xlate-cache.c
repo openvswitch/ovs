2776f1fea3d5614dd18e2289d59917157c38f32c
Ilya Maximets <i.maximets@ovn.org>
2026-03-18 00:02:59 +0100

    ofproto-dpif: Print bridge names in the ofproto/detrace.
    Current output of ofproto/detrace doesn't provide information on what
    bridge the rules and groups belong to.  This may be confusing in
    setups with multiple bridges, especially when packets are resubmitted
    multiple times through different patch ports.
    
    Translation cache does have the bridge information though as every
    time we lookup the rule we add the XC_TABLE entry that has the bridge
    information.  We also know when the rule lookup was executed but
    didn't result in a match.  We can report that as well, so it's easier
    to follow the resubmits.  Group entries also have a link to ofproto
    with the bridge name.
    
    Note: On a miss, the packet hits the internal drop rule in table 254.
    By reporting the fact that there was no match it's easier to
    understand why that internal rule is getting hit.
    
    There is no extra overhead for providing the bridge information as the
    data is already there, we just need to print it out.  Retis, that is
    collecting the detrace output, should also be able to take advantage
    of this enhancement.
    
    Reviewed-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- ofproto/ofproto-dpif-xlate-cache.c
+++ ofproto/ofproto-dpif-xlate-cache.c
@@ -306,22 +306,41 @@ void
 xlate_xcache_format(struct ds *s, const struct xlate_cache *xcache)
 {
     struct ofpbuf entries = xcache->entries;
+    struct ofproto *last_ofproto = NULL;
     struct xc_entry *entry;
     struct ofgroup *ofg;
 
     XC_ENTRY_FOR_EACH (entry, &entries) {
         switch (entry->type) {
         case XC_RULE:
+            ds_put_cstr(s, "  ");
             ofproto_rule_stats_ds(s, &entry->rule->up, true);
             break;
+
         case XC_GROUP:
             ofg = &entry->group.group->up;
+            if (last_ofproto != ofg->ofproto) {
+                ds_put_format(s, "%s\n", ofg->ofproto->name);
+                last_ofproto = ofg->ofproto;
+            }
+            ds_put_cstr(s, "  ");
             ofputil_group_format(s, ofg->group_id, ofg->type,
                                  entry->group.bucket, &ofg->buckets,
                                  &ofg->props, OFP15_VERSION,
                                  false, NULL, NULL);
             break;
+
         case XC_TABLE:
+            if (last_ofproto != &entry->table.ofproto->up) {
+                ds_put_format(s, "%s\n", entry->table.ofproto->up.name);
+                last_ofproto = &entry->table.ofproto->up;
+            }
+            if (!entry->table.match) {
+                ds_put_format(s, "  table_id=%"PRIu8", no match\n",
+                              entry->table.id);
+            }
+            break;
+
         case XC_BOND:
         case XC_NETDEV:
         case XC_NETFLOW:
